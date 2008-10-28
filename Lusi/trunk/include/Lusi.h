d875a8c33cabd4716588e1f64caece8b0a0e22b8
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2008-10-28 22:09:00 +0000

    Tag: V00-00-01 2008-10-07 Andy Salnikov - first import into SVN, empty Lusi.h
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@55 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ Lusi/trunk/include/Lusi.h
@@ -0,0 +1,16 @@
+#ifndef LUSI_LUSI_H
+#define LUSI_LUSI_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//      Standard include file to contain global "fixes" and type definitions.
+//      This file is intended to be included in every C++ source (.cpp) file.
+//
+//--------------------------------------------------------------------------
+
+
+
+#endif // LUSI_LUSI_H
