f784a3a42e809c1ba258f6e0e2da8221e5d02f3c
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2008-10-29 18:28:36 +0000

    Tag: V00-00-07 2008-10-29 Andy Salnikov - standardSConscript: adding support for building tests in test/,   plus minor refactoring
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@73 b967ad99-d558-0410-b138-e0f6c56caec7

--- SConsTools/trunk/src/standardSConscript.py
+++ SConsTools/trunk/src/standardSConscript.py
@@ -20,11 +20,27 @@ from SConsTools.dependencies import *
 
 _cplusplus_ext = [ 'cc', 'cpp', 'cxx', 'C', 'c' ]
 
-def _normbinsrc ( f ):
+# normalize source file path
+def _normbinsrc ( dir, f ):
     if not os.path.split(f)[0] :
-        return os.path.join("app",f)
+        return os.path.join(dir,f)
     else :
         return f
+
+# get list of strings from kw, single string is split
+def _getkwlist ( kw, name ):
+    if name in kw :
+        res = kw[name]
+        if isinstance(res,(str,unicode)) : res = res.split()
+        return res
+    return []
+
+# get package name
+def _getpkg ( kw ) :
+    pkg = kw.get('package', None )
+    if not pkg : pkg = os.path.basename(os.getcwd())
+    return pkg
+    
 
 #
 # This is the content of the standard SConscript
@@ -36,47 +52,58 @@ def standardSConscript( **kw ) :
         LIBS - list of additional libraries that have to be linked with applications
     """
 
-    pkg = os.path.basename(os.getcwd())
+    pkg = _getpkg ( kw )
     trace ( "Standard SConscript in `"+pkg+"'", "SConscript", 1 )
+
+    ukw = kw.copy()
+    ukw['package'] = pkg
     
-    env = DefaultEnvironment()
-    bindir = env['BINDIR']
-    libdir = env['LIBDIR']
-    pydir = env['PYDIR']
+    standardLib( **ukw )
+    standardPyLib( **ukw )
+    standardScripts( **ukw )
+    standardBins ( **ukw )
+    standardTests ( **ukw )
 
-    # Program options
-    binkw = {}
-    if 'LIBS' in kw :
-        libs = kw['LIBS']
-        if isinstance(libs,(str,unicode)) : libs = libs.split()
-        binkw['LIBS'] = libs
-    if 'LIBDIRS' in kw : binkw['LIBDIRS'] = kw['LIBDIRS']
-
-    #
-    # Process src/ directory, make library from all compilable files
-    #
+#
+# Process src/ directory, make library from all compilable files
+#
+def standardLib( **kw ) :
+    
     libsrcs = Flatten ( [ Glob("src/*."+ext, source=True ) for ext in _cplusplus_ext ] )
-    if libsrcs :        
+    if libsrcs :
+        
         trace ( "libsrcs = "+pformat([str(s) for s in libsrcs]), "SConscript", 2 )
 
-        # c++ files area compiled into library  
-        if libsrcs :
-             
-            lib = env.SharedLibrary ( pkg, source=libsrcs )
-            env.Install ( libdir, source=lib )
-            deps = findAllDependencies( lib[0] )
-            trace ( "deps = " + pformat(deps), "SConscript", 4 )
-            
-            setPkgDeps ( env, pkg, deps )
-            setPkgLibs ( env, pkg, [ pkg ] )
-            
-            binkw.setdefault('LIBS',[]).insert ( 0, pkg )
+        pkg = _getpkg( kw )
+        
+        env = DefaultEnvironment()
+        libdir = env['LIBDIR']
 
-    #
-    # Process src/ directory, link python sources
-    #
+        lib = env.SharedLibrary ( pkg, source=libsrcs )
+        env.Install ( libdir, source=lib )
+        
+        # get the list of dependencies for this package
+        deps = findAllDependencies( lib[0] )
+        setPkgDeps ( env, pkg, deps )
+        trace ( "deps = " + pformat(deps), "SConscript", 4 )
+
+        # get the list of libraries need for this package
+        libs = [pkg] + _getkwlist ( kw, 'LIBS' )
+        setPkgLibs ( env, pkg, libs )
+        
+#
+# Process src/ directory, link python sources
+#
+def standardPyLib( **kw ) :
+    
     pysrcs = Glob("src/*.py", source=True, strings=True )
     if pysrcs :
+        
+        pkg = _getpkg( kw )
+        
+        env = DefaultEnvironment()
+        pydir = env['PYDIR']
+
         trace ( "pysrcs = "+pformat(pysrcs), "SConscript", 2 )
 
         # python files area installed into python/Package
@@ -93,12 +120,17 @@ def standardSConscript( **kw ) :
             env.Command ( ini, "", [ Touch("$TARGET") ] )
             env.PyCompile ( ini+"c", source=ini )
 
-    #
-    # Process app/ directory, install all scripts
-    #
+#
+# Process app/ directory, install all scripts
+#
+def standardScripts( **kw ) :
+    
     app_files = Glob("app/*", source=True, strings=True )
     if app_files :
-        
+
+        env = DefaultEnvironment()
+        bindir = env['BINDIR']
+
         scripts = [ f for f in app_files if not os.path.splitext(f)[1] and os.path.isfile(f) ]
         trace ( "scripts = "+pformat(scripts), "SConscript", 2 )
 
@@ -106,32 +138,57 @@ def standardSConscript( **kw ) :
         for s in scripts : 
             env.ScriptInstall ( os.path.join(bindir,os.path.basename(s)), s )
 
-    #
-    # Process app/ directory, build all from C++ sources
-    #
-    bins = kw.get('BINS',{})
+#
+# Process app/ directory, build all executables from C++ sources
+#
+def standardBins( **kw ) :
+    _standardBins ( 'app', 'BINS', True, **kw )
+
+#
+# Process test/ directory, build all executables from C++ sources
+#
+def standardTests( **kw ) :
+    _standardBins ( 'test', 'TESTS', False, **kw )
+
+#
+# Build binaries, possibly install them
+#
+def _standardBins( appdir, binenv, install, **kw ) :
+
+    # make list of binaries and their dependencies if it has not been passed to us
+    bins = kw.get(binenv,{})
     if bins :
         for k in bins.iterkeys() :
             src = bins[k]
             if isinstance(src,(str,unicode)) : src = src.split()
-            src = [ _normbinsrc(s) for s in src ]
+            src = [ _normbinsrc(appdir,s) for s in src ]
             bins[k] = src
     else :
-        cpps = Flatten ( [ Glob("app/*."+ext, source=True, strings=True ) for ext in _cplusplus_ext ] )
+        cpps = Flatten ( [ Glob(appdir+"/*."+ext, source=True, strings=True ) for ext in _cplusplus_ext ] )
         for f in cpps :
             bin = os.path.splitext(os.path.basename(f))[0]
             bins[bin] = [ f ]
+            
+    # make rules for the binaries
     if bins :
 
         trace ( "bins = "+pformat(bins), "SConscript", 2 )
+
+        env = DefaultEnvironment()
+        bindir = env['BINDIR']
         
+        # Program options
+        binkw = {}
+        binkw['LIBS'] = _getkwlist ( kw, 'LIBS' )
+        #binkw['LIBS'].insert ( 0, _getpkg( kw ) )
+        binkw['LIBDIRS'] = _getkwlist ( kw, 'LIBDIRS' )
+    
         for bin, srcs in bins.iteritems() :
             
             b = env.Program( bin, source=srcs, **binkw )
-            env.Install ( bindir, source=b )
+            if install : env.Install ( bindir, source=b )
             
             deps = findAllDependencies( b[0] )
             trace ( bin+" deps = " + pformat(deps), "SConscript", 4 )
             
             setBinDeps ( env, b[0], deps )
-            
