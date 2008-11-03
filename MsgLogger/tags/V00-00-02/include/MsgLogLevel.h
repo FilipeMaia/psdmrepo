c93a38ca03ed35904589a3d680a400004c43cd2b
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2008-11-03 21:28:38 +0000

    Tagging $pkg with $tag
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@88 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ MsgLogger/tags/V00-00-02/include/MsgLogLevel.h
@@ -0,0 +1,145 @@
+#ifndef MSGLOGLEVEL_HH
+#define MSGLOGLEVEL_HH
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: MsgLogLevel.h,v 1.2 2005/07/26 18:09:14 salnikov Exp $
+//
+// Description:
+//	Class MsgLogLevel.
+//
+// Environment:
+//      This software was developed for the BaBar collaboration.  If you
+//      use all or part of it, please give an appropriate acknowledgement.
+//
+// Author List:
+//      Andy Salnikov
+//
+// Copyright Information:
+//      Copyright (C) 2005 SLAC
+//
+//------------------------------------------------------------------------
+
+//-------------
+// C Headers --
+//-------------
+extern "C" {
+}
+
+//---------------
+// C++ Headers --
+//---------------
+#include <iostream>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *  This class defines message logging levels, their names and ordering.
+ *
+ *  This software was developed for the BaBar collaboration.  If you
+ *  use all or part of it, please give an appropriate acknowledgement.
+ *
+ *  Copyright (C) 2005 SLAC
+ *
+ *  @see MsgLogLevelMsgLogLevel
+ *
+ *  @version $Id: MsgLogLevel.h,v 1.2 2005/07/26 18:09:14 salnikov Exp $
+ *
+ *  @author Andy Salnikov
+ */
+
+namespace MsgLogger {
+class MsgLogLevel {
+
+public:
+
+  /**
+   *  Message Level levels. The code in the package depends on the range
+   *  which should in [0...LAST_LEVEL]
+   */
+  enum Level { debug,
+	       trace,
+	       info,
+	       warning,
+	       error,
+	       nolog,      // There should be no messages with this level, it's only for loggers
+	       LAST_LEVEL = nolog
+             };
+
+  // Construct root logger
+  MsgLogLevel( Level code ) : _level(code) {}
+  explicit MsgLogLevel( int code ) ;
+  explicit MsgLogLevel( const std::string& name ) ;
+
+  // default copy ctor is OK
+  //MsgLogLevel( const MsgLogLevel& );
+
+  // Destructor
+  ~MsgLogLevel() {}
+
+  // default assignment is OK
+  //MsgLogLevel& operator= ( const MsgLogLevel& );
+  MsgLogLevel& operator= ( const Level code ) { _level = code ; return *this ;}
+
+  // comparison operators
+  bool operator == ( MsgLogLevel other ) const { return _level == other._level ; }
+  bool operator != ( MsgLogLevel other ) const { return _level != other._level ; }
+  bool operator < ( MsgLogLevel other ) const { return int(_level) < int(other._level) ; }
+  bool operator <= ( MsgLogLevel other ) const { return int(_level) <= int(other._level) ; }
+  bool operator > ( MsgLogLevel other ) const { return int(_level) > int(other._level) ; }
+  bool operator >= ( MsgLogLevel other ) const { return int(_level) >= int(other._level) ; }
+
+  // get full printable name of Level level
+  const char* levelName () const ;
+
+  // get 3-letter printable name of Level code
+  const char* level3 () const ;
+
+  // get one-char Level code
+  char levelLetter () const ;
+
+  // return level code, guaranteed between 0 and LAST_LEVEL
+  int code() const { return int(_level) ; }
+
+protected:
+
+  // Helper functions
+
+private:
+
+  // Data
+  Level _level ;
+
+//------------------
+// Static Members --
+//------------------
+
+public:
+
+  // default level for loggers
+  static MsgLogLevel defaultLevel () { return MsgLogLevel(info) ; }
+
+};
+
+inline std::ostream&
+operator<< ( std::ostream& o, MsgLogLevel sev ) {
+  return o << sev.levelName() ;
+}
+
+} // namespace MsgLogger
+
+#endif // MSGLOGLEVEL_HH
---
+++ MsgLogger/tags/V00-00-02/include/MsgLogStream.h
@@ -0,0 +1,153 @@
+#ifndef MSGLOGSTREAM_HH
+#define MSGLOGSTREAM_HH
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: MsgLogStream.h,v 1.3 2007/02/11 03:24:25 salnikov Exp $
+//
+// Description:
+//	Class MsgLogStream.
+//
+// Environment:
+//      This software was developed for the BaBar collaboration.  If you
+//      use all or part of it, please give an appropriate acknowledgement.
+//
+// Author List:
+//      Andy Salnikov
+//
+// Copyright Information:
+//      Copyright (C) 2005 SLAC
+//
+//------------------------------------------------------------------------
+
+//-------------
+// C Headers --
+//-------------
+extern "C" {
+}
+
+//---------------
+// C++ Headers --
+//---------------
+#include <string>
+#include <sstream>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogLevel.h"
+#include "MsgLogger/MsgLogger.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *  Macros(grrr!) for user's convenience
+ */
+#ifdef MsgLog
+#undef MsgLog
+#endif
+#define MsgLog(logger,sev,msg) \
+  if ( MsgLogger::MsgLogger(logger).logging(MsgLogger::MsgLogLevel(MsgLogger::MsgLogLevel::sev)) ) { \
+    MsgLogger::MsgLogStream stream(logger, MsgLogger::MsgLogLevel(MsgLogger::MsgLogLevel::sev), __FILE__, __LINE__) ;\
+    stream.ostream_hack() << msg ; \
+  }
+
+#ifdef MsgLogRoot
+#undef MsgLogRoot
+#endif
+#define MsgLogRoot(sev,msg) \
+  if ( MsgLogger::MsgLogger().logging(MsgLogger::MsgLogLevel(MsgLogger::MsgLogLevel::sev)) ) { \
+    MsgLogger::MsgLogStream stream(MsgLogger::MsgLogLevel(MsgLogger::MsgLogLevel::sev), __FILE__, __LINE__) ;\
+    stream.ostream_hack() << msg ; \
+  }
+
+#ifdef WithMsgLog
+#undef WithMsgLog
+#endif
+#define WithMsgLog(logger,sev,str) \
+  for ( MsgLogger::MsgLogStream str(logger, MsgLogger::MsgLogLevel(MsgLogger::MsgLogLevel::sev), __FILE__ , __LINE__) ; str.ok() ; str.finish() )
+
+#ifdef WithMsgLogRoot
+#undef WithMsgLogRoot
+#endif
+#define WithMsgLogRoot(sev,str) \
+  for ( MsgLogger::MsgLogStream str(MsgLogger::MsgLogLevel(MsgLogger::MsgLogLevel::sev), __FILE__ , __LINE__) ; str.ok() ; str.finish() )
+
+/**
+ *  Special stream class (subclass of standard stream class) which collects
+ *  the message source and forwards complete message to the logger class
+ *  on destruction.
+ *
+ *  This software was developed originally for the BaBar collaboration and
+ *  adapted/rewritten for LUSI.
+ *
+ *  Copyright (C) 2005 SLAC
+ *
+ *  @see MsgLogger
+ *
+ *  @version $Id: MsgLogStream.h,v 1.3 2007/02/11 03:24:25 salnikov Exp $
+ *
+ *  @author Andy Salnikov
+ */
+
+namespace MsgLogger {
+class MsgLogStream : public std::stringstream {
+
+public:
+
+  /**
+   *  Constructors. 'file' argument is usually a filenamestring constructed from
+   *  __FILE__ macros. It is char* type instead of std::string for optimization
+   *  reasons (crappy C++ has no compile-time constructors for classes.) The pointer is stored
+   *  in an object, content is not copied, so make sure that you don't overwrite the string you pass.
+   */
+  explicit MsgLogStream ( MsgLogLevel sev, const char* file = 0, int line = -1 ) ;
+  MsgLogStream ( const std::string& loggerName, MsgLogLevel sev, const char* file = 0, int line = -1 ) ;
+
+  // Destructor
+  virtual ~MsgLogStream() ;
+
+  // g++ somehow fails to recognize temporary MsgLogStream() as a good stream,
+  // had to add this "cast" operation
+  std::ostream& ostream_hack() { return *this ; }
+
+  // send my content to logger
+  void emit() ;
+
+  // get the state of the stream
+  bool ok() const { return _ok ; }
+
+  // set the state of the stream to "not OK"
+  void finish() { _ok = false ; }
+
+protected:
+
+private:
+
+  // Data members
+  std::string _logger ;
+  MsgLogLevel _sev ;
+  const char* _file ;
+  int _lineNum ;
+  bool _ok ;
+
+  // Note: if your class needs a copy constructor or an assignment operator,
+  //  make one of the following public and implement it.
+  MsgLogStream( const MsgLogStream& );                // Copy Constructor
+  MsgLogStream& operator= ( const MsgLogStream& );    // Assignment op
+
+};
+
+} // namespace MsgLogger
+
+#endif // MSGLOGSTREAM_HH
