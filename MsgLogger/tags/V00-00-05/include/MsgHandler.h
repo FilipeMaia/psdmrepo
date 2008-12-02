d25b40ccd51abb282b7095eded1b554f0dec436d
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2008-12-02 19:31:42 +0000

    Tagging $pkg with $tag
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@102 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ MsgLogger/tags/V00-00-05/include/MsgHandler.h
@@ -0,0 +1,119 @@
+#ifndef MSGHANDLER_HH
+#define MSGHANDLER_HH
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: MsgHandler.h,v 1.2 2005/07/26 18:09:14 salnikov Exp $
+//
+// Description:
+//	Class MsgHandler.
+//
+// Environment:
+//      This software was developed for the BaBar collaboration.  If you
+//      use all or part of it, please give an appropriate acknowledgement.
+//
+// Author List:
+//      Andy Salnikov
+//
+// Copyright Information:
+//      Copyright (C) 2005 SLAC
+//
+//------------------------------------------------------------------------
+
+//-------------
+// C Headers --
+//-------------
+extern "C" {
+}
+
+//---------------
+// C++ Headers --
+//---------------
+#include <iostream>
+#include <string>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogLevel.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *  The base class for handling messages. Handling means dispatching
+ *  messages to their final distantion, such as terminal, log file,
+ *  external logging service, etc. Handlers like loggers have corresponding
+ *  logging level which can be changed by clients. Default logging
+ *  level for handlers is debug, which means they will log whatever
+ *  loggers give them. For formatting the messages handlers use
+ *  MsgFormatter class, clients can attach their own formatter objects to
+ *  handlers, otherwise default formatter class will be used.
+ *
+ *  This software was developed for the BaBar collaboration.  If you
+ *  use all or part of it, please give an appropriate acknowledgement.
+ *
+ *  Copyright (C) 2005 SLAC
+ *
+ *  @see MsgLogger
+ *
+ *  @version $Id: MsgHandler.h,v 1.2 2005/07/26 18:09:14 salnikov Exp $
+ *
+ *  @author Andy Salnikov
+ */
+
+namespace MsgLogger {
+
+class MsgFormatter ;
+class MsgLogRecord ;
+
+class MsgHandler {
+
+public:
+
+  // Destructor
+  virtual ~MsgHandler() ;
+
+  /// attaches the formatter, will be owned by handler
+  virtual void setFormatter ( MsgFormatter* formatter ) ;
+
+  /// set the logger level, messages with the level below this won't be logged
+  virtual void setLevel ( MsgLogLevel level ) ;
+
+  /// check if the specified level will log any message
+  virtual bool logging ( MsgLogLevel sev ) const ;
+
+  /// get the stream for the specified log level
+  virtual bool log ( const MsgLogRecord& record ) const = 0 ;
+
+protected:
+
+  // Constructor
+  MsgHandler() ;
+
+  // get the formatter
+  MsgFormatter& formatter() const ;
+
+private:
+
+  // Data members
+  mutable MsgFormatter* _formatter ;
+  MsgLogLevel _level ;
+
+  // Disable copy
+  MsgHandler( const MsgHandler& );
+  MsgHandler& operator= ( const MsgHandler& );
+
+};
+} // namespace MsgLogger
+
+#endif // MSGHANDLER_HH
---
+++ MsgLogger/tags/V00-00-05/src/MsgLogLevel.cpp
@@ -0,0 +1,144 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: MsgLogLevel.cc,v 1.1 2005/07/22 22:23:32 salnikov Exp $
+//
+// Description:
+//	Class MsgLogLevel
+//
+// Environment:
+//	Software developed for the BaBar Detector at the SLAC B-Factory.
+//
+// Author List:
+//      Andy Salnikov
+//
+// Copyright Information:
+//      Copyright (C) 2005 SLAC
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "MsgLogger/MsgLogLevel.h"
+
+//---------------
+// C++ Headers --
+//---------------
+#include <stdexcept>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace MsgLogger {
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+MsgLogLevel::MsgLogLevel( int code )
+{
+  if ( code < int(debug) ) {
+    code = int(debug) ;
+  } else if ( code > int(error) ) {
+    code = int(error) ;
+  }
+  _level = Level(code) ;
+}
+
+MsgLogLevel::MsgLogLevel( const std::string& level )
+{
+  if ( level == "trace" ) {
+    _level = trace ;
+  } else if ( level == "debug" ) {
+    _level = debug ;
+  } else if ( level == "warning" ) {
+    _level = warning ;
+  } else if ( level == "info" ) {
+    _level = info ;
+  } else if ( level == "error" ) {
+    _level = error ;
+  } else if ( level == "fatal" ) {
+    _level = fatal ;
+  } else if ( level == "nolog" ) {
+    _level = nolog ;
+  } else {
+    throw std::out_of_range ( "unexpected logging level name" ) ;
+  }
+}
+
+// get full printable name of Level level
+const char*
+MsgLogLevel::levelName () const
+{
+  switch ( _level ) {
+    case debug:
+      return "debug" ;
+    case trace:
+      return "trace" ;
+    case info:
+      return "info" ;
+    case warning:
+      return "warning" ;
+    case error:
+      return "error" ;
+    case fatal:
+      return "fatal" ;
+    case nolog:
+    default:
+      return "no-log" ;
+  }
+}
+
+// get 3-letter printable name of Level code
+const char*
+MsgLogLevel::level3 () const
+{
+  switch ( _level ) {
+    case debug:
+      return "DBG" ;
+    case trace:
+      return "TRC" ;
+    case info:
+      return "INF" ;
+    case warning:
+      return "WRN" ;
+    case error:
+      return "ERR" ;
+    case fatal:
+      return "FTL" ;
+    case nolog:
+    default:
+      return "???" ;
+  }
+}
+
+// get one-char Level code
+char
+MsgLogLevel::levelLetter () const
+{
+  switch ( _level ) {
+    case debug:
+      return 'D' ;
+    case trace:
+      return 'T' ;
+    case info:
+      return 'I' ;
+    case warning:
+      return 'W' ;
+    case error:
+      return 'E' ;
+    case fatal:
+      return 'F' ;
+    case nolog:
+    default:
+      return '?' ;
+  }
+}
+
+} // namespace MsgLogger
