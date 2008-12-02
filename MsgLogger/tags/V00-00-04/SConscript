36869d747310035eb0331f6ab4315be7540e31f3
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2008-12-02 19:19:11 +0000

    Tagging $pkg with $tag
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@100 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ MsgLogger/tags/V00-00-04/SConscript
@@ -0,0 +1,20 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id: SConscript 4 2008-10-08 19:27:36Z salnikov $
+#
+# Description:
+#  SConscript file for package MsgLogger
+#------------------------------------------------------------------------
+
+# Do not delete following line, it must be present in 
+# SConscript file for any LUSI project 
+Import('*')
+
+#
+# The standard LUSI packages which build libraries, applications,
+# and python modules it is usually sufficient to call
+# standardSConscript() function which defines rules for all
+# above targets. In some cases additional parameters may be needed,
+# consult SConsTools/src/standardSConscript.py file.
+#
+standardSConscript( LIBS="rt" )
---
+++ MsgLogger/tags/V00-00-04/src/MsgHandler.cpp
@@ -0,0 +1,95 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: MsgHandler.cc,v 1.1 2005/07/22 22:23:32 salnikov Exp $
+//
+// Description:
+//	Class MsgHandler
+//
+// Environment:
+//	Software developed for the BaBar Detector at the SLAC B-Factory.
+//
+// Author List:
+//      Andy Salnikov
+//
+// Copyright Information:
+//      Copyright (C) 2005 SLAC
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "MsgLogger/MsgHandler.h"
+
+//-------------
+// C Headers --
+//-------------
+extern "C" {
+}
+
+//---------------
+// C++ Headers --
+//---------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgFormatter.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace MsgLogger {
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+// Constructor
+MsgHandler::MsgHandler()
+  : _formatter(0)
+  , _level(MsgLogLevel::debug)
+{
+}
+
+// Destructor
+MsgHandler::~MsgHandler()
+{
+}
+
+/// attaches the formatter, will be owned by handler
+void
+MsgHandler::setFormatter ( MsgFormatter* formatter )
+{
+  delete _formatter ;
+  _formatter = formatter ;
+}
+
+/// set the logger level, messages with the level below this won't be logged
+void
+MsgHandler::setLevel ( MsgLogLevel level )
+{
+  _level = level ;
+}
+
+/// check if the specified level will log any message
+bool
+MsgHandler::logging ( MsgLogLevel level ) const
+{
+  return level >= _level ;
+}
+
+// get the formatter
+MsgFormatter&
+MsgHandler::formatter() const
+{
+  if ( ! _formatter ) {
+    _formatter = new MsgFormatter ;
+  }
+  return *_formatter ;
+}
+
+} // namespace MsgLogger
+
