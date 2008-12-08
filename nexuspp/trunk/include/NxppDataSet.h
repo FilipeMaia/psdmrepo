a7d7e746e2f80b2f6481c610711dd30964095026
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2008-12-08 17:02:55 +0000

    Tag: V00-00-01 2008-12-08 Andy Salnikov - first import into SVN
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@126 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ nexuspp/trunk/include/NxppDataSet.h
@@ -0,0 +1,104 @@
+#ifndef NEXUSPP_NXPPDATASET_H
+#define NEXUSPP_NXPPDATASET_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class NxppDataSet.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "nexuspp/NxppTypeTraits.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *  C++ wrapper for napi.h
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgement.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+namespace nexuspp {
+
+template <typename T>
+class NxppDataSet  {
+public:
+
+  // Constructor
+  NxppDataSet ( NXhandle fileId ) : m_fileId(fileId) {}
+
+  // Destructor
+  ~NxppDataSet () {
+    // on destruction close the data set
+    NXclosedata ( m_fileId ) ;
+  }
+
+  // put the data
+  bool putData ( T* data ) {
+    void* addr = NxppTypeTraits<T>::dataAddress(data) ;
+    return NXputdata ( m_fileId, addr ) == NX_OK ;
+  }
+  bool putData ( T data ) {
+    void* addr = NxppTypeTraits<T>::dataAddress(data) ;
+    return NXputdata ( m_fileId, addr ) == NX_OK ;
+  }
+
+  // add attribute
+  template <typename U>
+  bool addAttribute ( const std::string& name, const U& value ) {
+    void* addr = NxppTypeTraits<U>::dataAddress(value) ;
+    int size = NxppTypeTraits<U>::size(value) ;
+    int type = NxppTypeTraits<U>::nxtype ;
+    return NXputattr ( m_fileId, name.c_str(), addr, size, type ) == NX_OK ;
+  }
+
+  // add attribute
+  template <typename U>
+  bool addAttribute ( const std::string& name, int size, const U* ptr ) {
+    void* addr = NxppTypeTraits<U>::dataAddress(ptr) ;
+    int type = NxppTypeTraits<U>::nxtype ;
+    return NXputattr ( m_fileId, name.c_str(), addr, size, type ) == NX_OK ;
+  }
+
+protected:
+
+private:
+
+  // Data members
+  NXhandle m_fileId;
+
+  // Copy constructor and assignment are disabled by default
+  NxppDataSet ( const NxppDataSet& ) ;
+  NxppDataSet operator = ( const NxppDataSet& ) ;
+
+};
+
+} // namespace nexuspp
+
+#endif // NEXUSPP_NXPPDATASET_H
---
+++ nexuspp/trunk/include/NxppTypeTraits.h
@@ -0,0 +1,112 @@
+#ifndef NEXUSPP_NXPPTYPETRAITS_H
+#define NEXUSPP_NXPPTYPETRAITS_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class NxppTypeTraits.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <stdint.h>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "nexus/napi.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *  Type traits package.
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgement.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+namespace nexuspp {
+
+template <typename T>
+struct NxppTypeTraits  {
+};
+
+template <typename T, int NxType>
+struct NxppTypeTraitsNum {
+
+  // nxtype determines nexus type, not known for unspecialized version
+  enum { nxtype = NxType } ;
+
+  // get the address of the data item
+  static void* dataAddress( T* ptr ) { return static_cast<void*>(ptr); }
+  static void* dataAddress( T val ) { return static_cast<void*>(&val); }
+
+  // size() returns the size of the data value
+  // in terms of the 'nxtype' type
+  static int size( T val ) { return 1 ; }
+};
+
+template <>
+struct NxppTypeTraits<float> : public NxppTypeTraitsNum<float,NX_FLOAT32> {
+};
+
+template <>
+struct NxppTypeTraits<double> : public NxppTypeTraitsNum<double,NX_FLOAT64> {
+};
+
+template <>
+struct NxppTypeTraits<int8_t> : public NxppTypeTraitsNum<int8_t,NX_INT8> {
+};
+
+template <>
+struct NxppTypeTraits<uint8_t> : public NxppTypeTraitsNum<uint8_t,NX_UINT8> {
+};
+
+template <>
+struct NxppTypeTraits<int16_t> : public NxppTypeTraitsNum<int16_t,NX_INT16> {
+};
+
+template <>
+struct NxppTypeTraits<uint16_t> : public NxppTypeTraitsNum<uint16_t,NX_UINT16> {
+};
+
+template <>
+struct NxppTypeTraits<int32_t> : public NxppTypeTraitsNum<int32_t,NX_INT32> {
+};
+
+template <>
+struct NxppTypeTraits<uint32_t> : public NxppTypeTraitsNum<uint32_t,NX_UINT32> {
+};
+
+template <>
+struct NxppTypeTraits<std::string>  {
+  enum { nxtype = NX_CHAR } ;
+  static void* dataAddress( const std::string& val ) { return (void*)(val.c_str()); }
+  static int size( const std::string& val ) { return val.size() ; }
+};
+
+
+} // namespace nexuspp
+
+#endif // NEXUSPP_NXPPTYPETRAITS_H
