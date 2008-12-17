b2ce15545826a5bf049e154bf54bd5775fa8130b
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2008-12-17 22:54:22 +0000

    Tagging $pkg with $tag
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@145 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ MsgLogger/tags/V00-00-06/include/MsgLogRecord.h
@@ -0,0 +1,128 @@
+#ifndef MSGLOGRECORD_HH
+#define MSGLOGRECORD_HH
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: MsgLogRecord.h,v 1.2 2005/07/26 18:09:14 salnikov Exp $
+//
+// Description:
+//	Class MsgLogRecord.
+//
+// Environment:
+//      This software was developed for the BaBar collaboration.  If you
+//      use all or part of it, please give an appropriate acknowledgement.
+//
+// Author List:
+//      Andy Salnikov
+//
+// Copyright Information:
+//      Copyright (C) 2005 SLAC
+//
+//------------------------------------------------------------------------
+
+//-------------
+// C Headers --
+//-------------
+extern "C" {
+}
+
+//---------------
+// C++ Headers --
+//---------------
+#include <iostream>
+#include <string>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *  Clkass which defines a single logging message (record.) It has such attributes
+ *  as message itself, logging level, corresponding logger name, file/line where
+ *  the message originated. For performance optimization purposes the message is
+ *  passed as a pointer to the streambuf. This complicates things a bit, you have
+ *  to be careful when extracting message test from the streambuf, but avoids
+ *  copying of the strings. Also for optimization reasons the timestamp is not a part
+ *  of the message, but is added to the formatted message only during formatting
+ *  (only if needed.)
+ *
+ *  This software was developed for the BaBar collaboration.  If you
+ *  use all or part of it, please give an appropriate acknowledgement.
+ *
+ *  Copyright (C) 2005 SLAC
+ *
+ *  @see MsgLogRecordMsgLogRecord
+ *
+ *  @version $Id: MsgLogRecord.h,v 1.2 2005/07/26 18:09:14 salnikov Exp $
+ *
+ *  @author Andy Salnikov
+ */
+
+namespace MsgLogger {
+
+class MsgLoggerImpl ;
+
+class MsgLogRecord {
+
+public:
+
+  // Construct root logger
+  MsgLogRecord( const std::string& logger,
+                MsgLogLevel level,
+                const char* fileName,
+                int linenum,
+                std::streambuf* msgbuf )
+    : _logger(logger), _level(level), _fileName(fileName), _lineNum(linenum), _msgbuf(msgbuf)
+    {}
+
+  // Destructor
+  ~MsgLogRecord() {}
+
+  /// get logger name
+  const std::string& logger() const { return _logger ; }
+
+  /// get message log level
+  MsgLogLevel level() const { return _level ; }
+
+  /// get message location
+  const char* fileName() const { return _fileName ; }
+  int lineNum() const { return _lineNum ; }
+
+  /// get the stream for the specified log level
+  std::streambuf* msgbuf() const { return _msgbuf ; }
+
+protected:
+
+  // Helper functions
+
+private:
+
+  // Friends
+
+  // Data members
+  const std::string& _logger ;
+  const MsgLogLevel _level ;
+  const char* _fileName ;
+  int _lineNum ;
+  std::streambuf* _msgbuf ;
+
+  MsgLogRecord( const MsgLogRecord& );
+  MsgLogRecord& operator= ( const MsgLogRecord& );
+
+};
+} // namespace MsgLogger
+
+#endif // MSGLOGRECORD_HH
---
+++ MsgLogger/tags/V00-00-06/src/MsgLogger.cpp
@@ -0,0 +1,93 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: MsgLogger.cc,v 1.1 2005/07/22 22:23:33 salnikov Exp $
+//
+// Description:
+//	Class MsgLogger
+//
+// Environment:
+//	Software developed for the BaBar Detector at the SLAC B-Factory.
+//
+// Author List:
+//      Andy Salnikov
+//
+// Copyright Information:
+//      Copyright (C) 2005 SLAC
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "MsgLogger/MsgLogger.h"
+
+//-------------
+// C Headers --
+//-------------
+extern "C" {
+}
+
+//---------------
+// C++ Headers --
+//---------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLoggerImpl.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace MsgLogger {
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+// Construct a logger
+MsgLogger::MsgLogger( const std::string& name )
+  : _impl ( MsgLoggerImpl::getLogger(name) )
+{
+}
+
+/// set the logger level, messages with the level below this won't be logged
+void
+MsgLogger::setLevel ( MsgLogLevel level )
+{
+  if ( _impl ) _impl->setLevel ( level ) ;
+}
+
+/// define wheter or not we need messages propagated to ancestors
+void
+MsgLogger::propagate ( bool flag )
+{
+  if ( _impl ) _impl->propagate( flag ) ;
+}
+
+/// add a handler for the messages, takes ownership of the object
+void
+MsgLogger::addHandler ( MsgHandler* handler )
+{
+  if ( _impl ) _impl->addHandler ( handler ) ;
+}
+
+
+/// check if the specified level will log any message
+bool
+MsgLogger::logging ( MsgLogLevel sev ) const
+{
+  return _impl ? _impl->logging ( sev ) : false ;
+}
+
+/// get the stream for the specified log level
+bool
+MsgLogger::log ( const MsgLogRecord& record ) const
+{
+  return _impl ? _impl->log ( record ) : false ;
+}
+
+
+} // namespace MsgLogger
