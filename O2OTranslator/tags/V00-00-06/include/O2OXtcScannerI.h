d0237576649bd03e1888b008d1dc953b74706a8f
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-04-08 01:04:49 +0000

    Tagging $pkg with $tag
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@253 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ O2OTranslator/tags/V00-00-06/include/O2OXtcScannerI.h
@@ -0,0 +1,87 @@
+#ifndef O2OTRANSLATOR_O2OXTCSCANNERI_H
+#define O2OTRANSLATOR_O2OXTCSCANNERI_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class O2OXtcScannerI.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "O2OTranslator/O2OPdsDataFwd.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *  Interface for a "visitor" class for XTC scanning.
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgement.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+namespace O2OTranslator {
+
+class O2OXtcScannerI  {
+public:
+
+  // Destructor
+  virtual ~O2OXtcScannerI () {}
+
+  // signal start/end of the event (datagram)
+  virtual void eventStart ( const Pds::Sequence& seq ) = 0 ;
+  virtual void eventEnd ( const Pds::Sequence& seq ) = 0 ;
+
+  // signal start/end of the level
+  virtual void levelStart ( const Pds::Src& src ) = 0 ;
+  virtual void levelEnd ( const Pds::Src& src ) = 0 ;
+
+  // visit the data object
+  virtual void dataObject ( const Pds::Acqiris::ConfigV1& data, const Pds::DetInfo& detInfo ) = 0 ;
+  virtual void dataObject ( const Pds::Acqiris::DataDescV1& data, const Pds::DetInfo& detInfo ) = 0 ;
+  virtual void dataObject ( const Pds::Camera::FrameFexConfigV1& data, const Pds::DetInfo& detInfo ) = 0 ;
+  virtual void dataObject ( const Pds::Camera::FrameV1& data, const Pds::DetInfo& detInfo ) = 0 ;
+  virtual void dataObject ( const Pds::Camera::TwoDGaussianV1& data, const Pds::DetInfo& detInfo ) = 0 ;
+  virtual void dataObject ( const Pds::EvrData::ConfigV1& data, const Pds::DetInfo& detInfo ) = 0 ;
+  virtual void dataObject ( const Pds::Opal1k::ConfigV1& data, const Pds::DetInfo& detInfo ) = 0 ;
+
+protected:
+
+  // Default constructor
+  O2OXtcScannerI () {}
+
+private:
+
+  // Copy constructor and assignment are disabled by default
+  O2OXtcScannerI ( const O2OXtcScannerI& ) ;
+  O2OXtcScannerI& operator = ( const O2OXtcScannerI& ) ;
+
+};
+
+} // namespace O2OTranslator
+
+#endif // O2OTRANSLATOR_O2OXTCSCANNERI_H
