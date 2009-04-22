b386fe8668a9ae7e44c593be6daeeea8fab3b0f2
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-04-22 22:51:52 +0000

    Tagging $pkg with $tag
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@283 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ H5DataTypes/tags/V00-00-03/include/CameraFrameFexConfigV1.h
@@ -0,0 +1,81 @@
+#ifndef H5DATATYPES_CAMERAFRAMEFEXCONFIGV1_H
+#define H5DATATYPES_CAMERAFRAMEFEXCONFIGV1_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class CameraFrameFexConfigV1.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "H5DataTypes/CameraFrameCoordV1.h"
+#include "hdf5pp/Group.h"
+#include "pdsdata/camera/FrameFexConfigV1.hh"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+namespace H5DataTypes {
+
+struct CameraFrameFexConfigV1_Data {
+  uint32_t   forwarding;
+  uint32_t   forward_prescale;
+  uint32_t   processing;
+  CameraFrameCoordV1_Data roiBegin;
+  CameraFrameCoordV1_Data roiEnd;
+  uint32_t   threshold;
+  uint32_t   number_of_masked_pixels;
+};
+
+class CameraFrameFexConfigV1  {
+public:
+
+  typedef Pds::Camera::FrameFexConfigV1 XtcType ;
+
+  CameraFrameFexConfigV1 () {}
+  CameraFrameFexConfigV1 (const Pds::Camera::FrameFexConfigV1& config) ;
+
+  static hdf5pp::Type stored_type() ;
+  static hdf5pp::Type native_type() ;
+
+  static void store ( const Pds::Camera::FrameFexConfigV1& config, hdf5pp::Group location ) ;
+
+private:
+  CameraFrameFexConfigV1_Data m_data ;
+};
+
+
+} // namespace H5DataTypes
+
+#endif // H5DATATYPES_CAMERAFRAMEFEXCONFIGV1_H
