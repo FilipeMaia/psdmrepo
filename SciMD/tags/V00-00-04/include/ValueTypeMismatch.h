2f1953137baa0de2f566e9e9dd72c31d0b6a5a13
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-05-06 02:05:25 +0000

    Tagging $pkg with $tag
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@310 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ SciMD/tags/V00-00-04/include/ValueTypeMismatch.h
@@ -0,0 +1,101 @@
+#ifndef SCIMD_VALUETYPEMISMATCH_H
+#define SCIMD_VALUETYPEMISMATCH_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class ValueTypeMismatch.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+#include <string>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+#include <exception>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace SciMD {
+
+/**
+ * Exception class to report wrong type usage in class or method templates.
+ *
+ * USAGE:
+ *
+ * This software was developed for the LUSI project.  If you use all or 
+ * part of it, please give an appropriate acknowledgement.
+ *
+ * @version $Id: template!C++!h 4 2008-10-08 19:27:36Z salnikov $
+ *
+ * @author Igor Gaponenko
+ */
+class ValueTypeMismatch : public std::exception {
+
+public:
+
+    /**
+      * Normal constructor.
+      *
+      * Create an object and explain a reason of the exception.
+      */
+    explicit ValueTypeMismatch (const std::string& reason) ;
+
+    /**
+      * Copy constructor.
+      */
+    ValueTypeMismatch ( const ValueTypeMismatch& ) ;
+
+    /**
+      * Assignment operator.
+      */
+    ValueTypeMismatch& operator = ( const ValueTypeMismatch& ) ;
+
+    // Destructor
+
+    virtual ~ValueTypeMismatch () throw() ;
+
+    /**
+      * Return a reason of the exception.
+      *
+      * The method overrides the corresponding method of the derived
+      * class.
+      *
+      * @see method std::exception::what()
+      */
+    virtual const char* what () const throw() ;
+
+private:
+
+    // Default constructor is disabled
+
+    ValueTypeMismatch () ;
+
+private:
+
+    // Data members
+
+    std::string m_reason;  // what caused the exception
+};
+
+} // namespace SciMD
+
+#endif // SCIMD_VALUETYPEMISMATCH_H
