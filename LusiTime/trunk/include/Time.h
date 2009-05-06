9ec75ce9e926deb96bac4be3c728fdee07806285
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-05-06 07:31:34 +0000

    - added two methods for bidirectional translation of timestamps   to and from regulat C++ strings.
    M    include/Time.h
    M    doc/ChangeLog
    M    src/Time.cpp
    
    
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@311 b967ad99-d558-0410-b138-e0f6c56caec7

--- LusiTime/trunk/include/Time.h
+++ LusiTime/trunk/include/Time.h
@@ -14,15 +14,16 @@
 // C/C++ Headers --
 //-----------------
 #include <time.h>
+#include <string>
 
 //----------------------
 // Base Class Headers --
 //----------------------
 
-
 //-------------------------------
 // Collaborating Class Headers --
 //-------------------------------
+#include "LusiTime/Exceptions.h"
 
 //------------------------------------
 // Collaborating Class Declarations --
@@ -54,6 +55,41 @@ public:
   /// Returns current time
   static Time now() ;
 
+  /**
+    * Parse an input string as the date and time.
+    *
+    * If successful then the method will return a valid object of
+    * the current class. Otherwise an exception will be thrown.
+    *
+    * The parsed string is expected to have the following format:
+    *
+    *   <year>-<month>-<day> <hours>:<minutes>:<seconds>
+    *
+    * For example:
+    *
+    *   2009-JUN-05 10:12:32
+    *   2009-06-05 10:12:32
+    *
+    * Other notes on a format of the input string:
+    *
+    *   - whitespaces, tabs, newline or carriage return symbols at the begining
+    *   of the string are allowed
+    *
+    *   - whitespaces, tabs, newline or carriage return symbols at the end
+    *   of the string are also allowed
+    *
+    *   - the date and time fields must be separated by at least one whitespace
+    *   character. It's also allowed to have more then one whitespace in there.
+    *
+    *   - month can be given either as a number or as a name. The name can be
+    *   short ('JAN', 'FEB', etc.) or complete ('JANUARY', 'FEBRUARY', etc.)
+    *
+    * TODO: The 'mktime' used in the current implementation of the method
+    *       uses local timezone, therefore the code will run correctly only
+    *       on a machine configured for PDT. We need to resolve this issue.
+    */
+  static Time parse(const std::string& inTimeStr) throw (Exception) ;
+
   // Default constructor, makes invalid time
   Time () {
     m_time.tv_sec = 0xFFFFFFFF ;
@@ -77,6 +113,31 @@ public:
 
   /// check if the time is valid
   bool isValid() const { return m_time.tv_nsec >= 0 ; }
+
+  /**
+    * Translate into the string representation.
+    *
+    * The output string will have the following format:
+    *
+    *   <year>-<month>-<day> <hours>:<minutes>:<seconds>
+    *
+    * Where the month will be represented by a number. For example:
+    *
+    *   2009-06-05 10:12:32
+    *
+    * Also note, that the result won't include nanoseconds. This is done
+    * to make the output format compatible with the one expected by
+    * the opposite method Time::parse().
+    *
+    * If the object is not valid then the method will throw an
+    * exception.
+    *
+    * TODO: The 'localtime_r' used in the current implementation of the method
+    *       uses local timezone, therefore the code will run correctly only
+    *       on a machine configured for PDT. We need to resolve this issue.
+    */
+  std::string toString() const throw (Exception) ;
+
 protected:
 
 private:
