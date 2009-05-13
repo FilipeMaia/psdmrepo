d4037bb32470423be49a3a14565d50356db5d4b5
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-05-13 09:19:08 +0000

    Tagging $pkg with $tag
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@318 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ AppUtils/tags/V00-01-03/include/AppBase.h
@@ -0,0 +1,126 @@
+#ifndef APPUTILS_APPBASE_H
+#define APPUTILS_APPBASE_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class AppBase.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+#include "AppUtils/AppCmdLine.h"
+#include "AppUtils/AppCmdOptIncr.h"
+
+//
+// Convenience macro for defining main() function which "runs" given app class
+//
+#define APPUTILS_MAIN(CLASS) \
+  int main( int argc, char* argv[] ) \
+  try { \
+    CLASS app ( argv[0] ) ; \
+    return app.run ( argc, argv ) ; \
+  } catch( std::exception& e ) { \
+    std::cerr << "Standard exception caught: " << e.what() << std::endl ; \
+  } catch( ... ) { \
+    std::cerr << "Unknown exception caught" << std::endl ; \
+  }
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+namespace AppUtils {
+
+class AppBase  {
+public:
+
+  // Default constructor
+  explicit AppBase ( const std::string& appName ) ;
+
+  // Destructor
+  virtual ~AppBase () ;
+
+  /**
+   *  Run the application
+   */
+  int run ( int argc, char** argv ) ;
+
+protected:
+
+  /**
+   *  add command line option or argument, typically called from subclass constructor
+   */
+  void setOptionsFile ( AppCmdOptList<std::string>& option ) ;
+  void addOption ( AppCmdOptBase& option ) ;
+  void addArgument ( AppCmdArgBase& arg ) ;
+
+  /**
+   *  Method called before runApp, can be overriden in subclasses.
+   *  Usually if you override it, call base class method too.
+   */
+  virtual int preRunApp () ;
+
+  /**
+   *  Will be implemented in subclasses
+   */
+  virtual int runApp () = 0 ;
+
+  /**
+   *  Method called after runApp, can be overridden in subclasses.
+   *  Usually if you override it, call base class method too.
+   */
+  virtual int postRunApp () ;
+
+  /**
+   *  print some additional info after the usage information is printed.
+   */
+  virtual void moreUsage ( std::ostream& out ) const ;
+
+private:
+
+  // Data members
+  AppCmdLine _cmdline ;
+  AppCmdOptIncr _optVerbose ;
+  AppCmdOptIncr _optQuiet ;
+
+  // Copy constructor and assignment are disabled by default
+  AppBase ( const AppBase& ) ;
+  AppBase& operator = ( const AppBase& ) ;
+
+};
+
+} // namespace AppUtils
+
+#endif // APPUTILS_APPBASE_H
