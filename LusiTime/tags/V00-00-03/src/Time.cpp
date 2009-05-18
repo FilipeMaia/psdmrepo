ef501da7b8a33a924fa5ea3f7469169a6ba38cde
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-05-18 16:51:09 +0000

    Tagging $pkg with $tag
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@321 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ LusiTime/tags/V00-00-03/src/Time.cpp
@@ -0,0 +1,138 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Time...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "LusiTime/Time.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "TimeFormat.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace LusiTime {
+
+/// Returns current time
+Time Time::now()
+{
+  struct timespec ts ;
+  int res = clock_gettime( CLOCK_REALTIME, &ts );
+  if ( res < 0 ) {
+    throw ExceptionErrno( "clock_gettime failed" );
+  }
+  return Time ( ts.tv_sec, ts.tv_nsec ) ;
+}
+
+/// Parse an input string as the date and time
+Time Time::parse( const std::string& inTimeStr ) throw (Exception)
+{
+  return TimeFormat::parse ( inTimeStr ) ;
+}
+
+/// Translate into the string representation.
+
+std::string Time::toString() const throw (Exception)
+{
+  // use default format for string presentation including nanoseconds 
+  // and timezone offset
+  return TimeFormat::format( *this, "%F %T%f%z" ) ;
+}
+
+/// Translate into the string representation.
+
+std::string Time::toString( const std::string& fmt ) const throw (Exception)
+{
+  return TimeFormat::format( *this, fmt ) ;
+}
+
+/// comparisons
+bool
+operator< ( const Time& t1, const Time& t2 )
+{
+  if ( not ( t1.isValid() and t2.isValid() ) ) {
+    throw Exception( "operator<(Time,Time): invalid time" ) ;
+  }
+  if ( t1.sec() < t2.sec() ) return true ;
+  if ( t1.sec() > t2.sec() ) return false ;
+  if ( t1.nsec() < t2.nsec() ) return true ;
+  return false ;
+}
+
+bool
+operator<= ( const Time& t1, const Time& t2 )
+{
+  if ( not ( t1.isValid() and t2.isValid() ) ) {
+    throw Exception( "operator<=(Time,Time): invalid time" ) ;
+  }
+  if ( t1.sec() < t2.sec() ) return true ;
+  if ( t1.sec() > t2.sec() ) return false ;
+  if ( t1.nsec() <= t2.nsec() ) return true ;
+  return false ;
+}
+
+bool
+operator> ( const Time& t1, const Time& t2 )
+{
+  if ( not ( t1.isValid() and t2.isValid() ) ) {
+    throw Exception( "operator>(Time,Time): invalid time" ) ;
+  }
+  if ( t1.sec() > t2.sec() ) return true ;
+  if ( t1.sec() < t2.sec() ) return false ;
+  if ( t1.nsec() > t2.nsec() ) return true ;
+  return false ;
+}
+
+bool
+operator>= ( const Time& t1, const Time& t2 )
+{
+  if ( not ( t1.isValid() and t2.isValid() ) ) {
+    throw Exception( "operator>=(Time,Time): invalid time" ) ;
+  }
+  if ( t1.sec() > t2.sec() ) return true ;
+  if ( t1.sec() < t2.sec() ) return false ;
+  if ( t1.nsec() >= t2.nsec() ) return true ;
+  return false ;
+}
+
+bool
+operator== ( const Time& t1, const Time& t2 )
+{
+  if ( not ( t1.isValid() and t2.isValid() ) ) {
+    throw Exception( "operator==(Time,Time): invalid time" ) ;
+  }
+  return t1.sec() == t2.sec() and t1.nsec() == t2.nsec() ;
+}
+
+bool
+operator!= ( const Time& t1, const Time& t2 )
+{
+  if ( not ( t1.isValid() and t2.isValid() ) ) {
+    throw Exception( "operator!=(Time,Time): invalid time" ) ;
+  }
+  return t1.sec() != t2.sec() or t1.nsec() != t2.nsec() ;
+}
+
+} // namespace LusiTime
