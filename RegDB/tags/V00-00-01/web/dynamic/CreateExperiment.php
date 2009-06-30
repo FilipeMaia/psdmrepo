5312d3e90256990d1f67bd9d0c9b94a2e69ccec9
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-06-30 01:20:36 +0000

    Tagging RegDB with V00-00-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@418 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ RegDB/tags/V00-00-01/web/dynamic/CreateExperiment.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once('RegDB/RegDB.inc.php');
+
+/*
+ * This script will lay out a form for creating a new experiment.
+ */
+
+/* Proceed with the operation
+ */
+try {
+
+    $regdb = new RegDB();
+    $regdb->begin();
+
+    $instrument_names = $regdb->instrument_names();
+    $posix_groups     = $regdb->posix_groups();
+
+    // Get the currnt time in the ISO format, then stripe out
+    // the date-time separator 'T' and timezone.
+    //
+    $now = new DateTime();
+    $now_str = $now->format(DateTime::ISO8601);
+    $now_str[10] = ' ';
+    $now_str = substr( $now_str, 0, 19 );
+
+    $logged_user = $_SERVER['WEBAUTH_USER'];
+
+    header( 'Content-type: text/html' );
+    header( "Cache-Control: no-cache, must-revalidate" ); // HTTP/1.1
+    header( "Expires: Sat, 26 Jul 1997 05:00:00 GMT" );   // Date in the past
+
+    /* Create a container with standard fields
+     */
+    $con = new RegDBHtml( 0, 0, 700, 250 );
+    echo $con
+        ->label         ( 300,   0, 'Description')
+        ->label         (   0,  25, 'Experiment: ' )
+        ->value_input   ( 100,  25, 'experiment_name' )
+        ->textarea_input( 300,  25, 'description', 500, 125 )
+        ->label         (   0,  50, 'Instrument: ' )
+        ->select_input  ( 100,  50, 'instrument_name', $instrument_names )
+        ->label         (   0, 100, 'Begin Time: ' )
+        ->value_input   ( 100, 100, 'begin_time', $now_str )
+        ->label         (   0, 125, 'End Time: '   )
+        ->value_input   ( 100, 125, 'end_time', $now_str )
+        ->label         ( 300, 175, 'Contact Info' )
+        ->label         (   0, 200, 'POSIX Group: ' )
+        ->select_input  ( 100, 200, 'group', $posix_groups )
+        ->textarea_input( 300, 200, 'contact', 500, 50 )
+        ->label         (   0, 225, 'Leader: '     )
+        ->value_input   ( 100, 225, 'leader', $logged_user )
+        ->html();
+
+    $regdb->commit();
+
+} catch( RegDBException $e ) {
+    print $e->toHtml();
+}
+?>
