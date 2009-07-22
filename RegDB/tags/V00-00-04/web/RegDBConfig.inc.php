c01ecc2b0ff129a91673f21f38f0b4325a6e3bf9
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-07-22 03:32:42 +0000

    Tagging RegDB with V00-00-04
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@456 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ RegDB/tags/V00-00-04/web/RegDBConfig.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Default connection parameters for MySQL
+ */
+DEFINE( REGDB_DEFAULT_HOST,      "www-lclsdev.slac.stanford.edu" );
+DEFINE( REGDB_DEFAULT_USER,      "gapon" );
+DEFINE( REGDB_DEFAULT_PASSWORD,  "" );
+DEFINE( REGDB_DEFAULT_DATABASE,  "regdb" );
+DEFINE( REGDB_DEFAULT_LDAP_HOST, "www-lclsdev.slac.stanford.edu" );
+?>
