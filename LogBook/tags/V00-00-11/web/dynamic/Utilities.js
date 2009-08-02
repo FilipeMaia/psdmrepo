315bb00ec8e65a90c8b768706158356ba4a1700a
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-08-02 18:57:47 +0000

    Tagging LogBook with V00-00-11
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@473 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ LogBook/tags/V00-00-11/web/dynamic/Utilities.js
@@ -0,0 +1,114 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ *
+ * Developed by Robert Nyman, http://www.robertnyman.com
+ * Code/licensing: http://code.google.com/p/getelementsbyclassname/
+ *
+ * _____________________________
+ * How To Call It And Parameters
+ *
+ * Parameters:
+ *
+ *   className - one or several class names, separated by space. Multiple class
+ *               names demands that each match have all of the classes specified.
+ *               Mandatory.
+ *
+ *   tag - specifies the tag name of the elements to match. Optional.
+ *
+ *   elm - reference to a DOM element to look amongst its children for matches.
+ *         Recommended for better performance in larger documents. Optional.
+ *
+ * Returns:
+ *
+ *   a collection of elements
+ *
+ * _____________
+ * Call Examples
+ *
+ *   To get all elements in the document with a "info-links" class.
+ *
+ *     getElementsByClassName("info-links");
+ *
+ *   To get all div elements within the element named "container", with a "col" class.
+ *
+ *     getElementsByClassName("col", "div", document.getElementById("container"));
+ *
+ *   To get all elements within in the document with a "click-me" and a “sure-thang” class.
+ *
+ *     getElementsByClassName("click-me sure-thang");
+ */
+var getElementsByClassName = function (className, tag, elm){
+	if (document.getElementsByClassName) {
+		getElementsByClassName = function (className, tag, elm) {
+			elm = elm || document;
+			var elements = elm.getElementsByClassName(className),
+				nodeName = (tag)? new RegExp("\\b" + tag + "\\b", "i") : null,
+				returnElements = [],
+				current;
+			for(var i=0, il=elements.length; i<il; i+=1){
+				current = elements[i];
+				if(!nodeName || nodeName.test(current.nodeName)) {
+					returnElements.push(current);
+				}
+			}
+			return returnElements;
+		};
+	}
+	else if (document.evaluate) {
+		getElementsByClassName = function (className, tag, elm) {
+			tag = tag || "*";
+			elm = elm || document;
+			var classes = className.split(" "),
+				classesToCheck = "",
+				xhtmlNamespace = "http://www.w3.org/1999/xhtml",
+				namespaceResolver = (document.documentElement.namespaceURI === xhtmlNamespace)? xhtmlNamespace : null,
+				returnElements = [],
+				elements,
+				node;
+			for(var j=0, jl=classes.length; j<jl; j+=1){
+				classesToCheck += "[contains(concat(' ', @class, ' '), ' " + classes[j] + " ')]";
+			}
+			try	{
+				elements = document.evaluate(".//" + tag + classesToCheck, elm, namespaceResolver, 0, null);
+			}
+			catch (e) {
+				elements = document.evaluate(".//" + tag + classesToCheck, elm, null, 0, null);
+			}
+			while ((node = elements.iterateNext())) {
+				returnElements.push(node);
+			}
+			return returnElements;
+		};
+	}
+	else {
+		getElementsByClassName = function (className, tag, elm) {
+			tag = tag || "*";
+			elm = elm || document;
+			var classes = className.split(" "),
+				classesToCheck = [],
+				elements = (tag === "*" && elm.all)? elm.all : elm.getElementsByTagName(tag),
+				current,
+				returnElements = [],
+				match;
+			for(var k=0, kl=classes.length; k<kl; k+=1){
+				classesToCheck.push(new RegExp("(^|\\s)" + classes[k] + "(\\s|$)"));
+			}
+			for(var l=0, ll=elements.length; l<ll; l+=1){
+				current = elements[l];
+				match = false;
+				for(var m=0, ml=classesToCheck.length; m<ml; m+=1){
+					match = classesToCheck[m].test(current.className);
+					if (!match) {
+						break;
+					}
+				}
+				if (match) {
+					returnElements.push(current);
+				}
+			}
+			return returnElements;
+		};
+	}
+	return getElementsByClassName(className, tag, elm);
+};
