533876f9e41342074bfb6c36439cf7a2f2d56491
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-08-12 02:11:55 +0000

    Tagging RegDB with V00-00-07
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@500 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ RegDB/tags/V00-00-07/web/dynamic/Welcome.php
@@ -0,0 +1,23 @@
+<!--
+The Welcome page is shown after the applications startap.
+-->
+<center style="font-family: Times, serif; font-size:18px;">
+    <br>
+    <p style="font-size:24px;"><b>Welcome to the Experiment Registry!</b></p>
+    <br>
+    <br>
+    <p style="width:800px;">This application is designed to help managing common parameters of LCLS
+    instruments and experiments. The contents of the database is used by
+    the Detector Control System and various Web applications of both ONLINE
+    and OFFLINE systems of LCLS.
+    Pease refer to the documentation on the application found in the 'Help'
+    section of the top-level menu. You can also submit bug reports, suggestions
+    using the built-in 'Help'.</p>
+    <br>
+    <p><b>NOTE</b></p>
+    <p style="width:640px;">In order to better assist with usability problems the application may
+    record your activities and get them stored in the database. The gathered
+    information will be used to analyze a sequence of actions and your (and
+    others') input made before a problem occured.</p>
+    <br>
+</center>
