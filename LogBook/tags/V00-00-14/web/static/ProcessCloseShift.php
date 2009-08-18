9cf7e8f7a7ab6a527091c15f5f2b768787a385ee
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-08-18 19:37:06 +0000

    Tagging LogBook with V00-00-14
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@505 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ LogBook/tags/V00-00-14/web/static/ProcessCloseShift.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once('LogBook/LogBook.inc.php');
+
+/*
+ * This script will process a request for closing a shift
+ * in the database.
+ */
+if( isset( $_POST['experiment_name'])) {
+    $experiment_name = trim( $_POST['experiment_name'] );
+    if( $experiment_name == '' )
+        die( "experiment name can't be empty" );
+} else
+    die( "no valid experiment name" );
+
+if( isset( $_POST['end_time'])) {
+    $end_time = LusiTime::parse( trim( $_POST['end_time'] ));
+    if( is_null( $end_time ))
+        die( "end time has invalid format" );
+} else
+    die( "no end time for shift" );
+
+/* Proceed to the operation
+ */
+try {
+    $logbook = new LogBook();
+    $logbook->begin();
+
+    $experiment = $logbook->find_experiment_by_name( $experiment_name )
+        or die("no such experiment" );
+
+    // Find the last shift (if any). Make sure it's not closed yet.
+    //
+    $shift = $experiment->find_last_shift();
+    if( !is_null( $shift )) {
+        if( !is_null( $shift->end_time()))
+            die( "last shift is already closed" );
+        $shift->close( $end_time );
+    }
+?>
+<!--
+The page for reporting the information about all runs of the experiment.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>All shifts</title>
+    </head>
+    <link rel="stylesheet" type="text/css" href="LogBookTest.css" />
+    <body>
+        <!------------------------------>
+        <h1>All shifts of an experiment</h1>
+        <h2><?php echo $experiment->name(); ?></h2>
+        <?php
+        LogBookTestTable::Shift()->show( $experiment->shifts());
+        ?>
+    </body>
+</html>
+<?php
+
+    $logbook->commit();
+
+} catch( LogBookException $e ) {
+    print $e->toHtml();
+}
+?>
---
+++ LogBook/tags/V00-00-14/web/static/ProcessCreateFFEntry.php
@@ -0,0 +1,209 @@
+<?php
+
+require_once('LogBook/LogBook.inc.php');
+
+/*
+ * This script will process a request for creating a new free-form
+ * entry in the database.
+ */
+$parent_entry_id = null;
+if( isset( $_POST['parent_entry_id'] )) {
+    $str = trim( $_POST['parent_entry_id'] );
+    if( $str != '' && ( 1 != sscanf( $str, "%ud", $parent_entry_id )))
+        die( "not a number at parent entry identifier" );
+} else
+    die( "no parent entry id" );
+
+if( isset( $_POST['experiment_name'] )) {
+    $experiment_name = trim( $_POST['experiment_name'] );
+    if( $experiment_name == '' )
+        die( "experiment name can't be empty" );
+} else
+    die( "no valid experiment name" );
+
+if( isset( $_POST['shift_id'] )) {
+    $str = trim( $_POST['shift_id'] );
+    if( $str != '' && ( 1 != sscanf( $str, "%ud", $shift_id )))
+        die( "not a number where shift identifier was expected" );
+} else
+    die( "no valid shift identifier" );
+
+if( isset( $_POST['runnum'] )) {
+    $str = trim( $_POST['runnum'] );
+    if( $str != '' && ( 1 != sscanf( $str, "%ud", $runnum )))
+        die( "not a number where run number was expected" );
+} else
+    die( "no valid run number" );
+
+if( isset( $_POST['relevance_time'])) {
+    $str = trim( $_POST['relevance_time'] );
+    if( $str != '' ) {
+        $relevance_time = LusiTime::parse( trim( $_POST['relevance_time'] ));
+        if( is_null( $relevance_time ))
+            die("relevance time has invalid format");
+    }
+} else
+    die( "no relevance time for shift" );
+
+if( isset( $_POST['author'] )) {
+    $author = trim( $_POST['author'] );
+    if( $author == '' )
+        die( "author account name can't be empty" );
+} else
+    die( "no valid author account" );
+
+if( isset( $_POST['content_type'] )) {
+    $content_type = trim( $_POST['content_type'] );
+    if( $content_type == '' )
+        die( "content type can't be empty" );
+} else
+    die( "no valid content type" );
+
+if( isset( $_POST['content'] ))
+    $content = trim( $_POST['content'] );
+else
+    die( "no valid content" );
+
+// Read optional tags submitted with the entry
+//
+define( MAX_TAGS, 3 );          // max number of tags to attach
+
+$tags = array();
+for( $i = 0; $i < MAX_TAGS; $i++ ) {
+
+    $tag_name_key  = 'tag_name'.$i;
+    if( isset( $_POST[$tag_name_key] )) {
+
+        $tag = trim( $_POST[$tag_name_key] );
+        if( $tag != '' ) {
+
+            $tag_value_key = 'tag_value'.$i;
+            if( !isset( $_POST[$tag_value_key] ))
+                die( "No valid value for tag {$tag_name_key}" );
+
+            $value = trim( $_POST[$tag_value_key] );
+
+            array_push(
+                $tags,
+                array(
+                    'tag' => $tag,
+                    'value' => $value ));
+        }
+    }
+}
+
+// Read the names of optional files submitted for uploading
+//
+define( MAX_SIZE,  1024*1024 );  // max size for each uploaded file (Bytes)
+define( MAX_FILES, 3 );          // max number of files to attach
+
+$files = array();
+for( $i = 0; $i < MAX_FILES; $i++ ) {
+
+    $file_key = 'file'.$i;
+
+    $file = $_FILES[$file_key]['name'];
+    if( $file ) {
+
+        $filetype = $_FILES[$file_key]['type'];
+
+        // Read file contents into alocal variable
+        //
+        $filename = $_FILES[$file_key]['tmp_name'];
+        $filesize = filesize( $filename );
+        if( $filesize > MAX_SIZE )
+            die( 'allowed server-side file size exceeded' );
+        $fd = fopen( $filename, 'r' )
+            or die( "failed to open file: {$filename}" );
+        $contents = fread( $fd, $filesize );
+        fclose( $fd );
+
+        // Get its description. If none is present then use the original
+        // name of the file at client's side.
+        //
+        $description_key = 'description'.$i;
+        if( isset( $_POST[$description_key] )) {
+            $description = trim( $_POST[$description_key] );
+            if( $description == '' )
+                $description = $file;
+        } else
+            die( "no valid description posted for file number {$i}" );
+
+        array_push(
+            $files,
+            array(
+                'type' => $filetype,
+                'description' => $description,
+                'contents' => $contents ));
+    }
+}
+
+/* Proceed with the operation
+ */
+try {
+    $logbook = new LogBook();
+    $logbook->begin();
+
+    if( is_null( $parent_entry_id )) {
+
+        $experiment = $logbook->find_experiment_by_name( $experiment_name )
+            or die("no such experiment" );
+
+        $run_id = null;
+        if( !is_null( $runnum )) {
+            $run = $experiment->find_run_by_num( $runnum )
+                or die( "no such run" );
+            $run_id = $run->id();
+        }
+
+        $entry = $experiment->create_entry(
+            $author, $content_type, $content,
+            $shift_id, $run_id, $relevance_time );
+
+    } else {
+
+        $parent_entry = $logbook->find_entry_by_id( $parent_entry_id )
+            or die( "no such entry" );
+
+        $entry = $parent_entry->create_child(
+            $author, $content_type, $content );
+    }
+
+    // Add tags (if any)
+    //
+    foreach( $tags as $t ) {
+        $tag = $entry->add_tag( $t['tag'], $t['value'] );
+    }
+
+    // Attach files (if any)
+    //
+    foreach( $files as $f ) {
+        $attachment = $entry->attach_document( $f['contents'], $f['type'], $f['description'] );
+    }
+?>
+<!--
+The page for reporting the information on the new entry.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Newely created entry</title>
+    </head>
+    <link rel="stylesheet" type="text/css" href="LogBookTest.css" />
+    <body>
+        <h1>Free-form entry which just has been created</h1>
+        <h2><?php echo $entry->parent()->name(); ?></h2>
+        <?php
+        LogBookTestTable::Entry( "table_4" )->show( array( $entry ));
+        ?>
+    </body>
+</html>
+<?php
+
+    $logbook->commit();
+
+} catch( LogBookException $e ) {
+    print $e->toHtml();
+}
+?>
