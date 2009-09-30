979e780ddd2c6e39745022a518aeb1c752bb4eb0
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-09-30 00:26:51 +0000

    Tagging MsgLogger with V00-01-00
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@584 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ MsgLogger/tags/V00-01-00/src/MsgHandlerStdStreams.cpp
@@ -0,0 +1,89 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class MsgHandlerStdStreams
+//
+// Environment:
+//	Software developed for the BaBar Detector at the SLAC B-Factory.
+//
+// Author List:
+//      Andy Salnikov
+//
+// Copyright Information:
+//      Copyright (C) 2005 SLAC
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "MsgLogger/MsgHandlerStdStreams.h"
+
+//-------------
+// C Headers --
+//-------------
+extern "C" {
+}
+
+//---------------
+// C++ Headers --
+//---------------
+#include <iostream>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgFormatter.h"
+#include "MsgLogger/MsgLogLevel.h"
+#include "MsgLogger/MsgLogRecord.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace MsgLogger {
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+// Constructor
+MsgHandlerStdStreams::MsgHandlerStdStreams()
+  : MsgHandler()
+  , m_mutex()
+{
+}
+
+// Destructor
+MsgHandlerStdStreams::~MsgHandlerStdStreams()
+{
+}
+
+/// get the stream for the specified log level
+bool
+MsgHandlerStdStreams::log ( const MsgLogRecord& record ) const
+{
+  if ( ! logging( record.level() ) ) {
+    return false ;
+  }
+
+  static bool threadSafe = getenv("MSGLOGTHREADSAFE") ;
+  if (threadSafe) m_mutex.lock() ;
+
+  if ( record.level() <= MsgLogLevel::info ) {
+    formatter().format ( record, std::cout ) ;
+    std::cout << std::endl ;
+  } else {
+    formatter().format ( record, std::cerr ) ;
+    std::cerr << std::endl ;
+  }
+
+  if (threadSafe) m_mutex.unlock() ;
+
+  return true ;
+}
+
+} // namespace MsgLogger
