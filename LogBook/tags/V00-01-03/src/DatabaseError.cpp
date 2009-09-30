43f257515659ce4860a9fbf34a4639e28ee3443c
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-09-30 19:10:08 +0000

    Tagging LogBook with V00-01-03
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@596 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ LogBook/tags/V00-01-03/src/DatabaseError.cpp
@@ -0,0 +1,65 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: $
+//
+// Description:
+//	Class DatabaseError...
+//
+// Author List:
+//      Igor Gaponenko
+//
+//------------------------------------------------------------------------
+
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+
+#include "LogBook/DatabaseError.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace LogBook {
+
+//----------------
+// Constructors --
+//----------------
+
+DatabaseError::DatabaseError (const std::string& reason) :
+    std::exception(),
+    m_reason("LogBook::DatabaseError: " + reason)
+{ }
+
+//--------------
+// Destructor --
+//--------------
+
+DatabaseError::~DatabaseError () throw ()
+{ }
+
+//-----------
+// Methods --
+//-----------
+
+const char*
+DatabaseError::what () const throw ()
+{
+    return m_reason.c_str() ;
+}
+
+} // namespace LogBook
---
+++ LogBook/tags/V00-01-03/web/dynamic/CloseShift.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once('LogBook/LogBook.inc.php');
+
+/*
+ * This script will process a request for closing an on-going shift.
+ */
+if( isset( $_POST['id'] )) {
+    $shift_id = trim( $_POST['id'] );
+    if( $shift_id == '' )
+        die( "shift identifier can't be empty" );
+} else
+    die( "no valid shift identifier" );
+
+if( isset( $_POST['actionSuccess'] )) {
+    $actionSuccess = trim( $_POST['actionSuccess'] );
+}
+
+try {
+
+    $logbook = new LogBook();
+    $logbook->begin();
+
+    $shift = $logbook->find_shift_by_id( $shift_id )
+        or die( "no such shift" );
+
+    $experiment = $shift->parent();
+    $instrument = $experiment->instrument();
+
+    // Check for the authorization
+    //
+    if( !LogBookAuth::instance()->canManageShifts( $experiment->id())) {
+        print( LogBookAuth::reporErrorHtml(
+            'You are not authorized to manage shifts of the experiment',
+            'index.php?action=select_experiment_and_shift'.
+                '&instr_id='.$instrument->id().
+                '&instr_name='.$instrument->name().
+                '&exper_id='.$experiment->id().
+                '&exper_name='.$experiment->name().
+                '&shift_id='.$shift->id()));
+        exit;
+    }
+
+    // Proceed to the operation
+    //
+    $shift->close( LusiTime::now());
+
+    if( isset( $actionSuccess )) {
+        if( $actionSuccess == 'select_experiment_and_shift' )
+            header( 'Location: index.php?action=select_experiment_and_shift'.
+                '&instr_id='.$instrument->id().
+                '&instr_name='.$instrument->name().
+                '&exper_id='.$experiment->id().
+                '&exper_name='.$experiment->name().
+                '&shift_id='.$shift->id());
+        else
+            ;
+    }
+    $logbook->commit();
+
+} catch( RegDBException $e ) {
+    print $e->toHtml();
+} catch( LogBookException $e ) {
+    print $e->toHtml();
+} catch( LusiTimeException $e ) {
+    print $e->toHtml();
+}
+?>
