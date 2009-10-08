8e2255bf36f4cb83e41e90cff3cbf44677e697e5
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-10-08 18:51:10 +0000

    Tagging O2OTranslator with V00-03-00
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@609 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ O2OTranslator/tags/V00-03-00/src/AcqirisDataDescV1Cvt.cpp
@@ -0,0 +1,200 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class AcqirisDataDescV1Cvt...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "O2OTranslator/AcqirisDataDescV1Cvt.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <algorithm>
+#include <sstream>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "H5DataTypes/AcqirisDataDescV1.h"
+#include "O2OTranslator/O2OExceptions.h"
+#include "MsgLogger/MsgLogger.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+  const char logger[] = "AcqirisDataDescV1Cvt" ;
+}
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace O2OTranslator {
+
+// comparison operator for Src objects
+bool
+AcqirisDataDescV1Cvt::_SrcCmp::operator()( const Pds::Src& lhs, const Pds::Src& rhs ) const
+{
+  if ( lhs.log() < rhs.log() ) return true ;
+  if ( lhs.log() > rhs.log() ) return false ;
+  if ( lhs.phy() < rhs.phy() ) return true ;
+  return false ;
+}
+
+//----------------
+// Constructors --
+//----------------
+AcqirisDataDescV1Cvt::AcqirisDataDescV1Cvt ( const std::string& typeGroupName,
+                                             hsize_t chunk_size,
+                                             int deflate )
+  : EvtDataTypeCvt<Pds::Acqiris::DataDescV1>(typeGroupName)
+  , m_chunk_size(chunk_size)
+  , m_deflate(deflate)
+  , m_config()
+  , m_timestampCont(0)
+  , m_waveformCont(0)
+  , m_timeCont(0)
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+AcqirisDataDescV1Cvt::~AcqirisDataDescV1Cvt ()
+{
+  delete m_timestampCont ;
+  delete m_waveformCont ;
+  delete m_timeCont ;
+}
+
+/// override base class method because we expect multiple types
+void
+AcqirisDataDescV1Cvt::convert ( const void* data,
+                                const Pds::TypeId& typeId,
+                                const O2OXtcSrc& src,
+                                const H5DataTypes::XtcClockTime& time )
+{
+  if ( typeId.id() == Pds::TypeId::Id_AcqConfig ) {
+
+    const ConfigXtcType& config = *static_cast<const ConfigXtcType*>( data ) ;
+
+    // got configuration object, make the copy
+    m_config.insert( ConfigMap::value_type( src.top(), config ) ) ;
+
+  } else if ( typeId.id() == Pds::TypeId::Id_AcqWaveform ) {
+
+    // follow regular path
+    const XtcType& typedData = *static_cast<const XtcType*>( data ) ;
+    typedConvert ( typedData, typeId, src, time ) ;
+
+  }
+
+}
+
+// typed conversion method
+void
+AcqirisDataDescV1Cvt::typedConvertSubgroup ( hdf5pp::Group group,
+                                             const XtcType& data,
+                                             const Pds::TypeId& typeId,
+                                             const O2OXtcSrc& src,
+                                             const H5DataTypes::XtcClockTime& time )
+{
+  // find corresponding configuration object
+  ConfigMap::const_iterator cit = m_config.find( src.top() ) ;
+  if ( cit == m_config.end() ) {
+    MsgLog ( logger, error, "AcqirisDataDescV1Cvt - no configuration object was defined" );
+  }
+  const ConfigXtcType& config = cit->second ;
+
+  // create all containers if running first time
+  if ( not m_waveformCont ) {
+
+    // create container for timestamps
+    CvtDataContFactoryAcqirisV1<uint64_t> tsContFactory( "timestamps", m_chunk_size, m_deflate, 'T' ) ;
+    m_timestampCont = new TimestampCont ( tsContFactory ) ;
+
+    // create container for waveforms
+    CvtDataContFactoryAcqirisV1<uint16_t> wfContFactory( "waveforms", m_chunk_size, m_deflate, 'W' ) ;
+    m_waveformCont = new WaveformCont ( wfContFactory ) ;
+
+    // make container for time
+    CvtDataContFactoryDef<H5DataTypes::XtcClockTime> timeContFactory ( "time", m_chunk_size, m_deflate ) ;
+    m_timeCont = new XtcClockTimeCont ( timeContFactory ) ;
+
+  }
+
+
+  // get few constants
+  const Pds::Acqiris::HorizV1& hconfig = config.horiz() ;
+  const uint32_t nChan = config.nbrChannels() ;
+  const uint32_t nSeg = hconfig.nbrSegments() ;
+  const uint32_t nSampl = hconfig.nbrSamples() ;
+
+  // allocate data
+  uint64_t timestamps[nChan][nSeg] ;
+  uint16_t waveforms[nChan][nSeg][nSampl] ;
+
+  // scan the data and fill arrays
+  // FIXME: few methods that we need from DataDescV1 declared as non-const
+  Pds::Acqiris::DataDescV1* dd = const_cast<Pds::Acqiris::DataDescV1*>( &data ) ;
+  for ( uint32_t ch = 0 ; ch < nChan ; ++ ch, dd = dd->nextChannel(hconfig) ) {
+
+    // first verify that the shape of the data returned corresponds to the config
+    if ( dd->nbrSamplesInSeg() != nSampl ) {
+      std::ostringstream msg ;
+      msg << "O2ONexusWriter::dataObject(Acqiris::DataDescV1) -"
+          << " number of samples in data object (" << dd->nbrSamplesInSeg()
+          << ") different from config object (" << nSampl << ")" ;
+      throw O2OXTCGenException ( msg.str() ) ;
+    }
+    if ( dd->nbrSegments() != nSeg ) {
+      std::ostringstream msg ;
+      msg << "O2ONexusWriter::dataObject(Acqiris::DataDescV1) -"
+          << " number of segments in data object (" << dd->nbrSegments()
+          << ") different from config object (" << nSeg << ")" ;
+      throw O2OXTCGenException ( msg.str() ) ;
+    }
+
+    for ( uint32_t seg = 0 ; seg < nSeg ; ++ seg ) {
+      timestamps[ch][seg] = dd->timestamp(seg).value();
+    }
+
+    int16_t* wf = dd->waveform(hconfig) ;
+    std::copy ( wf, wf+nSampl*nSeg, (int16_t*)waveforms[ch] ) ;
+  }
+
+
+  // store the data
+  hdf5pp::Type type = H5DataTypes::AcqirisDataDescV1::timestampType ( config ) ;
+  m_timestampCont->container(group,config)->append ( timestamps[0][0], type ) ;
+  type = H5DataTypes::AcqirisDataDescV1::waveformType ( config ) ;
+  m_waveformCont->container(group,config)->append ( waveforms[0][0][0], type ) ;
+  m_timeCont->container(group)->append ( time ) ;
+
+
+}
+
+/// method called when the driver closes a group in the file
+void
+AcqirisDataDescV1Cvt::closeSubgroup( hdf5pp::Group group )
+{
+  if ( m_timestampCont ) m_timestampCont->closeGroup( group ) ;
+  if ( m_waveformCont ) m_waveformCont->closeGroup( group ) ;
+  if ( m_timeCont ) m_timeCont->closeGroup( group ) ;
+}
+
+
+
+} // namespace O2OTranslator
---
+++ O2OTranslator/tags/V00-03-00/src/O2OXtcFileName.cpp
@@ -0,0 +1,148 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class O2OXtcFileName...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "O2OTranslator/O2OXtcFileName.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <stdlib.h>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "O2OTranslator/O2OExceptions.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+
+
+}
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace O2OTranslator {
+
+O2OXtcFileName::O2OXtcFileName()
+  : m_path()
+  , m_expNum(0)
+  , m_run(0)
+  , m_stream(0)
+  , m_chunk(0)
+{
+}
+
+O2OXtcFileName::O2OXtcFileName ( const std::string& path )
+  : m_path(path)
+  , m_expNum(0)
+  , m_run(0)
+  , m_stream(0)
+  , m_chunk(0)
+{
+
+  std::string name = basename() ;
+
+  // remove extension
+  std::string::size_type n = name.find('.') ;
+  if ( n != std::string::npos ) name.erase ( n ) ;
+
+  // file name expected to be eNNN_rNNN_sNNN_chNNN.xtc
+
+  // find underscore before chunk part
+  n = name.rfind('_') ;
+  if ( n == std::string::npos || (name.size()-n) < 4 || name[n+1] != 'c' || name[n+2] != 'h' ) {
+    return ;
+  }
+  m_chunk = _cvt ( name.c_str()+n+3 ) ;
+
+  // remove chunk part
+  name.erase(n) ;
+
+  // find underscore before stream part
+  n = name.rfind('_') ;
+  if ( n == std::string::npos || (name.size()-n) < 3 || name[n+1] != 's' ) {
+    return ;
+  }
+  m_stream = _cvt ( name.c_str()+n+2 ) ;
+
+  // remove stream part
+  name.erase(n) ;
+
+  // find underscore before run part
+  n = name.rfind('_') ;
+  if ( n == std::string::npos || (name.size()-n) < 3 || name[n+1] != 'r' ) {
+    return ;
+  }
+  m_run = _cvt ( name.c_str()+n+2 ) ;
+
+  // remove run part
+  name.erase(n) ;
+
+  // remaining must be experiment part
+  if ( name.size() < 2 || name[0] != 'e' ) {
+    return ;
+  }
+  m_expNum = _cvt ( name.c_str()+1 ) ;
+
+}
+
+
+// get base name
+std::string
+O2OXtcFileName::basename() const
+{
+  std::string name = m_path ;
+
+  // remove dirname
+  std::string::size_type n = name.rfind('/') ;
+  if ( n != std::string::npos ) name.erase ( 0, n+1 ) ;
+
+  return name ;
+}
+
+// compare two names
+bool
+O2OXtcFileName::operator<( const O2OXtcFileName& other ) const
+{
+  if ( m_expNum < other.m_expNum ) return true ;
+  if ( other.m_expNum < m_expNum ) return false ;
+
+  if ( m_run < other.m_run ) return true ;
+  if ( other.m_run < m_run ) return false ;
+
+  if ( m_stream < other.m_stream ) return true ;
+  if ( other.m_stream < m_stream ) return false ;
+
+  if ( m_chunk < other.m_chunk ) return true ;
+  return false ;
+
+}
+
+unsigned
+O2OXtcFileName::_cvt ( const char* ptr ) const
+{
+  char *eptr = 0 ;
+  unsigned val = strtoul ( ptr, &eptr, 10 ) ;
+  return val ;
+}
+
+} // namespace O2OTranslator
