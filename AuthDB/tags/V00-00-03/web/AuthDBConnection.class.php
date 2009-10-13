a093fb12a9b91952dbb37b2c125ed1d003534ec4
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-10-13 00:37:52 +0000

    Tagging AuthDB with V00-00-03
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@635 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ AuthDB/tags/V00-00-03/web/AuthDBConnection.class.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * Class AuthDBConnection encapsulates operations with the database
+ */
+class AuthDBConnection {
+
+    /* Parameters of the object
+     */
+    private $host;
+    private $user;
+    private $password;
+    private $database;
+
+    /* Current state of the object
+     */
+    private $link;
+    private $in_transaction = false;
+
+    /**
+     * Constructor
+     *
+     * The constructor won't make any actual connection attempts. This will be deffered
+     * to operations dealing with queries, transactions, etc.
+     *
+     * @param string $host
+     * @param string $user
+     * @param string $password
+     * @param string $database 
+     */
+    public function __construct ( $host, $user, $password, $database ) {
+        $this->host      = $host;
+        $this->user      = $user;
+        $this->password  = $password;
+        $this->database  = $database;
+    }
+
+    /**
+     * Destructor
+     */
+    public function __destruct () {
+        if( isset( $this->link )) mysql_close( $this->link );
+    }
+
+    /*
+     * ================================
+     *   MySQL TRANSACTION MANAGEMENT
+     * ================================
+     */
+    public function begin () {
+        $this->connect();
+        if( $this->in_transaction ) return;
+        $this->transaction( 'BEGIN' );
+        $this->in_transaction = true;
+    }
+
+    public function commit () {
+        $this->connect();
+        if( !$this->in_transaction ) return;
+        $this->transaction( 'COMMIT' );
+        $this->in_transaction = false;
+    }
+
+    public function rollback () {
+        $this->connect();
+        if( !$this->in_transaction ) return;
+        $this->transaction( 'ROLLBACK' );
+        $this->in_transaction = false;
+    }
+
+    private function transaction ( $transition ) {
+        if( !mysql_query( $transition, $this->link ))
+            throw new AuthDBException (
+                __METHOD__,
+                "MySQL error: ".mysql_error( $this->link ).', in query: '.$transition );
+    }
+
+    /* =================
+     *   MySQL QUERIES
+     * =================
+     */
+    public function query ( $sql ) {
+
+        $this->connect();
+
+        if( !$this->in_transaction )
+            throw new AuthDBException (
+                __METHOD__, "no active transaction" );
+
+        $result = mysql_query( $sql, $this->link );
+        if( !$result )
+            throw new AuthDBException (
+                __METHOD__,
+                "MySQL error: ".mysql_error( $this->link ).', in query: '.$sql );
+        return $result;
+    }
+
+    /* ==========================================================
+     *   MISC. OPERATIONS REQUIREING DATABASE SERVER CONNECTION
+     * ==========================================================
+     */
+    public function escape_string( $text ) {
+        return mysql_real_escape_string( $text, $this->link );  }
+ 
+    /**
+     * Make a connection if this hasn't been done yet.
+     */
+    private function connect () {
+        if( !isset( $this->link )) {
+
+            /* Connect to MySQL server
+             */
+            $new_link = true;
+            $this->link = mysql_connect( $this->host, $this->user, $this->password, $new_link );
+            if( !$this->link )
+                throw new AuthDBException (
+                    __METHOD__,
+                    "MySQL error: ".mysql_error( $this->link ).", in function: mysql_connect" );
+
+            if( !mysql_select_db( $this->database, $this->link ))
+                throw new AuthDBException (
+                    __METHOD__,
+                    "MySQL error: ".mysql_error( $this->link ).", in function: mysql_select_db" );
+
+            $sql = "SET SESSION SQL_MODE='ANSI'";
+            if( !mysql_query( $sql, $this->link ))
+                throw new AuthDBException (
+                    __METHOD__,
+                    "MySQL error: ".mysql_error( $this->link ).', in query: '.$sql );
+
+            $sql = "SET SESSION AUTOCOMMIT=0";
+            if( !mysql_query( $sql, $this->link ))
+                throw new AuthDBException (
+                    __METHOD__,
+                    "MySQL error: ".mysql_error( $this->link ).', in query: '.$sql );
+        }
+    }
+}
+
+/* ==========================
+ * UNIT TEST FOR LDAP METHODS
+ * ==========================
+ *
+
+require_once( "AuthDB.inc.php");
+
+$conn = new AuthDBConnection (
+    AUTHDB_DEFAULT_HOST,
+    AUTHDB_DEFAULT_USER,
+    AUTHDB_DEFAULT_PASSWORD,
+    AUTHDB_DEFAULT_DATABASE );
+
+try {
+    $conn->begin();
+    $conn->rollback();
+
+ } catch ( AuthDBException $e ) {
+    print( e.toHtml());
+}
+*/
+?>
---
+++ AuthDB/tags/V00-00-03/web/dynamic/RequestRolePlayersExper.php
@@ -0,0 +1,163 @@
+<?php
+
+require_once('AuthDB/AuthDB.inc.php');
+require_once('RegDB/RegDB.inc.php');
+
+/*
+ * This script will process requests for various information stored in the database.
+ * The result will be returned as JSON object (array).
+ */
+if( !isset( $_GET['type'] )) die( "no valid information type in the request" );
+$type = trim( $_GET['type'] );
+
+define( PLAYERS_INSTR, 1 ); // instruments
+define( PLAYERS_EXPER, 2 ); // experiments
+define( PLAYERS,       3 ); // roles themselves
+
+$instr_id = null;
+$exper_id = null;
+switch( $type ) {
+    case PLAYERS:
+        if( !isset( $_GET['exper_id'] )) die( "no valid experiment identifier in the request" );
+        $exper_id = trim( $_GET['exper_id'] );
+        break;
+    case PLAYERS_EXPER:
+        if( !isset( $_GET['instr_id'] )) die( "no valid instrument identifier in the request" );
+        $instr_id = trim( $_GET['instr_id'] );
+        break;
+    case PLAYERS_INSTR:
+        break;
+}
+
+function instr2json( $instr ) {
+    return json_encode(
+        array (
+            "label"    => '<em style="color:#0071bc;"><b>'.$instr->name().'</b></em>',
+            "expanded" => false,
+            "type"     => PLAYERS_EXPER,
+            "title"    => "select an experiment",
+            "instr_id" => $instr->id()
+        )
+    );
+}
+
+function exper2json( $exper ) {
+    return json_encode(
+        array (
+            "label"    => '<em style="color:#0071bc;"><b>'.$exper->name().'</b></em>',
+            "expanded" => false,
+            "isLeaf"   => true,
+            "type"     => PLAYERS,
+            "title"    => "click to see the user roles",
+            "exper_id" => $exper->id(),
+            "experiment" => $exper->name(),
+            "instrument" => $exper->instrument()->name()
+        )
+    );
+}
+
+function role2json( $role ) {
+    $player = $role->player();
+    $group_url = $player['group'];
+    if( $group_url == '' ) {
+        $group_url = '';
+        $user_url  = $player['user'];
+        $comment = '';
+    } else {
+        $group_url = '<a href="javascript:view_group('."'".$group_url."'".')">'.$group_url.'</a>';
+        $user_url  = '';
+        $comment = 'all members of the group;';
+    }
+    if( is_null( $role->exper_id())) {
+    	if( $comment != '' ) $comment .= '<br>';
+    	$comment .= 'also accross all experiments;';
+    }
+
+    return json_encode(
+        array (
+            "application" => $role->application(),
+            "role"        => $role->name(),
+            "group"       => $group_url,
+            "user"        => $player['user'],
+            "comment"     => $comment
+        )
+    );
+}
+
+/*
+ * Analyze and process the request
+ */
+try {
+
+    $regdb = new RegDB();
+    $regdb->begin();
+
+    $authdb = new AuthDB();
+    $authdb->begin();
+
+    // Proceed to the operation
+    //
+    header( "Content-type: application/json" );
+    header( "Cache-Control: no-cache, must-revalidate" ); // HTTP/1.1
+    header( "Expires: Sat, 26 Jul 1997 05:00:00 GMT" );   // Date in the past
+    print <<< HERE
+{
+  "ResultSet": {
+    "Result": [
+HERE;
+    $first = true;
+
+    if( $type == PLAYERS_INSTR ) {
+        $instruments = $regdb->instruments();
+        foreach( $instruments as $i ) {
+          if( $first ) {
+              $first = false;
+              echo "\n".instr2json( $i );
+          } else {
+              echo ",\n".instr2json( $i );
+          }
+        }
+    } else if( $type == PLAYERS_EXPER ) {
+        $instrument = $regdb->find_instrument_by_id( $instr_id )
+            or die("No such instrument");
+        $experiments = $regdb->experiments_for_instrument($instrument->name());
+        foreach( $experiments as $e ) {
+          if( $first ) {
+              $first = false;
+              echo "\n".exper2json( $e );
+          } else {
+              echo ",\n".exper2json( $e );
+          }
+        }
+    } else if( $type == PLAYERS ) {
+        $roles = $authdb->roles( $exper_id );
+        foreach( $roles as $r ) {
+        	// Skip roles which are known not to be associated with particular experiments
+        	//
+        	// TODO: This is a request from Andy Salnikov.
+        	//
+        	if(( $r->application() == 'RoleDB' ) || ( $r->application() == 'RegDB' )) {
+        		;
+        	} else {
+          		if( $first ) {
+            	  	$first = false;
+        	      	echo "\n".role2json( $r );
+    	      	} else {
+	              	echo ",\n".role2json( $r );
+    	      	}
+          	}
+        }
+    }
+    print <<< HERE
+ ] } }
+HERE;
+
+    $regdb->commit();
+    $authdb->commit();
+
+} catch( RegDBException $e ) {
+    print $e->toHtml();
+} catch( AuthDBException $e ) {
+    print $e->toHtml();
+}
+?>
