a4a32ec0eb6c0ae95c0d1477fbd0a3471a68bd04
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-10-13 00:36:19 +0000

    - added 'logout' to the Web app interface. It works only for WebAuth.
    M    doc/ChangeLog
    M    web/dynamic/index.php
    
    
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@632 b967ad99-d558-0410-b138-e0f6c56caec7

--- RegDB/trunk/web/dynamic/index.php
+++ RegDB/trunk/web/dynamic/index.php
@@ -262,6 +262,7 @@ menubar_data.push ( {
     title: 'Applications',
     title_style: 'font-weight:bold;',
     itemdata: [
+        { text: "Authorization Database Manager", url: "../authdb/" },
         { text: "Experiment Registry Database", url: "../regdb/" },
         { text: "Electronic Log Book", url: "../logbook/" } ],
     disabled: false }
@@ -379,6 +380,22 @@ function auth_timer_event() {
         '<b>'+hours_left_str+':'+minutes_left_str+'.'+seconds_left_str+'</b>';
 
     auth_timer_restart();
+}
+
+function logout() {
+    ask_yesno(
+        'popupdialogs',
+        '<span style="color:red; font-size:16px;">Session Logout Warning</span>',
+        '<p style="text-align:left;">You are about to logout from the current WebAuth session. '+
+        'Press <b>Yes</b> to proceed with the logout. Press <b>No</b> to stay in the session.</p>',
+        function() {
+            document.cookie = 'webauth_wpt_krb5=; expires=Fri, 27 Jul 2001 02:47:11 UTC; path=/';
+            document.cookie = 'webauth_at=; expires=Fri, 27 Jul 2001 02:47:11 UTC; path=/';
+            refresh_page();
+        },
+        function() {}
+    );
+    return;
 }
 
 function set_context( context ) {
@@ -1332,21 +1349,23 @@ function view_run_numbers( id, name ) {
         </div>
         <div style="float:right; height:50px;">
 <?php
-$remote_user = $_SERVER['REMOTE_USER'];
-if( $remote_user == '' ) echo <<<HERE
+if( $auth_svc->authName() == '' ) {
+    echo <<<HERE
           <br>
           <br>
           <a href="../../apps/regdb"><p title="login here to proceed to the full version of the application">login</p></a>
 HERE;
-else echo <<<HERE
+} else {
+	$logout = $auth_svc->authType() == 'WebAuth' ? '[<a href="javascript:logout()" title="close the current WebAuth session">logout</a>]' : '&nbsp;';
+	echo <<<HERE
           <table><tbody>
             <tr>
-              <td>&nbsp;</td>
               <td></td>
+              <td>{$logout}</td>
             </tr>
             <tr>
               <td>Welcome,&nbsp;</td>
-              <td><p><b>{$remote_user}</b></p></td>
+              <td><p><b>{$auth_svc->authName()}</b></p></td>
             </tr>
             <tr>
               <td>Session expires in:&nbsp;</td>
@@ -1354,6 +1373,7 @@ else echo <<<HERE
             </tr>
           </tbody></table>
 HERE;
+}
 ?>
         </div>
         <div style="height:40px;">&nbsp;</div>
