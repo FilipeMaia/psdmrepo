17fce89fdd347d62f0a93314dd5da0049160c3b3
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-10-13 16:03:13 +0000

    Tagging O2OTranslator with V00-03-03
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@637 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ O2OTranslator/tags/V00-03-03/include/O2OXtcSrc.h
@@ -0,0 +1,86 @@
+#ifndef O2OTRANSLATOR_O2OXTCSRC_H
+#define O2OTRANSLATOR_O2OXTCSRC_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class O2OXtcSrc.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <vector>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "pdsdata/xtc/Src.hh"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace O2OTranslator {
+
+/**
+ *  Representation of the nested XTC source info.
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class O2OXtcSrc  {
+public:
+
+  // Default constructor
+  O2OXtcSrc () { m_src.reserve(5) ; }
+
+  // Destructor
+  ~O2OXtcSrc () {}
+
+  // add one level
+  void push ( const Pds::Src& src ) { m_src.push_back( src ); }
+
+  // remove top level
+  void pop () { m_src.pop_back(); }
+
+  // get topmost level
+  const Pds::Src& top () const { return m_src.back() ; }
+
+  // get the name of the source
+  std::string name() const ;
+
+protected:
+
+private:
+
+  // Data members
+  std::vector<Pds::Src> m_src ;
+
+  // Copy constructor and assignment are disabled by default
+  O2OXtcSrc ( const O2OXtcSrc& ) ;
+  O2OXtcSrc& operator = ( const O2OXtcSrc& ) ;
+
+};
+
+} // namespace O2OTranslator
+
+#endif // O2OTRANSLATOR_O2OXTCSRC_H
