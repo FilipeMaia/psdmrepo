0b79aec0207bdbaa7d15cb637d24139a5732a441
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-10-27 22:09:16 +0000

    Tagging AuthDB with V00-00-04
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@651 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ AuthDB/tags/V00-00-04/web/AuthDBConfigSample.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Default connection parameters for MySQL
+ */
+DEFINE( AUTHDB_DEFAULT_HOST,      "www-lclsdev.slac.stanford.edu" );
+DEFINE( AUTHDB_DEFAULT_USER,      "gapon" );
+DEFINE( AUTHDB_DEFAULT_PASSWORD,  "" );
+DEFINE( AUTHDB_DEFAULT_DATABASE,  "roles" );
+?>
---
+++ AuthDB/tags/V00-00-04/web/dynamic/ProcessDeleteRole.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once('AuthDB/AuthDB.inc.php');
+
+/*
+ * This script will process a request for deleting an existing role
+ * from the database.
+ */
+// TODO: This needs to be changed with the real test
+//
+//if( !RegDBAuth::isAuthenticated()) return;
+
+if( isset( $_POST['id'] )) {
+    $id = trim( $_POST['id'] );
+    if( $id == '' )
+        die( "role identifier can't be empty" );
+} else
+    die( "no valid role identifier prtovided" );
+
+if( isset( $_POST['actionSuccess'] )) {
+    $actionSuccess = trim( $_POST['actionSuccess'] );
+}
+
+/* Proceed with the operation
+ */
+try {
+    $authdb = new AuthDB();
+    $authdb->begin();
+
+    $authdb->deleteRole( $id );
+
+    if( isset( $actionSuccess )) {
+        if( $actionSuccess == 'home' )
+            header( 'Location: index.php' );
+        else if( $actionSuccess == 'list_roles' )
+            header( 'Location: index.php?action=list_roles' );
+        else
+            ;
+    }
+    $authdb->commit();
+
+} catch( AuthDBException $e ) {
+    print $e->toHtml();
+}
+?>
