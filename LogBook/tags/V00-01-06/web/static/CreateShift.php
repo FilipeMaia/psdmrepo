1e3a24f5a72ae37e346bf498a339f874d65057d1
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-10-27 22:11:42 +0000

    Tagging LogBook with V00-01-06
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@655 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ LogBook/tags/V00-01-06/web/static/CreateShift.php
@@ -0,0 +1,105 @@
+<!--
+The page for creating a new shift.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Create a new shift</title>
+    </head>
+    <body>
+        <?php
+        $now = new DateTime();
+        $now_str = $now->format(DateTime::ISO8601);
+        $now_str[10] = ' ';  // get rid of date-time separator 'T'
+        ?>
+        <h1>Add new shift :</h1>
+        <form action="ProcessCreateShift.php" method="POST" style="margin-left:2em;">
+            <table cellpadding="3"  border="0" >
+                <thead style="color:#0071bc;">
+                    <th align="right">
+                        &nbsp;<b>Attribute</b>&nbsp;</th>
+                    <th align="left">
+                        &nbsp;<b>Value</b>&nbsp;</th>
+                    <th align="left">
+                        &nbsp;<b>Format</b>&nbsp;</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><hr></td>
+                        <td><hr></td>
+                        <td><hr></td>
+                    </tr>
+                    <tr>
+                        <td align="right" style="width:6em;">
+                            &nbsp;<b>Leader</b>&nbsp;</td>
+                        <td>
+                            &nbsp;<input align="left" size="16" type="text" name="leader" value=" <?php echo $_SERVER['WEBAUTH_USER']; ?> " />&nbsp;</td>
+                        <td>
+                            &nbsp;UNIX account name</td>
+                    </tr>
+                    <tr>
+                        <td align="right" style="width:6em;">
+                            &nbsp;<b>Experiment</b>&nbsp;</td>
+                        <td>
+                            &nbsp;<select align="center" type="text" name="experiment_name" ><?php
+                            require_once('LogBook/LogBook.inc.php');
+                            $logbook = new LogBook();
+                            $logbook->begin();
+                            $experiments = $logbook->experiments()
+                                or die("failed to find experiments" );
+                            foreach( $experiments as $e)
+                                echo '<option> '.$e->attr['name'].' </option>';
+                            $logbook->commit();
+                            ?></select>&nbsp;
+                        <td>
+                            &nbsp;</td>
+                    <tr>
+                        <td align="right">
+                            &nbsp;<b>Begin Time</b>&nbsp;</td>
+                        <td>
+                            &nbsp;<input align="left" size="32" type="text" name="begin_time" value="<?php echo ' '.$now_str; ?>" />&nbsp;</td>
+                        <td>
+                            &nbsp;YYYY-MM-DD hh:mm:ss-zzzz</td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            &nbsp;<b>End Time</b>&nbsp;</td>
+                        <td>
+                            &nbsp;<input align="left" size="32" type="text" name="end_time" value="" />&nbsp;</td>
+                        <td>
+                            &nbsp;YYYY-MM-DD hh:mm:ss-zzzz [optional] </td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <br>
+            <table ellpadding="3" border="0" >
+                <thead style="color:#0071bc;">
+                    <th align="left">
+                        &nbsp;<b>Other crew members</b>&nbsp;</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><hr></td>
+                    </tr>
+                    <tr>
+                        <td align="left">
+                            &nbsp;<input align="left" size="16" type="text" name="crew_member1" value="" />&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="left">
+                            &nbsp;<input align="left" size="16" type="text" name="crew_member2" value="" />&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="left">
+                            &nbsp;<input align="left" size="16" type="text" name="crew_member3" value="" />&nbsp;</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <input type="submit" value="Submit" name="submit_button" /><br>
+        </form>
+    </body>
+</html>
+
