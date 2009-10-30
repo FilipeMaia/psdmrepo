c0d5be1f908d8d791ff826cb5d55be9d56c78c82
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-10-30 16:36:22 +0000

    Tagging H5DataTypes with V00-00-10
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@675 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ H5DataTypes/tags/V00-00-10/include/ObjectContainer.h
@@ -0,0 +1,137 @@
+#ifndef H5DATATYPES_OBJECTCONTAINER_H
+#define H5DATATYPES_OBJECTCONTAINER_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class ObjectContainer.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5pp/DataSet.h"
+#include "hdf5pp/DataSpace.h"
+#include "hdf5pp/Group.h"
+#include "hdf5pp/TypeTraits.h"
+#include "hdf5pp/Type.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+namespace H5DataTypes {
+
+template <typename T>
+class ObjectContainer {
+public:
+
+  /// dataset type is determined by template type
+  ObjectContainer ( const std::string& name,
+                    hdf5pp::Group& location,
+                    hsize_t chunk_size = 10240,
+                    int deflate = -1,
+                    bool nbit = false )
+    : m_dataset()
+    , m_count(0)
+  {
+    // make extensible data space
+    hdf5pp::DataSpace dsp = hdf5pp::DataSpace::makeSimple ( 0, H5S_UNLIMITED ) ;
+
+    // use chunking
+    hdf5pp::PListDataSetCreate plDScreate ;
+    plDScreate.set_chunk(chunk_size) ;
+    if ( deflate >= 0 ) plDScreate.set_deflate(deflate) ;
+    if ( nbit ) plDScreate.set_nbit() ;
+
+    // make a data set
+    m_dataset = location.createDataSet<T> ( name, dsp, plDScreate ) ;
+  }
+
+  /// Provide type at run time
+  ObjectContainer ( const std::string& name,
+                    hdf5pp::Group& location,
+                    const hdf5pp::Type& stored_type,
+                    hsize_t chunk_size = 10240,
+                    int deflate = -1,
+                    bool nbit = false )
+    : m_dataset()
+    , m_count(0)
+  {
+    // make extensible data space
+    hdf5pp::DataSpace dsp = hdf5pp::DataSpace::makeSimple ( 0, H5S_UNLIMITED ) ;
+
+    // use chunking
+    hdf5pp::PListDataSetCreate plDScreate ;
+    plDScreate.set_chunk(chunk_size) ;
+    if ( deflate >= 0 ) plDScreate.set_deflate(deflate) ;
+    if ( nbit ) plDScreate.set_nbit() ;
+
+    // make a data set
+    m_dataset = location.createDataSet<T> ( name, stored_type, dsp, plDScreate ) ;
+  }
+
+  /**
+   *  Append one more data element to the end of data set
+   */
+  void append ( const T& obj, const hdf5pp::Type& native_type = hdf5pp::TypeTraits<T>::native_type() )
+  {
+    // extend data set
+    unsigned long newCount = m_count + 1 ;
+    m_dataset.set_extent ( newCount ) ;
+
+    // define hyperslab for file data set
+    hdf5pp::DataSpace fileDspc = m_dataset.dataSpace() ;
+    hsize_t start[] = { m_count } ;
+    hsize_t size[] = { 1 } ;
+    fileDspc.select_hyperslab ( H5S_SELECT_SET, start, 0, size, 0 ) ;
+
+    // define in-memory data space
+    hdf5pp::DataSpace memDspc = hdf5pp::DataSpace::makeScalar() ;
+
+    // store it
+    m_dataset.store ( memDspc, fileDspc, &obj, native_type ) ;
+
+    m_count = newCount ;
+  }
+
+  /// get access to data set
+  hdf5pp::DataSet<T>& dataset() { return m_dataset ; }
+
+private:
+
+  hdf5pp::DataSet<T> m_dataset ;
+  unsigned long m_count ;
+};
+
+} // namespace H5DataTypes
+
+#endif // H5DATATYPES_OBJECTCONTAINER_H
---
+++ H5DataTypes/tags/V00-00-10/src/EvrConfigV2.cpp
@@ -0,0 +1,106 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class EvrConfigV2...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "H5DataTypes/EvrConfigV2.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "H5DataTypes/EvrConfigData.h"
+#include "hdf5pp/CompoundType.h"
+#include "hdf5pp/EnumType.h"
+#include "hdf5pp/TypeTraits.h"
+#include "H5DataTypes/H5DataUtils.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace H5DataTypes {
+
+EvrConfigV2::EvrConfigV2 ( const Pds::EvrData::ConfigV2& data )
+{
+  m_data.beam = data.beam() ;
+  m_data.rate = data.rate() ;
+  m_data.npulses = data.npulses() ;
+  m_data.noutputs = data.noutputs() ;
+}
+
+hdf5pp::Type
+EvrConfigV2::stored_type()
+{
+  return native_type() ;
+}
+
+hdf5pp::Type
+EvrConfigV2::native_type()
+{
+  hdf5pp::EnumType<int16_t> rateEnumType = hdf5pp::EnumType<int16_t>::enumType() ;
+  rateEnumType.insert ( "r120Hz", Pds::EvrData::ConfigV2::r120Hz ) ;
+  rateEnumType.insert ( "r60Hz", Pds::EvrData::ConfigV2::r60Hz ) ;
+  rateEnumType.insert ( "r30Hz", Pds::EvrData::ConfigV2::r30Hz ) ;
+  rateEnumType.insert ( "r10Hz", Pds::EvrData::ConfigV2::r10Hz ) ;
+  rateEnumType.insert ( "r5Hz", Pds::EvrData::ConfigV2::r5Hz ) ;
+  rateEnumType.insert ( "r1Hz", Pds::EvrData::ConfigV2::r1Hz ) ;
+  rateEnumType.insert ( "r0_5Hz", Pds::EvrData::ConfigV2::r0_5Hz ) ;
+
+  hdf5pp::EnumType<int16_t> beamEnumType = hdf5pp::EnumType<int16_t>::enumType() ;
+  beamEnumType.insert ( "Off", Pds::EvrData::ConfigV2::Off ) ;
+  beamEnumType.insert ( "On", Pds::EvrData::ConfigV2::On ) ;
+  
+  hdf5pp::CompoundType confType = hdf5pp::CompoundType::compoundType<EvrConfigV2>() ;
+  confType.insert( "beam", offsetof(EvrConfigV2_Data,beam), beamEnumType ) ;
+  confType.insert( "rate", offsetof(EvrConfigV2_Data,rate), rateEnumType ) ;
+  confType.insert_native<uint32_t>( "npulses", offsetof(EvrConfigV2_Data,npulses) ) ;
+  confType.insert_native<uint32_t>( "noutputs", offsetof(EvrConfigV2_Data,noutputs) ) ;
+
+  return confType ;
+}
+
+void
+EvrConfigV2::store( const Pds::EvrData::ConfigV2& config, hdf5pp::Group grp )
+{
+  // make scalar data set for main object
+  EvrConfigV2 data ( config ) ;
+  storeDataObject ( data, "config", grp ) ;
+
+  // pulses data
+  const uint32_t npulses = config.npulses() ;
+  EvrPulseConfig pdata[npulses] ;
+  for ( uint32_t i = 0 ; i < npulses ; ++ i ) {
+    pdata[i] = EvrPulseConfig( config.pulse(i) ) ;
+  }
+  storeDataObjects ( npulses, pdata, "pulses", grp ) ;
+
+  // output map data
+  const uint32_t noutputs = config.noutputs() ;
+  EvrOutputMap mdata[noutputs] ;
+  for ( uint32_t i = 0 ; i < noutputs ; ++ i ) {
+    mdata[i] = EvrOutputMap( config.output_map(i) ) ;
+  }
+  storeDataObjects ( noutputs, mdata, "output_maps", grp ) ;
+
+}
+
+} // namespace H5DataTypes
