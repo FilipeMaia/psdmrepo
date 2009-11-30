46004868529613b2e015c1ecf5157fc3de7d81d4
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2009-11-30 17:59:25 +0000

    Tagging O2OTranslator with V00-05-02
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@737 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ O2OTranslator/tags/V00-05-02/app/o2o-scanner.cpp
@@ -0,0 +1,160 @@
+/**
+ * Original code for XTC scanning from Chris
+ *
+ * $Id$
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <list>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "AppUtils/AppBase.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "AppUtils/AppCmdArgList.h"
+#include "AppUtils/AppCmdOptBool.h"
+#include "MsgLogger/MsgLogger.h"
+#include "O2OTranslator/O2OXtcFileName.h"
+#include "O2OTranslator/O2OXtcMerger.h"
+#include "pdsdata/xtc/DetInfo.hh"
+#include "pdsdata/xtc/ProcInfo.hh"
+#include "pdsdata/xtc/XtcIterator.hh"
+#include "pdsdata/xtc/XtcFileIterator.hh"
+
+
+
+//
+//  XTC iterator class which dumps XTC-level info
+//
+class myLevelIter : public XtcIterator {
+public:
+  enum {Stop, Continue};
+  myLevelIter(Xtc* xtc) : XtcIterator(xtc), _depth(1) {}
+  int process(Xtc* xtc) {
+    for ( unsigned i=0 ; i < _depth ; ++ i ) printf("  ");
+    Level::Type level = xtc->src.level();
+    printf("%s level: ",Level::name(level));
+    printf("payloadSize %d ", xtc->sizeofPayload() );
+    printf("damage %x ", xtc->damage.value() );
+    if (level==Level::Source or level==Pds::Level::Reporter or level==Pds::Level::Control) {
+      DetInfo& info = *(DetInfo*)(&xtc->src);
+      printf("%s.%d %s.%d",
+             DetInfo::name(info.detector()),info.detId(),
+             DetInfo::name(info.device()),info.devId());
+    } else {
+      ProcInfo& info = *(ProcInfo*)(&xtc->src);
+      printf("IpAddress %#x ProcessId %#x",info.ipAddr(),info.processId());
+    }
+    if (xtc->contains.id() == TypeId::Id_Xtc ) {
+      printf("\n");
+      ++_depth;
+      this->iterate( xtc );
+      --_depth;
+    } else {
+      printf(" id=%d name=%s version=%d\n", xtc->contains.id(),
+          Pds::TypeId::name(xtc->contains.id()), xtc->contains.version() );
+    }
+    return Continue;
+  }
+private:
+  unsigned _depth;
+};
+
+
+namespace O2OTranslator {
+
+using namespace AppUtils ;
+
+//
+//  Application class
+//
+class O2O_Scanner : public AppBase {
+public:
+
+  // Constructor
+  explicit O2O_Scanner ( const std::string& appName ) ;
+
+  // destructor
+  ~O2O_Scanner () ;
+
+protected :
+
+  /**
+   *  Main method which runs the whole application
+   */
+  virtual int runApp () ;
+
+private:
+
+  // more command line options and arguments
+  AppCmdOptBool               m_skipDamaged ;
+  AppCmdArgList<std::string>  m_inputFiles ;
+
+};
+
+//----------------
+// Constructors --
+//----------------
+O2O_Scanner::O2O_Scanner ( const std::string& appName )
+  : AppBase( appName )
+  , m_skipDamaged( 'd', "skip-damaged",             "skip damaged datagrams", false )
+, m_inputFiles ( "input-xtc", "the list of the input XTC files" )
+{
+  addOption( m_skipDamaged ) ;
+  addArgument( m_inputFiles ) ;
+}
+
+//--------------
+// Destructor --
+//--------------
+O2O_Scanner::~O2O_Scanner ()
+{
+}
+
+/**
+ *  Main method which runs the whole application
+ */
+int
+O2O_Scanner::runApp ()
+{
+  std::list<O2OXtcFileName> files ;
+  for (AppCmdArgList<std::string>::const_iterator i = m_inputFiles.begin(); i!=m_inputFiles.end(); ++i) {
+    files.push_back( O2OXtcFileName(*i) ) ;
+  }
+
+  if (files.empty()) {
+    MsgLogRoot(error, "no input data files specified" ) ;
+    return 2 ;
+  }
+
+  O2OTranslator::O2OXtcMerger iter(files, 0x1000000, O2OTranslator::O2OXtcMerger::OneStream, m_skipDamaged.value());
+  while ( Dgram* dg = iter.next() ) {
+    const Pds::Sequence& seq = dg->seq ;
+    const Pds::ClockTime& clock = seq.clock() ;
+    const Pds::TimeStamp& stamp = seq.stamp() ;
+    printf("%s transition: damage %x, type %d, time %u sec %u nsec, ticks %u, fiducials %u, control %u, payloadSize %d\n",
+           TransitionId::name(seq.service()),
+           dg->xtc.damage.value(),
+           int(seq.type()),
+           clock.seconds(), clock.nanoseconds(),
+           stamp.ticks(),stamp.fiducials(),stamp.control(),
+           dg->xtc.sizeofPayload());
+    myLevelIter iter(&(dg->xtc));
+    iter.iterate();
+
+    delete [] (char*)dg ;
+  }
+
+  return 0;
+}
+
+} // namespace O2OTranslator
+
+
+// this defines main()
+APPUTILS_MAIN(O2OTranslator::O2O_Scanner)
---
+++ O2OTranslator/tags/V00-05-02/include/O2OXtcMerger.h
@@ -0,0 +1,90 @@
+#ifndef O2OTRANSLATOR_O2OXTCMERGER_H
+#define O2OTRANSLATOR_O2OXTCMERGER_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class O2OXtcMerger.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <list>
+#include <vector>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "pdsdata/xtc/Dgram.hh"
+#include "O2OTranslator/O2OXtcDechunk.h"
+#include "O2OTranslator/O2OXtcFileName.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace O2OTranslator {
+
+/**
+ *  Class responsible for merging of the datagrams from several
+ *  XTC streams.
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class O2OXtcMerger  {
+public:
+
+  /// Several merge modes supported:
+  ///   OneStream - all files come from one stream, chunked
+  ///   FilePerStream - single file per stream, no chunking
+  ///   FileName - streams and chunks are determined from file names
+  enum MergeMode { OneStream, NoChunking, FileName } ;
+
+  // Default constructor
+  O2OXtcMerger ( const std::list<O2OXtcFileName>& files, size_t maxDgSize, MergeMode mode, bool skipDamaged ) ;
+
+  // Destructor
+  ~O2OXtcMerger () ;
+
+  // read next datagram, return zero pointer after last file has been read,
+  // throws exception for errors.
+  Pds::Dgram* next() ;
+
+protected:
+
+private:
+
+  // Data members
+  std::vector<O2OXtcDechunk*> m_streams ;
+  std::vector<Pds::Dgram*> m_dgrams ;
+  MergeMode m_mode ;
+
+  // Copy constructor and assignment are disabled by default
+  O2OXtcMerger ( const O2OXtcMerger& ) ;
+  O2OXtcMerger& operator = ( const O2OXtcMerger& ) ;
+
+};
+
+} // namespace O2OTranslator
+
+#endif // O2OTRANSLATOR_O2OXTCMERGER_H
