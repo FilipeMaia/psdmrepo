f5c74ee96db3e4362c7a73f8be37284af8c7c108
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-03-24 17:38:01 +0000

    Tagging H5DataTypes with V00-00-14
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@794 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ H5DataTypes/tags/V00-00-14/src/EpicsPvHeader.cpp
@@ -0,0 +1,72 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class EpicsPvHeader...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "H5DataTypes/EpicsPvHeader.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5pp/CompoundType.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace H5DataTypes {
+
+//----------------
+// Constructors --
+//----------------
+EpicsPvHeader::EpicsPvHeader ( const XtcType& xtc )
+{
+  m_data.pvId = xtc.iPvId ;
+  m_data.dbrType = xtc.iDbrType ;
+  m_data.numElements = xtc.iNumElements ;
+}
+
+//--------------
+// Destructor --
+//--------------
+EpicsPvHeader::~EpicsPvHeader ()
+{
+}
+
+hdf5pp::Type
+EpicsPvHeader::stored_type()
+{
+  return native_type() ;
+}
+
+hdf5pp::Type
+EpicsPvHeader::native_type()
+{
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<EpicsPvHeader_Data>() ;
+  type.insert_native<int16_t>( "pvId", offsetof(EpicsPvHeader_Data,pvId) ) ;
+  type.insert_native<int16_t>( "dbrType", offsetof(EpicsPvHeader_Data,dbrType) ) ;
+  type.insert_native<int16_t>( "numElements", offsetof(EpicsPvHeader_Data,numElements) ) ;
+
+  return type ;
+}
+
+} // namespace H5DataTypes
