03177836f800fead852877103bebe28eab50e5dd
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-04-12 23:35:17 +0000

    Tagging pyana with V00-00-04
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@868 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pyana/tags/V00-00-04/src/input.py
@@ -0,0 +1,78 @@
+#
+# $Id$
+#
+# Copyright (c) 2010 SLAC National Accelerator Laboratory
+# 
+
+import logging
+import threading
+import Queue
+
+from pypdsdata import io
+
+_log = logging.getLogger("pyana.input")
+
+
+def dgramGen(names):
+    """ Datagram generator """
+
+    # group files by run number
+    runfiles = {}
+    for n in names : 
+        xname = io.XtcFileName(n)
+        runfiles.setdefault(xname.run(), []).append(n)
+
+    # scan all runs
+    runs = runfiles.keys()
+    runs.sort()
+    for run in runs :
+        
+        names = runfiles[run]
+        
+        logging.info("Processing run number %s" % run)
+        logging.info("File list: %s" % names)
+
+        # read datagrams one by one
+        dgiter = io.XtcMergeIterator( names )
+        for dg in dgiter :
+            
+            fileName = dgiter.fileName()
+            fpos = dgiter.fpos()
+
+            yield (dg, fileName, fpos)
+
+
+class _DgramReaderThread ( threading.Thread ):
+    
+    def __init__(self, queue, names):
+        
+        threading.Thread.__init__(self, name="DgramReader")
+        
+        self.queue = queue
+        self.names = names
+        
+    def run(self) :
+        
+        for dg in dgramGen(self.names) :
+            self.queue.put(dg)
+        # signal end of data
+        self.queue.put(None)
+            
+
+def threadedDgramGen( names, queueSize = 10 ):
+
+    """ datagram generator which does reading in a separate thread """
+
+    queue = Queue.Queue(queueSize)
+    thread = _DgramReaderThread(queue, names)
+    thread.start()
+    
+    while True:
+        
+        dg = queue.get()
+        if dg is None : break
+        yield dg
+        
+    # join the thread
+    thread.join()
+    
