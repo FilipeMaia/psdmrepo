1e62f722ba2406c55fbcc8697fc6b0f7bac1cd32
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-05-03 22:37:30 +0000

    Tagging pypdsdata with V00-00-03
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@969 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-03/pyext/EnumType.h
@@ -0,0 +1,87 @@
+#ifndef PYPDSDATA_ENUMTYPE_H
+#define PYPDSDATA_ENUMTYPE_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class EnumType.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <map>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "python/Python.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace pypdsdata {
+
+/**
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class EnumType {
+public:
+
+  struct Enum { const char* name; int value; };
+
+  // Constructor takes two arguments: name which may be a dotted
+  // name including package and module name, and array of enums,
+  // last array entry must have zero name pointer
+  EnumType ( const char* name, Enum* enums ) ;
+
+  // Destructor
+  ~EnumType () ;
+
+  // Returns _borrowed_ reference to Pyhton type object
+  PyObject* type() { return (PyObject*)&m_type; }
+
+  // Make instance of this type, returns new reference
+  PyObject* Enum_FromLong( long value ) ;
+  PyObject* Enum_FromString( const char* name ) ;
+
+protected:
+
+private:
+
+  typedef std::map<long,PyObject*> Int2Enum;
+
+  // Data members
+  PyTypeObject m_type ;
+  Int2Enum m_int2enum;
+
+  // Copy constructor and assignment are disabled by default
+  EnumType ( const EnumType& ) ;
+  EnumType& operator = ( const EnumType& ) ;
+
+};
+
+} // namespace pypdsdata
+
+#endif // PYPDSDATA_ENUMTYPE_H
