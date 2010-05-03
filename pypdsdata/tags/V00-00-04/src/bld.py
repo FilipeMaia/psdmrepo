ee3a4c2457208b26660550527085d609418f0aab
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-05-03 22:59:46 +0000

    Tagging pypdsdata with V00-00-04
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@985 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-04/src/bld.py
@@ -0,0 +1,51 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id$
+#
+# Description:
+#  Module bld...
+#
+#------------------------------------------------------------------------
+
+"""Wrapper module for _pdsdata.bld.
+
+This software was developed for the LUSI project.  If you use all or 
+part of it, please give an appropriate acknowledgment.
+
+@see RelatedModule
+
+@version $Id$
+
+@author Andrei Salnikov
+"""
+
+
+#------------------------------
+#  Module's version from CVS --
+#------------------------------
+__version__ = "$Revision$"
+# $Source$
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import sys
+
+#---------------------------------
+#  Imports of base class module --
+#---------------------------------
+import _pdsdata
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+#------------------------
+# Exported definitions --
+#------------------------
+
+from _pdsdata.bld import *
