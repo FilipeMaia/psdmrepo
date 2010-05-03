e50c252be98029476f5cc24c45c0c88a43e730e6
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-05-03 22:30:45 +0000

    Tagging iRODSAccess with V00-00-08
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@947 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ iRODSAccess/tags/V00-00-08/web/irodsws/controllers/files.py
@@ -0,0 +1,110 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id$
+#
+# Description:
+#  Module resources
+#------------------------------------------------------------------------
+
+""" Pylons controller for the iRODS resources.
+
+This software was developed for the LUSI project.  If you
+use all or part of it, please give an appropriate acknowledgement.
+
+Copyright (C) 2006 SLAC
+
+@version $Id$ 
+
+@author Andy Salnikov
+"""
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import logging
+
+#---------------------------------
+#  Imports of base class module --
+#---------------------------------
+from irodsws.lib.base import BaseController
+import formencode
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+from pylons import request, response, session, tmpl_context as c
+from pylons.controllers.util import abort, redirect_to
+from pylons.decorators import validate
+from pylons.decorators import jsonify
+from irodsws.lib.base import *
+from irodsws.model.irods_model import IrodsModel
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+class _ShowForm(formencode.Schema):
+    allow_extra_fields = True
+    filter_extra_fields = True
+    if_key_missing = None
+    recursive = formencode.validators.Int()
+
+#------------------------
+# Exported definitions --
+#------------------------
+
+#---------------------
+#  Class definition --
+#---------------------
+class FilesController(BaseController):
+
+    @jsonify
+    def show(self, path=''):
+        """GET /resources/*path: Show a specific item"""
+        # url('resource', id=ID)
+
+        # see if user can have an access
+        h.checkAccess(path)
+
+        # validate parameters
+        schema = _ShowForm()
+        try:
+            form_result = schema.to_python(dict(request.params))
+        except formencode.Invalid, error:
+            abort( 400, unicode(error) )
+
+        # empty exp_id ito become NULL
+        try:
+            recursive = int(form_result['recursive'])
+        except :
+            recursive = None
+
+        model = IrodsModel()
+        res = model.files( '/'+path, recursive )
+        if res is None :
+            abort(404)
+        else :
+            
+            # add URI for each file or collection
+            for r in res :
+                if r['type'] == 'collection' :
+                    name = r['name'].lstrip('/')
+                    url = h.url_for( action='show', path=name, qualified=True )
+                    r['url'] = url
+                elif r['type'] == 'object' :
+                    name = r['collName']+'/'+r['name']
+                    name = name.lstrip('/')
+                    url = h.url_for( action='show', path=name, qualified=True )
+                    r['url'] = url
+                    
+            return res
+
+
+    def environ(self):
+        result = '<html><body><h1>Environ</h1><table>'
+        keys = request.environ.keys()
+        keys.sort()
+        for key in keys :
+            result += '<tr><td>%s</td><td>%r</td></tr>'%(key, request.environ[key])
+        result += '</table></body></html>'
+        return result
