76d8038833fcd075cf3bd4ba55748f747bababda
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-05-03 22:03:38 +0000

    Tagging AuthDB with V00-00-09
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@905 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ AuthDB/tags/V00-00-09/web/dynamic/ProcessCreateRole.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once('AuthDB/AuthDB.inc.php');
+
+/*
+ * This script will process a request for creating a new role
+ * in the database.
+ */
+// TODO: This needs to be changed with the real test
+//
+//if( !RegDBAuth::isAuthenticated()) return;
+
+//print_r( $_POST );
+//echo "<br>";
+
+if( isset( $_POST['application_name'] )) {
+    $application_name = trim( $_POST['application_name'] );
+    if( $application_name == '' )
+        die( "application name can't be empty" );
+} else
+    die( "no valid application name" );
+
+if( isset( $_POST['role_name'] )) {
+    $role_name = trim( $_POST['role_name'] );
+    if( $role_name == '' )
+        die( "role name can't be empty" );
+} else
+    die( "no valid role name" );
+
+if( isset( $_POST['privileges'] )) {
+    $str = stripslashes( trim( $_POST['privileges'] ));
+    if( $str == 'null' ) $privileges = null;
+    else {
+        $privileges = json_decode( $str );
+        if( is_null( $privileges ))
+            die( "failed to translate JSON object with a list of privileges" );
+    }
+} else
+    die( "no valid role privileges collection" );
+
+if( isset( $_POST['actionSuccess'] )) {
+    $actionSuccess = trim( $_POST['actionSuccess'] );
+}
+
+//print_r( $privileges );
+//echo "<br>";
+
+/* Proceed with the operation
+ */
+try {
+    $authdb = new AuthDB();
+    $authdb->begin();
+
+    $authdb->createRole( $application_name, $role_name, $privileges );
+
+    if( isset( $actionSuccess )) {
+        if( $actionSuccess == 'home' )
+            header( 'Location: index.php' );
+        else if( $actionSuccess == 'list_roles' )
+            header( 'Location: index.php?action=list_roles' );
+        else
+            ;
+    }
+    $authdb->commit();
+
+} catch( AuthDBException $e ) {
+    print $e->toHtml();
+}
+?>
