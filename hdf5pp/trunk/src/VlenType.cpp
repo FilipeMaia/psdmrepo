aa0ea92df6ef733632f3e672f98fbcdbd61b762d
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-05-12 17:35:56 +0000

    Tag: V00-00-12 2010-05-12 Andy Salnikov - new type VlenType, support for variable length types
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@993 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ hdf5pp/trunk/src/VlenType.cpp
@@ -0,0 +1,48 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class VlenType...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "SITConfig/SITConfig.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "hdf5pp/VlenType.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5/hdf5.h"
+#include "hdf5pp/Exceptions.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace hdf5pp {
+
+// make a an array type of any rank
+VlenType
+VlenType::vlenType( const Type& baseType )
+{
+  hid_t tid = H5Tvlen_create( baseType.id() ) ;
+  if ( tid < 0 ) throw Hdf5CallException ( "VlenType::vlenType", "H5Tvlen_create" ) ;
+  return VlenType ( tid ) ;
+}
+
+} // namespace hdf5pp
