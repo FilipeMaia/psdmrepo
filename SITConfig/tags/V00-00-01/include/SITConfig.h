c97c146e191d082f8cd4f7dd5c64cbe5317e4e64
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-05-13 00:26:41 +0000

    Tagging SITConfig with V00-00-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1003 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ SITConfig/tags/V00-00-01/include/SITConfig.h
@@ -0,0 +1,16 @@
+#ifndef SITCONFIG_SITCONFIG_H
+#define SITCONFIG_SITCONFIG_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//      Standard include file to contain global "fixes" and type definitions.
+//      This file is intended to be included in every C++ source (.cpp) file.
+//
+//--------------------------------------------------------------------------
+
+
+
+#endif // SITCONFIG_SITCONFIG_H
