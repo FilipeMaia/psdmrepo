13f189ecf2783226337d00427b3d81866486c43f
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-06-16 00:47:12 +0000

    Tagging pypdsdata with V00-00-12
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1063 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-12/pyext/types/acqiris/DataDescV1.cpp
@@ -0,0 +1,175 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Acqiris_DataDescV1...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "DataDescV1.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "../../Exception.h"
+#include "../TypeLib.h"
+#include "HorizV1.h"
+#include "TimestampV1.h"
+#include "../../pdsdata_numpy.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  // list of enums
+  pypdsdata::TypeLib::EnumEntry enums[] = {
+        { "NumberOfBits",  Pds::Acqiris::DataDescV1::NumberOfBits },
+        { "BitShift",      Pds::Acqiris::DataDescV1::BitShift },
+
+        { 0, 0 }
+  };
+
+  // methods
+  FUN0_WRAPPER(pypdsdata::Acqiris::DataDescV1, nbrSamplesInSeg)
+  FUN0_WRAPPER(pypdsdata::Acqiris::DataDescV1, nbrSegments)
+  FUN0_WRAPPER(pypdsdata::Acqiris::DataDescV1, indexFirstPoint)
+  PyObject* timestamp( PyObject* self, PyObject* args );
+  PyObject* waveform( PyObject* self, PyObject* args );
+  PyObject* nextChannel( PyObject* self, PyObject* args );
+
+  PyMethodDef methods[] = {
+    {"nbrSamplesInSeg",  nbrSamplesInSeg,   METH_NOARGS,  "Returns integer number" },
+    {"nbrSegments",      nbrSegments,       METH_NOARGS,  "Returns integer number" },
+    {"indexFirstPoint",  indexFirstPoint,   METH_NOARGS,  "Returns integer number" },
+    {"timestamp",        timestamp,         METH_VARARGS, "Returns TimestampV1 object for a given segment" },
+    {"waveform",         waveform,          METH_VARARGS, "Returns waveform array given a HorizV1 object" },
+    {"nextChannel",      nextChannel,       METH_VARARGS, "Returns DataDescV1 for next channel (arg is HorizV1 object)" },
+    {0, 0, 0, 0}
+   };
+
+  char typedoc[] = "Python class wrapping C++ Pds::Acqiris::DataDescV1 class.";
+}
+
+//              ----------------------------------------
+//              -- Public Function Member Definitions --
+//              ----------------------------------------
+
+void
+pypdsdata::Acqiris::DataDescV1::initType( PyObject* module )
+{
+  PyTypeObject* type = BaseType::typeObject() ;
+  type->tp_doc = ::typedoc;
+  type->tp_methods = ::methods;
+
+  // define class attributes for enums
+  PyObject* tp_dict = PyDict_New();
+  pypdsdata::TypeLib::DefineEnums( tp_dict, ::enums );
+  type->tp_dict = tp_dict;
+
+  BaseType::initType( "DataDescV1", module );
+}
+
+namespace {
+
+PyObject*
+timestamp( PyObject* self, PyObject* args )
+{
+  Pds::Acqiris::DataDescV1* obj = pypdsdata::Acqiris::DataDescV1::pdsObject( self );
+  if ( not obj ) return 0;
+
+  unsigned seg;
+  if ( not PyArg_ParseTuple( args, "I:Acqiris.DataDescV1.timestamp", &seg ) ) return 0;
+
+  return pypdsdata::Acqiris::TimestampV1::PyObject_FromPds( &obj->timestamp(seg), self, sizeof(Pds::Acqiris::TimestampV1) );
+}
+
+
+PyObject*
+waveform( PyObject* self, PyObject* args )
+{
+  Pds::Acqiris::DataDescV1* obj = pypdsdata::Acqiris::DataDescV1::pdsObject( self );
+  if ( not obj ) return 0;
+
+  // parse args
+  PyObject* hconfigObj ;
+  if ( not PyArg_ParseTuple( args, "O:Acqiris.DataDescV1.waveform", &hconfigObj ) ) return 0;
+
+  // check type
+  if ( not pypdsdata::Acqiris::HorizV1::Object_TypeCheck( hconfigObj ) ) {
+    PyErr_SetString(PyExc_TypeError, "Error: parameter is not a Acqiris.HorizV1 object");
+    return 0;
+  }
+
+  // convert to Pds config object
+  const Pds::Acqiris::HorizV1* hconfig = pypdsdata::Acqiris::HorizV1::pdsObject( hconfigObj );
+
+  // get data size
+  unsigned size = hconfig->nbrSamples();
+
+  // NumPy type number
+  int typenum = NPY_SHORT;
+
+  // not writable
+  int flags = NPY_C_CONTIGUOUS ;
+
+  // dimensions
+  npy_intp dims[1] = { size };
+
+  // make array
+  int16_t* data = obj->waveform(*hconfig);
+  data += obj->indexFirstPoint();
+  PyObject* array = PyArray_New(&PyArray_Type, 1, dims, typenum, 0,
+                                (void*)data, 0, flags, 0);
+
+  // array does not own its data, set self as owner
+  Py_INCREF(self);
+  PyArrayObject* oarray = (PyArrayObject*)array;
+  oarray->base = self ;
+
+  return array;
+}
+
+PyObject*
+nextChannel( PyObject* self, PyObject* args )
+{
+  Pds::Acqiris::DataDescV1* obj = pypdsdata::Acqiris::DataDescV1::pdsObject( self );
+  if ( not obj ) return 0;
+
+  // parse args
+  PyObject* hconfigObj ;
+  if ( not PyArg_ParseTuple( args, "O:Acqiris.DataDescV1.nextChannel", &hconfigObj ) ) return 0;
+
+  // check type
+  if ( not pypdsdata::Acqiris::HorizV1::Object_TypeCheck( hconfigObj ) ) {
+    PyErr_SetString(PyExc_TypeError, "Error: parameter is not a Acqiris.HorizV1 object");
+    return 0;
+  }
+
+  // convert to Pds config object
+  const Pds::Acqiris::HorizV1* hconfig = pypdsdata::Acqiris::HorizV1::pdsObject( hconfigObj );
+
+  // get next object
+  Pds::Acqiris::DataDescV1* next = obj->nextChannel( *hconfig );
+
+  // make Python object
+  pypdsdata::Acqiris::DataDescV1* py_this = (pypdsdata::Acqiris::DataDescV1*) self;
+  size_t size = Pds::Acqiris::DataDescV1::totalSize( *hconfig );
+  return pypdsdata::Acqiris::DataDescV1::PyObject_FromPds( next, py_this->m_parent, size, py_this->m_dtor );
+}
+
+
+}
+
---
+++ pypdsdata/tags/V00-00-12/pyext/types/fccd/FccdConfigV2.cpp
@@ -0,0 +1,161 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class FccdConfigV2...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "FccdConfigV2.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "../../Exception.h"
+#include "../../EnumType.h"
+#include "../TypeLib.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+namespace {
+
+  pypdsdata::EnumType::Enum depthEnumValues[] = {
+      { "Eight_bit",   Pds::FCCD::FccdConfigV2::Eight_bit },
+      { "Sixteen_bit", Pds::FCCD::FccdConfigV2::Sixteen_bit },
+      { 0, 0 }
+  };
+  pypdsdata::EnumType depthEnum ( "Depth", depthEnumValues );
+
+  pypdsdata::EnumType::Enum outputSourceEnumValues[] = {
+      { "Output_FIFO",   Pds::FCCD::FccdConfigV2::Output_FIFO },
+      { "Test_Pattern1", Pds::FCCD::FccdConfigV2::Test_Pattern1 },
+      { "Test_Pattern2", Pds::FCCD::FccdConfigV2::Test_Pattern2 },
+      { "Test_Pattern3", Pds::FCCD::FccdConfigV2::Test_Pattern3 },
+      { "Test_Pattern4", Pds::FCCD::FccdConfigV2::Test_Pattern4 },
+      { 0, 0 }
+  };
+  pypdsdata::EnumType outputSourceEnum ( "Output_Source", outputSourceEnumValues );
+
+  // type-specific methods
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, width)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, height)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, trimmedWidth)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, trimmedHeight)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, outputMode)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, size)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, ccdEnable)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, focusMode)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, exposureTime)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage1)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage2)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage3)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage4)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage5)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage6)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage7)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage8)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage9)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage10)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage11)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage12)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage13)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage14)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage15)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage16)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, dacVoltage17)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform0)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform1)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform2)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform3)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform4)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform5)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform6)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform7)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform8)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform9)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform10)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform11)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform12)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform13)
+  FUN0_WRAPPER(pypdsdata::FCCD::FccdConfigV2, waveform14)
+
+  PyMethodDef methods[] = {
+    { "width",         width,         METH_NOARGS, "" },
+    { "height",        height,        METH_NOARGS, "" },
+    { "trimmedWidth",  trimmedWidth,  METH_NOARGS, "" },
+    { "trimmedHeight", trimmedHeight, METH_NOARGS, "" },
+    { "outputMode",    outputMode,    METH_NOARGS, "" },
+    { "size",          size,          METH_NOARGS, "" },
+    { "ccdEnable",     ccdEnable,     METH_NOARGS, "" },
+    { "focusMode",     focusMode,     METH_NOARGS, "" },
+    { "exposureTime",  exposureTime,  METH_NOARGS, "" },
+    { "dacVoltage1",   dacVoltage1,   METH_NOARGS, "" },
+    { "dacVoltage2",   dacVoltage2,   METH_NOARGS, "" },
+    { "dacVoltage3",   dacVoltage3,   METH_NOARGS, "" },
+    { "dacVoltage4",   dacVoltage4,   METH_NOARGS, "" },
+    { "dacVoltage5",   dacVoltage5,   METH_NOARGS, "" },
+    { "dacVoltage6",   dacVoltage6,   METH_NOARGS, "" },
+    { "dacVoltage7",   dacVoltage7,   METH_NOARGS, "" },
+    { "dacVoltage8",   dacVoltage8,   METH_NOARGS, "" },
+    { "dacVoltage9",   dacVoltage9,   METH_NOARGS, "" },
+    { "dacVoltage10",  dacVoltage10,  METH_NOARGS, "" },
+    { "dacVoltage11",  dacVoltage11,  METH_NOARGS, "" },
+    { "dacVoltage12",  dacVoltage12,  METH_NOARGS, "" },
+    { "dacVoltage13",  dacVoltage13,  METH_NOARGS, "" },
+    { "dacVoltage14",  dacVoltage14,  METH_NOARGS, "" },
+    { "dacVoltage15",  dacVoltage15,  METH_NOARGS, "" },
+    { "dacVoltage16",  dacVoltage16,  METH_NOARGS, "" },
+    { "dacVoltage17",  dacVoltage17,  METH_NOARGS, "" },
+    { "waveform0",     waveform0,     METH_NOARGS, "" },
+    { "waveform1",     waveform1,     METH_NOARGS, "" },
+    { "waveform2",     waveform2,     METH_NOARGS, "" },
+    { "waveform3",     waveform3,     METH_NOARGS, "" },
+    { "waveform4",     waveform4,     METH_NOARGS, "" },
+    { "waveform5",     waveform5,     METH_NOARGS, "" },
+    { "waveform6",     waveform6,     METH_NOARGS, "" },
+    { "waveform7",     waveform7,     METH_NOARGS, "" },
+    { "waveform8",     waveform8,     METH_NOARGS, "" },
+    { "waveform9",     waveform9,     METH_NOARGS, "" },
+    { "waveform10",    waveform10,    METH_NOARGS, "" },
+    { "waveform11",    waveform11,    METH_NOARGS, "" },
+    { "waveform12",    waveform12,    METH_NOARGS, "" },
+    { "waveform13",    waveform13,    METH_NOARGS, "" },
+    { "waveform14",    waveform14,    METH_NOARGS, "" },
+    {0, 0, 0, 0}
+   };
+
+  char typedoc[] = "Python class wrapping C++ Pds::FCCD::FccdConfigV2 class.";
+
+}
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+void
+pypdsdata::FCCD::FccdConfigV2::initType( PyObject* module )
+{
+  PyTypeObject* type = BaseType::typeObject() ;
+  type->tp_doc = ::typedoc;
+  type->tp_methods = ::methods;
+
+  // define class attributes for enums
+  PyObject* tp_dict = PyDict_New();
+  PyDict_SetItemString( tp_dict, "Depth", depthEnum.type() );
+  PyDict_SetItemString( tp_dict, "Output_Source", outputSourceEnum.type() );
+  type->tp_dict = tp_dict;
+
+  BaseType::initType( "FccdConfigV2", module );
+}
