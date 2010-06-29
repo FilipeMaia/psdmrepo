218888403aac1fc1c9c68c11242bd79ae62c56b2
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-06-29 22:24:15 +0000

    Tagging pypdsdata with V00-00-14
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1088 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-14/pyext/types/bld/BldDataEBeam.cpp
@@ -0,0 +1,71 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class BldDataEBeam...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "BldDataEBeam.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "../../Exception.h"
+#include "../TypeLib.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  // methods
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeam, uDamageMask)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeam, fEbeamCharge)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeam, fEbeamL3Energy)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeam, fEbeamLTUPosX)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeam, fEbeamLTUPosY)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeam, fEbeamLTUAngX)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeam, fEbeamLTUAngY)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeam, fEbeamPkCurrBC2)
+
+  PyGetSetDef getset[] = {
+    {"uDamageMask",    uDamageMask,    0, "", 0},
+    {"fEbeamCharge",   fEbeamCharge,   0, "in nC", 0},
+    {"fEbeamL3Energy", fEbeamL3Energy, 0, "in MeV", 0},
+    {"fEbeamLTUPosX",  fEbeamLTUPosX,  0, "in mm", 0},
+    {"fEbeamLTUPosY",  fEbeamLTUPosY,  0, "in mm", 0},
+    {"fEbeamLTUAngX",  fEbeamLTUAngX,  0, "in mrad", 0},
+    {"fEbeamLTUAngY",  fEbeamLTUAngY,  0, "in mrad", 0},
+    {"fEbeamPkCurrBC2", fEbeamPkCurrBC2, 0, "in Amps", 0},
+    {0, 0, 0, 0, 0}
+  };
+
+  char typedoc[] = "Python class wrapping C++ Pds::BldDataEBeam class.";
+}
+
+//              ----------------------------------------
+//              -- Public Function Member Definitions --
+//              ----------------------------------------
+
+void
+pypdsdata::BldDataEBeam::initType( PyObject* module )
+{
+  PyTypeObject* type = BaseType::typeObject() ;
+  type->tp_doc = ::typedoc;
+  type->tp_getset = ::getset;
+
+  BaseType::initType( "BldDataEBeam", module );
+}
---
+++ pypdsdata/tags/V00-00-14/pyext/types/encoder/ConfigV1.cpp
@@ -0,0 +1,94 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class ConfigV1...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "ConfigV1.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "../../EnumType.h"
+#include "../../Exception.h"
+#include "../TypeLib.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+namespace {
+
+  pypdsdata::EnumType::Enum countModeEnumValues[] = {
+      { "WRAP_FULL",   Pds::Encoder::ConfigV1::count_mode::WRAP_FULL },
+      { "LIMIT",       Pds::Encoder::ConfigV1::count_mode::LIMIT },
+      { "HALT",        Pds::Encoder::ConfigV1::count_mode::HALT },
+      { "WRAP_PRESET", Pds::Encoder::ConfigV1::count_mode::WRAP_PRESET },
+      { "END",         Pds::Encoder::ConfigV1::count_mode::END },
+      { 0, 0 }
+  };
+  pypdsdata::EnumType countModeEnum ( "count_mode", countModeEnumValues );
+
+  pypdsdata::EnumType::Enum quadModeEnumValues[] = {
+      { "CLOCK_DIR", Pds::Encoder::ConfigV1::quad_mode::CLOCK_DIR },
+      { "X1",        Pds::Encoder::ConfigV1::quad_mode::X1 },
+      { "X2",        Pds::Encoder::ConfigV1::quad_mode::X2 },
+      { "X4",        Pds::Encoder::ConfigV1::quad_mode::X4 },
+      { "END",       Pds::Encoder::ConfigV1::quad_mode::END },
+      { 0, 0 }
+  };
+  pypdsdata::EnumType quadModeEnum ( "quad_mode", quadModeEnumValues );
+
+  // methods
+  MEMBER_WRAPPER(pypdsdata::Encoder::ConfigV1, _chan_num)
+  ENUM_MEMBER_WRAPPER(pypdsdata::Encoder::ConfigV1, _count_mode, countModeEnum)
+  ENUM_MEMBER_WRAPPER(pypdsdata::Encoder::ConfigV1, _quadrature_mode, quadModeEnum)
+  MEMBER_WRAPPER(pypdsdata::Encoder::ConfigV1, _input_num)
+  MEMBER_WRAPPER(pypdsdata::Encoder::ConfigV1, _input_rising)
+  MEMBER_WRAPPER(pypdsdata::Encoder::ConfigV1, _ticks_per_sec)
+
+  PyGetSetDef getset[] = {
+    {"_chan_num",         _chan_num,         0, "", 0},
+    {"_count_mode",      _count_mode,       0, "", 0},
+    {"_quadrature_mode", _quadrature_mode,  0, "", 0},
+    {"_input_num",       _input_num,        0, "", 0},
+    {"_input_rising",    _input_rising,     0, "", 0},
+    {"_ticks_per_sec",   _ticks_per_sec,    0, "", 0},
+    {0, 0, 0, 0, 0}
+  };
+
+  char typedoc[] = "Python class wrapping C++ Pds::Encoder::ConfigV1 class.";
+
+}
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+void
+pypdsdata::Encoder::ConfigV1::initType( PyObject* module )
+{
+  PyTypeObject* type = BaseType::typeObject() ;
+  type->tp_doc = ::typedoc;
+  type->tp_getset = ::getset;
+
+  // define class attributes for enums
+  PyObject* tp_dict = PyDict_New();
+  PyDict_SetItemString( tp_dict, "count_mode", countModeEnum.type() );
+  PyDict_SetItemString( tp_dict, "quad_mode", quadModeEnum.type() );
+  type->tp_dict = tp_dict;
+
+  BaseType::initType( "ConfigV1", module );
+}
