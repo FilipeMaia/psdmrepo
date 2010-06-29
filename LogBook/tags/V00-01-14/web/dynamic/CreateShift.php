834723124825ffcf60df8ccc941ec08d2955e584
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-06-29 22:17:50 +0000

    Tagging LogBook with V00-01-14
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1086 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ LogBook/tags/V00-01-14/web/dynamic/CreateShift.php
@@ -0,0 +1,102 @@
+<?php
+
+require_once('LogBook/LogBook.inc.php');
+
+/*
+ * This script will process a request for creating a new shift.
+ */
+if( isset( $_POST['id'] )) {
+    $id = trim( $_POST['id'] );
+    if( $id == '' )
+        die( "experiment identifier can't be empty" );
+} else {
+    die( "no valid experiment identifier" );
+}
+if( isset( $_POST['leader'] )) {
+    $leader = trim( $_POST['leader'] );
+    if( $leader == '' )
+        die( "shift leader can't be empty" );
+} else {
+    die( "no valid shift leader" );
+}
+if( isset( $_POST['actionSuccess'] )) {
+    $actionSuccess = trim( $_POST['actionSuccess'] );
+}
+if( isset( $_POST['author'] )) {
+    $author = trim( $_POST['author'] );
+    if( $author == '' )
+        die( "shift request author can't be empty" );
+} else {
+    die( "no valid shift request author" );
+}
+if( isset( $_POST['goals'] )) {
+    $goals = trim( $_POST['goals'] );
+} else {
+    die( "no valid parameter for shift goals" );
+}
+
+$crew = array();
+if( isset( $_POST['max_crew_size'] )) {
+    $max_crew_size = (int)trim( $_POST['max_crew_size'] );
+    for( $i=0; $i<$max_crew_size; $i++) {
+        $key = 'member'.$i;
+        if( isset( $_POST[$key] )) {
+            $member = trim( $_POST[$key] );
+            if( $member != '' ) array_push( $crew, $member );
+        }
+    }
+}
+
+try {
+
+    $logbook = new LogBook();
+    $logbook->begin();
+
+    $experiment = $logbook->find_experiment_by_id( $id )
+        or die("failed to find the experiment" );
+
+    $instrument = $experiment->instrument();
+
+    // Check for the authorization
+    //
+    if( !LogBookAuth::instance()->canManageShifts( $experiment->id())) {
+        print( LogBookAuth::reporErrorHtml(
+            'You are not authorized to manage shifts of the experiment',
+            'index.php?action=select_experiment'.
+                '&instr_id='.$instrument->id().
+                '&instr_name='.$instrument->name().
+                '&exper_id='.$experiment->id().
+                '&exper_name='.$experiment->name()));
+        exit;
+    }
+
+    // Proceed to the operation
+    //
+    $begin_time = LusiTime::now();
+    $shift = $experiment->create_shift( $leader, $crew, $begin_time );
+
+    $entry = $experiment->create_entry (
+        $author, 'TEXT', $goals, $shift_id=$shift->id());
+    $entry->add_tag( 'SHIFT_GOALS', '' );
+
+    if( isset( $actionSuccess )) {
+        if( $actionSuccess == 'select_experiment_and_shift' )
+            header( 'Location: index.php?action=select_experiment_and_shift'.
+                '&instr_id='.$instrument->id().
+                '&instr_name='.$instrument->name().
+                '&exper_id='.$experiment->id().
+                '&exper_name='.$experiment->name().
+                '&shift_id='.$shift->id());
+        else
+            ;
+    }
+    $logbook->commit();
+
+} catch( RegDBException $e ) {
+    print $e->toHtml();
+} catch( LogBookException $e ) {
+    print $e->toHtml();
+} catch( LusiTimeException $e ) {
+    print $e->toHtml();
+}
+?>
---
+++ LogBook/tags/V00-01-14/web/static/CreateRunParam.php
@@ -0,0 +1,84 @@
+<!--
+The page for creating a new run summary parameter.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Create new run parameter</title>
+    </head>
+    <body>
+        <?php
+        $now = new DateTime();
+        $now_str = $now->format(DateTime::ISO8601);
+        $now_str[10] = ' ';  // get rid of date-time separator 'T'
+        ?>
+        <h1>Create new run parameter :</h1>
+        <form action="ProcessCreateRunParam.php" method="POST" style="margin-left:2em;">
+            <table cellpadding="3"  border="0" >
+                <thead style="color:#0071bc;">
+                    <th align="right">
+                        &nbsp;<b>Attribute</b>&nbsp;</th>
+                    <th align="left">
+                        &nbsp;<b>Value</b>&nbsp;</th>
+                    <th align="left">
+                        &nbsp;<b>Format</b>&nbsp;</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><hr></td>
+                        <td><hr></td>
+                        <td><hr></td></tr>
+                    <tr>
+                        <td align="right" style="width:6em;">
+                            &nbsp;<b>Parameter</b>&nbsp;</td>
+                        <td>
+                            &nbsp;<input align="left" size="32" type="text" name="param" value="" />&nbsp;</td>
+                        <td>
+                            &nbsp;Max. Len. 255</td>
+                    </tr>
+                    <tr>
+                        <td align="right" style="width:6em;">
+                            &nbsp;<b>Experiment</b>&nbsp;</td>
+                        <td>
+                            &nbsp;<select align="center" type="text" name="experiment_name" ><?php
+                            require_once('LogBook/LogBook.inc.php');
+                            $logbook = new LogBook();
+                            $logbook->begin();
+                            $experiments = $logbook->experiments()
+                                or die("failed to find experiments" );
+                            foreach( $experiments as $e)
+                                echo '<option> '.$e->attr['name'].' </option>';
+                            $logbook->commit();
+                            ?></select>&nbsp;
+                        <td>
+                            &nbsp;</td>
+                    <tr>
+                        <td align="right">
+                            &nbsp;<b>Type</b>&nbsp;</td>
+                        <td>
+                            &nbsp;<select align="center" type="text" name="type" >
+                            <option>INT</option>
+                            <option>DOUBLE</option>
+                            <option>TEXT</option>
+                            </select>&nbsp;
+                        <td>
+                            &nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            &nbsp;<b>Description</b>&nbsp;</td>
+                        <td>
+                            &nbsp;<input align="left" size="48" type="text" name="descr" value="" />&nbsp;</td>
+                        <td>
+                            &nbsp;[optional] </td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <br>
+            <input type="submit" value="Submit" name="submit_button" /><br>
+        </form>
+    </body>
+</html>
+
