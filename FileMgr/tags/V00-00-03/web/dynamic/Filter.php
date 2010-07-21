337bb6a326773a73b7c39b6ec7d7544b5af29033
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-07-21 00:06:54 +0000

    Tagging FileMgr with V00-00-03
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1106 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ FileMgr/tags/V00-00-03/web/dynamic/Filter.php
@@ -0,0 +1,83 @@
+<?php
+
+require_once('RegDB/RegDB.inc.php');
+
+/*
+ * This script will generate a module with input elements for the filter form
+ * in a context of the specified experiment.
+ */
+if( isset( $_GET['exper_id'] )) {
+    $exper_id = trim( $_GET['exper_id'] );
+    if( $exper_id == '' ) {
+        $exper_id( "experiment identifier can't be empty" );
+    }
+} else {
+    die( "no valid experiment identifier" );
+}
+
+/* Proceed with the operation
+ */
+try {
+    $regdb = new RegDB;
+    $regdb->begin();
+
+    $experiment = $regdb->find_experiment_by_id( $exper_id )
+        or die( "no such experiment" );
+
+    $instrument = $experiment->instrument();
+
+    // Check for the authorization
+    //
+    if( !RegDBAuth::instance()->canRead()) {
+        print( RegDBAuth::reporErrorHtml(
+            'You are not authorized to access any information from the Experiment Registration Database',
+            'index.php'));
+        exit;
+    }
+    $range_of_runs = '0-0';
+    if( !$experiment->begin_time()->greaterOrEqual( LusiTime::now())) {
+        $range = FileMgrIrodsWs::max_run_range( $instrument->name(), $experiment->name(), array( 'xtc', 'hdf5' ));
+        $range_of_runs = $range['min'].'-'.$range['max'];
+    }
+    $range_help = '1,3,5,10-20,200';
+
+    /* Proceed to the operation
+     */
+    header( 'Content-type: text/html' );
+    header( "Cache-Control: no-cache, must-revalidate" ); // HTTP/1.1
+    header( "Expires: Sat, 26 Jul 1997 05:00:00 GMT" );   // Date in the past
+
+    $con = new RegDBHtml( 0, 0, 350, 140 );
+    echo $con
+        ->label         (   0,   0, 'R u n s' )
+        ->radio_input   (   0,  25, 'runs', 'all',   true  )->label      ( 25, 20, 'all', false )
+        ->radio_input   (   0,  45, 'runs', 'range', false )->value_input( 25, 40, 'runs_range', $range_of_runs, $range_help, 8 )
+
+        ->label         ( 130,   0, 'A r c h i v e d' )
+        ->radio_input   ( 130,  25, 'archived', 'yes_or_no', true  )->label( 150,  20, 'yes or no', false )
+        ->radio_input   ( 130,  45, 'archived', 'yes',       false )->label( 150,  45, 'yes',       false )
+        ->radio_input   ( 130,  65, 'archived', 'no',        false )->label( 150,  65, 'no',        false )
+
+        ->label         ( 250,   0, 'D i s k' )
+        ->radio_input   ( 250,  25, 'local', 'yes_or_no', true  )->label( 270,  20, 'yes or no', false )
+        ->radio_input   ( 250,  45, 'local', 'yes',       false )->label( 270,  40, 'yes',       false )
+        ->radio_input   ( 250,  65, 'local', 'no',        false )->label( 270,  60, 'no',        false )
+
+        ->label         (   0,  90, 'T y p e s' )
+        ->checkbox_input(   0, 115, 'xtc',   'xtc',   true )->label(  20, 110, 'XTC',   false )
+        ->checkbox_input(   0, 135, 'hdf5',  'hdf5',  true )->label(  20, 130, 'HDF5',  false )
+
+        ->button        ( 130, 120, 'reset_filter_button',  'Reset', 'reset filter to its initial state' )
+        ->button        ( 205, 120, 'submit_filter_button', 'Apply', 'update the file list using this filter' )
+        ->button        ( 280, 120, 'import_list_button',   'Import', 'get an import list for selected files for which a disk copy is available' )
+        
+        ->html();
+
+    $regdb->commit();
+
+} catch( RegDBException $e ) {
+    print $e->toHtml();
+} catch( RegDBException $e ) {
+    print $e->toHtml();
+}
+?>
