d5fc81f49feab18a5c3b00daf0d068db5c73b30b
gapon <gapon@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-07-28 08:22:42 +0000

    Tagging LogBook with 00-01-17
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1135 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ LogBook/tags/00-01-17/web/LogBookSubscription.class.php
@@ -0,0 +1,28 @@
+<?php
+class LogBookSubscription {
+
+    /* Data members
+     */
+    private $connection;
+    private $experiment;
+    private $attr;
+
+    /* Constructor
+     */
+    public function __construct( $connection, $experiment, $attr ) {
+        $this->connection = $connection;
+        $this->experiment = $experiment;
+        $this->attr       = $attr;
+    }
+
+    /* Accessors
+     */
+    public function parent () { return $this->experiment; }
+
+    public function id () { return $this->attr['id']; }
+    public function subscriber() { return $this->attr['subscriber']; }
+    public function address   () { return $this->attr['address']; }
+    public function subscribed_time() { return LusiTime::from64( $this->attr['subscribed_time'] ); }
+    public function subscribed_host() { return $this->attr['subscribed_host']; }
+}
+?>
---
+++ LogBook/tags/00-01-17/web/dynamic/RequestAttachments.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once('LogBook/LogBook.inc.php');
+
+/*
+ * This script will process a request for displaying attachments of a free-form entry.
+ */
+if( isset( $_GET['id'] )) {
+    $id = trim( $_GET['id'] );
+    if( $id == '' )
+        die( "free-form entry identifier can't be empty" );
+} else
+    die( "no valid free-form entry identifier" );
+
+function attachment2json( $a ) {
+    /*
+    $attachment_url =
+        '<a href="javascript:show_attachment('.$a->id().
+        ')" title="download and see in a separate browser window" class="lb_link">'.
+        $a->description().'</a>';
+     */
+    $title = "download and see in a separate browser window";
+    $attachment_url = <<<HERE
+<a href="ShowAttachment.php?id={$a->id()}" target="_blank" title="{$title}" class="lb_link">{$a->description()}</a>
+HERE;
+    return json_encode(
+        array (
+            "attachment" => $attachment_url,
+            "size" => $a->document_size(),
+        )
+    );
+}
+
+/*
+ * Return JSON objects with a list of attachments.
+ */
+try {
+
+    $logbook = new LogBook();
+    $logbook->begin();
+
+    $entry = $logbook->find_entry_by_id( $id )
+        or die( "no such free-form entry" );
+
+    $experiment = $entry->parent();
+    $instrument = $experiment->instrument();
+
+    // Check for the authorization
+    //
+    if( !LogBookAuth::instance()->canRead( $experiment->id())) {
+        print( LogBookAuth::reporErrorHtml(
+            'You are not authorized to access any information about the experiment' ));
+        exit;
+    }
+
+    // Proceed to the operation
+    //
+    $attachments = $entry->attachments();
+
+    header( 'Content-type: application/json' );
+    header( "Cache-Control: no-cache, must-revalidate" ); // HTTP/1.1
+    header( "Expires: Sat, 26 Jul 1997 05:00:00 GMT" );   // Date in the past
+
+    print <<< HERE
+{
+  "ResultSet": {
+    "Result": [
+HERE;
+    $first = true;
+    foreach( $attachments as $a ) {
+      if( $first ) {
+          $first = false;
+          echo "\n".attachment2json( $a );
+      } else {
+          echo ",\n".attachment2json( $a );
+      }
+    }
+    print <<< HERE
+ ] } }
+HERE;
+
+    $logbook->commit();
+
+} catch( RegDBException $e ) {
+    print $e->toHtml();
+} catch( LogBookException $e ) {
+    print $e->toHtml();
+} catch( LusiTimeException $e ) {
+    print $e->toHtml();
+}
+?>
