b8ca432c08ff72bb15669eda67108b929c782c9d
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-08-27 00:03:22 +0000

    Tagging O2OTranslator with V00-07-07
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1169 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ O2OTranslator/tags/V00-07-07/app/o2o-translate.cpp
@@ -0,0 +1,316 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class O2O_Translate...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "SITConfig/SITConfig.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <vector>
+#include <boost/thread/thread.hpp>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "AppUtils/AppBase.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "AppUtils/AppCmdArgList.h"
+#include "AppUtils/AppCmdOpt.h"
+#include "AppUtils/AppCmdOptBool.h"
+#include "AppUtils/AppCmdOptIncr.h"
+#include "AppUtils/AppCmdOptList.h"
+#include "AppUtils/AppCmdOptSize.h"
+#include "AppUtils/AppCmdOptNamedValue.h"
+#include "LusiTime/Time.h"
+#include "MsgLogger/MsgLogger.h"
+#include "O2OTranslator/DgramQueue.h"
+#include "O2OTranslator/DgramReader.h"
+#include "O2OTranslator/MetaDataScanner.h"
+#include "O2OTranslator/O2OFileNameFactory.h"
+#include "O2OTranslator/O2OHdf5Writer.h"
+#include "O2OTranslator/O2OMetaData.h"
+#include "O2OTranslator/O2OXtcFileName.h"
+#include "O2OTranslator/O2OXtcIterator.h"
+#include "O2OTranslator/O2OXtcMerger.h"
+#include "O2OTranslator/O2OXtcScannerI.h"
+#include "O2OTranslator/O2OXtcValidator.h"
+#include "pdsdata/xtc/XtcFileIterator.hh"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace O2OTranslator {
+
+using namespace AppUtils ;
+
+//
+//  Application class
+//
+class O2O_Translate : public AppBase {
+public:
+
+  // Constructor
+  explicit O2O_Translate ( const std::string& appName ) ;
+
+  // destructor
+  ~O2O_Translate () ;
+
+protected :
+
+  /**
+   *  Main method which runs the whole application
+   */
+  virtual int runApp () ;
+
+private:
+
+  // more command line options and arguments
+  AppCmdOptList<std::string>  m_optionsFile ;
+  AppCmdOpt<int>              m_compression ;
+  AppCmdOptSize               m_dgramsize ;
+  AppCmdOpt<unsigned int>     m_dgramQSize ;
+  AppCmdOpt<std::string>      m_experiment ;
+  AppCmdOptBool               m_extGroups ;
+  AppCmdOpt<std::string>      m_instrument ;
+  AppCmdOpt<double>           m_l1offset ;
+  AppCmdOpt<std::string>      m_mdConnStr ;
+  AppCmdOptNamedValue<O2OXtcMerger::MergeMode> m_mergeMode ;
+  AppCmdOptList<std::string>  m_metadata ;
+  AppCmdOpt<std::string>      m_outputDir ;
+  AppCmdOpt<std::string>      m_outputName ;
+  AppCmdOptBool               m_overwrite ;
+  AppCmdOpt<std::string>      m_regConnStr ;
+  AppCmdOpt<unsigned long>    m_runNumber ;
+  AppCmdOpt<std::string>      m_runType ;
+  AppCmdOptNamedValue<O2OHdf5Writer::SplitMode> m_splitMode ;
+  AppCmdOptSize               m_splitSize ;
+  AppCmdArgList<std::string>  m_eventData ;
+};
+
+//----------------
+// Constructors --
+//----------------
+O2O_Translate::O2O_Translate ( const std::string& appName )
+  : AppBase( appName )
+  , m_optionsFile( 'o', "options-file", "path",     "file name with options, multiple allowed", '\0' )
+  , m_compression( 'c', "compression",  "number",   "compression level, -1..9, def: -1", -1 )
+  , m_dgramsize  ( 'g', "datagram-size","size",     "datagram buffer size. def: 16M", 16*1048576ULL )
+  , m_dgramQSize ( 'Q', "datagram-queue","number",     "datagram queue size. def: 32", 32 )
+  , m_experiment ( 'x', "experiment",   "string",   "experiment name", "" )
+  , m_extGroups  ( 'G', "group-time",               "use extended group names with timestamps", false )
+  , m_instrument ( 'i', "instrument",   "string",   "instrument name", "" )
+  , m_l1offset   (      "l1-offset",    "number",   "L1Accept time offset seconds, def: 0", 0 )
+  , m_mdConnStr  ( 'M', "md-conn",      "string",   "metadata ODBC connection string", "" )
+  , m_mergeMode  ( 'j', "merge-mode",   "mode-name","one of one-stream, no-chunking, file-name; def: file-name", O2OXtcMerger::FileName )
+  , m_metadata   ( 'm', "metadata",     "name:value", "science metadata values", '\0' )
+  , m_outputDir  ( 'd', "output-dir",   "path",     "directory to store output files, def: .", "." )
+  , m_outputName ( 'n', "output-name",  "template", "template string for output file names, def: {seq4}.h5", "{seq4}.h5" )
+  , m_overwrite  (      "overwrite",                "overwrite output file", false )
+  , m_regConnStr ( 'R', "regdb-conn",   "string",   "RegDB ODBC connection string", "" )
+  , m_runNumber  ( 'r', "run-number",   "number",   "run number, non-negative number; def: 0", 0 )
+  , m_runType    ( 't', "run-type",     "string",   "run type, DATA or CALIB, def: DATA", "DATA" )
+  , m_splitMode  ( 's', "split-mode",   "mode-name","one of none, or family; def: none", O2OHdf5Writer::NoSplit )
+  , m_splitSize  ( 'z', "split-size",   "size",   "max. size of output files. def: 10G", 10*1073741824ULL )
+  , m_eventData  ( "event-file",   "file name(s) with XTC event data" )
+{
+  setOptionsFile( m_optionsFile ) ;
+  addOption( m_compression ) ;
+  addOption( m_dgramsize ) ;
+  addOption( m_dgramQSize ) ;
+  addOption( m_experiment ) ;
+  addOption( m_extGroups ) ;
+  addOption( m_instrument ) ;
+  addOption( m_l1offset ) ;
+  addOption( m_mdConnStr ) ;
+  addOption( m_mergeMode ) ;
+  m_mergeMode.add ( "one-stream", O2OXtcMerger::OneStream ) ;
+  m_mergeMode.add ( "no-chunking", O2OXtcMerger::NoChunking ) ;
+  m_mergeMode.add ( "file-name", O2OXtcMerger::FileName ) ;
+  addOption( m_metadata ) ;
+  addOption( m_outputDir ) ;
+  addOption( m_outputName ) ;
+  addOption( m_overwrite ) ;
+  addOption( m_regConnStr ) ;
+  addOption( m_runNumber ) ;
+  addOption( m_runType ) ;
+  m_splitMode.add ( "none", O2OHdf5Writer::NoSplit ) ;
+  m_splitMode.add ( "family", O2OHdf5Writer::Family ) ;
+  addOption( m_splitMode ) ;
+  addOption( m_splitSize ) ;
+  addArgument( m_eventData ) ;
+}
+
+//--------------
+// Destructor --
+//--------------
+O2O_Translate::~O2O_Translate ()
+{
+}
+
+/**
+ *  Main method which runs the whole application
+ */
+int
+O2O_Translate::runApp ()
+{
+  LusiTime::Time start_time = LusiTime::Time::now() ;
+  MsgLogRoot( info, "Starting translator process " << start_time ) ;
+  MsgLogRoot( info, "Command line: " <<  this->cmdline() ) ;
+
+  WithMsgLogRoot( info, log ) {
+    typedef AppCmdOptList<std::string>::const_iterator Iter ;
+    log << "input files:";
+    for ( Iter it = m_eventData.begin() ; it != m_eventData.end() ; ++ it ) {
+      log << "\n    " << *it ;
+    }
+  }
+  MsgLogRoot( info, "output dir: " << m_outputDir.value() ) ;
+
+  // instantiate name factory for output files
+  O2OFileNameFactory nameFactory ( "{output-dir}/" + m_outputName.value() ) ;
+  std::string outputDir = m_outputDir.value() ;
+  if ( outputDir.empty() ) outputDir = "." ;
+  nameFactory.addKeyword ( "output-dir", outputDir ) ;
+  nameFactory.addKeyword ( "experiment", m_experiment.value() ) ;
+  nameFactory.addKeyword ( "instrument", m_instrument.value() ) ;
+  char runStr[16];
+  snprintf ( runStr, sizeof runStr, "%06lu", m_runNumber.value() ) ;
+  nameFactory.addKeyword ( "run", runStr ) ;
+
+  // make metadata object
+  O2OMetaData metadata ( m_runNumber.value(),
+                         m_runType.value(),
+                         m_instrument.value(),
+                         m_experiment.value(),
+                         m_metadata.value() ) ;
+
+  // instantiate XTC scanner, which is also output file writer
+  std::vector<O2OXtcScannerI*> scanners ;
+  scanners.push_back ( new O2OHdf5Writer ( nameFactory, m_overwrite.value(),
+                                  m_splitMode.value(), m_splitSize.value(),
+                                  m_compression.value(), m_extGroups.value(),
+                                  metadata ) ) ;
+
+  // instantiate metadata scanner
+  scanners.push_back ( new MetaDataScanner( metadata, m_mdConnStr.value(), m_regConnStr.value() ) ) ;
+
+  // make datagram queue
+  DgramQueue dgqueue( m_dgramQSize.value() ) ;
+
+  // start datagram reading thread
+  std::list<O2OXtcFileName> files ;
+  for ( AppCmdOptList<std::string>::const_iterator it = m_eventData.begin() ; it != m_eventData.end() ; ++ it ) {
+    files.push_back ( O2OXtcFileName(*it) ) ;
+  }
+  boost::thread readerThread( DgramReader ( files, dgqueue, m_dgramsize.value(),
+                                            m_mergeMode.value(), false, m_l1offset.value() ) ) ;
+
+  uint64_t count = 0 ;
+
+  // get all datagrams
+  while ( Pds::Dgram* dg = dgqueue.pop() ) {
+
+    ++ count ;
+
+    WithMsgLogRoot( trace, out ) {
+      const ClockTime& clock = dg->seq.clock() ;
+      out << "Transition: #" << count << " "
+          << std::left << std::setw(12) << Pds::TransitionId::name(dg->seq.service())
+          << "  time: " << clock.seconds() << '.'
+          << std::setfill('0') << std::setw(9) << clock.nanoseconds()
+          << "  payloadSize: " << dg->xtc.sizeofPayload()
+          << "  damage: " << std::hex << std::showbase << dg->xtc.damage.value() ;
+    }
+
+    // validate the XTC structure
+    O2OXtcValidator validator ;
+    if ( validator.process( &(dg->xtc) ) == 0 ) {
+
+      WithMsgLogRoot( error, out ) {
+        out << "Validation failed: Transition: #" << count << " "
+            << Pds::TransitionId::name(dg->seq.service())
+            << ", skipping datagram";
+      }
+
+    } else {
+
+      // give this event to every scanner
+      for ( std::vector<O2OXtcScannerI*>::iterator i = scanners.begin() ; i != scanners.end() ; ++ i ) {
+
+        O2OXtcScannerI* scanner = *i ;
+
+        try {
+          scanner->eventStart ( *dg ) ;
+
+          O2OXtcIterator iter( &(dg->xtc), scanner );
+          iter.iterate();
+
+          scanner->eventEnd ( *dg ) ;
+        } catch ( std::exception& e ) {
+          MsgLogRoot( error, "exception caught processing datagram: " << e.what() ) ;
+          return 3 ;
+        }
+      }
+
+    }
+
+    // all datagrams must be explicitely deleted
+    delete [] (char*)dg ;
+  }
+
+  // finish with the scanners
+  for ( std::vector<O2OXtcScannerI*>::iterator i = scanners.begin() ; i != scanners.end() ; ++ i ) {
+    try {
+      O2OXtcScannerI* scanner = *i ;
+      delete scanner ;
+    } catch ( std::exception& e ) {
+      MsgLogRoot( error, "exception caught while destroying a scanner: " << e.what() ) ;
+      return 4 ;
+    }
+  }
+  scanners.clear() ;
+
+  LusiTime::Time end_time = LusiTime::Time::now() ;
+  MsgLogRoot( info, "Translator process finished " << end_time ) ;
+
+  // dump some resource info
+  double delta_time = (end_time.sec()-start_time.sec()) + (end_time.nsec()-start_time.nsec()) / 1e9 ;
+  WithMsgLogRoot( info, out ) {
+    struct rusage u ;
+    getrusage(RUSAGE_SELF, &u);
+    out << "Resource usage summary:"
+        << "\n    real time: " << delta_time
+        << "\n    user time: " << (u.ru_utime.tv_sec + u.ru_utime.tv_usec/1e6)
+        << "\n    sys time : " << (u.ru_stime.tv_sec + u.ru_stime.tv_usec/1e6) ;
+  }
+
+  return 0 ;
+
+}
+
+} // namespace O2OTranslator
+
+
+// this defines main()
+APPUTILS_MAIN(O2OTranslator::O2O_Translate)
---
+++ O2OTranslator/tags/V00-07-07/include/DgramReader.h
@@ -0,0 +1,89 @@
+#ifndef O2OTRANSLATOR_DGRAMREADER_H
+#define O2OTRANSLATOR_DGRAMREADER_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class DgramReader.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <list>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "O2OTranslator/O2OXtcFileName.h"
+#include "O2OTranslator/O2OXtcMerger.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace O2OTranslator {
+
+class DgramQueue ;
+
+/**
+ *  Thread which reads datagrams from the list of XTC files
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class DgramReader  {
+public:
+
+  typedef std::list<O2OXtcFileName> FileList ;
+
+  // Default constructor
+  DgramReader ( const FileList& files,
+                DgramQueue& queue,
+                size_t maxDgSize,
+                O2OXtcMerger::MergeMode mode,
+                bool skipDamaged,
+                int l1OffsetSec = 0 ) ;
+
+  // Destructor
+  ~DgramReader () ;
+
+  // this is the "run" method used by the Boost.thread
+  void operator() () ;
+
+protected:
+
+private:
+
+  // Data members
+  FileList m_files ;
+  DgramQueue& m_queue ;
+  size_t m_maxDgSize ;
+  O2OXtcMerger::MergeMode m_mode ;
+  bool m_skipDamaged ;
+  int m_l1OffsetSec ;
+
+};
+
+} // namespace O2OTranslator
+
+#endif // O2OTRANSLATOR_DGRAMREADER_H
