4354ce2584cc18ede17ff074d78e16a5a5cbc78c
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-08-31 16:05:18 +0000

    Tagging hdf5pp with V00-00-14
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1171 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ hdf5pp/tags/V00-00-14/src/Type.cpp
@@ -0,0 +1,98 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Type...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "hdf5pp/Type.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5pp/Exceptions.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace hdf5pp {
+
+//----------------
+// Constructors --
+//----------------
+Type::Type ()
+  : m_id ()
+{
+}
+
+Type::Type ( hid_t id, bool doClose )
+  : m_id ( new hid_t(id), TypePtrDeleter(doClose) )
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+Type::~Type ()
+{
+}
+
+/// return size of the type in bytes
+size_t
+Type::size() const
+{
+  size_t size = H5Tget_size( *m_id ) ;
+  if ( size == 0 ) {
+    throw Hdf5CallException( "Type::size", "H5Tget_size" ) ;
+  }
+  return size ;
+}
+
+/// make unlocked copy of the type
+Type
+Type::copy() const
+{
+  hid_t tid = H5Tcopy(*m_id) ;
+  if ( tid < 0 ) {
+    throw Hdf5CallException( "Type::copy", "H5Tcopy" ) ;
+  }
+  return UnlockedType( tid ) ;
+}
+
+/// set type size
+void
+Type::set_size( size_t size )
+{
+  if ( H5Tset_size( *m_id, size ) ) {
+    throw Hdf5CallException( "Type::set_size", "H5Tset_size" ) ;
+  }
+}
+
+/// set type precision
+void
+Type::set_precision( size_t precision )
+{
+  if ( H5Tset_precision( *m_id, precision ) ) {
+    throw Hdf5CallException( "Type::set_precision", "H5Tset_precision" ) ;
+  }
+}
+
+} // namespace hdf5pp
