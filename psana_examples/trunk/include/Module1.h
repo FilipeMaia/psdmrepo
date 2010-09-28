f09676fdcb76c4d3f4dc5d09cddaf89322870de1
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-09-28 23:52:25 +0000

    Tag: V00-00-01 2010-09-23 Andy Salnikov - first commit, just one non-functional example module which I use   for development
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1211 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psana_examples/trunk/include/Module1.h
@@ -0,0 +1,74 @@
+#ifndef PSANA_EXAMPLES_MODULE1_H
+#define PSANA_EXAMPLES_MODULE1_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Module1.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "psana/Module.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace psana_examples {
+
+/**
+ *  Example of the user analysis module.
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class Module1 : public Module {
+public:
+
+  // Default constructor
+  Module1 (const std::string& name) ;
+
+  // Destructor
+  virtual ~Module1 () ;
+
+  /// Method which is called with event data
+  virtual void event(/*Event& evt, Env& env*/);
+  
+protected:
+
+private:
+
+  // Data members
+  unsigned m_count;
+  unsigned m_maxEvents;
+  bool m_filter;
+  
+};
+
+} // namespace psana_examples
+
+#endif // PSANA_EXAMPLES_MODULE1_H
