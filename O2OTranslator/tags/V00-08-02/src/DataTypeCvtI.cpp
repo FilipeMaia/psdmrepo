2da6bd498e7c0a5eb07440e2f5d906cc7d209b4a
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-10-01 22:15:40 +0000

    Tagging O2OTranslator with V00-08-02
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1218 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ O2OTranslator/tags/V00-08-02/src/DataTypeCvtI.cpp
@@ -0,0 +1,45 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class O2ODataTypeCvtI...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "SITConfig/SITConfig.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "O2OTranslator/DataTypeCvtI.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <sstream>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace O2OTranslator {
+
+//--------------
+// Destructor --
+//--------------
+DataTypeCvtI::~DataTypeCvtI ()
+{
+}
+
+} // namespace O2OTranslator
