1f2c59f398b0909945021017c09536adad3713fd
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2010-11-10 00:04:11 +0000

    Tag: V00-00-01 2010-11-08 Andy Salnikov - for now there is a single calibration class CsPadPedestalsV1
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1280 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pdscalibdata/trunk/src/CsPadPedestalsV1.cpp
@@ -0,0 +1,92 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class CsPadPedestalsV1...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "SITConfig/SITConfig.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "pdscalibdata/CsPadPedestalsV1.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <algorithm>
+#include <stdexcept>
+#include <fstream>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace pdscalibdata {
+
+//----------------
+// Constructors --
+//----------------
+CsPadPedestalsV1::CsPadPedestalsV1 ()
+{
+  // fill all pedestals fith zeros
+  std::fill_n(&m_pedestals[0][0][0][0], int(Size), 0.0f);
+}
+
+CsPadPedestalsV1::CsPadPedestalsV1 (const std::string& fname) 
+{ 
+  // open file
+  std::ifstream in(fname.c_str());
+  if (not in.good()) {
+    const std::string msg = "Failed to open pedestals file: "+fname;
+    MsgLogRoot(error, msg);
+    throw std::runtime_error(msg);
+  }
+
+  // read all numbers
+  float* it = &m_pedestals[0][0][0][0];
+  size_t count = 0;
+  while(in and count != Size) {
+    in >> *it++;
+    ++ count;
+  }
+
+  // check that we read whole array
+  if (count != Size) {
+    const std::string msg = "Pedestals file does not have enough data: "+fname;
+    MsgLogRoot(error, msg);
+    throw std::runtime_error(msg);
+  }
+
+  // and no data left after we finished reading
+  float tmp ;
+  if ( in >> tmp ) {
+    const std::string msg = "Pedestals file has extra data: "+fname;
+    MsgLogRoot(error, msg);
+    MsgLogRoot(error, "read " << count << " numbers, expecting " << Size );
+    throw std::runtime_error(msg);
+  }
+}
+
+//--------------
+// Destructor --
+//--------------
+CsPadPedestalsV1::~CsPadPedestalsV1 ()
+{
+}
+
+} // namespace pdscalibdata
