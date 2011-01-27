468b07fe90f0d1605c2806db5b6198f32e5e4310
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-01-27 23:55:49 +0000

    Tagging XtcInput with V00-00-02
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1455 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ XtcInput/tags/V00-00-02/src/DgramReader.cpp
@@ -0,0 +1,94 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class DgramReader...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "XtcInput/DgramReader.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <algorithm>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+#include "XtcInput/DgramQueue.h"
+#include "XtcInput/XtcStreamMerger.h"
+#include "pdsdata/xtc/Dgram.hh"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace XtcInput {
+
+//----------------
+// Constructors --
+//----------------
+DgramReader::DgramReader ( const FileList& files,
+                           DgramQueue& queue,
+                           size_t maxDgSize,
+                           XtcStreamMerger::MergeMode mode,
+                           bool skipDamaged,
+                           double l1OffsetSec )
+  : m_files( files )
+  , m_queue( queue )
+  , m_maxDgSize( maxDgSize )
+  , m_mode( mode )
+  , m_skipDamaged( skipDamaged )
+  , m_l1OffsetSec(l1OffsetSec)
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+DgramReader::~DgramReader ()
+{
+}
+
+// this is the "run" method used by the Boost.thread
+void
+DgramReader::operator() ()
+{
+  try {
+
+    XtcStreamMerger iter(m_files, m_maxDgSize, m_mode, m_skipDamaged, m_l1OffsetSec);
+    while ( Pds::Dgram* dg = iter.next() ) {
+
+      // move it to the queue
+      m_queue.push ( dg ) ;
+
+    }
+
+    // tell all we are done
+    m_queue.push ( (Pds::Dgram*)0 ) ;
+
+  } catch ( std::exception& e ) {
+
+    MsgLogRoot( error, "exception caught while reading datagram: " << e.what() ) ;
+
+    // TODO: there is no way yet to stop gracefully, will just abort
+    throw;
+
+  }
+}
+
+
+} // namespace XtcInput
---
+++ XtcInput/tags/V00-00-02/src/XtcDgIterator.cpp
@@ -0,0 +1,104 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcDgIterator...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "XtcInput/XtcDgIterator.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "XtcInput/Exceptions.h"
+#include "MsgLogger/MsgLogger.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  const char* logger = "XtcDgIterator";
+
+}
+
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace XtcInput {
+
+//----------------
+// Constructors --
+//----------------
+XtcDgIterator::XtcDgIterator (const std::string& path, size_t maxDgramSize)
+  : m_path(path)
+  , m_file(0)
+  , m_maxDgramSize(maxDgramSize)
+  , m_buf(new char[maxDgramSize])
+{
+  m_file = fopen( m_path.c_str(), "rb" );
+  if ( ! m_file ) {
+    MsgLog( logger, error, "failed to open input XTC file: " << m_path ) ;
+    throw FileOpenException(m_path) ;
+  }
+}
+
+//--------------
+// Destructor --
+//--------------
+XtcDgIterator::~XtcDgIterator ()
+{
+  fclose( m_file );
+  delete[] m_buf;
+}
+
+Pds::Dgram*
+XtcDgIterator::next()
+{
+  Pds::Dgram* dg = (Pds::Dgram*)m_buf;
+
+  // read header
+  if ( fread(dg, sizeof(Pds::Dgram), 1, m_file) != 1 ) {
+    if ( feof(m_file) ) {
+      return 0;
+    } else {
+      throw XTCReadException( m_path );
+    }
+  }
+
+  // check payload size
+  size_t payloadSize = dg->xtc.sizeofPayload();
+  if ((payloadSize+sizeof(dg))>m_maxDgramSize) {
+    throw XTCSizeLimitException(m_path, payloadSize+sizeof(dg), m_maxDgramSize);
+  }
+
+  // read rest of the data
+  if ( payloadSize ) {
+    if ( fread(dg->xtc.payload(), payloadSize, 1, m_file) != 1 ) {
+      if ( feof(m_file) ) {
+        throw XTCEOFException(m_path);
+      } else {
+        throw XTCReadException(m_path);
+      }
+    }
+  }
+
+  return dg;
+}
+
+} // namespace XtcInput
