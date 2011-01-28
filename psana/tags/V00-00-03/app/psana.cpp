2f138f1ac162f19675f423c0a73094ac1de883ac
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-01-28 00:00:33 +0000

    Tagging psana with V00-00-03
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1461 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psana/tags/V00-00-03/app/psana.cpp
@@ -0,0 +1,228 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class psana...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unistd.h>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "AppUtils/AppBase.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "AppUtils/AppCmdArgList.h"
+#include "AppUtils/AppCmdOpt.h"
+#include "ConfigSvc/ConfigSvc.h"
+#include "ConfigSvc/ConfigSvcImplFile.h"
+#include "MsgLogger/MsgLogger.h"
+#include "psana/DynLoader.h"
+#include "PsEnv/Env.h"
+#include "PsEvt/Event.h"
+#include "PsEvt/ProxyDict.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace psana {
+
+//
+//  Application class
+//
+class psanaapp : public AppUtils::AppBase {
+public:
+
+  // Constructor
+  explicit psanaapp ( const std::string& appName ) ;
+
+  // destructor
+  ~psanaapp () ;
+
+protected :
+
+  /**
+   *  Main method which runs the whole application
+   */
+  virtual int runApp () ;
+
+private:
+
+  // more command line options and arguments
+  AppUtils::AppCmdOpt<std::string> m_configOpt ;
+  AppUtils::AppCmdArgList<std::string>  m_files;
+
+
+};
+
+//----------------
+// Constructors --
+//----------------
+psanaapp::psanaapp ( const std::string& appName )
+  : AppUtils::AppBase( appName )
+  , m_configOpt( 'c', "config", "path", "configuration file, def: psana.cfg", "psana.cfg" )
+  , m_files  ( "data-file",   "file name(s) with input data", std::list<std::string>() )
+{
+  addOption( m_configOpt ) ;
+  addArgument( m_files ) ;
+}
+
+//--------------
+// Destructor --
+//--------------
+psanaapp::~psanaapp ()
+{
+}
+
+/**
+ *  Main method which runs the whole application
+ */
+int
+psanaapp::runApp ()
+{
+  // start with reading configuration file
+  std::auto_ptr<ConfigSvc::ConfigSvcImplI> cfgImpl ( 
+      new ConfigSvc::ConfigSvcImplFile(m_configOpt.value()) );
+  // initialize config service
+  ConfigSvc::ConfigSvc::init(cfgImpl);
+  ConfigSvc::ConfigSvc cfgsvc;
+  
+  // get list of modules to load
+  std::list<std::string> moduleNames = cfgsvc.getList("psana", "modules");
+  
+  // max number of events
+  unsigned maxEvents = cfgsvc.get("psana", "events", 0xFFFFFFFFU);
+  
+  DynLoader loader;
+
+  // Load input module, fixed name for now
+  const std::string& iname = "PsXtcInput.XtcInputModule";
+  psana::InputModule* input = loader.loadInputModule(iname);
+  MsgLogRoot(info, "Loaded input module " << iname);
+
+  // pass file names to the configuration so that input module can find them
+  typedef AppUtils::AppCmdArgList<std::string>::const_iterator FileIter;
+  std::string flist;
+  for (FileIter it = m_files.begin(); it != m_files.end(); ++it ) {
+    if (not flist.empty()) flist += " ";
+    flist += *it;
+  }
+  cfgsvc.put(iname, "files", flist);
+  
+  // instantiate all user modules
+  std::vector<Module*> modules;
+  for ( std::list<std::string>::const_iterator it = moduleNames.begin(); it != moduleNames.end() ; ++ it ) {
+    Module* m = loader.loadModule(*it);
+    modules.push_back(m);
+    MsgLogRoot(info, "Loaded module " << m->name());
+  }
+  
+  // Setup environment
+  PsEnv::Env env;
+  
+  // Start with beginJob for everyone
+  input->beginJob(env);
+  for (std::vector<Module*>::iterator it = modules.begin() ; it != modules.end() ; ++it) {
+    (*it)->beginJob(env);
+  }
+  
+  // event loop
+  bool stop = false ;
+  while ( maxEvents > 0 and not stop) {
+    
+    // Create event object
+    boost::shared_ptr<PsEvt::ProxyDict> dict(new PsEvt::ProxyDict);
+    Event evt(dict);
+    
+    // run input module to populate event
+    InputModule::Status istat = input->event(evt, env);
+    MsgLogRoot(debug, "input.event() returned " << istat)
+    
+    if (istat == InputModule::Skip) continue;
+    if (istat == InputModule::Stop) break;
+    if (istat == InputModule::Abort) {
+      MsgLogRoot(info, "Input module requested abort");
+      return 1;
+    }
+    
+    for (std::vector<Module*>::iterator it = modules.begin() ; it != modules.end() ; ++it) {
+      Module& mod = *(*it);
+      
+      // clear module status
+      mod.reset();
+      
+      // dispatch event to particular method based on vent type
+      if (istat == InputModule::BeginRun) {
+        mod.beginRun(env);
+      } else if (istat == InputModule::BeginCalibCycle) {
+        mod.beginCalibCycle(env);
+      } else if (istat == InputModule::DoEvent) {
+        mod.event(evt, env);
+      } else if (istat == InputModule::EndCalibCycle) {
+        mod.endCalibCycle(env);
+      } else if (istat == InputModule::EndRun) {
+        mod.endRun(env);
+      }
+      
+      // check what module wants to tell us
+      switch (mod.status()) {
+      case Module::Skip:
+        break;
+      case Module::Stop:
+        MsgLogRoot(info, "module " << mod.name() << " requested stop");
+        stop = true;
+        break;
+      case Module::Abort:
+        MsgLogRoot(info, "module " << mod.name() << " requested abort");
+        return 1;
+        break;
+      case Module::OK:
+        break;
+      }
+      
+    }
+    
+    --maxEvents;
+  }
+  
+  
+  // End with endJob for everyone, note that the order is the same
+  input->endJob(env);
+  for (std::vector<Module*>::iterator it = modules.begin() ; it != modules.end() ; ++it) {
+    (*it)->endJob(env);
+  }
+
+  // cleanup
+  delete input;
+  for (std::vector<Module*>::iterator it = modules.begin() ; it != modules.end() ; ++it) {
+    delete *it;
+  }
+  
+  // return 0 on success, other values for error (like main())
+  return 0 ;
+}
+
+} // namespace psana
+
+
+// this defines main()
+APPUTILS_MAIN(psana::psanaapp)
---
+++ psana/tags/V00-00-03/include/Module.h
@@ -0,0 +1,142 @@
+#ifndef PSANA_MODULE_H
+#define PSANA_MODULE_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Module.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <boost/utility.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "psana/Configurable.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "PsEnv/Env.h"
+#include "PsEvt/Event.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+// this is not nice thing to do but we want to simplify user's life
+// and provide bunch of simple interfaces to our system
+
+namespace psana {}
+using namespace psana;
+using namespace PsEnv;
+using namespace PsEvt;
+
+#if defined(PSANACAT2_)
+#undef PSANACAT2_
+#endif
+#define PSANACAT2_(a,b) a ## b
+#define PSANA_MODULE_FACTORY(UserModule) \
+  extern "C" \
+  psana::Module* \
+  PSANACAT2_(_psana_module_,UserModule)(const std::string& name) {\
+    return new UserModule(name);\
+  }
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+
+namespace psana {
+
+/**
+ *  @brief Base class for user modules in PSANA
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class Module : boost::noncopyable, protected Configurable {
+public:
+
+  /// Event processing status
+  enum Status { OK, 
+                Skip,   ///< skip all remaining modules for this event
+                Stop,   ///< finish with the events
+                Abort   ///< abort immediately, no finalization
+  };
+  
+  // Destructor
+  virtual ~Module () ;
+
+  /// get the name of the module
+  using Configurable::name;
+  
+  /// get the class name of the module
+  using Configurable::className;
+  
+  /// Method which is called once at the beginning of the job
+  virtual void beginJob(Env& env);
+  
+  /// Method which is called at the beginning of the run
+  virtual void beginRun(Env& env);
+  
+  /// Method which is called at the beginning of the calibration cycle
+  virtual void beginCalibCycle(Env& env);
+  
+  /// Method which is called with event data
+  virtual void event(Event& evt, Env& env) = 0;
+  
+  /// Method which is called at the end of the calibration cycle
+  virtual void endCalibCycle(Env& env);
+
+  /// Method which is called at the end of the run
+  virtual void endRun(Env& env);
+
+  /// Method which is called once at the end of the job
+  virtual void endJob(Env& env);
+
+  /// reset module status
+  void reset() { m_status = OK; }
+  
+  /// get status
+  Status status() const { return m_status; }
+  
+protected:
+
+  // constructor needs module name
+  Module (const std::string& name) ;
+
+  /// change the status
+  void skip() { m_status = Skip; }
+  void stop() { m_status = Stop; }
+  void abort() { m_status = Abort; }
+
+private:
+
+  // Data members
+  Status m_status;
+
+};
+
+// formatting for enum
+std::ostream&
+operator<<(std::ostream& out, Module::Status stat);
+
+} // namespace psana
+
+#endif // PSANA_MODULE_H
