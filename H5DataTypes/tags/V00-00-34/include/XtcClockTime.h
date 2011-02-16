23a0e33d75e1064ec33822fdd86a23b1802843a2
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-02-16 02:24:14 +0000

    Tagging H5DataTypes with V00-00-34
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1523 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ H5DataTypes/tags/V00-00-34/include/XtcClockTime.h
@@ -0,0 +1,71 @@
+#ifndef H5DATATYPES_XTCCLOCKTIME_H
+#define H5DATATYPES_XTCCLOCKTIME_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcClockTime.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5pp/Group.h"
+#include "pdsdata/xtc/ClockTime.hh"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+namespace H5DataTypes {
+
+struct XtcClockTime_Data  {
+  uint32_t seconds;
+  uint32_t nanoseconds;
+};
+
+
+class XtcClockTime  {
+public:
+
+  // Default constructor
+  XtcClockTime () {}
+  XtcClockTime ( const Pds::ClockTime& time ) ;
+
+  static hdf5pp::Type stored_type() ;
+  static hdf5pp::Type native_type() ;
+
+private:
+  XtcClockTime_Data m_data ;
+};
+
+} // namespace H5DataTypes
+
+#endif // H5DATATYPES_XTCCLOCKTIME_H
