623aa056b10d4cf076be57ca13220171f06206dc
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-03-21 04:42:00 +0000

    Tagging PSXtcInput with V00-00-05
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1620 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSXtcInput/tags/V00-00-05/include/XtcInputModule.h
@@ -0,0 +1,95 @@
+#ifndef PSXTCINPUT_XTCINPUTMODULE_H
+#define PSXTCINPUT_XTCINPUTMODULE_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcInputModule.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <boost/thread/thread.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "psana/InputModule.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "XtcInput/Dgram.h"
+#include "psddl_pds2psana/XtcConverter.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+namespace XtcInput {
+  class DgramQueue;
+}
+
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSXtcInput {
+
+/**
+ *  @brief PSANA module for reading XTC files.
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class XtcInputModule : public InputModule {
+public:
+
+  // Default constructor
+  XtcInputModule (const std::string& name) ;
+
+  // Destructor
+  virtual ~XtcInputModule () ;
+
+  /// Method which is called once at the beginning of the job
+  virtual void beginJob(Env& env);
+
+  /// Method which is called with event data
+  virtual Status event(Event& evt, Env& env);
+
+  /// Method which is called once at the end of the job
+  virtual void endJob(Env& env);
+
+protected:
+  
+  // Fill event with datagram contents
+  void fillEvent(const XtcInput::Dgram::ptr& dg, Event& evt, Env& env);
+  
+  // Fill environment with datagram contents
+  void fillEnv(const XtcInput::Dgram::ptr& dg, Env& env);
+
+private:
+
+  // Data members
+  boost::scoped_ptr<XtcInput::DgramQueue> m_dgQueue;
+  XtcInput::Dgram::ptr m_putBack;
+  boost::scoped_ptr<boost::thread> m_readerThread;
+  psddl_pds2psana::XtcConverter m_cvt;
+};
+
+} // namespace PSXtcInput
+
+#endif // PSXTCINPUT_XTCINPUTMODULE_H
