7f258de1c878f0bb198fdcc75d8729e4333afcb7
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-03-23 22:56:03 +0000

    Tagging PSEnv with V00-00-05
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1681 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSEnv/tags/V00-00-05/include/Env.h
@@ -0,0 +1,73 @@
+#ifndef PSENV_ENV_H
+#define PSENV_ENV_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Env.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <boost/scoped_ptr.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "PSEnv/ConfigStore.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSEnv {
+
+/**
+ *  @brief Class representing an environment object for psana jobs.
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class Env : boost::noncopyable {
+public:
+
+  // Default constructor
+  Env () ;
+
+  // Destructor
+  ~Env () ;
+  
+  /// Access Configuration Store
+  ConfigStore& configStore() { return *m_cfgStore; }
+
+protected:
+
+private:
+
+  // Data members
+  boost::scoped_ptr<ConfigStore> m_cfgStore;
+  
+};
+
+} // namespace PSEnv
+
+#endif // PSENV_ENV_H
