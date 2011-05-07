ed657707bd2dc2238da31a49019221bd254ec340
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-05-07 01:51:24 +0000

    Tagging PSTime with V00-01-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1845 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSTime/tags/V00-01-01/src/Time.cpp
@@ -0,0 +1,309 @@
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class PSTime...
+//
+// Author List:
+//      Mikhail S. Dubrovin
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "PSTime/Time.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <iostream>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "PSTime/Exceptions.h"
+#include "PSTime/TimeFormat.h"
+#include "PSTime/TimeUtils.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  // number of nanoseconds in a second
+  const uint32_t nsecInASec = 1000000000U;
+  
+}
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace PSTime {
+
+//----------------
+// Constructors --
+//----------------
+
+Time::Time () 
+  : m_utcSec(0)
+  , m_utcNsec(0)
+{
+}
+
+Time::Time (time_t utcSec, time_t utcNsec) 
+  : m_utcSec(utcSec)
+  , m_utcNsec(utcNsec)
+{
+}
+
+Time::Time ( int  year,
+             int  month,
+             int  day,
+             int  hour,
+             int  min,
+             int  sec,
+             int  nsec,
+             Zone zone)
+  : m_utcSec(0)
+  , m_utcNsec(nsec)
+{
+  struct tm stm;
+  stm.tm_year  = year - 1900; // Human: 2010,   tm year from: 1900
+  stm.tm_mon   = month - 1;   // Human: [1,12], tm and POSIX: [0,11]
+  stm.tm_mday  = day; 
+  stm.tm_hour  = hour;
+  stm.tm_min   = min;
+  stm.tm_sec   = sec;
+  stm.tm_isdst = -1;          // Let mktime figure out whether DST is in force
+
+  if (zone == UTC) {
+    m_utcSec  = TimeUtils::timegm( &stm );
+  } else {
+    m_utcSec  = mktime( &stm );
+  }
+}
+
+Time::Time (struct timespec& ts)
+{
+  m_utcSec  = ts.tv_sec;
+  m_utcNsec = ts.tv_nsec;
+}
+
+Time::Time (struct tm& stm, Zone zone) 
+  : m_utcSec(0)
+  , m_utcNsec(0)
+{
+  if (zone == UTC) {
+    m_utcSec  = TimeUtils::timegm( &stm );
+  } else {
+    m_utcSec  = mktime( &stm );
+  }
+}
+
+// tstamp can be presented in formats understood by in parse(...) method
+Time::Time (const std::string& tstamp)
+  : m_utcSec(0)
+  , m_utcNsec(0)
+{
+  Time t = TimeFormat::parseTime(tstamp);
+  m_utcSec  = t.m_utcSec; // or t.getUTCSec();
+  m_utcNsec = t.m_utcNsec; // or t.getUTCNsec();
+}
+
+//--------------
+//  -- Operators
+//--------------
+
+// Arithmetic operators with Duration
+
+// d = |t2 - t1|
+Duration 
+Time::operator-( const Time& t1 ) const
+{
+    // This code forms |t2-t1| without having to use signed intergers.
+
+    time_t t2Sec;
+    time_t t2Nsec;
+    time_t t1Sec;
+    time_t t1Nsec;
+
+    if ( *this > t1 )
+        {
+            t2Sec  = m_utcSec;
+            t2Nsec = m_utcNsec;
+            t1Sec  = t1.m_utcSec;
+            t1Nsec = t1.m_utcNsec;
+        }
+    else
+        {
+            t2Sec  = t1.m_utcSec;
+            t2Nsec = t1.m_utcNsec;
+            t1Sec  = m_utcSec;
+            t1Nsec = m_utcNsec;
+        }
+
+    if ( t2Nsec < t1Nsec )
+        {
+            // borrow a second from t2Sec
+            t2Nsec += ::nsecInASec;
+            t2Sec--;
+        }
+
+    time_t sec  = t2Sec  - t1Sec;
+    time_t nsec = t2Nsec - t1Nsec;
+
+    Duration diff( sec, nsec );
+
+    return diff;
+}
+
+
+
+// t -= d
+Time& 
+Time::operator-=( const Duration& d )
+{
+  // if t1 - d < 0 then return t = 0
+  if ( ( m_utcSec  < d.getSec() ) ||
+       ( m_utcSec == d.getSec()   && m_utcNsec < d.getNsec() ) ) {
+    m_utcSec  = 0;
+    m_utcNsec = 0;
+  }
+  else {
+    time_t tempSec  = m_utcSec;
+    time_t tempNsec = m_utcNsec;
+    
+    if ( tempNsec < d.getNsec() ) {
+      // if t1.m_utcNsec < d._nsec borrow a second from t1.m_utcSec
+      tempNsec += ::nsecInASec;
+      tempSec--;
+    }
+    
+    m_utcSec  = tempSec  - d.getSec();
+    m_utcNsec = tempNsec - d.getNsec();
+  }
+  
+  return *this;
+}
+
+// t += d
+Time& 
+Time::operator+=( const Duration& d )
+{
+  time_t totalSec  = m_utcSec  + d.getSec();
+  time_t totalNsec = m_utcNsec + d.getNsec();
+  
+  if ( totalNsec >= ::nsecInASec ) {
+    // carry nanoseconds over into seconds
+    time_t extraSec   = totalNsec / ::nsecInASec;
+    time_t remainNsec = totalNsec % ::nsecInASec;
+    totalSec         += extraSec;
+    totalNsec         = remainNsec;
+  }
+  
+  m_utcSec  = totalSec;
+  m_utcNsec = totalNsec;
+
+  return *this;
+}
+
+
+//--------------------
+//  -- Public methods
+//--------------------
+
+struct timespec
+Time::getTimeSpec() const
+{
+  struct timespec ts;
+  ts.tv_sec  = m_utcSec;
+  ts.tv_nsec = m_utcNsec;
+  return ts;
+}
+
+struct tm
+Time::gettm( Zone zone ) const
+{
+  struct tm stm;
+  memset( &stm, 0, sizeof(stm)) ;
+
+  if (zone == UTC) {
+    gmtime_r(&m_utcSec, &stm);
+  } else {
+    localtime_r(&m_utcSec, &stm);    
+  }
+  
+  return stm;
+}
+
+// Time stamp in fromat YYYY-MM-DD HH:MM:SS.NNNNNNNNN+HH:MM
+std::string 
+Time::asString( Zone zone, int nsecPrecision ) const
+{
+  std::string fmt;
+  if (nsecPrecision < 1) {
+    fmt = "%F %T%z";
+  } else {
+    if (nsecPrecision > 9) nsecPrecision = 9;
+    fmt = "%F %T%.9f%z";
+    fmt[7] = char('0'+nsecPrecision);
+  }
+  return TimeFormat::format(*this, fmt, zone);
+}
+
+
+// Time stamp in fromat HHMMSSTHHMMSS+HHMM
+std::string 
+Time::asStringCompact( Zone zone, int nsecPrecision  ) const
+{
+  std::string fmt;
+  if (nsecPrecision < 1) {
+    fmt = "%Y%m%dT%H%M%S%z";
+  } else {
+    if (nsecPrecision > 9) nsecPrecision = 9;
+    fmt = "%Y%m%dT%H%M%S%.9f%z";
+    fmt[15] = char('0'+nsecPrecision);
+  }
+  return TimeFormat::format(*this, fmt, zone);
+}
+
+
+// Time stamp in free fromat
+std::string 
+Time::asStringFormat( const std::string& fmt, Zone zone ) const
+{
+  return TimeFormat::format(*this, fmt, zone);
+}
+
+/**
+ *  Returns current time.
+ */
+Time 
+Time::now() 
+{
+  struct timespec ts;
+  int status = clock_gettime( CLOCK_REALTIME, &ts );
+
+  if( status == 0 ) {
+    return Time(ts.tv_sec, ts.tv_nsec);
+  } else {
+    throw ErrnoException( ERR_LOC, "clock_gettime failed");
+  }
+  
+}
+
+std::ostream& 
+operator <<( std::ostream& os, const Time& t ) 
+{
+  TimeFormat::format(os, t, "%F %T%f%z", Time::Local);
+  return os;
+}
+
+//--------------
+} // namespace PSTime
+
---
+++ PSTime/tags/V00-01-01/src/TimeUtils.cpp
@@ -0,0 +1,130 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class TimeUtils...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "PSTime/TimeUtils.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+#define USE_TIMEGM 1
+
+
+#if !USE_TIMEGM
+
+namespace {
+
+  bool is_leap_year(int year) 
+  {
+    if (year % 400 == 0) return true;
+    if (year % 100 == 0) return false;
+    if (year % 4 == 0) return true;
+    return false;
+  }
+  
+  int mdays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+  // Returns the number of days in a month. 
+  int days(int year, int month) 
+  {
+    int days = mdays[month];
+    if (is_leap_year(year) and month > 0) ++ days;
+    return days;
+  }
+  
+  int run_days[12] = { 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+
+  // Returns the number of days since the beginning of the year 
+  // right after the month ends. 
+  int running_days(int year, int month) 
+  {
+    int days = run_days[month];
+    if (is_leap_year(year) and month > 0) ++ days;
+    return days;
+  }
+  
+  // number of leap years between given year and 1970
+  // not including given year
+  int leap_years(int year) {
+    return (year-1969)/4 - (year-1901)/100 + (year-1601)/400;
+  }
+  
+}
+
+#endif
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace PSTime {
+namespace TimeUtils {
+
+/// Convert broken time to UTC time (in UTC timezone)
+time_t 
+timegm(struct tm* timeptr)
+{
+#if USE_TIMEGM
+  
+  return ::timegm(timeptr);
+
+#else 
+  
+  // normalize, completely ignore leap seconds
+  while (timeptr->tm_sec > 59) {
+    timeptr->tm_sec -= 60;
+    timeptr->tm_min += 1;
+  }
+  while (timeptr->tm_min > 59) {
+    timeptr->tm_min -= 60;
+    timeptr->tm_hour += 1;
+  }
+  while (timeptr->tm_hour > 23) {
+    timeptr->tm_hour -= 24;
+    timeptr->tm_mday += 1;
+  }
+  while (timeptr->tm_mday > ::days(timeptr->tm_year, timeptr->tm_mon)) {
+    timeptr->tm_mday -= ::days(timeptr->tm_year, timeptr->tm_mon);
+    timeptr->tm_mon += 1;
+  }
+  while (timeptr->tm_mon > 11) {
+    timeptr->tm_mon -= 12;
+    timeptr->tm_year += 1;
+  }
+
+  int year = timeptr->tm_year+1900;
+  int days = (year-1970)*365 + ::leap_years(year);
+  days += timeptr->tm_mon == 0 ? 0 : ::running_days(year, timeptr->tm_mon-1);
+  days += timeptr->tm_mday-1;
+
+  time_t result = days * time_t(24*3600);
+  result += timeptr->tm_hour*3600 + timeptr->tm_min*60 + timeptr->tm_sec;
+  
+  return result;
+
+#endif
+}
+
+
+} // namespace PSTime
+} // namespace TimeUtils
