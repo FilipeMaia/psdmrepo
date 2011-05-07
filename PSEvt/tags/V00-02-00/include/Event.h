c1972bd0a7d328afc19ccbe9dadc9eb766417cfc
salnikov <salnikov@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-05-07 01:48:54 +0000

    Tagging PSEvt with V00-02-00
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1841 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSEvt/tags/V00-02-00/include/Event.h
@@ -0,0 +1,301 @@
+#ifndef PSEVT_EVENT_H
+#define PSEVT_EVENT_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Event.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <list>
+#include <typeinfo>
+#include <boost/shared_ptr.hpp>
+#include <boost/utility.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "PSEvt/EventKey.h"
+#include "PSEvt/Proxy.h"
+#include "PSEvt/DataProxy.h"
+#include "PSEvt/ProxyDictI.h"
+#include "PSEvt/Source.h"
+#include "pdsdata/xtc/Src.hh"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *  @defgroup PSEvt  PSEvt package
+ *  
+ *  @brief PSEvt package contains classes which provide storage and 
+ *  access to event data in the context of psana framework.
+ *  
+ *  The core of the package is the proxy dictionary classes which allow 
+ *  storage of the arbitrary types. Dictionaries to not store data 
+ *  directly, instead they store proxy objects which can either contain
+ *  data objects or implement algorithm to generate data objects when
+ *  necessary.
+ *  
+ *  Main user interface to this package is the Event class which is a 
+ *  wrapper for proxy dictionary providing more user-friendly interface.
+ */
+
+namespace PSEvt {
+
+/**
+ *  @ingroup PSEvt
+ *  
+ *  @brief Class which manages event data in psana framework.
+ *  
+ *  This class is a user-friendly interface to proxy dictionary object. 
+ *  It provides a number of put() and get() methods to store/retrieve 
+ *  arbitrarily typed data.
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see ProxyDictI
+ *
+ *  @version \$Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class Event {
+public:
+
+  /// Special class used for type-less return from get()
+  struct GetResultProxy {
+    
+    /// Convert the result of Event::get() call to smart pointer to data object
+    template<typename T>
+    operator boost::shared_ptr<T>() {
+      boost::shared_ptr<void> vptr = m_dict->get(&typeid(const T), m_source, m_key, m_foundSrc);
+      return boost::static_pointer_cast<T>(vptr);
+    }
+    
+    boost::shared_ptr<ProxyDictI> m_dict; ///< Proxy dictionary containing the data
+    Source m_source;         ///< Data source address
+    std::string m_key;       ///< String key
+    Pds::Src* m_foundSrc;    ///< Pointer to where to store the exact address of found object
+  };
+  
+  
+  /**
+   *  @brief Standard constructor takes proxy dictionary object
+   *  
+   *  @param[in] dict Pointer to proxy dictionary
+   */
+  Event(const boost::shared_ptr<ProxyDictI>& dict) : m_dict(dict) {}
+
+  //Destructor
+  ~Event () {}
+
+  /**
+   *  @brief Add one more proxy object to the event
+   *  
+   *  @param[in] proxy  Proxy object for type T.
+   *  @param[in] key    Optional key to distinguish different objects of the same type.
+   */
+  template <typename T>
+  void putProxy(const boost::shared_ptr<Proxy<T> >& proxy, const std::string& key=std::string()) 
+  {
+    EventKey evKey(&typeid(const T), Pds::Src(), key);
+    m_dict->put(boost::static_pointer_cast<ProxyI>(proxy), evKey);
+  }
+  
+  /**
+   *  @brief Add one more proxy object to the event
+   *  
+   *  @param[in] proxy   Proxy object for type T.
+   *  @param[in] source Source detector address.
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   */
+  template <typename T>
+  void putProxy(const boost::shared_ptr<Proxy<T> >& proxy, 
+                const Pds::Src& source, 
+                const std::string& key=std::string()) 
+  {
+    EventKey evKey(&typeid(const T), source, key);
+    m_dict->put(boost::static_pointer_cast<ProxyI>(proxy), evKey);
+  }
+  
+  /**
+   *  @brief Add one more object to the event
+   *  
+   *  @param[in] data   Object to store in the event.
+   *  @param[in] key    Optional key to distinguish different objects of the same type.
+   */
+  template <typename T>
+  void put(const boost::shared_ptr<T>& data, const std::string& key=std::string()) 
+  {
+    boost::shared_ptr<ProxyI> proxyPtr( new DataProxy<T>(data) );
+    EventKey evKey(&typeid(const T), Pds::Src(), key);
+    m_dict->put(proxyPtr, evKey);
+  }
+  
+  /**
+   *  @brief Add one more object to the event
+   *  
+   *  @param[in] data    Object to store in the event.
+   *  @param[in] source Source detector address.
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   */
+  template <typename T>
+  void put(const boost::shared_ptr<T>& data, 
+           const Pds::Src& source, 
+           const std::string& key=std::string()) 
+  {
+    boost::shared_ptr<ProxyI> proxyPtr( new DataProxy<T>(data) );
+    EventKey evKey(&typeid(const T), source, key);
+    m_dict->put(proxyPtr, evKey);
+  }
+  
+  /**
+   *  @brief Get an object from event
+   *  
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   *  @param[out] foundSrc If pointer is non-zero then pointed object will be assigned 
+   *                       with the exact source address of the returned object.
+   *  @return Shared pointer which can be zero if object not found.
+   */
+  GetResultProxy get(const std::string& key=std::string(), Pds::Src* foundSrc=0)
+  {
+    GetResultProxy pxy = {m_dict, Source(Source::null), key, foundSrc};
+    return pxy;
+  }
+  
+  /**
+   *  @brief Get an object from event
+   *  
+   *  @param[in] source Source detector address.
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   *  @param[out] foundSrc If pointer is non-zero then pointed object will be assigned 
+   *                       with the exact source address of the returned object (must 
+   *                       be the same as source)
+   *  @return Shared pointer which can be zero if object not found.
+   */
+  GetResultProxy get(const Pds::Src& source, const std::string& key=std::string(), Pds::Src* foundSrc=0) 
+  {
+    GetResultProxy pxy = {m_dict, Source(source), key, foundSrc};
+    return pxy;
+  }
+  
+  /**
+   *  @brief Get an object from event
+   *  
+   *  @param[in] source Source detector address.
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   *  @param[out] foundSrc If pointer is non-zero then pointed object will be assigned 
+   *                       with the exact source address of the returned object.
+   *  @return Shared pointer which can be zero if object not found.
+   */
+  GetResultProxy get(const Source& source, const std::string& key=std::string(), Pds::Src* foundSrc=0) 
+  {
+    GetResultProxy pxy = {m_dict, source, key, foundSrc};
+    return pxy;
+  }
+  
+  /**
+   *  @brief Check if object (or proxy) of given type exists in the event
+   *  
+   *  This is optimized version of get() which only checks whether the proxy
+   *  is there but does not ask proxy to do any real work.
+   *  
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   *  @return true if object or proxy exists
+   */
+  template <typename T>
+  bool exists(const std::string& key=std::string()) 
+  {
+    EventKey evKey(&typeid(const T), Pds::Src(), key);
+    return m_dict->exists(evKey);
+  }
+  
+  /**
+   *  @brief Check if object (or proxy) of given type exists in the event
+   *  
+   *  This is optimized version of get() which only checks whether the proxy
+   *  is there but does not ask proxy to do any real work.
+   *  
+   *  @param[in] source Source detector address.
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   *  @return true if object or proxy exists
+   */
+  template <typename T>
+  bool exists(const Pds::Src& source, 
+              const std::string& key=std::string()) 
+  {
+    EventKey evKey(&typeid(const T), source, key);
+    return m_dict->exists(evKey);
+  }
+  
+  /**
+   *  @brief Remove object of given type from the event
+   *  
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   *  @return false if object did not exist before this call
+   */
+  template <typename T>
+  bool remove(const std::string& key=std::string()) 
+  {
+    EventKey evKey(&typeid(const T), Pds::Src(), key);
+    return m_dict->remove(evKey);
+  }
+  
+  /**
+   *  @brief Remove object of given type from the event
+   *  
+   *  @param[in] source Source detector address.
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   *  @return false if object did not exist before this call
+   */
+  template <typename T>
+  bool remove(const Pds::Src& source, 
+              const std::string& key=std::string()) 
+  {
+    EventKey evKey(&typeid(const T), source, key);
+    return m_dict->remove(evKey);
+  }
+  
+  /**
+   *  @brief Get the list of event keys defined in event
+   *  
+   *  @return list of the EventKey objects
+   */
+  std::list<EventKey> keys() const
+  {
+    std::list<EventKey> result;
+    m_dict->keys(result);
+    return result;
+  }
+  
+protected:
+
+private:
+
+  // Data members
+  boost::shared_ptr<ProxyDictI> m_dict;   ///< Proxy dictionary object 
+
+};
+
+} // namespace PSEvt
+
+#endif // PSEVT_EVENT_H
---
+++ PSEvt/tags/V00-02-00/include/ProxyDictI.h
@@ -0,0 +1,202 @@
+#ifndef PSEVT_PROXYDICTI_H
+#define PSEVT_PROXYDICTI_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class ProxyDictI.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <list>
+#include <typeinfo>
+#include <boost/utility.hpp>
+#include <boost/shared_ptr.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "pdsdata/xtc/Src.hh"
+#include "PSEvt/EventKey.h"
+#include "PSEvt/ProxyI.h"
+#include "PSEvt/Source.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSEvt {
+
+/**
+ *  @ingroup PSEvt
+ *  
+ *  @brief Class defining an interface for all proxy dictionary classes.
+ *  
+ *  The client-side interface of this class is non-virtual and it 
+ *  forwards every class to virtual methods which define customization
+ *  oints to be implemented in subclasses.
+ *  
+ *  Proxy dictionary stores proxy objects of type ProxyI which represent
+ *  actual objects and know how to create or retrieve an object when
+ *  requested. Proxy collection is indexed by the EventKey which identifies
+ *  object data type, data source address, and additional string key.
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see ProxyDict
+ *
+ *  @version \$Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class ProxyDictI : boost::noncopyable {
+public:
+
+  // Destructor
+  virtual ~ProxyDictI () {}
+
+  /**
+   *  @brief Add one more proxy object to the dictionary.
+   *  
+   *  By default the request is forwarded to the virtual method  
+   *  (customization point) but there is a possibility to do something 
+   *  else too if needed.
+
+   *  @param[in] proxy   Proxy object for type T.
+   *  @param[in] key     Event key for the data object.
+   */
+  void put( const boost::shared_ptr<ProxyI>& proxy, const EventKey& key ) 
+  {
+    this->putImpl(proxy, key);
+  }
+
+  /**
+   *  @brief Get an object from event
+   * 
+   *  @param[in] typeinfo  Dynamic type info object
+   *  @param[in] source    Source detector address.
+   *  @param[in] key       Optional key to distinguish different objects of the same type.
+   *  @param[out] foundSrc If pointer is non-zero then pointed object will be assigned
+   *                       with the exact source address of the returned object.
+   *  @return Shared pointer of void type.
+   */
+  boost::shared_ptr<void> get( const std::type_info* typeinfo, 
+                               const Source& source, 
+                               const std::string& key,
+                               Pds::Src* foundSrc)
+  {
+    return this->getImpl(typeinfo, source, key, foundSrc);
+  }
+
+
+  /**
+   *  @brief Check if proxy of given type exists in the event
+   *  
+   *  This is optimized version of get() which only checks whether the proxy
+   *  is there but does not ask proxy to do any real work.
+   *  
+   *  @param[in] key     Event key for the data object.
+   *  @return true if proxy exists
+   */
+  bool exists(const EventKey& key)
+  {
+    return this->existsImpl(key);
+  }
+
+  /**
+   *  @brief Remove object of given type from the event
+   *  
+   *  @param[in] key     Event key for the data object.
+   *  @return false if object did not exist before this call
+   */
+  bool remove(const EventKey& key)
+  {
+    return this->removeImpl(key);
+  }
+
+  /**
+   *  @brief Get the list of event keys defined in event
+   *  
+   *  @param[out] keys list of the EventKey objects
+   */
+  void keys(std::list<EventKey>& keys) const
+  {
+    this->keysImpl(keys);
+  }
+
+protected:
+
+  // Default constructor
+  ProxyDictI () {}  
+  
+  
+  /**
+   *  @brief Add one more proxy object to the dictionary.
+   *  
+   *  @param[in] proxy   Proxy object for type T.
+   *  @param[in] key     Event key for the data object.
+   */
+  virtual void putImpl( const boost::shared_ptr<ProxyI>& proxy, const EventKey& key ) = 0;
+
+  /**
+   *  @brief Get an object from event
+   * 
+   *  @param[in] typeinfo  Dynamic type info object
+   *  @param[in] source Source detector address.
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   *  @param[out] foundSrc If pointer is non-zero then pointed object will be assigned
+   *                       with the exact source address of the returned object.
+   *  @return Shared pointer of void type.
+   */
+  virtual boost::shared_ptr<void> getImpl( const std::type_info* typeinfo, 
+                                           const Source& source, 
+                                           const std::string& key,
+                                           Pds::Src* foundSrc ) = 0;
+
+  /**
+   *  @brief Check if proxy of given type exists in the event
+   *  
+   *  @param[in] key     Event key for the data object.
+   *  @return true if proxy exists
+   */
+  virtual bool existsImpl(const EventKey& key) = 0;
+
+  /**
+   *  @brief Remove object of given type from the event
+   *  
+   *  @param[in] key     Event key for the data object.
+   *  @return false if object did not exist before this call
+   */
+  virtual bool removeImpl(const EventKey& key) = 0;
+
+  /**
+   *  @brief Get the list of event keys defined in event
+   *  
+   *  @param[out] keys list of the EventKey objects
+   */
+  virtual void keysImpl(std::list<EventKey>& keys) const = 0;
+
+private:
+
+
+};
+
+} // namespace PSEvt
+
+#endif // PSEVT_PROXYDICTI_H
