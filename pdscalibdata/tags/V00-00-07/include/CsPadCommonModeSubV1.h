63df2002774dbde4bc6868bffc57fe7ff07fd841
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-05-23 07:20:58 +0000

    Tagging pdscalibdata with V00-00-07
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1930 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pdscalibdata/tags/V00-00-07/include/CsPadCommonModeSubV1.h
@@ -0,0 +1,112 @@
+#ifndef PDSCALIBDATA_CSPADCOMMONMODESUBV1_H
+#define PDSCALIBDATA_CSPADCOMMONMODESUBV1_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class CsPadCommonModeSubV1.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "pdsdata/cspad/Detector.hh"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace pdscalibdata {
+
+/**
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class CsPadCommonModeSubV1  {
+public:
+
+  enum CommonMode { 
+    None = 0,
+    Default = 1
+  };
+  
+  enum { DataSize = 16 };
+
+  // constant for unknown common mode
+  enum { UnknownCM = -10000 };
+  
+  /// Default constructor, all pixel codes set to 0
+  CsPadCommonModeSubV1 () ;
+  
+  /**
+   *  Read constant from file.
+   *  
+   *  @throw std::exception
+   */
+  CsPadCommonModeSubV1 (const std::string& fname) ;
+
+  // Destructor
+  ~CsPadCommonModeSubV1 () ;
+
+  // access common mode
+  CommonMode mode() const { return CommonMode(m_mode); }
+
+  // access common mode data
+  const double* data() const { return m_data; }
+
+  /**
+   *  Find common mode for an CsPad  section.
+   *  
+   *  Function will return UnknownCM value if the calculation 
+   *  cannot be performed (or need not be performed).
+   *  
+   *  @param sdata  pixel data
+   *  @param peddata  pedestal data, can be zero pointer
+   *  @param pixStatus  pixel status data, can be zero pointer
+   *  @param ssize  size of all above arrays
+   */ 
+  float findCommonMode(const uint16_t* sdata, 
+                       const float* peddata, 
+                       const  uint16_t *pixStatus, 
+                       unsigned ssize) const;
+  
+protected:
+
+private:
+
+  // Data members  
+  uint32_t m_mode;
+  double m_data[DataSize];
+
+  // Copy constructor and assignment are disabled by default
+  CsPadCommonModeSubV1 ( const CsPadCommonModeSubV1& ) ;
+  CsPadCommonModeSubV1& operator = ( const CsPadCommonModeSubV1& ) ;
+};
+
+} // namespace pdscalibdata
+
+#endif // PDSCALIBDATA_CSPADCOMMONMODESUBV1_H
