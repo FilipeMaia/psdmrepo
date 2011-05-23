6590f99ab4ccffa1501f713cd9db14d615abc21d
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-05-23 07:50:54 +0000

    Tagging pypdsdata with V00-00-29
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1938 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-29/pyext/types/evr/ConfigV2.h
@@ -0,0 +1,62 @@
+#ifndef PYPDSDATA_EVRDATA_CONFIGV2_H
+#define PYPDSDATA_EVRDATA_CONFIGV2_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class EvrData_ConfigV2.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "../PdsDataType.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+#include "pdsdata/evr/ConfigV2.hh"
+
+//    ---------------------
+//    -- Class Interface --
+//    ---------------------
+
+namespace pypdsdata {
+namespace EvrData {
+
+/**
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class ConfigV2 : public PdsDataType<ConfigV2,Pds::EvrData::ConfigV2> {
+public:
+
+  typedef PdsDataType<ConfigV2,Pds::EvrData::ConfigV2> BaseType;
+
+  /// Initialize Python type and register it in a module
+  static void initType( PyObject* module );
+
+};
+
+} // namespace EvrData
+} // namespace pypdsdata
+
+#endif // PYPDSDATA_EVRDATA_CONFIGV2_H
