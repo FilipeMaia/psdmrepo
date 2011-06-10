b93b0281d155c51435a3a1d29e771e5bf3d7ac5d
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-06-10 23:31:15 +0000

    Tagging AuthDB with V00-00-18
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1968 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ AuthDB/tags/V00-00-18/web/html/LDAPManagers.php
@@ -0,0 +1,243 @@
+<?php
+
+require_once( 'authdb/authdb.inc.php' );
+require_once( 'logbook/logbook.inc.php' );
+require_once( 'regdb/regdb.inc.php' );
+
+use AuthDB\AuthDB;
+use AuthDB\AuthDBException;
+
+use LogBook\LogBook;
+use LogBook\LogBookException;
+
+use RegDB\RegDB;
+use RegDB\RegDBException;
+
+/** Harvest the optional parameters first.
+ */
+if( isset( $_GET['instr_name'] )) {
+	$instr_name = strtoupper( trim( $_GET['instr_name'] ));
+	if( $instr_name == '' ) die( "<b>error:</b> instrument name parameter if present can't have an empty value" );
+}
+$fix = isset( $_GET['fix'] );
+
+try {
+	$authdb = AuthDB::instance();
+	$authdb->begin();
+
+	if( $fix && !$authdb->canEdit()) die( "<b>error:</b> your account doesn't posseses sufficient privileges to perform the operaton" );
+
+	$logbook = new LogBook();
+	$logbook->begin();
+
+	$instrument_names = array();
+	if( isset( $instr_name )) {
+		if( is_null( $logbook->regdb()->find_instrument_by_name( $instr_name ))) die( "<b>error:</b> the specified instrument isn't known" );
+		array_push( $instrument_names, $instr_name  );
+	} else {
+
+		// Get all known instruments. Skip  pseudo-instruments.
+		//
+		foreach( $logbook->regdb()->instrument_names() as $name ) {
+			$instrument = $logbook->regdb()->find_instrument_by_name($name);
+			if( $instrument->is_location()) continue;
+			array_push( $instrument_names, $name  );
+		}
+	}
+
+	$extra_operations = <<<HERE
+  <h2>Extra Operations</h2>
+  <div style="padding-left:20px;">
+	<ul>
+      <li><a href="LDAPManagers.php">check</a> or <a href="LDAPManagers.php?fix">fix</a> authorizations status of all instruments</li>
+HERE;
+	foreach( $instrument_names as $name ) {
+		$extra_operations .= <<<HERE
+      <li><a href="LDAPManagers.php?instr_name={$name}">check</a> or <a href="LDAPManagers.php?instr_name={$name}&fix">fix</a> authorizations status for instrument {$name}</li>
+HERE;
+	}
+	$extra_operations .= <<<HERE
+    </ul>
+  </div>
+HERE;
+
+?>
+
+<!DOCTYPE html"> 
+<html>
+<head>
+
+<title>Authorize PIs to manage LDAP Groups of their Experiments</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+
+<style type="text/css">
+
+td.table_hdr {
+  background-color:#d0d0d0;
+  padding: 2px 8px 2px 8px;
+  border: solid 1px #000000;
+  border-top: none;
+  border-left: none;
+  font-family: Arial, sans-serif;
+  font-weight: bold;
+  font-size: 75%;
+}
+td.table_cell {
+  border:solid 1px #d0d0d0;
+  border-top: none;
+  border-left: none;
+  padding: 4px 8px 4px 8px;
+  font-family: Arial, sans-serif;
+  font-size: 75%;
+}
+td.table_cell_left {
+  font-weight: bold;
+}
+td.table_cell_right {
+  border-right: none;
+}
+td.table_cell_bottom {
+  border-bottom: none;
+}
+td.table_cell_within_group {
+  border-bottom: none;
+}
+</style>
+</head>
+
+<body>
+
+<div style="padding-left:20px; padding-right:20px;">
+
+  <h2>About</h2>
+  <div style="padding-left:20px;">
+    <p>This script will check if experiment PIs are authorized as 'Admin's of POSIX groups
+       of their experiments, and if the optional parameter <b>fix</b> is present then assign
+       the role to the PIs. Specifically, for each experiment, the tool will:</p>
+    <ol>
+      <li>find a POSIX group associated with the experiment</li>
+      <li>get a PI account associated with the experiment (normally this is a shared account assigned to the real PI's name)</li>
+      <li>if the account has the same name as the experiment then look for another UNIX account whose owner matches the owner's name of the shared account</li>
+      <li>if one (and only one!) such account is found then check if the group is already managed by that account</li>
+      <li>(optionally) if not then authorize the account as 'Admin' of the experiment's group</li>
+      <li>(optionally) report which changes which had to be made at step #5</li></ol>
+    <p>The script has two optional parameters:</p>
+    <ul>
+      <li><b>instr_name='INSTRUMENT'</b> - limit a scope of the operation to the specified instrument</li>
+      <li><b>fix</b> - proceed with the actual fix and authorize the groups. By default</li></ul>
+  </div>
+  <?php echo $extra_operations; ?>
+  <h2>Authorizations</h2>
+  <div style="padding-left:20px;">
+
+<?php
+
+	$experiments_by_names = array();
+	foreach( $logbook->experiments() as $e ) $experiments_by_names[$e->name()] = $e;
+
+	$application = 'LDAP';
+	$role = 'Admin';
+	$header =
+		'<tr>'.
+		'<td class="table_hdr">Instrument</td>'.
+		'<td class="table_hdr">Id</td>'.
+		'<td class="table_hdr">Experiment</td>'.
+		'<td class="table_hdr">Group</td>'.
+		'<td class="table_hdr">PI</td>'.
+		'<td class="table_hdr">LDAP Role</td>'.
+		'<td class="table_hdr">Is Authorized</td>'.
+		'<td class="table_hdr">Current authorizations</td>'.
+	($fix ? '<td class="table_hdr">Action</td>' : '').
+	'</tr>';
+	print '<table><tbody>';
+	foreach( $instrument_names as $instr_name ) {
+
+		print $header;
+
+		$experiments = $logbook->experiments_for_instrument( $instr_name );
+		foreach( $experiments as $experiment ) {
+
+			// Get the real UNIX account of the PI if the one has such account.
+			// A problem is that at a time when we're reegistering new experiments many PIs
+			// may still not have personal UNIX accounts at SLAC, so instead we're using
+			// shared accounts of their experiments. The simple algorithm below will try
+			// to guess the real PIs by obtaining the owner's name of the shared account
+			// and using that information to search for some other account. And if that would
+			// result in EXACTLY two accounts (the first one is the shared account) then we'll
+			// asssume the second one as the real account.
+			// 
+			$real_pi_uid = $experiment->leader_account();
+			if( $real_pi_uid == $experiment->name()) {
+				$account = $logbook->regdb()->find_user_account( $real_pi_uid );
+				$accounts = $logbook->regdb()->find_user_accounts( $account['gecos'], array('uis' => False, 'gecos' => True ));
+
+				// Eliminate all accounts matching the names of experiment. We'll succeed only
+				// if we'll end up with one and only one account.
+				//
+				$nonshared_accounts = array();
+				foreach( $accounts as $account )
+					if( !array_key_exists( $account['uid'], $experiments_by_names ))
+						array_push( $nonshared_accounts, $account );
+
+				if( count( $nonshared_accounts ) == 1 )
+					foreach( $nonshared_accounts as $account )
+						if( $account['uid'] != $real_pi_uid ) {
+							$real_pi_uid = $account['uid'];
+							break;
+						}
+
+			}
+			$has_role = $authdb->hasRole( $real_pi_uid, $experiment->id(), $application, $role ) || $authdb->hasRole( $real_pi_uid, null, $application, $role );
+			print
+				'<tr>'.
+				'<td class="table_cell table_cell_left">'.$instr_name.'</td>'.
+				'<td class="table_cell">'.$experiment->id().'</td>'.
+			    '<td class="table_cell"><a href="../portal/?exper_id='.$experiment->id().'">'.$experiment->name().'</a></td>'.
+				'<td class="table_cell"><a href="../authdb/?action=view_group&gid='.$experiment->POSIX_gid().'">'.$experiment->POSIX_gid().'</a></td>'.
+				'<td class="table_cell"><a href="../authdb/?action=view_account&uid='.$real_pi_uid.'">'.$real_pi_uid.'</a></td>'.
+				'<td class="table_cell">'.$role.'</td>';
+			if( $fix ) {
+				if( !$has_role  ) {
+					if( $real_pi_uid != $experiment->name()) {
+						//$authdb->createRolePlayer( $application, $role, $experiment->id(), $group4auth );
+						print
+							'<td class="table_cell"><span style="color:red; font-weight:bold;">No</span></td>'.
+							'<td class="table_cell"></td>'.
+							'<td class="table_cell table_cell_right"><span style="color:red; font-weight:bold;">fix is not implemented yet</span></td>';
+					} else {
+						print
+							'<td class="table_cell"><span style="color:red; font-weight:bold;">No</span></td>'.
+							'<td class="table_cell"></td>'.
+							'<td class="table_cell table_cell_right"></td>';
+						}
+				} else {
+					print
+						'<td class="table_cell">Yes</td>'.
+						'<td class="table_cell"></td>'.
+						'<td class="table_cell table_cell_right"></td>';
+				}
+			} else {
+				print
+					'<td class="table_cell">'.($has_role ? 'Yes' : '<span style="color:red; font-weight:bold;">No</span>').'</td>'.
+					'<td class="table_cell table_cell_right"></td>';
+			}
+			print
+				'</tr>';
+		}
+	}
+	print '</tbody><table>';
+	
+	$logbook->commit();
+	$authdb->commit();
+	
+} catch( AuthDBException  $e ) { print $e->toHtml(); }
+  catch( LogBookException $e ) { print $e->toHtml(); }
+  catch( RegDBException   $e ) { print $e->toHtml(); }
+  
+?>
+
+  </div>
+</div>
+
+</body>
+</html>
---
+++ AuthDB/tags/V00-00-18/web/html/ProcessDeleteApplication.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once( 'authdb/authdb.inc.php' );
+require_once( 'regdb/regdb.inc.php' );
+
+use AuthDB\AuthDB;
+use AuthDB\AuthDBException;
+
+use RegDB\RegDBAuth;
+
+/*
+ * This script will process a request for deleting an existing application
+ * from the database.
+ */
+// TODO: This needs to be changed with the real test
+//
+//if( !RegDBAuth::isAuthenticated()) return;
+
+if( isset( $_POST['name'] )) {
+    $name = trim( $_POST['name'] );
+    if( $name == '' )
+        die( "application name can't be empty" );
+} else
+    die( "no valid application name prtovided" );
+
+if( isset( $_POST['actionSuccess'] )) {
+    $actionSuccess = trim( $_POST['actionSuccess'] );
+}
+
+/* Proceed with the operation
+ */
+try {
+    $authdb = new AuthDB();
+    $authdb->begin();
+
+    $authdb->deleteApplication( $name );
+
+    if( isset( $actionSuccess )) {
+        if( $actionSuccess == 'home' )
+            header( 'Location: index.php' );
+        else if( $actionSuccess == 'list_roles' )
+            header( 'Location: index.php?action=list_roles' );
+        else
+            ;
+    }
+    $authdb->commit();
+
+} catch( AuthDBException $e ) {
+    print $e->toHtml();
+}
+?>
