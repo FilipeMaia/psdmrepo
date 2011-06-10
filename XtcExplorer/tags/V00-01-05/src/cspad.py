b9571a0ca34f3d646958d3931d5815a61636d638
ofte@SLAC.STANFORD.EDU <ofte@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-06-10 23:37:59 +0000

    Tagging XtcExplorer with V00-01-05
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@1971 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ XtcExplorer/tags/V00-01-05/src/cspad.py
@@ -0,0 +1,151 @@
+import numpy as np
+
+class CsPad( object ):
+
+    npix_quad = 850
+    
+    # origin of section in quad coordinate system
+    #
+    # x-position correspond to column number
+    xpos_sec2x1 = [[ 414,  626,    0,    0,  213,    1,  418,  419],  # 2:5 were not measured
+                   [ 421,  634,    0,    0,  213,    1,  424,  425],
+                   [ 417,  630,    0,    1,  212,    0,  425,  426],
+                   [ 416,  630,    0,    0,  213,    1,  420,  421]] # 2:5 were not measured
+    # y-position correspond to maxrows - row number    
+    ypos_sec2x1 = [[   0,    0,  214,    1,  425,  425,  615,  402],  # 2:5 were not measured
+                   [   0,    0,  214,    1,  425,  425,  615,  402],
+                   [   0,    0,  215,    3,  431,  431,  616,  403],
+                   [   0,    0,  214,    1,  425,  425,  615,  403]] # 2:5 were not measured
+    
+
+    def __init__(self, config):
+        quads = range(4)
+        self.sections = map(config.sections, quads)
+        pass
+
+    def CsPadElement( self, data3d, qn ):
+        # Construct one image for each quadrant, each with 8 sections
+        # from a data3d = 3 x 2*194 x 185 data array
+        #   +---+---+-------+
+        #   |   |   |   6   |
+        #   + 5 | 4 +-------+
+        #   |   |   |   7   |
+        #   +---+---+---+---+
+        #   |   2   |   |   |
+        #   +-------+ 0 | 1 |
+        #   |   3   |   |   |
+        #   +-------+---+---+
+
+        # min and max
+        #print "CsPad (min,max) for quad %d: (%d,%d)" % (qn,np.min(data3d),np.max(data3d))
+
+
+        # if any sections are missing, insert zeros
+        if len( data3d ) < 8 :
+            zsec = np.zeros( (185,388), dtype=data3d.dtype)
+            #zsec = zsec * -99
+            for i in range (8) :
+                if i not in self.sections[qn] :
+                    data3d = np.insert( data3d, i, zsec, axis=0 )
+
+        pairs = []
+        for i in range (8) :
+        
+            # insert gap between asics in the 2x1
+            asics = np.hsplit( data3d[i], 2)
+            gap = np.zeros( (185,4), dtype=data3d.dtype )
+            #gap = gap * -99
+            pair = np.hstack( (asics[0], gap, asics[1]) )
+
+                
+            # sections 2,3 and 6,7 are as is. The others need some rotation:
+            if i==0 or i==1 :
+                pair = pair[:,::-1].T
+            if i==4 or i==5 :
+                pair = pair[::-1,:].T
+
+            pairs.append( pair )
+
+
+        # make the array for this quadrant
+        quadrant = np.zeros( (self.npix_quad, self.npix_quad), dtype=data3d.dtype )
+        #quadrant = quadrant * -99
+
+        # insert the 2x1 sections according to
+        for sec in range (8):
+            nrows, ncols = pairs[sec].shape
+
+            # x,y  in quadrant coordinate system
+            xpos = self.xpos_sec2x1[qn][sec]
+            ypos = self.ypos_sec2x1[qn][sec]
+            colp = xpos
+            rowp = self.npix_quad-ypos
+
+            quadrant[rowp-nrows:rowp, colp:colp+ncols] = pairs[sec][0:nrows,0:ncols]
+
+
+        # Finally, rotate the quadrant as needed
+        if qn>0 : quadrant = np.rot90( quadrant, 4-qn)
+        return quadrant
+
+
+
+    def CsPadElementUnaligned( self, data3d, qn ):
+        # Construct one image for each quadrant, each with 8 sections
+        # from a data3d = 3 x 2*194 x 185 data array
+        #   +---+---+-------+
+        #   |   |   |   6   |
+        #   + 5 | 4 +-------+
+        #   |   |   |   7   |
+        #   +---+---+---+---+
+        #   |   2   |   |   |
+        #   +-------+ 0 | 1 |
+        #   |   3   |   |   |
+        #   +-------+---+---+
+
+        zeros = np.zeros((18,388),dtype=data3d.dtype)
+        zeros9 = np.zeros((9,388),dtype=data3d.dtype)
+        zeros6 = np.zeros((6,388),dtype=data3d.dtype)
+
+        # if any sections are missing, insert zeros
+        if len( data3d ) < 8 :
+            zsec = np.zeros( (185,388), dtype=data3d.dtype)
+            for i in range (8) :
+                if i not in self.sections[qn] :
+                    data3d = np.insert( data3d, i, zsec, axis=0 )
+                #print "section ", i
+                #print data3d[i]
+
+
+        s01 = np.concatenate( (zeros6.T,
+                               data3d[0][:,::-1].T,
+                               zeros6.T,
+                               data3d[1][:,::-1].T,
+                               zeros6.T),
+                              1)
+        s23 = np.concatenate( (zeros6,
+                               data3d[2], 
+                               zeros6,
+                               data3d[3],
+                               zeros6 ),
+                              0 )
+        s45 = np.concatenate( (zeros6.T,
+                               data3d[5][::-1,:].T,
+                               zeros6.T,
+                               data3d[4][::-1,:].T,
+                               zeros6.T), 
+                              1 )
+        s67 = np.concatenate( (zeros6,
+                               data3d[6], 
+                               zeros6,
+                               data3d[7],
+                               zeros6 ),
+                              0 )
+
+        m1 = np.hstack( (s23, s01) )
+        m2 = np.hstack( (s45, s67) )
+        e0 = np.vstack( (m2, m1) )
+
+        if qn>0 : e0 = np.rot90( e0, 4-qn)
+        return e0
+
---
+++ XtcExplorer/tags/V00-01-05/src/pyana_waveform.py
@@ -0,0 +1,317 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id: template!pyana-module!py 1095 2010-07-07 23:01:23Z salnikov $
+#
+# Description:
+#  Pyana user analysis module pyana_waveform...
+#
+#------------------------------------------------------------------------
+
+"""User analysis module for pyana framework.
+
+This software was developed for the LCLS project.  If you use all or 
+part of it, please give an appropriate acknowledgment.
+
+@see RelatedModule
+
+@version $Id: template!pyana-module!py 1095 2010-07-07 23:01:23Z salnikov $
+
+@author Ingrid Ofte
+"""
+
+#------------------------------
+#  Module's version from SVN --
+#------------------------------
+__version__ = "$Revision: 1095 $"
+# $Source$
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import sys
+import logging
+import time
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pypdsdata import xtc
+
+from utilities import PyanaOptions
+from utilities import WaveformData
+
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+# local definitions usually start with _
+
+#---------------------
+#  Class definition --
+#---------------------
+class pyana_waveform (object) :
+    """Class whose instance will be used as a user analysis module. """
+
+    #----------------
+    #  Constructor --
+    #----------------
+    def __init__ ( self, 
+                   sources = None,
+                   plot_every_n = None,
+                   accumulate_n = "0",
+                   fignum = "1" ) :
+        """Class constructor. The parameters to the constructor are passed
+        from pyana configuration file. If parameters do not have default 
+        values  here then the must be defined in pyana.cfg. All parameters 
+        are passed as strings, convert to correct type before use.
+
+        @param sources         List of DetInfo addresses of Acqiris type
+        @param plot_every_n    Frequency of plot updates
+        @param accumulate_n    Accumulate all or reset the array every n shots
+        @param fignum          Figure number for matplotlib
+        """
+
+        # initialize data
+        opt = PyanaOptions()
+        self.sources = opt.getOptStrings( sources )
+        self.plot_every_n = opt.getOptInteger( plot_every_n )
+        self.accumulate_n = opt.getOptInteger( accumulate_n )
+        self.mpl_num = opt.getOptInteger( fignum )
+
+        # other
+        self.n_shots = None
+        self.accu_start = None
+
+
+    def initlists(self):
+        # containers to store data from this job
+        self.ctr = {} # source-specific event counter 
+        self.ts = {} # time waveform
+        self.wf = {} # voltage waveform
+        self.wf2 = {} # waveform squared (for computation of RMS)
+        for label in self.src_ch :
+            self.ctr[label] = 0
+            self.ts[label] = None
+            self.wf[label] = None
+            self.wf2[label] = None
+                
+    def resetlists(self):
+        self.accu_start = self.n_shots
+        for label in self.src_ch :
+            self.ctr[label] = 0
+            del self.wf[label] 
+            del self.wf2[label]
+            self.wf[label] = None
+            self.wf2[label] = None
+
+    #-------------------
+    #  Public methods --
+    #-------------------
+
+    def beginjob( self, evt, env ) :
+        """This method is called once at the beginning of the job.
+        It will also be called in any Xtc Configure transition, if 
+        configurations have changed since last it was run.
+
+        @param evt    event data object
+        @param env    environment object
+        """
+
+        # Preferred way to log information is via logging package
+        logging.info( "pyana_waveform.beginjob() called " )
+
+        self.n_shots = 0 # total
+        self.accu_start = 0
+
+        # list of channels and source (names)
+        self.cfg = {} # configuration object
+        self.src_ch = []
+
+        self.data = {}
+        for source in self.sources:
+            print source
+
+            cfg = env.getAcqConfig(source)
+            self.cfg[source] = { 'nCh' : cfg.nbrChannels(),
+                                 'nSamp' : cfg.horiz().nbrSamples(),
+                                 'smpInt' : cfg.horiz().sampInterval() }
+            
+            nch = cfg.nbrChannels()
+            for i in range (0, nch ):
+                label =  "%s Ch%s" % (source,i) 
+                self.src_ch.append(label)                    
+
+                self.data[label] = WaveformData( label )
+                
+        # lists to fill numpy arrays
+        self.initlists()
+
+
+
+    def beginrun( self, evt, env ) :
+        """This optional method is called if present at the beginning 
+        of the new run.
+
+        @param evt    event data object
+        @param env    environment object
+        """
+
+        logging.info( "pyana_waveform.beginrun() called ")
+
+                
+    def begincalibcycle( self, evt, env ) :
+        """This optional method is called if present at the beginning 
+        of the new calibration cycle.
+
+        @param evt    event data object
+        @param env    environment object
+        """
+        logging.info( "pyana_waveform.begincalibcycle() called ")
+            
+    def event( self, evt, env ) :
+        """This method is called for every L1Accept transition.
+
+        @param evt    event data object
+        @param env    environment object
+        """
+        logging.info( "pyana_waveform.event() called ")
+        self.n_shots+=1
+        
+        if evt.get('skip_event') :
+            return
+
+        for label in self.src_ch :
+
+            parts = label.split(' Ch')
+            source = parts[0]
+            channel = int(parts[1])
+            
+            acqData = evt.getAcqValue( source, channel, env)
+            if acqData :
+
+                if self.ts[label] is None:
+                    self.ts[label] = acqData.timestamps()
+                        
+                # average waveform
+                awf = acqData.waveform()
+
+                if self.wf[label] is None :
+                    self.ctr[label] = 1
+                    self.wf[label] = awf
+                    self.wf2[label] = (awf*awf)
+                else :
+                    self.ctr[label] += 1
+                    self.wf[label] += awf
+                    self.wf[label] += (awf*awf)
+
+                            
+        if self.plot_every_n != 0 and (self.n_shots%self.plot_every_n)==0 :
+            self.make_plots()
+
+            data_waveform = []
+            for label in self.src_ch :
+                data_waveform.append( self.data[label] )
+            evt.put( data_waveform, 'data_waveform' )
+                
+        if  ( self.accumulate_n != 0 and (self.n_shots%self.accumulate_n)==0 ):
+            self.resetlists()
+
+                    
+    def endcalibcycle( self, env ) :
+        """This optional method is called if present at the end of the 
+        calibration cycle.
+        
+        @param env    environment object
+        """
+        
+        logging.info( "pyana_waveform.endcalibcycle() called" )
+
+    def endrun( self, env ) :
+        """This optional method is called if present at the end of the run.
+        
+        @param env    environment object
+        """
+        logging.info( "pyana_waveform.endrun() called" )
+
+    def endjob( self, evt, env ) :
+        """This method is called at the end of the job. It should do 
+        final cleanup, e.g. close all open files.
+        
+        @param env    environment object
+        """
+        
+        logging.info( "pyana_waveform.endjob() called" )
+
+        self.make_plots()
+        wfdata = []
+        for label in self.src_ch :
+            wfdata.append( self.data[label] )
+        evt.put( wfdata, 'data_waveform' )
+
+    def make_plots(self):
+
+        nplots = len(self.src_ch)
+        ncols = 1
+        nrows = 1
+        if nplots == 2: ncols = 2
+        if nplots == 3: ncols = 3
+        if nplots == 4: ncols = 2; nrows = 2
+        if nplots > 4:
+            ncols = 3
+            nrows = nplots / 3
+            if nplots%3 > 0 : nrows += 1
+            
+        height=4.2
+        if (nrows * height) > 14 : height = 14/nrows
+        width=height*1.3
+        
+        fig = plt.figure(num=self.mpl_num, figsize=(width*ncols,height*nrows) )
+        fig.clf()
+        fig.suptitle("Average waveform of shots %d-%d" % (self.accu_start,self.n_shots))
+
+        if nplots > 1 :
+            fig.subplots_adjust(wspace=0.45, hspace=0.45)
+
+        pos = 1
+        for source in self.src_ch :
+            print "plotting %d events from source %s" % (self.ctr[source],source)
+            
+            self.wf_avg = self.wf[source] / self.ctr[source]
+            self.wf2_avg = self.wf2[source] / self.ctr[source]
+            self.wf_rms = np.sqrt( self.wf2_avg - self.wf_avg*self.wf_avg ) / np.sqrt(self.ctr[source])
+
+            dim1 = np.shape(self.wf_avg)
+            dim2 = np.shape(self.wf_rms)
+            dim3 = np.shape(self.ts[source])
+
+            ts_axis = self.ts[source]
+            if dim3 != dim1 :
+                ts_axis = self.ts[source][0:dim1[0]]
+
+            ax = fig.add_subplot(nrows,ncols,pos)
+            pos+=1 
+
+            # scientific notation for time axis
+            plt.ticklabel_format(style='sci',axis='x',scilimits=(0,0))
+
+            unit = ts_axis[1]-ts_axis[0]
+            plt.xlim(ts_axis[0]-unit,ts_axis[-1]+unit)
+                
+            plt.errorbar(ts_axis, self.wf_avg,yerr=self.wf_rms, mew=0.0)
+            plt.title(source)
+            plt.xlabel("time   [s]")
+            plt.ylabel("voltage   [V]")
+
+            self.data[source].wf_voltage = self.wf_avg
+            self.data[source].wf_time = self.ts[source]
+            
+
+        plt.draw()
+
+        print "shot#%d: wf avg histogram plotted for %d sources" % (self.n_shots,nplots)
+        
+
