1afd64f553cac7185fec988e418c3db94f7e7f4f
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-07-25 16:45:55 +0000

    Work in progress. Save all changes.
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2139 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSCalib/trunk/include/CSPadCalibPars.h
@@ -0,0 +1,189 @@
+#ifndef PSCALIB_CSPADCALIBPARS_H
+#define PSCALIB_CSPADCALIBPARS_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class CSPadCalibPars.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>  // open, close etc.
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "psddl_psana/cspad.ddl.h"
+
+#include "pdscalibdata/CalibParsCenterV1.h"      
+#include "pdscalibdata/CalibParsCenterCorrV1.h"  
+#include "pdscalibdata/CalibParsMargGapShiftV1.h"
+#include "pdscalibdata/CalibParsOffsetV1.h"      
+#include "pdscalibdata/CalibParsOffsetCorrV1.h"  
+#include "pdscalibdata/CalibParsRotationV1.h"    
+#include "pdscalibdata/CalibParsTiltV1.h"        
+#include "pdscalibdata/CalibParsQuadRotationV1.h"
+#include "pdscalibdata/CalibParsQuadTiltV1.h"    
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSCalib {
+
+/**
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Mikhail S. Dubrovin
+ */
+
+//enum { NQuad = Psana::CsPad::MaxQuadsPerSensor};
+//enum { NSect = Psana::CsPad::SectorsPerQuad};
+
+//----------------
+
+class CSPadCalibPars  {
+public:
+
+  // Default constructor
+  CSPadCalibPars () {}
+
+  // Test constructor
+  CSPadCalibPars ( const std::string &xtc_file_name ) ;
+
+  // Regular constructor
+  CSPadCalibPars ( const std::string&   calibDir,           //  /reg/d/psdm/cxi/cxi35711/calib
+                   const std::string&   typeGroupName,      //  CsPad::CalibV1
+                   const std::string&   source,             //  CxiDs1.0:Cspad.0
+                   const unsigned long& runNumber ) ;       //  10
+
+  // Destructor
+  virtual ~CSPadCalibPars () ;
+
+  size_t   getNRows             (){ return m_nrows;   };
+  size_t   getNCols             (){ return m_ncols;   };
+
+  void fillCalibNameVector();
+  void getCalibFileName   ();
+  void loadCalibPars      ();
+  void openCalibFile      ();
+  void closeCalibFile     ();
+  void readCalibPars      ();
+  void printCalibPars     ();
+
+  void fillCalibParsV1();
+
+  float getCenterX(size_t quad, size_t sect){ return m_center -> getCenterX(quad,sect); };
+  float getCenterY(size_t quad, size_t sect){ return m_center -> getCenterY(quad,sect); };
+  float getCenterZ(size_t quad, size_t sect){ return m_center -> getCenterZ(quad,sect); };
+
+  float getCenterCorrX(size_t quad, size_t sect){ return m_center_corr -> getCenterCorrX(quad,sect); };
+  float getCenterCorrY(size_t quad, size_t sect){ return m_center_corr -> getCenterCorrY(quad,sect); };
+  float getCenterCorrZ(size_t quad, size_t sect){ return m_center_corr -> getCenterCorrZ(quad,sect); };
+
+  float getQuadMargX () { return m_marg_gap_shift -> getQuadMargX ();};
+  float getQuadMargY () { return m_marg_gap_shift -> getQuadMargY ();};
+  float getQuadMargZ () { return m_marg_gap_shift -> getQuadMargZ ();};
+
+  float getMargX () { return m_marg_gap_shift -> getMargX ();};
+  float getMargY () { return m_marg_gap_shift -> getMargY ();};
+  float getMargZ () { return m_marg_gap_shift -> getMargZ ();};
+					                 
+  float getGapX  () { return m_marg_gap_shift -> getGapX  ();};
+  float getGapY  () { return m_marg_gap_shift -> getGapY  ();};
+  float getGapZ  () { return m_marg_gap_shift -> getGapZ  ();};
+					                 
+  float getShiftX() { return m_marg_gap_shift -> getShiftX(); };
+  float getShiftY() { return m_marg_gap_shift -> getShiftY(); };
+  float getShiftZ() { return m_marg_gap_shift -> getShiftZ(); };
+
+  float getOffsetX(size_t quad) { return m_offset -> getOffsetX(quad); };
+  float getOffsetY(size_t quad) { return m_offset -> getOffsetY(quad); };
+  float getOffsetZ(size_t quad) { return m_offset -> getOffsetZ(quad); };
+
+  float getOffsetCorrX(size_t quad) { return m_offset_corr -> getOffsetCorrX(quad); };
+  float getOffsetCorrY(size_t quad) { return m_offset_corr -> getOffsetCorrY(quad); };
+  float getOffsetCorrZ(size_t quad) { return m_offset_corr -> getOffsetCorrZ(quad); };
+
+  float getRotation(size_t quad, size_t sect) { return m_rotation -> getRotation(quad,sect); };
+  float getTilt    (size_t quad, size_t sect) { return m_tilt     -> getTilt    (quad,sect); };
+
+  float getQuadRotation(size_t quad) { return m_quad_rotation -> getQuadRotation(quad); };
+  float getQuadTilt    (size_t quad) { return m_quad_tilt     -> getQuadTilt    (quad); };
+
+
+private:
+
+  // Copy constructor and assignment are disabled by default
+  CSPadCalibPars ( const CSPadCalibPars& ) ;
+  CSPadCalibPars operator = ( const CSPadCalibPars& ) ;
+
+//------------------
+// Static Members --
+//------------------
+
+  // Data members for TEST constructor       
+  //std::string m_expdir;       // /reg/d/psdm/CXI/cxi35711 
+  //std::string m_calibtype;    // CsPad::CalibV1 
+  //std::string m_calibsrc;     // CxiDs1.0:Cspad.0 
+  std::string m_calibdir;       // /reg/neh/home/dubrovin/LCLS/CSPadAlignment-v01/calib-cxi35711-r0009-det
+  std::string m_calibfilename;  // 1-20.data
+
+
+  // Data members for regular constructor // /reg/d/psdm/CXI/cxi35711/calib/CsPad::CalibV1/CxiDs1.0:Cspad.0/pedestals/1-20.data
+  std::string   m_calibDir;
+  std::string   m_typeGroupName;
+  std::string   m_source;
+  std::string   m_dataType;
+  unsigned long m_runNumber;
+
+  std::vector<std::string> v_calibname; // center, center_corr, off_gap_shift, offset, offset_corr, rotation, tilt, ...
+  std::vector<float>       v_parameters;
+
+  std::string m_cur_calibname;  
+  std::string m_fname;
+
+  bool m_isTestMode;
+
+  size_t m_nrows; 
+  size_t m_ncols; 
+
+  std::ifstream m_file;
+
+  pdscalibdata::CalibParsCenterV1       *m_center;
+  pdscalibdata::CalibParsCenterCorrV1   *m_center_corr;
+  pdscalibdata::CalibParsMargGapShiftV1 *m_marg_gap_shift;
+  pdscalibdata::CalibParsOffsetV1       *m_offset;
+  pdscalibdata::CalibParsOffsetCorrV1   *m_offset_corr;
+  pdscalibdata::CalibParsRotationV1     *m_rotation;    
+  pdscalibdata::CalibParsTiltV1         *m_tilt;   
+  pdscalibdata::CalibParsQuadRotationV1 *m_quad_rotation;    
+  pdscalibdata::CalibParsQuadTiltV1     *m_quad_tilt;   
+};
+
+} // namespace PSCalib
+
+#endif // PSCALIB_CSPADCALIBPARS_H
---
+++ PSCalib/trunk/src/CSPadCalibPars.cpp
@@ -0,0 +1,210 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class CSPadCalibPars...
+//
+// Author List:
+//      Mikhail S. Dubrovin
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "PSCalib/CSPadCalibPars.h"
+#include "PSCalib/CalibFileFinder.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+#include <iostream> // for cout
+//#include <fstream>
+
+using namespace std;
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace PSCalib {
+
+//----------------
+// Constructors --
+//----------------
+
+CSPadCalibPars::CSPadCalibPars ( const std::string &xtc_file_name )
+{
+  cout << "CSPadCalibPars::CSPadCalibPars" 
+       << "\nHere we have to find from the xtc_file_name the run number and find the calib directory..." << endl;
+
+    // Temporary staff:
+
+    m_isTestMode = true;
+
+    m_calibdir      = "/reg/neh/home/dubrovin/LCLS/CSPadAlignment-v01/calib-cxi35711-r0009-det";
+    m_calibfilename = "0-end.data";
+
+    fillCalibNameVector ();
+    loadCalibPars ();
+    printCalibPars();
+}
+
+//----------------
+
+CSPadCalibPars::CSPadCalibPars ( const std::string&   calibDir,           //  /reg/d/psdm/cxi/cxi35711/calib
+                                 const std::string&   typeGroupName,      //  CsPad::CalibV1
+                                 const std::string&   source,             //  CxiDs1.0:Cspad.0
+                                 const unsigned long& runNumber )         //  10
+  : m_calibDir(calibDir)
+  , m_typeGroupName(typeGroupName)
+  , m_source(source)
+  , m_runNumber(runNumber)
+{
+    m_isTestMode = false;
+
+    fillCalibNameVector ();
+    loadCalibPars ();
+    printCalibPars();
+}
+
+//----------------
+
+void CSPadCalibPars::fillCalibNameVector ()
+{
+    v_calibname.push_back("center");
+    v_calibname.push_back("center_corr");
+    v_calibname.push_back("marg_gap_shift");
+    v_calibname.push_back("offset");
+    v_calibname.push_back("offset_corr");
+    v_calibname.push_back("rotation");
+    v_calibname.push_back("tilt");
+    v_calibname.push_back("quad_rotation");
+    v_calibname.push_back("quad_tilt");
+}
+
+//----------------
+
+void CSPadCalibPars::loadCalibPars ()
+{
+    for( vector<std::string>::const_iterator iterCalibName  = v_calibname.begin();
+                                             iterCalibName != v_calibname.end(); iterCalibName++ )
+      {
+        m_cur_calibname = *iterCalibName;
+
+	getCalibFileName();
+	openCalibFile   ();
+	readCalibPars   ();
+	closeCalibFile  ();
+	fillCalibParsV1 ();
+      }
+}
+
+//----------------
+
+void CSPadCalibPars::getCalibFileName ()
+{
+  if ( m_isTestMode ) 
+    {
+      m_fname  = m_calibdir; 
+      m_fname += "/"; 
+      m_fname += m_cur_calibname; 
+      m_fname += "/"; 
+      m_fname += m_calibfilename; // "/0-end.data"; // !!! THIS IS A SIMPLIFIED CASE OF THE FILE NAME!!!
+    }
+  else
+    {
+      PSCalib::CalibFileFinder *calibfinder = new PSCalib::CalibFileFinder(m_calibDir, m_typeGroupName, m_source);
+      m_fname = calibfinder -> findCalibFile(m_cur_calibname, m_runNumber);
+    }
+    cout << "\nCSPadCalibPars::openCalibFile\n" << m_fname << endl;
+}
+
+//----------------
+
+void CSPadCalibPars::openCalibFile ()
+{
+   m_file.open(m_fname.c_str());
+}
+
+//----------------
+
+void CSPadCalibPars::closeCalibFile ()
+{
+   m_file.close();
+}
+
+//----------------
+
+void CSPadCalibPars::readCalibPars ()
+{
+  v_parameters.clear();
+  std::string str;
+  do{ 
+      m_file >> str; 
+      if(m_file.good()) {
+         v_parameters.push_back(std::atof(str.c_str())); // cout << str << " "; 
+      }
+    } while( m_file.good() );                            // cout << endl << endl;
+}
+
+//----------------
+
+void CSPadCalibPars::fillCalibParsV1 ()
+{
+       if( m_cur_calibname == v_calibname[0] ) m_center         = new pdscalibdata::CalibParsCenterV1(v_parameters);
+  else if( m_cur_calibname == v_calibname[1] ) m_center_corr    = new pdscalibdata::CalibParsCenterCorrV1(v_parameters);
+  else if( m_cur_calibname == v_calibname[2] ) m_marg_gap_shift = new pdscalibdata::CalibParsMargGapShiftV1(v_parameters);
+  else if( m_cur_calibname == v_calibname[3] ) m_offset         = new pdscalibdata::CalibParsOffsetV1(v_parameters);
+  else if( m_cur_calibname == v_calibname[4] ) m_offset_corr    = new pdscalibdata::CalibParsOffsetCorrV1(v_parameters);
+  else if( m_cur_calibname == v_calibname[5] ) m_rotation       = new pdscalibdata::CalibParsRotationV1(v_parameters);
+  else if( m_cur_calibname == v_calibname[6] ) m_tilt           = new pdscalibdata::CalibParsTiltV1(v_parameters);
+  else if( m_cur_calibname == v_calibname[7] ) m_quad_rotation  = new pdscalibdata::CalibParsQuadRotationV1(v_parameters);
+  else if( m_cur_calibname == v_calibname[8] ) m_quad_tilt      = new pdscalibdata::CalibParsQuadTiltV1(v_parameters);
+}
+
+//----------------
+
+void CSPadCalibPars::printCalibPars()
+{
+    WithMsgLog("CSPadCalibPars", info, str) {
+      str << "CSPadCalibPars::printCSPadCalibPars()" ;
+    }        
+
+     m_center         -> print();
+     m_center_corr    -> print();
+     m_marg_gap_shift -> print();
+     m_offset         -> print();
+     m_offset_corr    -> print();
+     m_rotation       -> print();
+     m_tilt           -> print();
+     m_quad_rotation  -> print();
+     m_quad_tilt      -> print();
+}
+
+//--------------
+// Destructor --
+//--------------
+
+CSPadCalibPars::~CSPadCalibPars ()
+{
+  //delete [] m_data; 
+}
+
+//----------------
+//----------------
+//----------------
+//----------------
+
+} // namespace PSCalib
