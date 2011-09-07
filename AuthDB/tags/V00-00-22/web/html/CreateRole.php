fc23e6d4328330a9692c066abf6017f23d5a169d
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-09-07 00:20:34 +0000

    Tagging AuthDB with V00-00-22
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2321 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ AuthDB/tags/V00-00-22/web/html/CreateRole.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once( 'authdb/authdb.inc.php' );
+require_once( 'regdb/regdb.inc.php' );
+
+use AuthDB\AuthDB;
+use AuthDB\AuthDBException;
+
+use RegDB\RegDBHtml;
+
+/*
+ * This script will lay out a form for creating a new role.
+ */
+
+// TODO: This needs to be changed with the real test
+//
+//if( !RegDBAuth::isAuthenticated()) return;
+
+/* Proceed with the operation
+ */
+try {
+    $authdb = new AuthDB();
+    $authdb->begin();
+
+    $applications = $authdb->applications();
+
+    header( 'Content-type: text/html' );
+    header( "Cache-Control: no-cache, must-revalidate" ); // HTTP/1.1
+    header( "Expires: Sat, 26 Jul 1997 05:00:00 GMT" );   // Date in the past
+
+    /* Create a container with standard fields
+     */
+    $con = new RegDBHtml( 0, 0, 700, 125 );
+    echo $con
+        ->label         (   0,  25, 'Application:' )
+        ->value_input   ( 100,  25, 'application_name' )
+        ->label         ( 275,  25, 'or' )
+        ->select_input  ( 305,  22, 'application_name_select', $applications )
+        ->label         (   0,  75, 'Role:' )
+        ->value_input   ( 100,  75, 'role_name' )
+        ->html();
+
+    $authdb->commit();
+
+} catch( AuthDBException $e ) {
+    print $e->toHtml();
+}
+?>
