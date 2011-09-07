e4097a523ea519e5508fc4270ddbbfe13dbf7324
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-09-07 18:04:49 +0000

    Tagging H5DataTypes with V00-00-42
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2332 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ H5DataTypes/tags/V00-00-42/include/CsPadMiniPedestalsV1.h
@@ -0,0 +1,74 @@
+#ifndef H5DATATYPES_CSPADMINIPEDESTALSV1_H
+#define H5DATATYPES_CSPADMINIPEDESTALSV1_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class CsPadMiniPedestalsV1.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5pp/Group.h"
+#include "hdf5pp/Type.h"
+#include "pdscalibdata/CsPadMiniPedestalsV1.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace H5DataTypes {
+
+//
+// Helper type for pdscalibdata::CsPadMiniPedestalsV1
+//
+class CsPadMiniPedestalsV1  {
+public:
+
+  typedef pdscalibdata::CsPadMiniPedestalsV1 DataType ;
+
+  // Default constructor
+  CsPadMiniPedestalsV1 () ;
+
+  // Construct from transient object
+  CsPadMiniPedestalsV1 (const DataType& data) ;
+
+  // Destructor
+  ~CsPadMiniPedestalsV1 () ;
+
+  static hdf5pp::Type stored_type() ;
+  static hdf5pp::Type native_type() ;
+
+  // store single object at specified location
+  static void store( const DataType& data,
+                     hdf5pp::Group location,
+                     const std::string& fileName = std::string()) ;
+  
+protected:
+
+private:
+
+  DataType::Pedestals pedestals;
+
+};
+
+} // namespace H5DataTypes
+
+#endif // H5DATATYPES_CSPADMINIPEDESTALSV1_H
