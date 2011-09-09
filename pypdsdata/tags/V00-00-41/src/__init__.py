0e35398e5359736233096da06b0546df1884da35
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-09-09 23:53:44 +0000

    Tagging pypdsdata with V00-00-41
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2344 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-41/src/__init__.py
@@ -0,0 +1,13 @@
+#
+# $Id$
+#
+# Copyright (c) 2010 SLAC National Accelerator Laboratory
+# 
+
+# import stuff from extension module
+from _pdsdata import Error
+
+__all__ = ['Error', 'acqiris', 'bld', 'camera', 'control',  'encoder', 
+           'epics', 'evr', 'fccd', 'io', 'ipimb', 'lusi', 'opal1k', 
+           'pnccd', 'pulnix', 'princeton', 'xtc']
+
