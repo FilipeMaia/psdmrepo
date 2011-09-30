9a7bd59303ab0bbefb5433d47f1f87ed94cdc9a0
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-09-30 19:06:42 +0000

    Tagging hdf5pp with V00-00-17
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2438 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ hdf5pp/tags/V00-00-17/include/Group.h
@@ -0,0 +1,159 @@
+#ifndef HDF5PP_GROUP_H
+#define HDF5PP_GROUP_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Group.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <boost/shared_ptr.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5/hdf5.h"
+#include "hdf5pp/Attribute.h"
+#include "hdf5pp/DataSet.h"
+#include "hdf5pp/DataSpace.h"
+#include "hdf5pp/PListDataSetCreate.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+/**
+ *  Class representing HDF5 group.
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+namespace hdf5pp {
+
+class File ;
+
+class Group  {
+public:
+
+  // Default constructor
+  Group() {}
+
+  // Destructor
+  ~Group () ;
+
+  /// Create new group, group name treated as relative to this group
+  /// (if not absolute).
+  Group createGroup ( const std::string& name ) {
+    return createGroup ( *m_id, name ) ;
+  }
+
+  /// Open existing group, group name treated as relative to this group
+  /// (if not absolute).
+  Group openGroup ( const std::string& name ) {
+    return openGroup ( *m_id, name ) ;
+  }
+
+  /// Determines if the group has a child (link) with the given nae
+  bool hasChild ( const std::string& name ) const ;
+
+  /// create attribute for this group
+  template <typename T>
+  Attribute<T> createAttr ( const std::string& name, const DataSpace& dspc = DataSpace::makeScalar() ) {
+    return Attribute<T>::createAttr ( *m_id, name, dspc ) ;
+  }
+
+  /// open existing attribute
+  template <typename T>
+  Attribute<T> openAttr ( const std::string& name ) {
+    return Attribute<T>::openAttr ( *m_id, name ) ;
+  }
+
+  // create new data set, type is determined by the template type
+  template <typename T>
+  DataSet<T> createDataSet ( const std::string& name,
+                             const DataSpace& dspc,
+                             const PListDataSetCreate& plistDScreate = PListDataSetCreate() )
+  {
+    return DataSet<T>::createDataSet ( *m_id, name, TypeTraits<T>::stored_type(), dspc, plistDScreate ) ;
+  }
+
+  // create new data set, type is determined by explicit parameter
+  template <typename T>
+  DataSet<T> createDataSet ( const std::string& name,
+                             const Type& type,
+                             const DataSpace& dspc,
+                             const PListDataSetCreate& plistDScreate = PListDataSetCreate() )
+  {
+    return DataSet<T>::createDataSet ( *m_id, name, type, dspc, plistDScreate ) ;
+  }
+
+  // open existing dataset
+  template <typename T>
+  DataSet<T> openDataSet ( const std::string& name)
+  {
+    return DataSet<T>::openDataSet (*m_id, name) ;
+  }
+
+  // close the group
+  void close() ;
+
+  // returns true if there is a real object behind
+  bool valid() const { return m_id.get() ; }
+
+  // groups can be used as keys for associative containers, need compare operators
+  bool operator<( const Group& other ) const ;
+  bool operator==( const Group& other ) const ;
+  bool operator!=( const Group& other ) const { return ! this->operator==(other) ; }
+
+protected:
+
+  // allow this guy to call my factory methods
+  friend class File ;
+
+  // factory methods
+  static Group createGroup ( hid_t parent, const std::string& name ) ;
+  static Group openGroup ( hid_t parent, const std::string& name ) ;
+
+  // constructor
+  Group ( hid_t grp ) ;
+
+private:
+
+  // deleter for  boost smart pointer
+  struct GroupPtrDeleter {
+    void operator()( hid_t* id ) {
+      if ( id ) H5Gclose ( *id );
+      delete id ;
+    }
+  };
+
+  // Data members
+  boost::shared_ptr<hid_t> m_id ;
+
+};
+
+} // namespace hdf5pp
+
+#endif // HDF5PP_GROUP_H
---
+++ hdf5pp/tags/V00-00-17/src/Group.cpp
@@ -0,0 +1,137 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Group...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+#include "Lusi/Lusi.h"
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "hdf5pp/Group.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5pp/Exceptions.h"
+#include "MsgLogger/MsgLogger.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace hdf5pp {
+
+//----------------
+// Constructors --
+//----------------
+Group::Group ( hid_t grp )
+  : m_id( new hid_t(grp), GroupPtrDeleter() )
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+Group::~Group ()
+{
+}
+
+// factory methods
+Group
+Group::createGroup ( hid_t parent, const std::string& name )
+{
+  MsgLog("hdf5pp::Group", debug, "Group::createGroup: parent=" << parent << " name=" << name ) ;
+  // allow creation of intermediate directories
+  hid_t lcpl_id = H5Pcreate( H5P_LINK_CREATE ) ;
+  H5Pset_create_intermediate_group( lcpl_id, 1 ) ;
+  hid_t f_id = H5Gcreate2 ( parent, name.c_str(), lcpl_id, H5P_DEFAULT, H5P_DEFAULT ) ;
+  H5Pclose( lcpl_id ) ;
+  if ( f_id < 0 ) {
+    throw Hdf5CallException( "Group::createGroup", "H5Gcreate2") ;
+  }
+  return Group(f_id) ;
+}
+
+Group
+Group::openGroup ( hid_t parent, const std::string& name )
+{
+  hid_t f_id = H5Gopen2 ( parent, name.c_str(), H5P_DEFAULT ) ;
+  if ( f_id < 0 ) {
+    throw Hdf5CallException( "Group::openGroup", "H5Gopen2") ;
+  }
+  return Group(f_id) ;
+}
+
+bool
+Group::hasChild ( const std::string& name ) const
+{
+  std::string child = name;
+  std::string::size_type p = name.find('/');
+  if (p != std::string::npos) child.erase(p);
+    
+  // check that the group exists
+  hid_t lapl_id = H5Pcreate( H5P_LINK_ACCESS ) ;
+  htri_t stat = H5Lexists ( *m_id, child.c_str(), lapl_id ) ;
+  H5Pclose( lapl_id ) ;
+  if (p == std::string::npos) return stat>0 ;
+
+  if (stat <= 0) return false;
+  
+  // open child group
+  hid_t f_id = H5Gopen2 ( *m_id, child.c_str(), H5P_DEFAULT ) ;
+  if ( f_id < 0 ) {
+    throw Hdf5CallException( "Group::openGroup", "H5Gopen2") ;
+  }
+  return Group(f_id).hasChild(std::string(name, p+1));
+}
+
+// close the group
+void
+Group::close()
+{
+  m_id.reset();
+}
+
+// groups can be used as keys for associative containers, need compare operators
+bool
+Group::operator<( const Group& other ) const
+{
+  // if this group is None then it is less that other group that is not None
+  if ( not m_id.get() ) return other.m_id.get() ;
+
+  // this group is not None, but other is None - false
+  if ( not other.m_id.get() ) return false ;
+
+  // compare IDs
+  return *m_id < *(other.m_id) ;
+}
+
+bool
+Group::operator==( const Group& other ) const
+{
+  // if this group is None then it is less that other group that is not None
+  if ( not m_id.get() ) return not other.m_id.get() ;
+
+  // this group is not None, but other is None - false
+  if ( not other.m_id.get() ) return false ;
+
+  // compare IDs
+  return *m_id == *(other.m_id) ;
+}
+
+} // namespace hdf5pp
