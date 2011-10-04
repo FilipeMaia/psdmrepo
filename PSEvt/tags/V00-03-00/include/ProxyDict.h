a530be1d3cf3f1e9b6e3ba7524904aee9ff630c6
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-10-04 23:50:44 +0000

    Tagging PSEvt with V00-03-00
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2465 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSEvt/tags/V00-03-00/include/ProxyDict.h
@@ -0,0 +1,127 @@
+#ifndef PSEVT_PROXYDICT_H
+#define PSEVT_PROXYDICT_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class ProxyDict.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <map>
+#include <typeinfo>
+#include <boost/shared_ptr.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "PSEvt/ProxyDictI.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "PSEvt/EventKey.h"
+#include "pdsdata/xtc/Src.hh"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSEvt {
+
+/**
+ *  @ingroup PSEvt
+ *  
+ *  @brief Implementation of the proxy dictionary interface.
+ *  
+ *  This implementation is based on std::map which matches event keys 
+ *  (EventKey class) with the corresponding proxy object. 
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version \$Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class ProxyDict : public ProxyDictI {
+public:
+
+  // Default constructor
+  ProxyDict () ;
+
+  // Destructor
+  virtual ~ProxyDict () ;
+
+protected:
+
+  /**
+   *  @brief Add one more proxy object to the dictionary.
+   *  
+   *  @param[in] proxy   Proxy object for type T.
+   *  @param[in] key     Event key for the data object.
+   */
+  virtual void putImpl( const boost::shared_ptr<ProxyI>& proxy, const EventKey& key );
+
+  /**
+   *  @brief Get an object from event
+   * 
+   *  @param[in] typeinfo  Dynamic type info object
+   *  @param[in] source Source detector address.
+   *  @param[in] key     Optional key to distinguish different objects of the same type.
+   *  @param[out] foundSrc If pointer is non-zero then pointed object will be assigned
+   *                       with the exact source address of the returned object.
+   *  @return Shared pointer of void type.
+   */
+  virtual boost::shared_ptr<void> getImpl( const std::type_info* typeinfo, 
+                                           const Source& source, 
+                                           const std::string& key,
+                                           Pds::Src* foundSrc );
+
+  /**
+   *  @brief Check if proxy of given type exists in the event
+   *  
+   *  @param[in] key     Event key for the data object.
+   *  @return true if proxy exists
+   */
+  virtual bool existsImpl(const EventKey& key);
+
+  /**
+   *  @brief Remove object of given type from the event
+   *  
+   *  @param[in] key     Event key for the data object.
+   *  @return false if object did not exist before this call
+   */
+  virtual bool removeImpl(const EventKey& key);
+
+  /**
+   *  @brief Get the list of event keys defined in event
+   *  
+   *  @param[out] keys list of the EventKey objects
+   */
+  virtual void keysImpl(std::list<EventKey>& keys) const;
+
+private:
+
+  typedef boost::shared_ptr<ProxyI> proxy_ptr;
+  typedef std::map<EventKey,proxy_ptr> Dict;
+
+  // Data members
+  Dict m_dict;
+
+};
+
+} // namespace PSEvt
+
+#endif // PSEVT_PROXYDICT_H
---
+++ PSEvt/tags/V00-03-00/src/ProxyDict.cpp
@@ -0,0 +1,143 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class ProxyDict...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "PSEvt/ProxyDict.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "PSEvt/Exceptions.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace PSEvt {
+
+//----------------
+// Constructors --
+//----------------
+ProxyDict::ProxyDict ()
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+ProxyDict::~ProxyDict ()
+{
+}
+
+
+void 
+ProxyDict::putImpl( const boost::shared_ptr<ProxyI>& proxy, 
+                    const EventKey& key )
+{
+  // there should not be existing key
+  Dict::iterator it = m_dict.find(key);
+  if ( it != m_dict.end() ) {
+    throw ExceptionDuplicateKey(ERR_LOC, key);
+  }
+
+  m_dict.insert(Dict::value_type(key, proxy));
+}
+
+
+boost::shared_ptr<void> 
+ProxyDict::getImpl( const std::type_info* typeinfo, 
+                    const Source& source, 
+                    const std::string& key,
+                    Pds::Src* foundSrc )
+{
+  if (source.isExact()) {
+    
+    EventKey proxyKey(typeinfo, source.src(), key);
+    Dict::const_iterator it = m_dict.find(proxyKey);
+    if ( it != m_dict.end() ) {
+      // call proxy to get the value
+      if (foundSrc) *foundSrc = it->first.src();
+      return it->second->get(this, it->first.src(), key);
+    }
+
+    // try special any-source proxy
+    EventKey proxyKeyAny(typeinfo, EventKey::anySource(), key);
+    it = m_dict.find(proxyKeyAny);
+    if ( it != m_dict.end() ) {
+      // call proxy to get the value
+      if (foundSrc) *foundSrc = source.src();
+      return it->second->get(this, source.src(), key);
+    }
+
+  } else {
+
+    // When source is a match then no-source objects have priority. Try to
+    // find no-source object first and see if it matches
+    if (source.match(Pds::Src())) {
+      EventKey proxyKey(typeinfo, Pds::Src(), key);
+      Dict::const_iterator it = m_dict.find(proxyKey);
+      if ( it != m_dict.end() ) {
+        // call proxy to get the value
+        if (foundSrc) *foundSrc = it->first.src();
+        return it->second->get(this, it->first.src(), key);
+      }
+    }
+
+    // Do linear search, find first match
+    for (Dict::const_iterator it = m_dict.begin(); it != m_dict.end(); ++ it) {
+      if (*typeinfo == *it->first.typeinfo() and
+          key == it->first.key() and 
+          source.match(it->first.src()) ) {
+        // call proxy to get the value
+        if (foundSrc) *foundSrc = it->first.src();
+        return it->second->get(this, it->first.src(), key);
+      }
+    }
+    
+  }
+
+  return proxy_ptr();
+
+}
+
+bool 
+ProxyDict::existsImpl(const EventKey& key)
+{
+  return m_dict.find(key) != m_dict.end();
+}
+
+bool 
+ProxyDict::removeImpl(const EventKey& key)
+{
+  return m_dict.erase(key) > 0;
+}
+
+void 
+ProxyDict::keysImpl(std::list<EventKey>& keys) const
+{
+  keys.clear();
+  for (Dict::const_iterator it = m_dict.begin(); it != m_dict.end(); ++ it) {
+    keys.push_back(it->first);
+  }
+}
+
+} // namespace PSEvt
