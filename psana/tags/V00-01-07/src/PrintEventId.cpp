06acf2be77f5160d8a54cf10ffad0eb0b70f192d
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-10-05 16:29:14 +0000

    Tagging psana with V00-01-07
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2477 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psana/tags/V00-01-07/src/PrintEventId.cpp
@@ -0,0 +1,112 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class PrintEventId...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "psana/PrintEventId.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+#include "PSEvt/EventId.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+using namespace psana;
+PSANA_MODULE_FACTORY(PrintEventId)
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace psana {
+
+//----------------
+// Constructors --
+//----------------
+PrintEventId::PrintEventId (const std::string& name)
+  : Module(name)
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+PrintEventId::~PrintEventId ()
+{
+}
+
+/// Method which is called once at the beginning of the job
+void 
+PrintEventId::beginJob(Event& evt, Env& env)
+{
+  MsgLog(name(), info, "in beginJob()");
+}
+
+/// Method which is called at the beginning of the run
+void 
+PrintEventId::beginRun(Event& evt, Env& env)
+{
+  MsgLog(name(), info, "in beginRun()");
+}
+
+/// Method which is called at the beginning of the calibration cycle
+void 
+PrintEventId::beginCalibCycle(Event& evt, Env& env)
+{
+  MsgLog(name(), info, "in beginCalibCycle()");
+}
+
+/// Method which is called with event data, this is the only required 
+/// method, all other methods are optional
+void 
+PrintEventId::event(Event& evt, Env& env)
+{
+  // get event ID
+  shared_ptr<EventId> eventId = evt.get();
+  if (not eventId.get()) {
+    MsgLog(name(), info, "event ID not found");
+  } else {
+    MsgLog(name(), info, "event ID: " << *eventId);
+  }
+}
+  
+/// Method which is called at the end of the calibration cycle
+void 
+PrintEventId::endCalibCycle(Event& evt, Env& env)
+{
+  MsgLog(name(), info, "in endCalibCycle()");
+}
+
+/// Method which is called at the end of the run
+void 
+PrintEventId::endRun(Event& evt, Env& env)
+{
+  MsgLog(name(), info, "in endRun()");
+}
+
+/// Method which is called once at the end of the job
+void 
+PrintEventId::endJob(Event& evt, Env& env)
+{
+  MsgLog(name(), info, "in endJob()");
+}
+
+} // namespace psana
