5f7d04cc08be1a4bdd8d9d54239c512bd4ecdddf
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-10-10 23:35:01 +0000

    Tagging RegDB with V00-00-34
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2495 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ RegDB/tags/V00-00-34/src/RegDb.py
@@ -0,0 +1,142 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id: $
+#
+# Description:
+#  Module RegDb...
+#
+#------------------------------------------------------------------------
+
+""" Interface class for RegDb.
+
+This software was developed for the LUSI project.  If you use all or
+part of it, please give an appropriate acknowledgment.
+
+@see RelatedModule
+
+@version $Id: $
+
+@author Igor Gaponenko
+"""
+
+
+#------------------------------
+#  Module's version from SVN --
+#------------------------------
+__version__ = "$Revision: 702 $"
+# $Source$
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import sys
+import os
+import logging
+
+#---------------------------------
+#  Imports of base class module --
+#---------------------------------
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+
+from LusiTime.Time import Time
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+#------------------------
+# Exported definitions --
+#------------------------
+
+#---------------------
+#  Class definition --
+#---------------------
+
+class RegDb ( object ) :
+
+    #--------------------
+    #  Class variables --
+    #--------------------
+
+    #----------------
+    #  Constructor --
+    #----------------
+    def __init__ ( self, conn, log=None ) :
+        """Constructor.
+
+        @param conn      database connection object
+        """
+
+        self._conn = conn
+        self._log = log or logging.getLogger()
+
+    def begin(self):
+        self._conn.cursor().execute( 'BEGIN' )
+
+    def commit(self):
+        self._conn.cursor().execute( 'COMMIT' )
+
+
+    # =================================
+    # Find experiment by its identifier
+    # =================================
+
+    def find_experiment_by_id(self, id):
+        """
+        Find experiment by its identifier. Return none or False of no such experiment
+        or a dictionary with parameters of the experiment. The followinng keys will
+        be found in the dictionary:
+            - instr_id
+            - instr_name
+            - instr_descr
+            - id
+            - name
+            - descr
+            - leader_account
+            - posix_gid
+            - contact_info
+            - registration_time
+            - begin_time
+            - end_time
+        """
+
+        # find the experiment
+        cursor = self._conn.cursor( True )
+        cursor.execute( """SELECT i.name AS instr_name, i.descr AS instr_descr, e.* FROM experiment e, instrument i WHERE e.id=%s AND e.instr_id=i.id""",  (id, ))
+        rows = cursor.fetchall()
+        if not rows : return None
+        if len(rows) > 1:
+            raise Exception("Too many rows for experiment id %s" % id)
+
+        row = rows[0]
+
+        row['registration_time'] = Time.from64( row['registration_time'] )
+        row['begin_time']        = Time.from64( row['begin_time'] )
+        row['end_time']          = Time.from64( row['end_time'] )
+
+        return row
+
+
+    def last_experiment_switch(self, instr):
+        """Get the latest experiment for the given instrument name,
+        returns tuple (expName, time, user), time is LusiTime.Time object,
+        user is user name who requested switch. Returns None if cannot find
+        information for given instrument name."""
+
+        cursor = self._conn.cursor()
+        q = """SELECT e.name, sw.switch_time, sw.requestor_uid 
+            FROM expswitch sw, experiment e, instrument i
+            WHERE sw.exper_id = e.id AND e.instr_id = i.id AND i.name=%s
+            ORDER BY sw.switch_time DESC LIMIT 1"""
+        cursor.execute(q, (instr,))
+        
+        rows = cursor.fetchall()
+        if not rows: return None
+
+        name, time, user = rows[0]
+        time = Time.from64(time);
+            
+        return (name, time, user)
