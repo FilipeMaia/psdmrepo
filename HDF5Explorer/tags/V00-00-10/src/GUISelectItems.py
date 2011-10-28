f7adbbeb3682f9d5498f7298064af0674e91d088
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-10-28 22:07:27 +0000

    Tagging HDF5Explorer with V00-00-10
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2576 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ HDF5Explorer/tags/V00-00-10/src/GUISelectItems.py
@@ -0,0 +1,309 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id$
+#
+# Description:
+#  Module GUISelectItems...
+#
+#------------------------------------------------------------------------
+
+"""Shows the HDF5 file tree-structure and allows to select data items
+
+This software was developed for the SIT project.  If you use all or 
+part of it, please give an appropriate acknowledgment.
+
+@see RelatedModule
+
+@version $Id: template!python!py 4 2008-10-08 19:27:36Z salnikov $
+
+@author Mikhail S. Dubrovin
+"""
+
+
+#------------------------------
+#  Module's version from CVS --
+#------------------------------
+__version__ = "$Revision: 4 $"
+# $Source$
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import sys
+from PyQt4 import QtGui, QtCore
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+import HDF5TreeViewModel    as h5model
+import ConfigParameters     as cp
+import PrintHDF5            as printh5
+import AppUtils.AppDataPath as apputils
+
+#---------------------
+#  Class definition --
+#---------------------
+#class GUISelectItems ( QtGui.QWidget ) :
+class GUISelectItems ( QtGui.QMainWindow ) :
+    """Shows the HDF5 file tree-structure and allows to select data items.
+
+    @see BaseClass
+    @see OtherClass
+    """
+
+    #----------------
+    #  Constructor --
+    #----------------
+
+    def __init__(self, parent=None):
+        #super(GUISelectItems, self).__init__(parent)
+        QtGui.QWidget.__init__(self, parent)
+
+        """Constructor."""
+        #self.parent = parent # See setParent for bypass
+
+        self.setGeometry(10, 10, 350, 1000)
+        self.setWindowTitle('HDF5 tree, select items')
+
+        # !!!!! THE /data/ SUBDIRECTORY SHOULD BE OMITTED IN PATH !!!!!
+        self.apppath_icon_folder_open   = apputils.AppDataPath('HDF5Explorer/icons/folder_open.gif')
+        self.apppath_icon_folder_closed = apputils.AppDataPath('HDF5Explorer/icons/folder_closed.gif')
+        self.apppath_icon_data          = apputils.AppDataPath('HDF5Explorer/icons/table.gif')
+        self.apppath_icon_apply         = apputils.AppDataPath('HDF5Explorer/icons/button_ok.png')
+        self.apppath_icon_reset         = apputils.AppDataPath('HDF5Explorer/icons/undo.png')
+        self.apppath_icon_retreve       = apputils.AppDataPath('HDF5Explorer/icons/redo.png')
+        self.apppath_icon_exit          = apputils.AppDataPath('HDF5Explorer/icons/exit.png')
+        self.apppath_icon_expand        = apputils.AppDataPath('HDF5Explorer/icons/folder_open.gif')
+        self.apppath_icon_collapse      = apputils.AppDataPath('HDF5Explorer/icons/folder_closed.gif')
+        self.apppath_icon_expcheck      = apputils.AppDataPath('HDF5Explorer/icons/folder_open_checked.png')
+        self.apppath_icon_print         = apputils.AppDataPath('HDF5Explorer/icons/contents.png')
+
+        self.icon_folder_open   = QtGui.QIcon(self.apppath_icon_folder_open  .path())
+        self.icon_folder_closed = QtGui.QIcon(self.apppath_icon_folder_closed.path())
+        self.icon_data          = QtGui.QIcon(self.apppath_icon_data         .path())
+        self.icon_apply         = QtGui.QIcon(self.apppath_icon_apply        .path())
+        self.icon_reset         = QtGui.QIcon(self.apppath_icon_reset        .path())
+        self.icon_retreve       = QtGui.QIcon(self.apppath_icon_retreve      .path())
+        self.icon_exit          = QtGui.QIcon(self.apppath_icon_exit         .path())
+        self.icon_expand        = QtGui.QIcon(self.apppath_icon_expand       .path())
+        self.icon_collapse      = QtGui.QIcon(self.apppath_icon_collapse     .path())
+        self.icon_expcheck      = QtGui.QIcon(self.apppath_icon_expcheck     .path())
+        self.icon_print         = QtGui.QIcon(self.apppath_icon_print        .path())         
+        self.icon_expcoll       = self.icon_expand
+
+        actExit         = QtGui.QAction(self.icon_exit,     'Exit',           self)
+        actApply        = QtGui.QAction(self.icon_apply,    'Apply',          self)
+        actReset        = QtGui.QAction(self.icon_reset,    'Reset',          self)
+        actRetreve      = QtGui.QAction(self.icon_retreve,  'Retreve',        self)
+        actExpand       = QtGui.QAction(self.icon_expand,   'Expand',         self)
+        actCollapse     = QtGui.QAction(self.icon_collapse, 'Collapse',       self)
+        self.actExpColl = QtGui.QAction(self.icon_expcoll,  'Expand tree',    self)
+        actExpCheck     = QtGui.QAction(self.icon_expcheck, 'Expand checked', self)
+        actPrint        = QtGui.QAction(self.icon_print,    'Print tree',     self)
+
+        self.connect(actExit,         QtCore.SIGNAL('triggered()'), self.processExit)
+        self.connect(actApply,        QtCore.SIGNAL('triggered()'), self.processApply)
+        self.connect(actReset,        QtCore.SIGNAL('triggered()'), self.processReset)
+        self.connect(actRetreve,      QtCore.SIGNAL('triggered()'), self.processRetreve)
+        self.connect(actExpand,       QtCore.SIGNAL('triggered()'), self.processExpand)
+        self.connect(actCollapse,     QtCore.SIGNAL('triggered()'), self.processCollapse)
+        self.connect(self.actExpColl, QtCore.SIGNAL('triggered()'), self.processExpColl)
+        self.connect(actExpCheck,     QtCore.SIGNAL('triggered()'), self.processExpCheck)
+        self.connect(actPrint,        QtCore.SIGNAL('triggered()'), self.processPrint)
+        #self.connect(actExit,  QtCore.SIGNAL('triggered()'), QtCore.SLOT('close()'))
+
+        self.menubar = self.menuBar()
+        optAct = self.menubar.addMenu('&Actions')
+        optAct.addAction(actApply)
+        optAct.addAction(actReset)
+        optAct.addAction(actRetreve)
+        optAct.addAction(actExit)
+
+        optView = self.menubar.addMenu('&View')
+        optView.addAction(actExpand)
+        optView.addAction(actCollapse)
+        optView.addAction(actExpCheck)
+        optView.addAction(actPrint)
+
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.setMovable(True)
+        self.toolbar.addAction(actExit)
+        #self.toolbar.insertSeparator(....)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(actApply)
+        self.toolbar.addAction(actReset)
+        self.toolbar.addAction(actRetreve)
+        self.toolbar.addSeparator()
+        #self.toolbar.addAction(actExpand)
+        #self.toolbar.addAction(actCollapse)
+        self.toolbar.addAction(self.actExpColl)
+        self.toolbar.addAction(actExpCheck)
+        self.toolbar.addAction(actPrint)
+
+        self.model = h5model.HDF5TreeViewModel()
+
+        #self.view = QtGui.QListView()
+        #self.view = QtGui.QTableView()
+        self.view = QtGui.QTreeView()
+        self.view.setModel(self.model)
+        #print 'Root is decorated ? ', self.view.rootIsDecorated() # Returns True
+        #self.view.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
+        #self.view.setExpanded(cp.confpars.indExpandedItem,True)
+        self.view.setAnimated(True)
+        self.setCentralWidget(self.view)
+
+        #self.show()
+        
+        #self.connect(self.view.selectionModel(), QtCore.SIGNAL('selectionChanged(QItemSelection, QItemSelection)'), self.itemSelected)
+        self.connect(self.view.selectionModel(), QtCore.SIGNAL('currentChanged(QModelIndex, QModelIndex)'), self.cellSelected)
+        #self.view.clicked.connect(self.someMethod1)       # This works
+        #self.view.doubleClicked.connect(self.someMethod2) # This works
+        self.model.itemChanged.connect(self.itemChanged)
+        self.view.expanded.connect(self.itemExpanded)
+        self.view.collapsed.connect(self.itemCollapsed)
+
+        self.processRetreve() # Set all check box states for current configuration
+        if cp.confpars.treeViewIsExpanded :
+            #self.processExpand()  # Expand the tree 
+            self.processExpCheck() # Expand the tree for checked data items 
+
+        cp.confpars.treeWindowIsOpen = True
+
+                
+    #-------------------
+    # Private methods --
+    #-------------------
+
+    #-------------------
+    #  Public methods --
+    #-------------------
+
+    def setParent(self,parent) :
+        self.parent = parent
+
+
+    def closeEvent(self, event): # if the 'x' (in the top-right corner of the window) is clicked
+        #print 'closeEvent'
+        #self.parent.processDisplay() # in order to close this window as from GUIMain
+        #self.disconnect()
+        self.view.close()
+        #self.model.close()
+        self.menubar.close()
+        self.toolbar.close()
+        cp.confpars.treeWindowIsOpen = False
+        #self.display.setText('Open')
+
+
+    def processExit(self):
+        #print 'Exit button is clicked'
+        self.close()
+
+
+    def processApply(self):
+        print 'Apply button is clicked, use all checked items in the tree model for display'
+        cp.confpars.list_of_checked_item_names = self.model.get_list_of_checked_item_names_for_model()
+        if cp.confpars.wtdWindowIsOpen :
+            cp.confpars.guiwhat.processRefresh()
+
+
+    def processReset(self):
+        print 'Reset button is clicked, uncheck all items'
+        self.model.reset_checked_items()
+
+    def processRetreve(self):
+        print 'Retreve button is clicked,\n', \
+        'retreve the list of checked items from config. pars. and use them in the tree model.'
+        self.model.retreve_checked_items(cp.confpars.list_of_checked_item_names)
+
+    def processExpCheck(self):
+        print 'ExpandChecked button is clicked, expand the tree for checked items only.'
+        self.processCollapse() # first, collapse the tree
+        self.model.expand_checked_items(self.view)
+        cp.confpars.treeViewIsExpanded = True       # Change status for expand/collapse button
+        self.actExpColl.setIcon(self.icon_collapse)
+        self.actExpColl.setText('Collapse tree')
+
+    def processExpand(self):
+        print 'Expand button is clicked'
+        self.model.set_all_group_icons(self.icon_expand)
+        self.view.expandAll()
+        cp.confpars.treeViewIsExpanded = True
+
+    def processCollapse(self):
+        print 'Collapse button is clicked'
+        self.model.set_all_group_icons(self.icon_collapse)
+        self.view.collapseAll()
+        cp.confpars.treeViewIsExpanded = False
+
+    def processExpColl(self): # Flip/flop between Expand and Collaple the HDF5 tree
+        print 'Expand/Collapse button is clicked :',
+        if cp.confpars.treeViewIsExpanded == True :
+            self.actExpColl.setIcon(self.icon_expand)
+            self.actExpColl.setText('Expand tree')
+            self.processCollapse()
+        else :
+            self.actExpColl.setIcon(self.icon_collapse)
+            self.actExpColl.setText('Collapse tree')
+            self.processExpand()
+ 
+    def processPrint(self):
+        print 'Print button is clicked'
+        fname = cp.confpars.dirName+'/'+cp.confpars.fileName
+        print 'Print structure of the HDF5 file:\n %s' % (fname)
+        printh5.print_hdf5_file_structure(fname)
+
+    def someMethod1(self):
+        print '1-clicked!'
+
+    def someMethod2(self):
+        print '2-clicked!'
+
+    def itemExpanded(self, ind): 
+        item = self.model.itemFromIndex(ind)
+        item.setIcon(self.icon_folder_open)
+        print 'Item expanded : ', item.text()  
+
+    def itemCollapsed(self, ind):
+        item = self.model.itemFromIndex(ind)
+        item.setIcon(self.icon_folder_closed)
+        print 'Item collapsed : ', item.text()  
+
+    def itemSelected(self, selected, deselected):
+        print len(selected),   "items selected"
+        print len(deselected), "items deselected"
+
+    def cellSelected(self, ind_sel, ind_desel):
+        #print "ind   selected row, col = ", ind_sel.row(),  ind_sel.column()
+        #print "ind deselected row, col = ", ind_desel.row(),ind_desel.column() 
+        #item       = self.model.itemFromIndex(ind_sel)
+        #dsfullname = str(self.model.getFullNameFromItem(item))
+        dsfullname = str(self.model.getFullNameFromIndex(ind_sel))
+        print "Item with name '%s' is selected" % ( dsfullname )
+        #print ' isEnabled=',item.isEnabled() 
+        #print ' isCheckable=',item.isCheckable() 
+        #print ' checkState=',item.checkState()
+        #print ' isSelectable=',item.isSelectable() 
+        #print ' isTristate=',item.isTristate() 
+        #print ' isEditable=',item.isEditable() 
+        #print ' isExpanded=',self.view.isExpanded(ind_sel)
+        printh5.print_dataset_metadata_from_file(dsfullname)
+
+    def itemChanged(self, item):
+        state = ['UNCHECKED', 'TRISTATE', 'CHECKED'][item.checkState()]
+        print "Item with full name %s, is at state %s\n" % (self.model.getFullNameFromItem(item), state)
+
+#------------------
+#  Main for test --
+#------------------
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex  = GUISelectItems()
+    ex.show()
+    app.exec_()
+
+if __name__ == "__main__" :
+    main()
+    sys.exit ( "Module is not supposed to be run as main module" )
+
+#------------------
---
+++ HDF5Explorer/tags/V00-00-10/src/PlotsForWaveform.py
@@ -0,0 +1,329 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id$
+#
+# Description:
+#  Module PlotsForWaveform...
+#
+#------------------------------------------------------------------------
+
+"""Plots for any 'waveform' record in the EventeDisplay project.
+
+This software was developed for the SIT project.  If you use all or 
+part of it, please give an appropriate acknowledgment.
+
+@see RelatedModule
+
+@version $Id: template!python!py 4 2008-10-08 19:27:36Z salnikov $
+
+@author Mikhail S. Dubrovin
+"""
+
+#------------------------------
+#  Module's version from CVS --
+#------------------------------
+__version__ = "$Revision: 4 $"
+# $Source$
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import sys
+import matplotlib.pyplot as plt
+from numpy import *
+#import h5py
+
+#---------------------------------
+#  Imports of base class module --
+#---------------------------------
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+import ConfigParameters as cp
+import PrintHDF5        as printh5
+
+#---------------------
+#  Class definition --
+#---------------------
+class PlotsForWaveform ( object ) :
+    """Plots for any 'waveform' record in the EventeDisplay project."""
+
+    #----------------
+    #  Constructor --
+    #----------------
+    def __init__ ( self ) :
+        """Constructor, initialization"""
+        pass
+
+    #-------------------
+    #  Public methods --
+    #-------------------
+  
+    def plotWFWaveform( self, ds1ev, fig, h5file=None ):
+        """Plot waveform from input array."""
+
+        print 'plotWFWaveform'
+        print 'ds1ev.shape', ds1ev.shape
+        numberOfWF, par2, self.dimX = ds1ev.shape
+        #print 'numberOfWF, par2, dimX = ', numberOfWF, par2, dimX
+        #print 'arrwf.shape', arrwf.shape
+        self.nwin   = nwin = fig.nwin
+        self.ds1ev  = ds1ev
+        self.h5file = h5file
+
+        if cp.confpars.waveformWindowParameters[self.nwin][1] > 3: # if vertical or horizontal units are requested
+            self.getScaleConfigParameters()
+
+        self.arrT = self.getTimeArray()
+            
+        par = []
+
+        indwf1 = cp.confpars.waveformWindowParameters[nwin][7]
+        indwf2 = cp.confpars.waveformWindowParameters[nwin][8]
+        indwf3 = cp.confpars.waveformWindowParameters[nwin][9]
+        indwf4 = cp.confpars.waveformWindowParameters[nwin][10]
+
+
+        if indwf1 != None : par.append( (self.arrT, self.getAmplitudeArray(indwf1), 'k-') )
+        if indwf2 != None : par.append( (self.arrT, self.getAmplitudeArray(indwf2), 'r-') )
+        if indwf3 != None : par.append( (self.arrT, self.getAmplitudeArray(indwf3), 'g-') )
+        if indwf4 != None : par.append( (self.arrT, self.getAmplitudeArray(indwf4), 'b-') )
+
+        fig.canvas.set_window_title(cp.confpars.current_item_name_for_title) 
+        plt.clf() # clear plot
+        fig.subplots_adjust(left=0.10, bottom=0.10, right=0.95, top=0.94, wspace=0.1, hspace=0.1)        
+
+        print 'Number of waves to draw =', len(par) 
+
+        if len(par) == 1 :
+            plt.plot( par[0][0],par[0][1],par[0][2] )
+
+        elif len(par) == 2 :
+            plt.plot( par[0][0],par[0][1],par[0][2],\
+                      par[1][0],par[1][1],par[1][2] )
+
+        elif len(par) == 3 :
+            plt.plot( par[0][0],par[0][1],par[0][2],\
+                      par[1][0],par[1][1],par[1][2],\
+                      par[2][0],par[2][1],par[2][2] )
+            
+        elif len(par) == 4 :
+            plt.plot( par[0][0],par[0][1],par[0][2],\
+                      par[1][0],par[1][1],par[1][2],\
+                      par[2][0],par[2][1],par[2][2],\
+                      par[3][0],par[3][1],par[3][2] )
+        else :
+            print 'Wrong number of waves !!!', len(par) 
+
+
+        if self.getBitStatus( 1 ):
+            plt.ylim(cp.confpars.waveformWindowParameters[nwin][2],\
+                     cp.confpars.waveformWindowParameters[nwin][3]  )
+            
+        if self.getBitStatus( 2 ):
+            plt.xlim(cp.confpars.waveformWindowParameters[nwin][4],\
+                     cp.confpars.waveformWindowParameters[nwin][5]  )
+        else :
+            plt.xlim(self.arrT.min(), self.arrT.max())
+        
+        str_title='Waveform, event ' + str(cp.confpars.eventCurrent)
+        
+        plt.title(str_title,color='r',fontsize=20) # pars like in class Text
+
+        XTitle = 'Index'
+        YTitle = 'ADC units'
+        if self.getBitStatus( 4 ) : YTitle = 'Amplitude (V)'     
+        if self.getBitStatus( 8 ) : XTitle = 'Time (ns)'    
+        plt.xlabel(XTitle)
+        plt.ylabel(YTitle)
+
+
+
+    def getAmplitudeArray(self,indwf) :
+        arrA0 = self.ds1ev[indwf,0,...]
+        if self.getBitStatus( 4 ): # Apply units
+            arrA = array(arrA0,dtype=float)
+            scale = self.vertFullScale / 2**16
+            return arrA0 * scale + self.vertOffset
+        else :                     # Do not apply units
+            return arrA0
+        
+
+    def getTimeArray(self) :
+        arrT0 = arange(self.dimX,dtype=float)
+        #print 'arrT = ',arrT0
+
+        if self.getBitStatus( 8 ): # Apply units
+            return arrT0 * self.horizSampleInterval + self.horizDelayTime
+        else :                     # Do not apply units
+            return arrT0
+
+
+    def getBitStatus(self,bit):
+        return cp.confpars.waveformWindowParameters[self.nwin][1] & bit
+        #rangeUnitsBits : 1-ALimits, 2-TLimits, 4-AUnits, 8-TUnits
+
+
+
+    def getScaleConfigParameters(self):
+        dsname = cp.confpars.waveformWindowParameters[self.nwin][0]
+        #print 'Waveforms for dataset:', dsname
+        dsgroup = '/Configure:0000/Acqiris::ConfigV1/' + printh5.get_item_second_to_last_name(dsname)
+        dsname_config_vert  = dsgroup + '/vert'
+        dsname_config_horiz = dsgroup + '/horiz'
+        #print 'Configuration dataset vert : ', dsname_config_vert
+        #print 'Configuration dataset horiz: ', dsname_config_horiz
+
+        self.vertFullScale = 1 
+        self.vertOffset    = 0
+        self.horizSampleInterval = 1
+        self.horizDelayTime      = 0
+
+        try: 
+            dsvert  = self.h5file[dsname_config_vert]
+        except KeyError:
+            #if not printh5.isDataset(dsvert) :
+            print 80*'!'
+            print 'WARNING:', dsname_config_vert, ' DATASET DOES NOT EXIST IN HDF5\n',\
+                  'PROGRAM CAN NOT USE VERTICAL UNITS FROM CONFIGURATION; USE DEFAULT, SCALE=1, OFFSET=0'
+            print 80*'!'
+            return
+
+        try: 
+            dshoriz = self.h5file[dsname_config_horiz]
+        except KeyError:
+            #if not printh5.isDataset(dshoriz) :
+            print 80*'!'
+            print 'WARNING:', dsname_config_horiz, ' DATASET DOES NOT EXIST IN HDF5\n',\
+                  'PROGRAM CAN NOT USE HORIZONTAL UNITS FROM CONFIGURATION; USE DEFAULT, SCALE=1, OFFSET=0'
+            print 80*'!'
+            return
+
+        #print 'dsvert.dtype =', dsvert.dtype
+        #print 'dsvert[0] =', dsvert[0]
+        self.vertFullScale = dsvert[0][0] 
+        self.vertOffset    = dsvert[0][1]
+        self.vertCoupling  = dsvert[0][2]
+        self.vertBandwidth = dsvert[0][3]
+
+        print 'self.vertFullScale      =',self.vertFullScale         
+        print 'self.vertOffset         =',self.vertOffset         
+        print 'self.vertCoupling       =',self.vertCoupling         
+        print 'self.vertBandwidth      =',self.vertBandwidth         
+
+        #print 'dshoriz.dtype =',   dshoriz.dtype
+        #print 'dshoriz.value =',   dshoriz.value
+        self.horizSampleInterval = dshoriz.value[0] * 1e9 # time in ns 
+        self.horizDelayTime      = dshoriz.value[1] * 1e9 # time in ns
+        self.horizNSamples       = dshoriz.value[2]
+        self.horizNSegments      = dshoriz.value[3]
+        print 'self.horizSampleInterval=',self.horizSampleInterval
+        print 'self.horizDelayTime     =',self.horizDelayTime     
+        print 'self.horizNSamples      =',self.horizNSamples
+        print 'self.horizNSegments     =',self.horizNSegments
+
+#--------------------------------
+#--------------------------------
+#--------------------------------
+#--------------------------------
+
+
+    def plotWaveVsEvent(self, fig, h5file):
+        #print "plotWaveVsEvent(...)"
+
+        self.nwin   = nwin = fig.nwin
+        self.h5file = h5file
+        dsname = cp.confpars.waveformWindowParameters[self.nwin][0]
+        cp.confpars.current_item_name_for_title = printh5.get_item_name_for_title_4(dsname)
+        #cp.confpars.current_item_name_for_title = str(dsname)
+
+        ds     = self.h5file[dsname]
+        Nevents = ds.shape[0]     
+        event = 0
+        self.ds1ev = ds[event]
+        numberOfWF, par2, self.dimX = self.ds1ev.shape
+        #print 'Nevents, dimX = ', Nevents, self.dimX      
+
+        # Use the 1st defined index for this wave.
+        for par in range(10,6,-1):
+            if               cp.confpars.waveformWindowParameters[nwin][par] != None :
+                self.indwf = cp.confpars.waveformWindowParameters[nwin][par]
+
+        if cp.confpars.waveformWindowParameters[self.nwin][1] > 3: # if vertical or horizontal units are requested
+            self.getScaleConfigParameters()
+
+        self.arrA = self.getAmplitudeArray(self.indwf)
+        self.arrT = self.getTimeArray()
+
+        # Make 2D float array of the wave vs event
+        self.arrWaveVsEv = empty([Nevents, self.dimX], dtype=float)
+
+        for event in range(Nevents) :
+            #print 'event =', event
+            self.ds1ev = ds[event]            
+            self.arrWaveVsEv[event,...] = self.getAmplitudeArray(self.indwf)
+
+        # Define A range
+        ARange = None
+        if self.getBitStatus( 1 ): # A limits is on
+            ARange = (cp.confpars.waveformWindowParameters[nwin][2],\
+                      cp.confpars.waveformWindowParameters[nwin][3] )
+
+
+        #arr2d = self.arrWaveVsEv
+        # Define T INDEX range
+        self.TIndMin = 0
+        self.TIndMax = self.arrT.shape[0] - 1 
+        self.TMin = self.TIndMin
+        self.TMax = self.TIndMax
+
+        if self.getBitStatus( 2 ): # T limits is on            
+            self.TMin = cp.confpars.waveformWindowParameters[nwin][4]
+            self.TMax = cp.confpars.waveformWindowParameters[nwin][5]
+        else:
+            self.TMin = self.arrT.min()
+            self.TMax = self.arrT.max()
+
+
+        if self.getBitStatus( 8 ): # T Units
+            self.TIndMin = (self.TMin - self.horizDelayTime) / self.horizSampleInterval
+            self.TIndMax = (self.TMax - self.horizDelayTime) / self.horizSampleInterval
+        else:
+            self.TIndMin = self.TMin
+            self.TIndMax = self.TMax
+
+        Range  = (self.TMin, self.TMax, 0, Nevents)     
+        arr2d   = self.arrWaveVsEv[...,self.TIndMin:self.TIndMax]
+
+        # Begin plot
+        fig.canvas.set_window_title(cp.confpars.current_item_name_for_title) 
+        plt.clf() # clear plot
+        fig.subplots_adjust(left=0.10, bottom=0.10, right=0.95, top=0.94, wspace=0.1, hspace=0.1)        
+        axsp = fig.add_subplot(111)
+
+        #plt.plot( self.arrT, self.arrA, 'k-' )
+
+        axim = axsp.imshow(arr2d, origin='lower',extent=Range, interpolation='nearest', aspect='auto')
+        mycb = fig.colorbar(axim, fraction=0.1, pad=0.1, shrink=1.0, aspect=40, orientation=1) # ticks=coltickslocs
+
+        axim.set_clim(ARange) # (0,15000)
+
+        XTitle = 'Index'
+        YTitle = 'Event'
+        ZTitle = 'ADC units'
+        if self.getBitStatus( 4 ) : ZTitle = 'Amplitude (V)'     
+        if self.getBitStatus( 8 ) : XTitle = 'Time (ns)'    
+        plt.xlabel(XTitle)
+        plt.ylabel(YTitle)
+        #plt.zlabel(ZTitle)
+
+
+#--------------------------------
+#  In case someone decides to run this module
+#
+if __name__ == "__main__" :
+
+    sys.exit ( "Module is not supposed to be run as main module" )
+
+#--------------------------------
