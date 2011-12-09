e7389476474ed972b849fd739fd74f463c777131
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2011-12-09 21:48:58 +0000

    Tagging InterfaceCtlr with V01-00-10
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2699 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ InterfaceCtlr/tags/V01-00-10/web/icws/lib/base.py
@@ -0,0 +1,18 @@
+"""The base Controller API
+
+Provides the BaseController class for subclassing.
+"""
+from pylons.controllers import WSGIController
+from pylons.templating import render_mako as render
+from pylons import request, response, session
+
+import icws.lib.helpers as h
+
+class BaseController(WSGIController):
+
+    def __call__(self, environ, start_response):
+        """Invoke the Controller"""
+        # WSGIController.__call__ dispatches to the Controller method
+        # the request is routed to. This routing information is
+        # available in environ['pylons.routes_dict']
+        return WSGIController.__call__(self, environ, start_response)
