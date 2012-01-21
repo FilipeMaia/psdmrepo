8fe51225c0d5e3a8998ae0316ee724753274d968
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-01-21 01:27:31 +0000

    Tagging PyCSPadImage with V00-00-02
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2822 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PyCSPadImage/tags/V00-00-02/src/HDF5Methods.py
@@ -0,0 +1,164 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id$
+#
+# Description:
+#  Module HDF5Methods...
+#
+#------------------------------------------------------------------------
+
+"""This module provides access to the calibration parameters
+
+This software was developed for the SIT project.  If you use all or 
+part of it, please give an appropriate acknowledgment.
+
+@see RelatedModule
+
+@version $Id: 2008-09-22$
+
+@author Mikhail S. Dubrovin
+"""
+
+#------------------------------
+#  Module's version from CVS --
+#------------------------------
+__version__ = "$Revision: 4 $"
+# $Source$
+
+#----------
+#  Imports 
+#----------
+import sys
+import os
+import h5py
+#import numpy as np
+
+#import ConfigParameters as cp
+import CSPadConfigPars as ccp
+
+#---------------------
+#  Class definition --
+#---------------------
+
+class HDF5Methods(object) :
+    """This class contains a few methods to manipulate with hdf5 files"""
+
+#---------------------
+
+    def __init__ (self) :
+        #print """HDF5Methods: Initialization"""
+        self.h5file = None
+        self.dset   = None   
+
+#---------------------
+
+    def open_hdf5_file(self, fname_input=None) :
+
+        if fname_input==None : #self.fname = cp.confpars.dirName+'/'+cp.confpars.fileName
+            print 'open_hdf5_file(fname) : THE HDF5 FILE NAME NEEDS TO BE SPECIFIED !!!'
+        else :
+            self.fname = fname_input
+
+        #print '=== Open HDF5 file: ' + self.fname
+
+        try :
+            self.h5file = h5py.File(self.fname,'r') # open read-only
+            return self.h5file
+
+        except IOError:
+            print 'IOError: CAN NOT OPEN FILE:', self.fname
+            return None
+
+#---------------------
+
+    def close_hdf5_file(self) :
+        self.h5file.close()
+        #print '=== Close HDF5 file ==='
+
+#---------------------
+
+    def get_dataset_from_hdf5_file(self,dsname) :
+        #print 'From hdf5 file get dataset :', dsname
+
+        try :
+            self.dset =  self.h5file[str(dsname)]
+            return self.dset
+        except KeyError:
+            print 80*'!'
+            print 'WARNING:', dsname, ' DATASET DOES NOT EXIST IN HDF5\n'
+            print 80*'!'
+            return None
+
+#---------------------
+
+def print_hdf5_item_structure(g, offset='    ') :
+    """Prints the input file/group/dataset (g) name and begin iterations on its content"""
+    if   isinstance(g,h5py.File) :
+        print g.file, '(File)', g.name
+
+    elif isinstance(g,h5py.Dataset) :
+        print '(Dataset)', g.name, '    len =', g.shape #, g.dtype
+
+    elif isinstance(g,h5py.Group) :
+        print '(Group)', g.name
+
+    else :
+        print 'WORNING: UNKNOWN ITEM IN HDF5 FILE', g.name
+        sys.exit ( "EXECUTION IS TERMINATED" )
+
+    if isinstance(g, h5py.File) or isinstance(g, h5py.Group) :
+        for key,val in dict(g).iteritems() :
+            subg = val
+            print offset, key, #,"   ", subg.name #, val, subg.len(), type(subg),
+            print_hdf5_item_structure(subg, offset + '    ')
+
+#---------------------
+
+def getOneCSPadEventForTest( fname  = '/reg/d/psdm/CXI/cxi35711/hdf5/cxi35711-r0009.h5',
+                             dsname = '/Configure:0000/Run:0000/CalibCycle:0000/CsPad::ElementV2/CxiDs1.0:Cspad.0/data',
+                             event  = 1 ) :
+
+    #ccp.cspadconfig.setCSPadConfiguration( fname, dsname, event )
+    #ccp.cspadconfig.printCSPadConfigPars()
+
+    #file    = h5py.File(fname, 'r')
+    #dataset = file[dsname]
+    #evdata  = dataset[event]
+    #file.close()
+    
+    hdf5file = hdf5mets.open_hdf5_file(fname)
+    ccp.cspadconfig.setCSPadConfigurationFromOpenFile( hdf5file, dsname, event )
+    dataset  = hdf5mets.get_dataset_from_hdf5_file(dsname)
+    evdata   = dataset[event]
+    hdf5mets.close_hdf5_file()
+    return evdata
+
+#---------------------
+
+hdf5mets = HDF5Methods()
+
+#----------------------------------------------
+
+def main() :
+    event   = 1
+    fname   = '/reg/d/psdm/CXI/cxi35711/hdf5/cxi35711-r0009.h5'
+    dsname  = '/Configure:0000/Run:0000/CalibCycle:0000/CsPad::ElementV2/CxiDs1.0:Cspad.0/data'
+
+    h5file = hdf5mets.open_hdf5_file(fname)
+
+    grp = hdf5mets.get_dataset_from_hdf5_file('/')    
+    print_hdf5_item_structure(grp)
+
+    arr = hdf5mets.get_dataset_from_hdf5_file(dsname)
+    print 'arr[event]=\n', arr[event]
+
+    hdf5mets.close_hdf5_file()
+
+#---------------------
+
+if __name__ == "__main__" :
+
+    main()
+    sys.exit ( 'End of test.' )
+
+#----------------------------------------------
