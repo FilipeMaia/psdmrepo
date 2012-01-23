8c4c604bbbc5c267985d067d377437953f0cbda2
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-01-23 20:17:21 +0000

    Work in progress. Save all changes.
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2823 b967ad99-d558-0410-b138-e0f6c56caec7

--- PyCSPadImage/trunk/src/HDF5Methods.py
+++ PyCSPadImage/trunk/src/HDF5Methods.py
@@ -34,7 +34,8 @@ import h5py
 #import numpy as np
 
 #import ConfigParameters as cp
-import CSPadConfigPars as ccp
+import CSPadConfigPars  as ccp
+import GlobalMethods    as gm
 
 #---------------------
 #  Class definition --
@@ -90,6 +91,10 @@ class HDF5Methods(object) :
             return None
 
 #---------------------
+#---------------------
+# Out of class methods
+#---------------------
+#---------------------
 
 def print_hdf5_item_structure(g, offset='    ') :
     """Prints the input file/group/dataset (g) name and begin iterations on its content"""
@@ -111,6 +116,69 @@ def print_hdf5_item_structure(g, offset='    ') :
             subg = val
             print offset, key, #,"   ", subg.name #, val, subg.len(), type(subg),
             print_hdf5_item_structure(subg, offset + '    ')
+
+#---------------------
+
+def get_cspad_name_and_data_type_from_dataset_name( dsname ) :
+    """Returns the detector name and data type as a parts of the dataset name
+    """
+    cspad_name      = ''
+    cspad_data_type = ''
+    if gm.CSpadIsInTheName(dsname) :
+        name1, name2, name3 = gm.get_item_last_three_names(dsname)
+        cspad_name      = name2 # something like CxiDs1.0:Cspad.0
+        cspad_data_type = name3 # something like CsPad::ElementV2
+        #print 'CSPad is found in dataset', dsname
+        #print 'CSPad name and data type =', cs.confcspad.cspad_name, cs.confcspad.cspad_data_type
+        #return name2
+    else :
+        print 'ERROR in get_cspad_name_and_data_type_from_dataset_name( dsname ) for\ndsname =', dsname,\
+              '\nReturn empty the CSPad name and data_type !!!'
+    return cspad_name, cspad_data_type
+
+#---------------------
+
+def get_root_calib_dir_from_hdf5_path( path ) :
+    """Returns the root calib directory, i.e. /reg/d/psdm/CXI/cxi80410/calib/.
+       Search for the "/hdf5" in the path or full file name, split it and add the "/calib/".
+    """
+    ind1 = path.find('/hdf5')
+    if ind1==-1 :
+        print 'ERROR in get_root_calib_dir_from_hdf5_path(): The input path:', path, \
+        'does not contain the expected "/hdf" subdir !!! Return empty root path.'
+        return ''
+        
+    return path[0:ind1] + '/calib/'
+
+#---------------------
+
+def get_cspad_calib_dir( path, cspad_name='CxiDs1.0:Cspad.0' ) :
+    """Constructs the calibration directory name from the path to *.h5 file and the detector name.
+       The lates version of calibration is picked up automatically.
+    """
+    root_calib_dir = get_root_calib_dir_from_hdf5_path( path )
+
+    dirList=gm.getListOfFilesInDir(root_calib_dir)
+    calib_version = '' 
+    for name in dirList: # loop
+        if name[0:5] == 'CsPad' :
+            calib_version = name # something like CsPad::CalibV1
+
+    return root_calib_dir + calib_version + '/' + cspad_name     
+
+#---------------------
+
+def get_run_number_from_hdf5_file_name( fname ) :
+    """Returns the run number from the *-r<run_number>.h5 file name
+    """
+    ind1 = fname.find('-r')
+    ind2 = fname.find('.h5')
+    if ind1==-1 or ind2==-1 or ind2<=ind1 :
+        print 'ERROR in get_run_number_from_hdf5_file_name(): The input file name:', fname, \
+        'is not an expected *-r<run_number>.h5 file name (i.e. cxi80410-r0742.h5) !!! Return the run_num=0'
+        return 0
+    #print 'File name:', fname, 'Run number:', cs.confcspad.run_num
+    return int(fname[ind1+2:ind2])
 
 #---------------------
 
@@ -138,8 +206,12 @@ def getOneCSPadEventForTest( fname  = '/reg/d/psdm/CXI/cxi35711/hdf5/cxi35711-r0
 hdf5mets = HDF5Methods()
 
 #----------------------------------------------
+#----------------------------------------------
+#----------------------------------------------
+#----------------------------------------------
 
-def main() :
+def main_test1() :
+
     event   = 1
     fname   = '/reg/d/psdm/CXI/cxi35711/hdf5/cxi35711-r0009.h5'
     dsname  = '/Configure:0000/Run:0000/CalibCycle:0000/CsPad::ElementV2/CxiDs1.0:Cspad.0/data'
@@ -156,9 +228,29 @@ def main() :
 
 #---------------------
 
+def main_test2() :
+    
+    fname = 'cxi80410-r019742.h5'
+    print '\nTest get_run_number_from_hdf5_file_name(', fname, '), \nrun=',\
+          get_run_number_from_hdf5_file_name(fname)
+
+    dsname = '/Configure:0000/Run:0000/CalibCycle:0000/CsPad::ElementV2/CxiDs1.0:Cspad.0/data'
+    print '\nTest get_cspad_name_and_data_type_from_dataset_name(', dsname,') \nCSPad (name, data_type) =',\
+          get_cspad_name_and_data_type_from_dataset_name(dsname)
+
+    path_and_fname = '/reg/d/psdm/CXI/cxi35711/hdf5/cxi35711-r0009.h5'
+    print '\nTest get_root_calib_dir_from_hdf5_path(', path_and_fname,') \nroot_calib_dir =',\
+          get_root_calib_dir_from_hdf5_path( path_and_fname )
+
+    print '\nTest get_cspad_calib_dir(', path_and_fname,') \ncspad_calib_dir =',\
+          get_cspad_calib_dir( path_and_fname, cspad_name='CxiDs1.0:Cspad.0' )
+
+#---------------------
+
 if __name__ == "__main__" :
 
-    main()
+    #main_test1()
+    main_test2()
     sys.exit ( 'End of test.' )
 
 #----------------------------------------------
