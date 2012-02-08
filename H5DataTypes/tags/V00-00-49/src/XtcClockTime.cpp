7848e9841e8f2f8d9f95c8e1d0bdea0e8467948d
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-02-08 21:32:11 +0000

    Tagging H5DataTypes with V00-00-49
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2898 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ H5DataTypes/tags/V00-00-49/src/XtcClockTime.cpp
@@ -0,0 +1,59 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcClockTime...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "H5DataTypes/XtcClockTime.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5pp/CompoundType.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace H5DataTypes {
+
+XtcClockTime::XtcClockTime ( const Pds::ClockTime& time )
+  : seconds(time.seconds())
+  , nanoseconds(time.nanoseconds())
+{
+}
+
+hdf5pp::Type
+XtcClockTime::stored_type()
+{
+  return native_type() ;
+}
+
+hdf5pp::Type
+XtcClockTime::native_type()
+{
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<XtcClockTime>() ;
+  type.insert_native<uint32_t>( "seconds", offsetof(XtcClockTime,seconds) ) ;
+  type.insert_native<uint32_t>( "nanoseconds", offsetof(XtcClockTime,nanoseconds) ) ;
+
+  return type ;
+}
+
+} // namespace H5DataTypes
