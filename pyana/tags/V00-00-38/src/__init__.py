3dae4826174e629dfe1466aa547ea62bdb9e33f0
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-02-15 19:52:05 +0000

    Tagging pyana with V00-00-38
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2940 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pyana/tags/V00-00-38/src/__init__.py
@@ -0,0 +1,17 @@
+#
+# $Id$
+#
+# Copyright (c) 2010 SLAC National Accelerator Laboratory
+# 
+"""
+This package collects code which deals with various aspects of the analysis tasks.
+"""
+
+__all__ = ['event', 'histo', 'input']
+
+
+# Set of constants used by user modules to 
+# control what framework does
+Skip = 1
+Stop = 2
+Terminate = 3
