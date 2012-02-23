ca1f4b760baf69f559eab7f880c3b48f537b2946
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-02-23 20:48:54 +0000

    Commiting to resolve conflict
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@2981 b967ad99-d558-0410-b138-e0f6c56caec7

--- ImgPixSpectra/trunk/include/CSPad2x2PixSpectra.h
+++ ImgPixSpectra/trunk/include/CSPad2x2PixSpectra.h
@@ -1,12 +1,12 @@
-#ifndef IMGPIXSPECTRA_MINICSPADPIXSPECTRA_H
-#define IMGPIXSPECTRA_MINICSPADPIXSPECTRA_H
+#ifndef IMGPIXSPECTRA_CSPAD2X2PIXSPECTRA_H
+#define IMGPIXSPECTRA_CSPAD2X2PIXSPECTRA_H
 
 //--------------------------------------------------------------------------
 // File and Version Information:
 // 	$Id$
 //
 // Description:
-//	Class MiniCSPadPixSpectra.
+//	Class CSPad2x2PixSpectra.
 //
 //------------------------------------------------------------------------
 
@@ -38,10 +38,10 @@ namespace ImgPixSpectra {
 /**
  *  @ingroup ImgPixSpectra
  *
- *  @brief Creates the spectal array for all pixels in the Mini-CSPad detector.
+ *  @brief Creates the spectal array for all pixels in the CSPad2x2 detector.
  *
- *  MiniCSPadPixSpectra class is a psana module which creates and fills 
- *  the spectral array for all pixels in the Mini-CSPad array. The spectral
+ *  CSPad2x2PixSpectra class is a psana module which creates and fills 
+ *  the spectral array for all pixels in the CSPad2x2 array. The spectral
  *  array has two dimensions, the total number of pixels and
  *  the number of amplitude bins requested in the list of configuration parameters.
  *
@@ -50,43 +50,43 @@ namespace ImgPixSpectra {
  *    @code
  *    [psana]
  *    files         = /reg/d/psdm/CXI/cxi49812/xtc/e157-r0203-s00-c00.xtc
- *    modules       = ImgPixSpectra.MiniCSPadPixSpectra
+ *    modules       = ImgPixSpectra.CSPad2x2PixSpectra
  *    
- *    [ImgPixSpectra.MiniCSPadPixSpectra]
+ *    [ImgPixSpectra.CSPad2x2PixSpectra]
  *    source        = CxiSc1.0:Cspad2x2.0
  *    amin          =   500.
  *    amax          =  1000.
  *    nbins         =   100
- *    arr_fname     = mini-cspad-pix-spectra.txt
+ *    arr_fname     = cspad2x2-pix-spectra.txt
  *    #events       =   500                                                  
  *    @endcode
  *
- *  The output file "mini-cspad-pix-spectra.txt" contains the spectral array 
+ *  The output file "cspad2x2-pix-spectra.txt" contains the spectral array 
  *  for CSPad pixels accumulated in job. 
  *  Axillary file with additional name extension ".sha" contains the shape parameters
  *  of the spectral array. The file(s) can be used for further analysis 
  *  or presentation, for example, using the python script:
  *
  *    @code
- *    ImgPixSpectra/data/PlotSpectralArrayFromFile.py mini-cspad-pix-spectra.txt
+ *    ImgPixSpectra/data/PlotSpectralArrayFromFile.py cspad2x2-pix-spectra.txt
  *    @endcode
  *
  *  This software was developed for the LCLS project. If you use all or 
  *  part of it, please give an appropriate acknowledgment.
  *
- *  @version \$Id: MiniCSPadPixSpectra.h$
+ *  @version \$Id: CSPad2x2PixSpectra.h$
  *
  *  @author Mikhail S. Dubrovin
  */
 
-class MiniCSPadPixSpectra : public Module {
+class CSPad2x2PixSpectra : public Module {
 public:
 
   // Default constructor
-  MiniCSPadPixSpectra (const std::string& name) ;
+  CSPad2x2PixSpectra (const std::string& name) ;
 
   // Destructor
-  virtual ~MiniCSPadPixSpectra () ;
+  virtual ~CSPad2x2PixSpectra () ;
 
   /// Method which is called once at the beginning of the job
   virtual void beginJob(Event& evt, Env& env);
@@ -124,7 +124,7 @@ private:
 
   // Data members, this is for example purposes only
 
-  enum {m_npix_mini1 = 185 * 388 * 2};
+  enum {m_npix_2x2 = 185 * 388 * 2};
   
   Source        m_src;         // Data source set from config file
   Pds::Src      m_actualSrc;
@@ -146,4 +146,4 @@ private:
 
 } // namespace ImgPixSpectra
 
-#endif // IMGPIXSPECTRA_MINICSPADPIXSPECTRA_H
+#endif // IMGPIXSPECTRA_CSPAD2X2PIXSPECTRA_H
--- ImgPixSpectra/trunk/src/CSPad2x2PixSpectra.cpp
+++ ImgPixSpectra/trunk/src/CSPad2x2PixSpectra.cpp
@@ -3,7 +3,7 @@
 // 	$Id$
 //
 // Description:
-//	Class MiniCSPadPixSpectra...
+//	Class CSPad2x2PixSpectra...
 //
 // Author List:
 //      Mikhail S. Dubrovin
@@ -13,7 +13,7 @@
 //-----------------------
 // This Class's Header --
 //-----------------------
-#include "ImgPixSpectra/MiniCSPadPixSpectra.h"
+#include "ImgPixSpectra/CSPad2x2PixSpectra.h"
 
 //-----------------
 // C/C++ Headers --
@@ -28,7 +28,9 @@
 // header from psddl_psana package
 // #include "psddl_psana/acqiris.ddl.h"
 
+//#include "psddl_psana/cspad.ddl.h"
 #include "psddl_psana/cspad2x2.ddl.h"
+
 #include "PSEvt/EventId.h"
 #include "CSPadPixCoords/Image2D.h"
 
@@ -39,7 +41,7 @@
 
 // This declares this class as psana module
 using namespace ImgPixSpectra;
-PSANA_MODULE_FACTORY(MiniCSPadPixSpectra)
+PSANA_MODULE_FACTORY(CSPad2x2PixSpectra)
 
 //		----------------------------------------
 // 		-- Public Function Member Definitions --
@@ -50,7 +52,7 @@ namespace ImgPixSpectra {
 //----------------
 // Constructors --
 //----------------
-MiniCSPadPixSpectra::MiniCSPadPixSpectra (const std::string& name)
+CSPad2x2PixSpectra::CSPad2x2PixSpectra (const std::string& name)
   : Module(name)
   , m_src()
   , m_key()
@@ -68,7 +70,7 @@ MiniCSPadPixSpectra::MiniCSPadPixSpectra (const std::string& name)
   m_amin          = config   ("amin",      0.);
   m_amax          = config   ("amax",   1000.);
   m_nbins         = config   ("nbins",    100);
-  m_arr_fname     = configStr("arr_fname", "mini_cspad_spectral_array.txt");
+  m_arr_fname     = configStr("arr_fname", "cspad2x2_spectral_array.txt");
   m_maxEvents     = config   ("events", 1<<31U);
   m_filter        = config   ("filter", false);
 }
@@ -76,13 +78,13 @@ MiniCSPadPixSpectra::MiniCSPadPixSpectra (const std::string& name)
 //--------------
 // Destructor --
 //--------------
-MiniCSPadPixSpectra::~MiniCSPadPixSpectra ()
+CSPad2x2PixSpectra::~CSPad2x2PixSpectra ()
 {
 }
 
 /// Method which is called once at the beginning of the job
 void 
-MiniCSPadPixSpectra::beginJob(Event& evt, Env& env)
+CSPad2x2PixSpectra::beginJob(Event& evt, Env& env)
 {
   this -> printInputPars();
   this -> arrayInit();
@@ -90,20 +92,20 @@ MiniCSPadPixSpectra::beginJob(Event& evt, Env& env)
 
 /// Method which is called at the beginning of the run
 void 
-MiniCSPadPixSpectra::beginRun(Event& evt, Env& env)
+CSPad2x2PixSpectra::beginRun(Event& evt, Env& env)
 {
 }
 
 /// Method which is called at the beginning of the calibration cycle
 void 
-MiniCSPadPixSpectra::beginCalibCycle(Event& evt, Env& env)
+CSPad2x2PixSpectra::beginCalibCycle(Event& evt, Env& env)
 {
 }
 
 /// Method which is called with event data, this is the only required 
 /// method, all other methods are optional
 void 
-MiniCSPadPixSpectra::event(Event& evt, Env& env)
+CSPad2x2PixSpectra::event(Event& evt, Env& env)
 {
   //// example of getting non-detector data from event
   //shared_ptr<PSEvt::EventId> eventId = evt.get();
@@ -122,27 +124,27 @@ MiniCSPadPixSpectra::event(Event& evt, Env& env)
      or (m_count<500 and m_count%100  == 0) 
      or                  m_count%1000 == 0  ) WithMsgLog(name(), info, log) { log << "event=" << m_count; }
   
-  shared_ptr<Psana::CsPad2x2::ElementV1> mini1 = evt.get(m_src, m_key, &m_actualSrc);
-  if (mini1.get()) {
+  shared_ptr<Psana::CsPad2x2::ElementV1> el_cspad2x2 = evt.get(m_src, m_key, &m_actualSrc);
+  if (el_cspad2x2.get()) {
 
-      const Psana::CsPad2x2::ElementV1& el = *mini1;
+      const Psana::CsPad2x2::ElementV1& el = *el_cspad2x2;
       
       //const int16_t* data = el.data(); // depricated
       const ndarray<int16_t,3>& data_nda = el.data();
       const int16_t* data = &data_nda[0][0][0];
 
       const unsigned* dshape = data_nda.shape();
-      int npix_mini1 = dshape[0] * dshape[1] * dshape[2];  
+      int npix_2x2 = dshape[0] * dshape[1] * dshape[2];  
 
-      if (npix_mini1 != m_npix_mini1) {
-          MsgLog(name(), error, "Unexpected data shape: " << npix_mini1 << ", expected MiniElementV1 data size is " << m_npix_mini1);
+      if (npix_2x2 != m_npix_2x2) {
+          MsgLog(name(), error, "Unexpected data shape: " << npix_2x2 << ", expected ElementV1 data size is " << m_npix_2x2);
           stop();
       }
 
       //WithMsgLog(name(), info, log) { 
-      //  log << "CsPad2x2::ElementV1  dshape.size() = " << dshape.size() << "  shape = ";
+      //  log << "CsPad2x2::ElementV1  dshape.size() = " << dshape.size() << "  shape = "; 
       //for(uint i=0; i<dshape.size(); i++) log << dshape[i] << ", "; 
-      //log << "    npix_mini1 = " << npix_mini1 << "\n";
+      //log << "    npix_2x2 = " << npix_2x2 << "\n";
       //
 
       this -> arrayFill (data);
@@ -154,21 +156,21 @@ MiniCSPadPixSpectra::event(Event& evt, Env& env)
   
 /// Method which is called at the end of the calibration cycle
 void 
-MiniCSPadPixSpectra::endCalibCycle(Event& evt, Env& env)
+CSPad2x2PixSpectra::endCalibCycle(Event& evt, Env& env)
 {
 }
 
 /// Method which is called at the end of the run
 void 
-MiniCSPadPixSpectra::endRun(Event& evt, Env& env)
+CSPad2x2PixSpectra::endRun(Event& evt, Env& env)
 {
 }
 
 /// Method which is called once at the end of the job
 void 
-MiniCSPadPixSpectra::endJob(Event& evt, Env& env)
+CSPad2x2PixSpectra::endJob(Event& evt, Env& env)
 {
-  MsgLog(name(), info, "MiniCSPadPixSpectra::endJob");
+  MsgLog(name(), info, "CSPad2x2PixSpectra::endJob");
   this -> saveArrayInFile();
   this -> saveShapeInFile();
   this -> arrayDelete();
@@ -180,12 +182,12 @@ MiniCSPadPixSpectra::endJob(Event& evt, Env& env)
 //--------------------
 
 void 
-MiniCSPadPixSpectra::arrayInit()
+CSPad2x2PixSpectra::arrayInit()
 {
   m_factor = double(m_nbins) / (m_amax-m_amin);  // scale factor for histogramm index
   m_nbins1 = m_nbins - 1;
 
-  int size = m_npix_mini1 * m_nbins;
+  int size = m_npix_2x2 * m_nbins;
   m_arr    = new int [size];
   for(int i=0; i<size; i++) m_arr[i] = 0;
 }
@@ -193,7 +195,7 @@ MiniCSPadPixSpectra::arrayInit()
 //--------------------
 
 void 
-MiniCSPadPixSpectra::arrayDelete()
+CSPad2x2PixSpectra::arrayDelete()
 {
   delete [] m_arr;
 }
@@ -201,9 +203,9 @@ MiniCSPadPixSpectra::arrayDelete()
 //--------------------
 
 void
-MiniCSPadPixSpectra::arrayFill(const int16_t* data)
+CSPad2x2PixSpectra::arrayFill(const int16_t* data)
 {
-             for (uint32_t i=0; i<m_npix_mini1; i++) {
+             for (uint32_t i=0; i<m_npix_2x2; i++) {
                double amp = (double)data[i];
                int iamp = this -> ampToIndex(amp);
 	       //cout << "pix=" << i << " amp=" << amp << endl;  
@@ -214,23 +216,23 @@ MiniCSPadPixSpectra::arrayFill(const int16_t* data)
 //--------------------
 
 void 
-MiniCSPadPixSpectra::saveArrayInFile()
+CSPad2x2PixSpectra::saveArrayInFile()
 { 
     MsgLog(name(), info, "Save the spectral array in file " << m_arr_fname);
-    CSPadPixCoords::Image2D<int>* arr = new CSPadPixCoords::Image2D<int>(&m_arr[0], m_npix_mini1, m_nbins); 
+    CSPadPixCoords::Image2D<int>* arr = new CSPadPixCoords::Image2D<int>(&m_arr[0], m_npix_2x2, m_nbins); 
     arr -> saveImageInFile(m_arr_fname,0);
 }
 
 //--------------------
 
 void 
-MiniCSPadPixSpectra::saveShapeInFile()
+CSPad2x2PixSpectra::saveShapeInFile()
 { 
     m_arr_shape_fname = m_arr_fname + ".sha";
     MsgLog(name(), info, "Save the spectral array configuration in file " << m_arr_shape_fname);
     ofstream file; 
     file.open(m_arr_shape_fname.c_str(),ios_base::out);
-    file << "NPIXELS  " << m_npix_mini1      << "\n";
+    file << "NPIXELS  " << m_npix_2x2        << "\n";
     file << "NBINS    " << m_nbins           << "\n";
     file << "AMIN     " << m_amin            << "\n";
     file << "AMAX     " << m_amax            << "\n";
@@ -242,7 +244,7 @@ MiniCSPadPixSpectra::saveShapeInFile()
 //--------------------
 
 int  
-MiniCSPadPixSpectra::ampToIndex(double amp)
+CSPad2x2PixSpectra::ampToIndex(double amp)
 {
     int ind = (int) (m_factor*(amp-m_amin));
     if( ind < 0       ) return 0;
@@ -253,7 +255,7 @@ MiniCSPadPixSpectra::ampToIndex(double amp)
 //--------------------
 
 void 
-MiniCSPadPixSpectra::printInputPars()
+CSPad2x2PixSpectra::printInputPars()
 {
   WithMsgLog(name(), info, log) { log 
         << "\n    Input parameters:"
