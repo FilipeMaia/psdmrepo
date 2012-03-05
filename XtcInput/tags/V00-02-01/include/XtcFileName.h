c1b3b291e97eeaf41dacb0056dedc5ed41d8a4ff
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-03-05 22:38:37 +0000

    Tagging XtcInput with V00-02-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3014 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ XtcInput/tags/V00-02-01/include/XtcFileName.h
@@ -0,0 +1,102 @@
+#ifndef XTCINPUT_XTCFILENAME_H
+#define XTCINPUT_XTCFILENAME_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcFileName.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <iosfwd>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace XtcInput {
+
+/**
+ *  Representation of the XTC file name.
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class XtcFileName  {
+public:
+
+  // Default constructor
+  XtcFileName() ;
+  
+  // Construct from a full path name
+  explicit XtcFileName( const std::string& path ) ;
+
+  // Destructor
+  ~XtcFileName () {}
+
+  // get full name
+  const std::string& path() const { return m_path ; }
+
+  // get base name
+  std::string basename() const ;
+
+  // get experiment number
+  unsigned expNum() const { return m_expNum ; }
+
+  // get run number
+  unsigned run() const { return m_run ; }
+
+  // get stream number
+  unsigned stream() const { return m_stream ; }
+
+  // get chunk number
+  unsigned chunk() const { return m_chunk ; }
+
+  // compare two names
+  bool operator<( const XtcFileName& other ) const ;
+
+protected:
+
+private:
+
+  // Data members
+  std::string m_path ;
+  unsigned m_expNum ;
+  unsigned m_run ;
+  unsigned m_stream ;
+  unsigned m_chunk ;
+
+};
+
+std::ostream&
+operator<<(std::ostream& out, const XtcFileName& fn);
+
+} // namespace XtcInput
+
+#endif // XTCINPUT_XTCFILENAME_H
