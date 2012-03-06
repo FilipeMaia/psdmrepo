5521663990b7b8593008d31692b01890aed12c4b
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-03-06 22:14:33 +0000

    Tagging pypdsdata with V00-00-51
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3017 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-51/src/fccd.py
@@ -0,0 +1,48 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id$
+#
+# Description:
+#  Module fccd...
+#
+#------------------------------------------------------------------------
+
+"""Wrapper module for _pdsdata.fccd. This module imports all _pdsdata.fccd 
+classes without any change. 
+
+This software was developed for the LUSI project.  If you use all or 
+part of it, please give an appropriate acknowledgment.
+
+@version $Id$
+
+@author Andrei Salnikov
+"""
+
+
+#------------------------------
+#  Module's version from CVS --
+#------------------------------
+__version__ = "$Revision$"
+# $Source$
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+
+#---------------------------------
+#  Imports of base class module --
+#---------------------------------
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+#------------------------
+# Exported definitions --
+#------------------------
+
+from _pdsdata.fccd import *
---
+++ pypdsdata/tags/V00-00-51/src/opal1k.py
@@ -0,0 +1,48 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id$
+#
+# Description:
+#  Module opal1k...
+#
+#------------------------------------------------------------------------
+
+"""Wrapper module for _pdsdata.opal1k. This module imports all _pdsdata.opal1k 
+classes without any change. 
+
+This software was developed for the LUSI project.  If you use all or 
+part of it, please give an appropriate acknowledgment.
+
+@version $Id$
+
+@author Andrei Salnikov
+"""
+
+
+#------------------------------
+#  Module's version from CVS --
+#------------------------------
+__version__ = "$Revision$"
+# $Source$
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+
+#---------------------------------
+#  Imports of base class module --
+#---------------------------------
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+#------------------------
+# Exported definitions --
+#------------------------
+
+from _pdsdata.opal1k import *
