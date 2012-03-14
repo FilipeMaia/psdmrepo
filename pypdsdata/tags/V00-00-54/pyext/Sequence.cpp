68f02d44016d5bd0ba6ebe1786f702ff71176d32
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-03-14 22:20:45 +0000

    Tagging pypdsdata with V00-00-54
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3081 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-54/pyext/Sequence.cpp
@@ -0,0 +1,112 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Sequence...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "Sequence.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <new>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "ClockTime.h"
+#include "EnumType.h"
+#include "TimeStamp.h"
+#include "TransitionId.h"
+#include "types/TypeLib.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  pypdsdata::EnumType::Enum typeEnumValues[] = {
+      { "Event",      Pds::Sequence::Event },
+      { "Occurrence", Pds::Sequence::Occurrence },
+      { "Marker",     Pds::Sequence::Marker },
+      { 0, 0 }
+  };
+  pypdsdata::EnumType typeEnum ( "Type", typeEnumValues );
+
+  // type-specific methods
+  ENUM_FUN0_WRAPPER_EMBEDDED(pypdsdata::Sequence, type, typeEnum);
+  PyObject* Sequence_service( PyObject* self, PyObject* );
+  FUN0_WRAPPER_EMBEDDED(pypdsdata::Sequence, isExtended);
+  FUN0_WRAPPER_EMBEDDED(pypdsdata::Sequence, isEvent);
+  PyObject* Sequence_clock( PyObject* self, PyObject* );
+  PyObject* Sequence_stamp( PyObject* self, PyObject* );
+
+  PyMethodDef methods[] = {
+    { "type",       type,          METH_NOARGS,
+        "self.type() -> Type\n\nReturns type of this sequence, one of Type.Event, Type.Occurrence, or Type.Marker" },
+    { "service",    Sequence_service, METH_NOARGS, "self.service() -> TransitionId\n\nReturns the TransitionId type" },
+    { "isExtended", isExtended,     METH_NOARGS, "self.isExtended() -> bool\n\nReturns True for extended sequence" },
+    { "isEvent",    isEvent,        METH_NOARGS, "self.isEvent() -> bool\n\nReturns True for event sequence" },
+    { "clock",      Sequence_clock, METH_NOARGS, "self.clock() -> ClockTime\n\nReturns clock value (xtc.ClockTime) for sequence" },
+    { "stamp",      Sequence_stamp, METH_NOARGS, "self.stamp() -> TimeStamp\n\nReturns timestamp value (xtc.TimeStamp) for sequence" },
+    {0, 0, 0, 0}
+   };
+
+  char typedoc[] = "Python class wrapping C++ Pds::Sequence class.";
+
+}
+
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+void
+pypdsdata::Sequence::initType( PyObject* module )
+{
+  PyTypeObject* type = BaseType::typeObject() ;
+  type->tp_doc = ::typedoc;
+  type->tp_methods = ::methods;
+
+  // define class attributes for enums
+  PyObject* tp_dict = PyDict_New();
+  PyDict_SetItemString( tp_dict, "Type", typeEnum.type() );
+  type->tp_dict = tp_dict;
+
+  BaseType::initType( "Sequence", module );
+}
+
+namespace {
+
+PyObject*
+Sequence_service( PyObject* self, PyObject* )
+{
+  pypdsdata::Sequence* py_this = (pypdsdata::Sequence*) self;
+  return pypdsdata::TransitionId::TransitionId_FromInt( py_this->m_obj.service() );
+}
+
+PyObject*
+Sequence_clock( PyObject* self, PyObject* )
+{
+  pypdsdata::Sequence* py_this = (pypdsdata::Sequence*) self;
+  return pypdsdata::ClockTime::PyObject_FromPds( py_this->m_obj.clock() );
+}
+
+PyObject*
+Sequence_stamp( PyObject* self, PyObject* )
+{
+  pypdsdata::Sequence* py_this = (pypdsdata::Sequence*) self;
+  return pypdsdata::TimeStamp::PyObject_FromPds( py_this->m_obj.stamp() );
+}
+
+}
---
+++ pypdsdata/tags/V00-00-54/pyext/types/bld/BldDataEBeamV2.cpp
@@ -0,0 +1,92 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class BldDataEBeamV2...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "BldDataEBeamV2.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "../../Exception.h"
+#include "../TypeLib.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  // methods
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV2, uDamageMask)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV2, fEbeamCharge)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV2, fEbeamL3Energy)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV2, fEbeamLTUPosX)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV2, fEbeamLTUPosY)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV2, fEbeamLTUAngX)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV2, fEbeamLTUAngY)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV2, fEbeamPkCurrBC2)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV2, fEbeamEnergyBC2)
+  PyObject* _repr( PyObject *self );
+
+  PyGetSetDef getset[] = {
+    {"uDamageMask",    uDamageMask,    0, "integer number", 0},
+    {"fEbeamCharge",   fEbeamCharge,   0, "floating number, in nC", 0},
+    {"fEbeamL3Energy", fEbeamL3Energy, 0, "floating number, in MeV", 0},
+    {"fEbeamLTUPosX",  fEbeamLTUPosX,  0, "floating number, in mm", 0},
+    {"fEbeamLTUPosY",  fEbeamLTUPosY,  0, "floating number, in mm", 0},
+    {"fEbeamLTUAngX",  fEbeamLTUAngX,  0, "floating number, in mrad", 0},
+    {"fEbeamLTUAngY",  fEbeamLTUAngY,  0, "floating number, in mrad", 0},
+    {"fEbeamPkCurrBC2", fEbeamPkCurrBC2, 0, "floating number, in Amps", 0},
+    {"fEbeamEnergyBC2", fEbeamEnergyBC2, 0, "floating number, in MeV", 0},
+    {0, 0, 0, 0, 0}
+  };
+
+  char typedoc[] = "Python class wrapping C++ Pds::BldDataEBeamV2 class.";
+}
+
+//              ----------------------------------------
+//              -- Public Function Member Definitions --
+//              ----------------------------------------
+
+void
+pypdsdata::BldDataEBeamV2::initType( PyObject* module )
+{
+  PyTypeObject* type = BaseType::typeObject() ;
+  type->tp_doc = ::typedoc;
+  type->tp_getset = ::getset;
+  type->tp_str = _repr;
+  type->tp_repr = _repr;
+
+  BaseType::initType( "BldDataEBeamV2", module );
+}
+
+namespace {
+
+PyObject*
+_repr( PyObject *self )
+{
+  Pds::BldDataEBeamV2* pdsObj = pypdsdata::BldDataEBeamV2::pdsObject(self);
+  if(not pdsObj) return 0;
+
+  char buf[64];
+  snprintf( buf, sizeof buf, "BldDataEBeamV2(Charge=%f, L3Energy=%f, ...)",
+            pdsObj->fEbeamCharge, pdsObj->fEbeamL3Energy );
+  return PyString_FromString( buf );
+}
+
+}
