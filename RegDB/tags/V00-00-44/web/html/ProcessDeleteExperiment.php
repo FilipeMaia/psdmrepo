4c86a09b27d04894f63fdd33cee7d79814078afc
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-04-23 22:56:37 +0000

    Tagging RegDB with V00-00-44
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3237 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ RegDB/tags/V00-00-44/web/html/ProcessDeleteExperiment.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once( 'regdb/regdb.inc.php' );
+
+use RegDB\RegDB;
+use RegDB\RegDBAuth;
+use RegDB\RegDBException;
+
+/*
+ * This script will process a request for deleting an experiment
+ * from the database.
+ */
+if( !RegDBAuth::instance()->canEdit()) {
+    print( RegDBAuth::reporErrorHtml(
+        'You are not authorized to manage the contents of the Experiment Registry Database'));
+    exit;
+}
+
+if( isset( $_POST['id'] )) {
+    $id = trim( $_POST['id'] );
+    if( $id == '' )
+        die( "experiment identifier can't be empty" );
+} else
+    die( "no valid experiment identifier" );
+
+if( isset( $_POST['actionSuccess'] )) {
+    $actionSuccess = trim( $_POST['actionSuccess'] );
+}
+
+/* Proceed with the operation
+ */
+try {
+    $regdb = new RegDB();
+    $regdb->begin();
+
+    $regdb->delete_experiment_by_id ( $id );
+
+    if( isset( $actionSuccess )) {
+        if( $actionSuccess == 'home' )
+            header( 'Location: index.php' );
+        else if( $actionSuccess == 'list_experiments' )
+            header( 'Location: index.php?action=list_experiments' );
+        else
+            ;
+    }
+    $regdb->commit();
+
+} catch( RegDBException $e ) {
+    print $e->toHtml();
+}
+?>
