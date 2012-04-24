f1304d59faaf481671b51b88ea31efacc0b81e60
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-04-24 00:06:36 +0000

    - fixed a bug in the test
    M    src/test_experiment_info.py
    
    
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3239 b967ad99-d558-0410-b138-e0f6c56caec7

--- RegDB/trunk/src/test_experiment_info.py
+++ RegDB/trunk/src/test_experiment_info.py
@@ -34,12 +34,12 @@ for instr in ('AMO','SXR','XPP','XCS','CXI','MEC','XYZ'):
 
 print ''
 print 'open files of the last run of experiment id 161:'
-for file in get_open_files(161):
+for file in expinfo.get_open_files(161):
     print file
 
 print ''
 print 'open files of run 1332 of experiment id 55:'
-for file in get_open_files(55,1332):
+for file in expinfo.get_open_files(55,1332):
     print file
 
 
