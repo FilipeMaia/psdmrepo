57ab65f0cd1c2071aa1be2ed71a2c6a20700d099
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-05-01 16:40:52 +0000

    Tagging PSCalib with V00-01-02
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3279 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSCalib/tags/V00-01-02/include/CalibFileFinder.h
@@ -0,0 +1,110 @@
+#ifndef PSCALIB_CALIBFILEFINDER_H
+#define PSCALIB_CALIBFILEFINDER_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class CalibFileFinder.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "pdsdata/xtc/Src.hh"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSCalib {
+
+/// @addtogroup PSCalib PSCalib
+
+/**
+ *  @ingroup PSCalib
+ *
+ *  @brief CalibFileFinder class finds the pass to calibration file.
+ *
+ *  When all input parameters are provided at class initialization
+ *  the method findCalibFile(...) returns the path/name to the file 
+ *  with requested calibration parameters.
+ *
+ *  This software was developed for the LCLS project. If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see CSPadCalibPars
+ *
+ *  @version $Id$
+ *
+ *  @author Mikhail S. Dubrovin
+ */
+
+class CalibFileFinder  {
+public:
+
+  // Default constructor
+  CalibFileFinder () {}
+
+  // Default constructor
+
+  /**
+   *  @brief Creates object with elements of the path to the calibration file.
+   *  
+   *  Calibration directory typically comes from environment for psana jobs.
+   *
+   *  @param[in] calibDir   Calibration directory for current experiment.
+   *  @param[in] className  Calibration class name, e.g. CsPad::CalibV1
+   */
+  CalibFileFinder (const std::string& calibDir,
+                   const std::string& className);
+
+  // Destructor
+  ~CalibFileFinder () ;
+
+  /**
+   *  @brief Returns complete path/name of the calibration file.
+   *
+   *  @param[in] src        The name of the data source, e.g. CxiDs1.0:Cspad.0
+   *  @param[in] datatype   Type of the calibration parameters (i.e. "rotation").
+   *  @param[in] runNumber  Run number to search the valid file name.
+   */
+  std::string findCalibFile(const std::string& src, const std::string& datatype, unsigned long runNumber) const;
+
+  /**
+   *  @brief Returns complete path/name of the calibration file.
+   *
+   *  @param[in] src        Address of the data source, only DetInfo addresses are accepted.
+   *  @param[in] datatype   Type of the calibration parameters (i.e. "rotation").
+   *  @param[in] runNumber  Run number to search the valid file name.
+   */
+  std::string findCalibFile(const Pds::Src& src, const std::string& datatype, unsigned long runNumber) const;
+ 
+protected:
+
+private:
+
+  // Data members
+  const std::string m_calibDir;
+  const std::string m_typeGroupName;
+
+};
+
+} // namespace PSCalib
+
+#endif // PSCALIB_CALIBFILEFINDER_H
