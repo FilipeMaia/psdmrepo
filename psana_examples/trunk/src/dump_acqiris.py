084988cdc67daa7fe0a33aa21ffcdcba031d5129
jbarrera@SLAC.STANFORD.EDU <jbarrera@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-05-18 21:44:09 +0000

    Enabled more code now that arrays of structs are working.
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3462 b967ad99-d558-0410-b138-e0f6c56caec7

--- psana_examples/trunk/src/dump_acqiris.py
+++ psana_examples/trunk/src/dump_acqiris.py
@@ -39,6 +39,8 @@ import logging
 
 # local definitions usually start with _
 
+import numpy
+
 #---------------------
 #  Class definition --
 #---------------------
@@ -74,17 +76,15 @@ class dump_acqiris (object) :
         print "nbrSamples =", h.nbrSamples()
 
         nch = config.nbrChannels()
+        vert = config.vert()
         for ch in range(nch):
-            try:
-                v = config.vert(ch)
-                print "  vert(%d):" % ch,
-                print "fullScale =", v.fullScale(),
-                print "slope =", v.slope(),
-                print "offset =", v.offset(),
-                print "coupling =", v.coupling(),
-                print "bandwidth=", v.bandwidth()
-            except:
-                print "  [ ERROR fetching vert(%d) ]" % ch
+            v = vert[ch]
+            print "  vert(%d):" % ch,
+            print "fullScale =", v.fullScale()
+            print "slope =", v.slope()
+            print "offset =", v.offset()
+            print "coupling =", v.coupling()
+            print "bandwidth=", v.bandwidth()
 
     def event( self, evt, env ) :
         acqData = evt.get("Psana::Acqiris::DataDesc", self.source)
@@ -98,33 +98,24 @@ class dump_acqiris (object) :
         nchan = acqData.data_shape()[0];
         for chan in range(nchan):
             elem = acqData.data(chan);
-            print "%s: %s: channel = %d" % (elem.__class__.__name__, self.m_src, chan)
-            print "  nbrSegments =", elem.nbrSegments()
-            print "  nbrSamplesInSeg =", elem.nbrSamplesInSeg()
+            v = acqConfig.vert()[chan]
+            slope = v.slope()
+            offset = v.offset()
 
-            v_array = acqConfig.vert();
-            """
-            v = v_array.at(chan);
-            print "v=", v
-            slope = v.slope();
-            print "slope=", slope
-            offset = v.offset();
-            print "offset=", offset
-            """
+            print "Acqiris::DataDescV1: channel=%d" % chan ### XXX should print real class name instead
+            print "  nbrSegments=%d" % elem.nbrSegments()
+            print "  nbrSamplesInSeg=%d" % elem.nbrSamplesInSeg()
+            print "  indexFirstPoint=%d" % elem.indexFirstPoint()
 
-            timestamp = elem.timestamp()
-            print "type(timestamp)=", type(timestamp)
-            print "timestamp.ndim=", timestamp.ndim
-            print "timestamp.shape=", timestamp.shape
-            print "timestamp.dtype=", timestamp.dtype
-            print "timestamp.itemsize=", timestamp.itemsize
-            print "timestamp.size=", timestamp.size
-            """
-            print "timestamp[0]=", timestamp[0]
-            print "timestamp=", timestamp
-            """
-            #print "elem.timestamp()=", elem.timestamp()
-            #print "  timestamps =", [elem.timestamp(seg) for seg in range(elem.nbrSegments())]
+            timestamps = elem.timestamp()
+            waveforms = elem.waveforms()
 
-            wf = elem.waveforms()
-            print "  waveform [len=%d] = %s" % (len(wf), wf)
+            # loop over segments
+            for seg in range(elem.nbrSegments()):
+                print "  Segment #%d" % seg
+                print "    timestamp=%d" % timestamps[seg].pos()
+                print "    data=[",
+                size = min(elem.nbrSamplesInSeg(), 32)
+                for i in range(size):
+                    print "%f," % (waveforms[seg][i]*slope + offset),
+                print "...]"
