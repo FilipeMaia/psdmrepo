08f61d8b30d9c5cc51d0c5835980b0fda4a93863
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-05-21 19:01:01 +0000

    - saving results of the on-going development
    A    doc
    A    doc/ChangeLog
    A    doc/README
    A    SConscript
    AM   web
    A    web/html
    A    web/lib
    A    web/lib/neocaptar
    A  + web/lib/neocaptar/neocaptarcablenumberalloc.class.php
    A  + web/lib/neocaptar/neocaptardictrouting.class.php
    A  + web/lib/neocaptar/neocaptardictdevicelocation.class.php
    A  + web/lib/neocaptar/neocaptardictconnector.class.php
    A  + web/lib/neocaptar/neocaptarexception.class.php
    A  + web/lib/neocaptar/neocaptarjobnumberalloc.class.php
    A  + web/lib/neocaptar/neocaptarconnection.class.php
    A  + web/lib/neocaptar/neocaptarutils.class.php
    A  + web/lib/neocaptar/neocaptardictinstr.class.php
    A  + web/lib/neocaptar/neocaptarcable.class.php
    A  + web/lib/neocaptar/neocaptarproject.class.php
    A  + web/lib/neocaptar/neocaptardictdeviceregion.class.php
    A  + web/lib/neocaptar/neocaptarcableevent.class.php
    A  + web/lib/neocaptar/neocaptarnotify.class.php
    A  + web/lib/neocaptar/neocaptardictcable.class.php
    A  + web/lib/neocaptar/neocaptardictlocation.class.php
    A  + web/lib/neocaptar/neocaptarnotifyeventtype.class.php
    A  + web/lib/neocaptar/neocaptaruser.class.php
    A  + web/lib/neocaptar/neocaptar.class.php
    A  + web/lib/neocaptar/neocaptarevent.class.php
    A  + web/lib/neocaptar/neocaptarjobnumber.class.php
    A  + web/lib/neocaptar/neocaptardictrack.class.php
    A  + web/lib/neocaptar/neocaptardictpinlist.class.php
    A  + web/lib/neocaptar/neocaptardictdevicecomponent.class.php
    A    web/css
    A    web/js
    
    
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3532 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ NeoCaptar/trunk/web/lib/neocaptar/neocaptarcable.class.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace DataPortal;
+
+require_once( 'dataportal.inc.php' );
+
+use LusiTime\LusiTime;
+
+/**
+ * Class NeoCaptarCable is an abstraction for cables.
+ *
+ * @author gapon
+ */
+class NeoCaptarCable {
+
+   /* Data members
+     */
+    private $connection;
+    private $project;
+
+    public $attr;
+
+    /* Constructor
+     */
+    public function __construct ( $connection, $project, $attr ) {
+        $this->connection = $connection;
+        $this->project = $project;
+        $this->attr = $attr;
+    }
+
+    /*
+     * ======================
+     *   OBJECT ATTRIBUTES
+     * ======================
+     */
+    public function project() { return $this->project; }
+
+    public function id     () { return $this->attr['id'    ]; }
+    public function status () { return $this->attr['status']; }
+
+    public function cable  () { return $this->attr['cable' ]; }
+
+    public function device          () { return $this->attr['device'          ]; }
+    public function device_location () { return $this->attr['device_location' ]; }
+    public function device_region   () { return $this->attr['device_region'   ]; }
+    public function device_component() { return $this->attr['device_component']; }
+    public function device_counter  () { return $this->attr['device_counter'  ]; }
+    public function device_suffix   () { return $this->attr['device_suffix'   ]; }
+
+    public function func      () { return $this->attr['func'      ]; }
+    public function length    () { return $this->attr['length'    ]; }
+    public function cable_type() { return $this->attr['cable_type']; }
+    public function routing   () { return $this->attr['routing'   ]; }
+
+    public function origin_name    () { return $this->attr['origin_name'    ]; }
+	public function origin_loc     () { return $this->attr['origin_loc'     ]; }
+	public function origin_rack    () { return $this->attr['origin_rack'    ]; }
+	public function origin_ele     () { return $this->attr['origin_ele'     ]; }
+	public function origin_side    () { return $this->attr['origin_side'    ]; }
+	public function origin_slot    () { return $this->attr['origin_slot'    ]; }
+	public function origin_station () { return $this->attr['origin_station' ]; }
+	public function origin_conn    () { return $this->attr['origin_conn'    ]; }
+	public function origin_conntype() { return $this->attr['origin_conntype']; }
+	public function origin_pinlist () { return $this->attr['origin_pinlist' ]; }
+	public function origin_instr   () { return $this->attr['origin_instr'   ]; }
+
+    public function destination_name    () { return $this->attr['destination_name'    ]; }
+	public function destination_loc     () { return $this->attr['destination_loc'     ]; }
+	public function destination_rack    () { return $this->attr['destination_rack'    ]; }
+	public function destination_ele     () { return $this->attr['destination_ele'     ]; }
+	public function destination_side    () { return $this->attr['destination_side'    ]; }
+	public function destination_slot    () { return $this->attr['destination_slot'    ]; }
+	public function destination_station () { return $this->attr['destination_station' ]; }
+	public function destination_conn    () { return $this->attr['destination_conn'    ]; }
+	public function destination_conntype() { return $this->attr['destination_conntype']; }
+	public function destination_pinlist () { return $this->attr['destination_pinlist' ]; }
+	public function destination_instr   () { return $this->attr['destination_instr'   ]; }
+
+    public function history() {
+        $list = array();
+        $result = $this->connection->query("SELECT * FROM {$this->connection->database}.cable_history WHERE cable_id={$this->id()} ORDER BY event_time");
+        for( $i = 0, $nrows = mysql_numrows( $result ); $i < $nrows; $i++ )
+            array_push(
+                $list,
+                new NeoCaptarCableEvent(
+                    $this->connection,
+                    $this,
+                    mysql_fetch_array( $result, MYSQL_ASSOC )));
+        return $list;
+    }
+    public function history_last_entry() {
+        $list = array();
+        $result = $this->connection->query("SELECT * FROM {$this->connection->database}.cable_history WHERE cable_id={$this->id()} ORDER BY event_time DESC LIMIT 1");
+        $nrows = mysql_numrows($result);
+        if( !$nrows ) return null;
+        if( 1 != $nrows )
+            throw new NeoCaptarException(__METHOD__, "database schema problem");
+        return new NeoCaptarCableEvent(
+            $this->connection,
+            $this,
+            mysql_fetch_array($result, MYSQL_ASSOC));
+    }
+
+    /*
+     * ======================
+     *   DATABASE MODIFIERS
+     * ======================
+     */
+    public function clone_self() {
+        return $this->project()->clone_cable($this);
+    }
+
+    /**
+     * Update cable attributes from the specified attribute/value pairs
+     * passed in the input array.
+     *
+     * @param array $params
+     * @return NeoCaptarCable
+     */
+    public function update_self($params) {
+        return $this->project()->update_cable($this, $params);
+    }
+}
+?>
---
+++ NeoCaptar/trunk/web/lib/neocaptar/neocaptarproject.class.php
@@ -0,0 +1,222 @@
+<?php
+
+namespace DataPortal;
+
+require_once( 'dataportal.inc.php' );
+
+use LusiTime\LusiTime;
+
+/**
+ * Class NeoCaptarProject is an abstraction for projects.
+ *
+ * @author gapon
+ */
+class NeoCaptarProject {
+
+   /* Data members
+     */
+    private $connection;
+    private $neocaptar;
+
+    public $attr;
+
+    /* Constructor
+     */
+    public function __construct ( $connection, $neocaptar, $attr ) {
+        $this->connection = $connection;
+        $this->neocaptar = $neocaptar;
+        $this->attr = $attr;
+    }
+
+    /*
+     * ======================
+     *   OBJECT ATTRIBUTES
+     * ======================
+     */
+    public function neocaptar    () { return $this->neocaptar; }
+    public function id           () { return $this->attr['id']; }
+    public function owner        () { return $this->attr['owner']; }
+    public function title        () { return $this->attr['title']; }
+    public function job          () { return $this->attr['job']; }
+    public function description  () { return $this->attr['description']; }
+    public function created_time () { return LusiTime::from64( $this->attr['created_time'] ); }
+    public function due_time     () { return LusiTime::from64( $this->attr['due_time'] ); }
+    public function modified_time() { return LusiTime::from64( $this->attr['modified_time'] ); }
+
+    /*
+     * =================================
+     *   INFORMATION REQUEST OPERATION
+     * =================================
+     */
+    public function status() {
+        $counters = array(
+            'total'        => 0,
+            'Planned'      => 0,
+            'Registered'   => 0,
+            'Labeled'      => 0,
+            'Fabrication'  => 0,
+            'Ready'        => 0,
+            'Installed'    => 0,
+            'Commissioned' => 0,
+            'Damaged'      => 0,
+            'Retired'      => 0
+        );
+        $sql = "SELECT status, count(status) as 'count' FROM {$this->connection->database}.cable WHERE project_id={$this->id()} GROUP BY status";
+        $result = $this->connection->query ( $sql );
+        $nrows = mysql_numrows( $result );
+        for( $i = 0; $i < $nrows; $i++ ) {
+            $attr = mysql_fetch_array( $result, MYSQL_ASSOC );
+            $status = $attr['status'];
+            $count  = $attr['count'];
+            $counters[$status] = $count;
+            $counters['total'] += $count;
+        }
+        return $counters;
+    }
+    public function cables() {
+        $list = array();
+        $sql = "SELECT * FROM {$this->connection->database}.cable WHERE project_id={$this->id()} ORDER BY id DESC";
+        $result = $this->connection->query( $sql );
+        $nrows = mysql_numrows( $result );
+        for( $i = 0; $i < $nrows; $i++ )
+            array_push (
+                $list,
+                new NeoCaptarCable (
+                    $this->connection,
+                    $this,
+                    mysql_fetch_array( $result, MYSQL_ASSOC )));
+
+        return $list;
+    }
+    public function find_cable_by_id( $id ) {
+    	return $this->neocaptar()->find_cable_by_id( $id );
+    }
+    private function find_cable_by_( $condition ) {
+        $sql = "SELECT * FROM {$this->connection->database}.cable WHERE project_id={$this->id()} AND {$condition}";
+        $result = $this->connection->query ( $sql );
+        $nrows = mysql_numrows( $result );
+        if( $nrows == 0 ) return null;
+        if( $nrows != 1 )
+        	throw new NeoCaptarException (
+        		__METHOD__, "inconsistent result returned by the query. Database may be corrupt." );
+        return new NeoCaptarCable (
+        	$this->connection,
+            $this,
+            mysql_fetch_array( $result, MYSQL_ASSOC ));
+    }
+
+    /*
+     * ======================
+     *   DATABASE MODIFIERS
+     * ======================
+     */
+    public function add_cable() {
+        $sql = "INSERT INTO {$this->connection->database}.cable VALUES(NULL,{$this->id()},'Planned','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','')";
+        $this->connection->query($sql);
+        $new_cable = $this->find_cable_by_('id IN (SELECT LAST_INSERT_ID())');
+        if( is_null($new_cable)) return null;
+        $comments = array();
+        $this->neocaptar->add_cable_event($new_cable,'Created',$comments);
+        return $new_cable;
+    }
+    public function clone_cable($c) {
+        if(is_null($c))
+            throw new NeoCaptarException (
+        		__METHOD__, "wrong parameter: null object passed into the method." );
+        $sql =
+            "INSERT INTO {$this->connection->database}.cable VALUES(NULL,{$this->id()},".
+            "'Planned',".   // status must be reset
+            "'',".                              // cable number can't be cloned
+            "'{$this->connection->escape_string(trim($c->device()))}',".
+            "'{$this->connection->escape_string(trim($c->device_location()))}',".
+            "'{$this->connection->escape_string(trim($c->device_region()))}',".
+            "'{$this->connection->escape_string(trim($c->device_component()))}',".
+            "'{$this->connection->escape_string(trim($c->device_counter()))}',".
+            "'{$this->connection->escape_string(trim($c->device_suffix()))}',".
+            "'{$this->connection->escape_string(trim($c->func()))}',".
+            "'{$this->connection->escape_string(trim($c->length()))}',".
+            "'{$this->connection->escape_string(trim($c->cable_type()))}',".
+            "'{$this->connection->escape_string(trim($c->routing()))}',".
+
+            "'{$this->connection->escape_string(trim($c->origin_name()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_loc()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_rack()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_ele()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_side()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_slot()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_conn()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_conntype()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_pinlist()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_station()))}',".
+            "'{$this->connection->escape_string(trim($c->origin_instr()))}',".
+
+            "'{$this->connection->escape_string(trim($c->destination_name()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_loc()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_rack()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_ele()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_side()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_slot()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_conn()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_conntype()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_pinlist()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_station()))}',".
+            "'{$this->connection->escape_string(trim($c->destination_instr()))}'".
+            ")";
+        $this->connection->query($sql);
+        $new_cable = $this->find_cable_by_('id IN (SELECT LAST_INSERT_ID())');
+        if( is_null($new_cable)) return null;
+        $comments = array();
+        array_push($comments,"Cloned from project: {$c->project()->title()}");
+        foreach( $new_cable->attr as $property => $new_value ) {
+            array_push($comments, "{$property}: {$new_value}");
+        }
+        $this->neocaptar->add_cable_event($new_cable,'Created',$comments);
+        return $new_cable;
+    }
+    public function update_cable($cable,$params) {
+        if(is_null($cable))
+            throw new NeoCaptarException (
+        		__METHOD__, "wrong parameter: null cable object passed into the method." );
+        if(is_null($params))
+            throw new NeoCaptarException (
+        		__METHOD__, "wrong parameter: null attributes object passed into the method." );
+
+
+        // Update the cable
+        //
+        $first = true;
+        $sql = "UPDATE {$this->connection->database}.cable ";
+        foreach( $params as $p => $v ) {
+            $sql .= $first ? "SET " : ",";
+            $sql .= "{$p}='".$this->connection->escape_string(trim($v))."'";
+            $first = false;
+        }
+        if( $first ) return $cable; // nothing to be updated
+        $sql .= " WHERE id={$cable->id()}";
+        $this->connection->query($sql);
+
+        // Update the host project's modificaton time too
+        //
+        $now_64 = LusiTime::now()->to64();
+        $sql = "UPDATE {$this->connection->database}.project SET modified_time={$now_64} WHERE id={$this->id()}";
+        $this->connection->query($sql);
+
+        $new_cable = $this->find_cable_by_id($cable->id());  // fetch the updated object
+        if( is_null($new_cable)) return null;
+
+        $comments = array();
+        foreach( $new_cable->attr as $property => $new_value ) {
+            $old_value = $cable->attr[$property];
+            if( $new_value != $old_value ) {
+                array_push($comments, "{$property}: {$old_value} -> {$new_value}");
+            }
+        }
+        if( !count($comments )) array_push($comments,'no changes to the cable were made');
+        $this->neocaptar->add_cable_event($new_cable,'Modified', $comments);
+        return $new_cable;
+    }
+    public function delete_cable_by_id( $id ) {
+    	return $this->neocaptar()->delete_cable_by_id( $id );
+    }
+}
+?>
