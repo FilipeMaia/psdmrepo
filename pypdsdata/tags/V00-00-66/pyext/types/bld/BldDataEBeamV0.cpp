052647caa1b1227cc6552dfff131f95f312c8c2e
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-06-07 01:02:16 +0000

    Tagging pypdsdata with V00-00-66
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3751 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-66/pyext/types/bld/BldDataEBeamV0.cpp
@@ -0,0 +1,88 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class BldDataEBeamV0...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "BldDataEBeamV0.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "../../Exception.h"
+#include "../TypeLib.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  // methods
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV0, uDamageMask)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV0, fEbeamCharge)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV0, fEbeamL3Energy)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV0, fEbeamLTUPosX)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV0, fEbeamLTUPosY)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV0, fEbeamLTUAngX)
+  MEMBER_WRAPPER(pypdsdata::BldDataEBeamV0, fEbeamLTUAngY)
+  PyObject* _repr( PyObject *self );
+
+  PyGetSetDef getset[] = {
+    {"uDamageMask",    uDamageMask,    0, "integer number", 0},
+    {"fEbeamCharge",   fEbeamCharge,   0, "floating number, in nC", 0},
+    {"fEbeamL3Energy", fEbeamL3Energy, 0, "floating number, in MeV", 0},
+    {"fEbeamLTUPosX",  fEbeamLTUPosX,  0, "floating number, in mm", 0},
+    {"fEbeamLTUPosY",  fEbeamLTUPosY,  0, "floating number, in mm", 0},
+    {"fEbeamLTUAngX",  fEbeamLTUAngX,  0, "floating number, in mrad", 0},
+    {"fEbeamLTUAngY",  fEbeamLTUAngY,  0, "floating number, in mrad", 0},
+    {0, 0, 0, 0, 0}
+  };
+
+  char typedoc[] = "Python class wrapping C++ Pds::BldDataEBeamV0 class.";
+}
+
+//              ----------------------------------------
+//              -- Public Function Member Definitions --
+//              ----------------------------------------
+
+void
+pypdsdata::BldDataEBeamV0::initType( PyObject* module )
+{
+  PyTypeObject* type = BaseType::typeObject() ;
+  type->tp_doc = ::typedoc;
+  type->tp_getset = ::getset;
+  type->tp_str = _repr;
+  type->tp_repr = _repr;
+
+  BaseType::initType( "BldDataEBeamV0", module );
+}
+
+namespace {
+
+PyObject*
+_repr( PyObject *self )
+{
+  Pds::BldDataEBeamV0* pdsObj = pypdsdata::BldDataEBeamV0::pdsObject(self);
+  if(not pdsObj) return 0;
+
+  char buf[64];
+  snprintf( buf, sizeof buf, "BldDataEBeamV0(Charge=%g, L3Energy=%g, ...)",
+            pdsObj->fEbeamCharge, pdsObj->fEbeamL3Energy );
+  return PyString_FromString( buf );
+}
+
+}
