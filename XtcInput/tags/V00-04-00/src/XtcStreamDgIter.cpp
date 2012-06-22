8d6c81235cffe0efdd5d525086cb39e311e9dd40
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-06-22 21:19:16 +0000

    Tagging XtcInput with V00-04-00
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3883 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ XtcInput/tags/V00-04-00/src/XtcStreamDgIter.cpp
@@ -0,0 +1,124 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcStreamDgIter...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "XtcInput/XtcStreamDgIter.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+#include "XtcInput/Exceptions.h"
+#include "XtcInput/XtcChunkDgIter.h"
+#include "pdsdata/xtc/Xtc.hh"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  const char* logger = "XtcStreamDgIter" ;
+
+}
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace XtcInput {
+
+//----------------
+// Constructors --
+//----------------
+XtcStreamDgIter::XtcStreamDgIter ( const std::list<XtcFileName>& files, size_t maxDgSize, bool skipDamaged )
+  : m_files(files)
+  , m_maxDgSize(maxDgSize)
+  , m_skipDamaged(skipDamaged)
+  , m_iter()
+  , m_dgiter(0)
+  , m_count(0)
+{
+  m_iter = m_files.begin();
+}
+
+//--------------
+// Destructor --
+//--------------
+XtcStreamDgIter::~XtcStreamDgIter ()
+{
+  delete m_dgiter ;
+}
+
+// read next datagram, return zero pointer after last file has been read,
+// throws exception for errors.
+Dgram::ptr
+XtcStreamDgIter::next()
+{
+  Dgram::ptr dgram;
+  while ( not dgram.get() ) {
+
+    if ( not m_dgiter ) {
+
+      if ( m_iter == m_files.end() ) break ;
+
+      // open next xtc file if there is none open
+      MsgLog( logger, trace, "processing file: " << m_iter->path() ) ;
+      m_dgiter = new XtcChunkDgIter ( m_iter->path(), m_maxDgSize ) ;
+      m_count = 0 ;
+    }
+
+    // try to read next event from it
+    dgram = m_dgiter->next() ;
+    ++ m_count ;
+
+    // if failed to read go to next file
+    if ( not dgram.get() ) {
+      delete m_dgiter ;
+      m_dgiter = 0 ;
+
+      ++ m_iter ;
+    } else if ( m_skipDamaged ) {
+
+      // get rid of damaged datagrams
+      const Pds::Xtc& xtc = dgram->xtc ;
+      if ( xtc.damage.value() ) {
+        MsgLog( logger, warning, "XTC damage: " << std::hex << xtc.damage.value() << std::dec
+            << " level: " << int(xtc.src.level()) << '#' << Pds::Level::name(xtc.src.level())
+            << " type: " << int(xtc.contains.id()) << '#' << Pds::TypeId::name(xtc.contains.id())
+            << "/V" << xtc.contains.version()
+            << "\n    Skipping damaged event -- file: " << m_iter->path() << " event: " << m_count ) ;
+        dgram.reset();
+      }
+
+    }
+
+  }
+
+  return dgram ;
+}
+
+// get current file name
+XtcFileName
+XtcStreamDgIter::chunkName() const
+{
+  if ( m_iter == m_files.end() ) return XtcFileName() ;
+  return *m_iter ;
+}
+
+} // namespace XtcInput
