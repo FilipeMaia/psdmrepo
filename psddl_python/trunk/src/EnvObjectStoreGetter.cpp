b2b22a52327b9dca35f5494a13dbf896b16b47cd
jbarrera@SLAC.STANFORD.EDU <jbarrera@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-06-28 18:44:33 +0000

    Move common code to GenericGetter.h
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3944 b967ad99-d558-0410-b138-e0f6c56caec7

--- psddl_python/trunk/src/EnvObjectStoreGetter.cpp
+++ psddl_python/trunk/src/EnvObjectStoreGetter.cpp
@@ -1,31 +1,8 @@
-#include <psddl_python/EnvObjectStoreGetter.h>
+#define CLASS EnvObjectStoreGetter
+#define CLASS_NAME "EnvObjectStoreGetter"
+#define MAP envObjectStoreGetterMap
+#define ARGS store, source, foundSrc
+#define ARGS_DECL string typeName, EnvObjectStore& store, const Source& source, Src* foundSrc
 
-namespace Psana {
-  object EnvObjectStoreGetter::get(string typeName, EnvObjectStore& store, const Source& source, Src* foundSrc) {
-    EnvObjectStoreGetter* getter = (EnvObjectStoreGetter*) envObjectStoreGetterMap.getGetter(typeName);
-    if (getter) {
-      return getter->get(store, source, foundSrc);
-    }
-    int versionMin, versionMax;
-    const char* _template = envObjectStoreGetterMap.getTemplate(typeName, &versionMin, &versionMax);
-    if (! _template) {
-      return object();
-    }
-    for (int version = versionMin; version <= versionMax; version++) {
-      char vTypeName[128];
-      sprintf(vTypeName, _template, version);
-      getter = (EnvObjectStoreGetter*) envObjectStoreGetterMap.getGetter(vTypeName);
-      if (getter) {
-        object result(getter->get(store, source, foundSrc));
-        if (result != object()) {
-          return result;
-        }
-        printf("EnvObjectStoreGetter: Tried %s but it returned null\n", vTypeName);
-      } else {
-        printf("EnvObjectStoreGetter: %s does not exist\n", vTypeName);
-      }
-    }
-    printf("EnvObjectStoreGetter: found no object for %s\n", _template);
-    return object();
-  }
-}
+#include <psddl_python/EnvObjectStoreGetter.h>
+#include <psddl_python/GenericGetter.h>
--- psddl_python/trunk/src/EventGetter.cpp
+++ psddl_python/trunk/src/EventGetter.cpp
@@ -1,33 +1,8 @@
-#include <psddl_python/EventGetter.h>
+#define CLASS EventGetter
+#define CLASS_NAME "EventGetter"
+#define MAP eventGetterMap
+#define ARGS event, source, key, foundSrc
+#define ARGS_DECL string typeName, Event& event, Source& source, const std::string& key, Src* foundSrc
 
-namespace Psana {
-  object EventGetter::get(string typeName, Event& event, Source& source, const std::string& key, Src* foundSrc) {
-    EventGetter* getter = (EventGetter*) eventGetterMap.getGetter(typeName);
-    if (getter) {
-      return getter->get(event, source, key, foundSrc);
-    }
-    int versionMin, versionMax;
-    const char* _template = eventGetterMap.getTemplate(typeName, &versionMin, &versionMax);
-    if (! _template) {
-      printf("EventGetter: %s does not exist\n", typeName.c_str());
-      return object();
-    }
-    for (int version = versionMin; version <= versionMax; version++) {
-      char vTypeName[128];
-      sprintf(vTypeName, _template, version);
-      getter = (EventGetter*) eventGetterMap.getGetter(vTypeName);
-      if (getter) {
-        object result(getter->get(event, source, key, foundSrc));
-        if (result != object()) {
-          printf("EventGetter: returning result of type %s\n", vTypeName);
-          return result;
-        }
-        //printf("EventGetter: tried %s but it returned null\n", vTypeName);
-      } else {
-        printf("EventGetter: %s does not exist\n", vTypeName);
-      }
-    }
-    printf("EventGetter: found no object for %s\n", _template);
-    return object();
-  }
-}
+#include <psddl_python/EventGetter.h>
+#include <psddl_python/GenericGetter.h>
