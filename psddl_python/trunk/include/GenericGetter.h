ec4b8a28bcd8321732bbd75246c2c3b530ec33de
jbarrera@SLAC.STANFORD.EDU <jbarrera@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-06-28 18:43:47 +0000

    Put common code for EnvObjectStoreGetter.h and EventGetter.h into this file.
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@3943 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl_python/trunk/include/GenericGetter.h
@@ -0,0 +1,54 @@
+// This is not a conventional include file, but rather a template
+// included by EventGetter.cpp and EnvObjectStore.cpp.
+
+#include <sstream>
+#include "PSEvt/EventKey.h" // operator<<(Pds::Src) defined here
+
+namespace Psana {
+  static string toString(const Src& src) {
+    std::ostringstream stream;
+    stream << src;
+    return stream.str();
+  }
+
+  static void printGetResult(const char* typeName, const char* foundTypeName, const Src* foundSrc) {
+    printf("get(%s) -> %s -> %s\n",
+           typeName, foundTypeName, toString(*foundSrc).c_str());
+  }
+
+  object CLASS::get(ARGS_DECL) {
+    Src src;
+    if (foundSrc == NULL) {
+      foundSrc = &src;
+    }
+    CLASS* getter = (CLASS*) MAP.getGetter(typeName);
+    if (getter) {
+      object result(getter->get(ARGS));
+      printGetResult(typeName.c_str(), typeName.c_str(), foundSrc);
+      return result;
+    }
+    int versionMin, versionMax;
+    const char* _template = MAP.getTemplate(typeName, &versionMin, &versionMax);
+    if (! _template) {
+      printf("%s: %s does not exist\n", CLASS_NAME, typeName.c_str());
+      return object();
+    }
+    for (int version = versionMin; version <= versionMax; version++) {
+      char vTypeName[128];
+      sprintf(vTypeName, _template, version);
+      getter = (CLASS*) MAP.getGetter(vTypeName);
+      if (getter) {
+        object result(getter->get(ARGS));
+        if (result != object()) {
+          printGetResult(typeName.c_str(), vTypeName, foundSrc);
+          return result;
+        }
+        //printf("%s: tried %s but it returned null\n", CLASS_NAME, vTypeName);
+      } else {
+        printf("%s: %s does not exist\n", CLASS_NAME, vTypeName);
+      }
+    }
+    printf("%s: found no object for %s\n", CLASS_NAME, _template);
+    return object();
+  }
+}
