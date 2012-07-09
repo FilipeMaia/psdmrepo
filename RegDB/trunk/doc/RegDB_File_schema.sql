67299f8165274cd5d9b708a2839e64ca838ce26d
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-07-09 22:05:11 +0000

    - extended MySQL schema to support Fast Feed Back (FFB) data recording
    M    doc/RegDB_File_schema.sql
    M    doc/ChangeLog
    
    
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4089 b967ad99-d558-0410-b138-e0f6c56caec7

--- RegDB/trunk/doc/RegDB_File_schema.sql
+++ RegDB/trunk/doc/RegDB_File_schema.sql
@@ -27,6 +27,30 @@ CREATE  TABLE IF NOT EXISTS `REGDB`.`FILE` (
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `REGDB`.`FILE_FFB`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `REGDB`.`FILE_FFB` ;
+
+CREATE  TABLE IF NOT EXISTS `REGDB`.`FILE_FFB` (
+  `exper_id` INT NOT NULL ,
+  `run` INT NOT NULL,
+  `stream` INT NOT NULL,
+  `chunk` INT NOT NULL,
+  `open` BIGINT UNSIGNED NOT NULL ,
+  `host` VARCHAR(255) NOT NULL ,
+  `dirpath` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`exper_id`,`run`,`stream`,`chunk`) ,
+  KEY `FILE_FFB_FK_1` (`exper_id`) ,
+  CONSTRAINT `FILE_FFB_FK_1`
+    FOREIGN KEY (`exper_id` )
+    REFERENCES `REGDB`.`EXPERIMENT` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
