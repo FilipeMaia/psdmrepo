40f31ac90659c4b44191942b35515bcaceabb500
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-07-09 21:46:40 +0000

    - extended support for the data migration
    M    doc/ChangeLog
    M    web/lib/regdb/regdbfile.class.php
    
    
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4085 b967ad99-d558-0410-b138-e0f6c56caec7

--- RegDB/trunk/web/lib/regdb/regdbfile.class.php
+++ RegDB/trunk/web/lib/regdb/regdbfile.class.php
@@ -43,5 +43,35 @@ class RegDBFile {
         
     public function open_time () {
         return LusiTime::from64( $this->attr['open'] ); }
+
+    /**
+     * Find the migration status of the file if this information is available.
+     * Otherwise return null.
+     * 
+     * NOTES: It's not guaranteed that any information will be found for the file
+     * in the data migration database. Normally it's only available for the latest
+     * XTC files. And it's not guaranteed to be preserved in the database
+     * forever. So, always check for a value returned by the method.
+     */
+    public function data_migration_file() {
+        $table    = "{$this->connection->database}.data_migration";
+        $filetype = 'xtc';
+        $filename = sprintf("e%d-r%03d-s%02d-c%02d.%s", $this->experiment->id(), $this->run(), $this->stream(), $this-chunk(), $filetype);
+        $result = $this->connection->query(
+            "SELECT * FROM {$table} WHERE exper_id={$this->experiment()->id()} AND file='{$filename}' AND file_type='{$filetype}'" );
+
+        $nrows = mysql_numrows( $result );
+        if( 0 == $nrows ) return null;
+        if( 1 != $nrows )
+            throw new RegDBException(
+                __METHOD__,
+                "unexpected size of result set returned by the query; the database may be corrupt or has improper schema" );
+
+        return new RegDBDataMigrationFile (
+            $this->connection,
+            $this->experiment(),
+            mysql_fetch_array( $result, MYSQL_ASSOC ));
+   }
+   
 }
 ?>
