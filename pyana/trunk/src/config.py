f608d18e04d08937f1be2de2d9f8d1239db1b0a2
jbarrera@SLAC.STANFORD.EDU <jbarrera@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-07-09 17:06:13 +0000

    In code to handle psana-style '\' continuation characters, guard against zero-length strings.
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4077 b967ad99-d558-0410-b138-e0f6c56caec7

--- pyana/trunk/src/config.py
+++ pyana/trunk/src/config.py
@@ -197,10 +197,11 @@ class config ( object ) :
             try:
                 _log.debug("getJobConfig section=%s option=%s", section, option)
                 strval = self._config.get(section, option)
-                # remove psana-style continuation characters
-                strval = strval.replace("\\\n", " ")
-                if strval[len(strval) - 1] == "\\":
-                    strval = strval[:len(strval) - 1]
+                if len(strval) > 0:
+                    # remove psana-style continuation characters
+                    strval = strval.replace("\\\n", " ")
+                    if strval[len(strval) - 1] == "\\":
+                        strval = strval[:len(strval) - 1]
                 # convert it to correct type
                 return opt[1](strval)
             except ConfigParser.NoSectionError:
