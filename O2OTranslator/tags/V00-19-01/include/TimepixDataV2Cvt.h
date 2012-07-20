64e4526e4baf9286f628e588633437281cf51969
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-07-20 17:36:19 +0000

    Tagging O2OTranslator with V00-19-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4179 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ O2OTranslator/tags/V00-19-01/include/TimepixDataV2Cvt.h
@@ -0,0 +1,100 @@
+#ifndef O2OTRANSLATOR_TIMEPIXDATAV2CVT_H
+#define O2OTRANSLATOR_TIMEPIXDATAV2CVT_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class TimepixDataV2Cvt.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "O2OTranslator/EvtDataTypeCvt.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "H5DataTypes/TimepixDataV2.h"
+#include "O2OTranslator/CvtDataContainer.h"
+#include "O2OTranslator/CvtDataContFactoryDef.h"
+#include "O2OTranslator/CvtDataContFactoryTyped.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace O2OTranslator {
+
+/**
+ *  Special converter class for Pds::Timepix::DataV2 XTC class
+ *
+ *  This software was developed for the LCLS project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class TimepixDataV2Cvt : public EvtDataTypeCvt<Pds::Timepix::DataV2> {
+public:
+
+  typedef Pds::Timepix::DataV2 XtcType ;
+
+  // constructor
+  TimepixDataV2Cvt ( const std::string& typeGroupName,
+                     hsize_t chunk_size,
+                     int deflate ) ;
+
+  // Destructor
+  virtual ~TimepixDataV2Cvt () ;
+
+protected:
+
+  // typed conversion method
+  virtual void typedConvertSubgroup ( hdf5pp::Group group,
+                                      const XtcType& data,
+                                      size_t size,
+                                      const Pds::TypeId& typeId,
+                                      const O2OXtcSrc& src,
+                                      const H5DataTypes::XtcClockTime& time ) ;
+
+  /// method called when the driver closes a group in the file
+  virtual void closeSubgroup( hdf5pp::Group group ) ;
+
+private:
+
+  typedef CvtDataContainer<CvtDataContFactoryDef<H5DataTypes::TimepixDataV2> > DataCont ;
+  typedef CvtDataContainer<CvtDataContFactoryTyped<uint16_t> > ImageCont ;
+  typedef CvtDataContainer<CvtDataContFactoryDef<H5DataTypes::XtcClockTime> > XtcClockTimeCont ;
+
+  // Data members
+  hsize_t m_chunk_size ;
+  int m_deflate ;
+  DataCont* m_dataCont ;
+  ImageCont* m_imageCont ;
+  XtcClockTimeCont* m_timeCont ;
+
+  // Copy constructor and assignment are disabled by default
+  TimepixDataV2Cvt ( const TimepixDataV2Cvt& ) ;
+  TimepixDataV2Cvt& operator = ( const TimepixDataV2Cvt& ) ;
+
+};
+
+} // namespace O2OTranslator
+
+#endif // O2OTRANSLATOR_TIMEPIXDATAV2CVT_H
---
+++ O2OTranslator/tags/V00-19-01/src/O2OXtcSrc.cpp
@@ -0,0 +1,106 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class O2OXtcSrc...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "O2OTranslator/O2OXtcSrc.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <sstream>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "pdsdata/xtc/BldInfo.hh"
+#include "pdsdata/xtc/DetInfo.hh"
+#include "pdsdata/xtc/ProcInfo.hh"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  // convert ProcInfo to name
+  std::string toName( const Pds::ProcInfo& info )
+  {
+    std::ostringstream str ;
+    uint32_t ip = info.ipAddr() ;
+    str << info.processId() << '@' << ((ip>>24)&0xff) << '.'
+        << ((ip>>16)&0xff) << '.' << ((ip>>8)&0xff) << '.' << (ip&0xff) ;
+    return str.str() ;
+  }
+
+  // convert BldInfo to name
+  std::string toName( const Pds::BldInfo& info )
+  {
+    return Pds::BldInfo::name(info) ;
+  }
+
+  // convert DetInfo to name
+  std::string toName( const Pds::DetInfo& info )
+  {
+    std::ostringstream str ;
+    str << Pds::DetInfo::name(info.detector()) << '.' << info.detId()
+        << ':' << Pds::DetInfo::name(info.device()) << '.' << info.devId() ;
+    return str.str() ;
+  }
+
+  // convert Src to name
+  std::string toName( const Pds::Src& src )
+  {
+    if ( src.level() == Pds::Level::Source ) {
+      const Pds::DetInfo& info = static_cast<const Pds::DetInfo&>( src ) ;
+      return toName( info ) ;
+    } else if ( src.level() == Pds::Level::Reporter ) {
+      const Pds::BldInfo& info = static_cast<const Pds::BldInfo&>( src ) ;
+      return toName( info ) ;
+    } else if ( src.level() == Pds::Level::Control ) {
+      // special case for control data
+      return "Control";
+    } else {
+      const Pds::ProcInfo& info = static_cast<const Pds::ProcInfo&>( src ) ;
+      return toName( info ) ;
+    }
+  }
+
+
+}
+
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace O2OTranslator {
+
+// get the name of the source
+std::string
+O2OXtcSrc::name() const
+{
+  if ( m_src.empty() ) return std::string() ;
+
+  const Pds::Src& top = m_src.back() ;
+  return ::toName(top) ;
+}
+
+} // namespace O2OTranslator
+
+/// Helper method to format Pds::Src to a standard stream
+std::ostream&
+operator<<(std::ostream& out, const Pds::Src& src)
+{
+  return out << ::toName(src);
+}
