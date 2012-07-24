9b78b36f65e9f04edcfb26a900bc6ab2a6b93896
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-07-24 17:23:18 +0000

    Tagging psddl_python with V00-00-02
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4222 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl_python/tags/V00-00-02/src/DdlWrapper.cpp
@@ -0,0 +1,25 @@
+#include <psddl_python/DdlWrapper.h>
+
+using boost::python::numeric::array;
+
+namespace Psana {
+  static bool did_init = false;
+
+  static void init() {
+    if (! did_init) {
+      // Required initialization of numpy array support
+      _import_array();
+      array::set_module_and_type("numpy", "ndarray");
+      did_init = true;
+    }
+  }
+
+  PyObject* ndConvert(const unsigned ndim, const unsigned* shape, int ptype, void* data) {
+    init();
+    npy_intp dims[ndim];
+    for (unsigned i = 0; i < ndim; i++) {
+      dims[i] = shape[i];
+    }
+    return PyArray_SimpleNewFromData(ndim, dims, ptype, data);
+  }
+} // namespace Psana
