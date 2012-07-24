82991a42f701613bf125221eb3dc7fbe2a442f45
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-07-24 17:15:05 +0000

    Tagging RdbMySQL with V00-00-03
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4213 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ RdbMySQL/tags/V00-00-03/include/Conn.h
@@ -0,0 +1,162 @@
+#ifndef RDBMYSQL_CONN_HH
+#define RDBMYSQL_CONN_HH
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Conn.
+//
+// Environment:
+//      This software was developed for the BaBar collaboration.  If you
+//      use all or part of it, please give an appropriate acknowledgement.
+//
+// Author List:
+//      Andy Salnikov
+//
+// Copyright Information:
+//      Copyright (C) 2005 SLAC
+//
+//------------------------------------------------------------------------
+
+//---------------
+// C++ Headers --
+//---------------
+#include <string>
+#include <boost/utility.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+namespace RdbMySQL {
+class Client ;
+}
+struct st_mysql ;
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace RdbMySQL {
+
+/**
+ *  This class represents MySQL connection.
+ *
+ *  This software was developed for the BaBar collaboration.  If you
+ *  use all or part of it, please give an appropriate acknowledgement.
+ *
+ *  Copyright (C) 2005 SLAC
+ *
+ *  @see ConnTable
+ *
+ *  @version $Id$
+ *
+ *  @author Andy Salnikov
+ */
+
+class Conn : boost::noncopyable {
+
+public:
+
+  /**
+   *  Constructor takes parameters which are later passed to mysql_real_connect.
+   *
+   *  @param host     host name, if empty then localhost is assumed
+   *  @param user     user name, if empty then current login name is used
+   *  @param passwd   password, a big secuirity hole :)
+   *  @param db       database name, if not empty then set default database name
+   *  @param port     remote port number to connect to, if 0 then default value is used
+   *  @param socket   socket name for local connections
+   *  @param client_flag bit mask with various flags, see mysql_real_connect description
+   *  @param client   if non-zero pointer is passed it will be used instead default
+   *                  client instance, will be deleted in destructor
+   */
+  Conn( const std::string& host,
+		const std::string& user, 
+		const std::string& passwd, 
+		const std::string& db,
+		unsigned int port = 0,
+		const std::string& socket = std::string(),
+		unsigned long client_flag = 0,
+		Client* client = 0 ) ;
+
+  /**
+   *  @brief Constructor takes connection string.
+   *
+   *  Format of the connection string:
+   *  "Server=hostname;Port=1234;Database=myDataBase;Uid=myUsername;Pwd=myPassword;".
+   *  Any part of the connection string can be omitted.
+   *
+   *  @param connStr  ODBC-like connection string
+   *  @param client   if non-zero pointer is passed it will be used instead default
+   *                  client instance, will be deleted in destructor
+   */
+  Conn( const std::string& connStr, Client* client = 0 ) ;
+
+  /// Destructor, closes connection if it was not closed yet
+  ~Conn () ;
+
+  /// open connection
+  bool open() ;
+
+  /// close connection
+  bool close() ;
+
+  /**
+   *  Get the MySQL error code of the last operation, 0 means no error
+   */
+  unsigned int errnum() const ;
+
+  /**
+   *  Get the MySQL error message from the last operation
+   */
+  const char* error() const ;
+
+  /** 
+   *  Get database connection. Returns 0 pointer if not open.
+   */
+  st_mysql* mysql() ;
+
+  /**
+   *  Get a database name
+   */
+  const std::string& db() const { return _db; }
+
+  /**
+   *  Get a pointer to the client interface
+   */
+  Client& client() const { return *_client ; }
+
+protected:
+
+  // Helper functions
+
+private:
+
+  // Friends
+
+  // Data members
+  std::string _host ;
+  std::string _user ;
+  std::string _passwd ;
+  std::string _db ;
+  unsigned int _port ;
+  const std::string _socket ;
+  const unsigned long _client_flag ;
+  st_mysql* _mysql ;
+  Client* _client ;
+
+};
+
+} // namespace RdbMySQL
+
+#endif // RDBMYSQL_CONN_HH
---
+++ RdbMySQL/tags/V00-00-03/test/rdb-mysql-type-traits-test.cpp
@@ -0,0 +1,190 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Test suite case for the rdb-mysql-type-traits-test.
+//
+//------------------------------------------------------------------------
+
+//---------------
+// C++ Headers --
+//---------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "RdbMySQL/TypeTraits.h"
+
+using namespace RdbMySQL ;
+using namespace std ;
+
+#define BOOST_TEST_MODULE rdb-mysql-type-traits-test
+#include <boost/test/included/unit_test.hpp>
+
+/**
+ * Simple test suite for module rdb-mysql-type-traits-test.
+ * See http://www.boost.org/doc/libs/1_36_0/libs/test/doc/html/index.html
+ */
+
+namespace {
+
+  template <typename T> struct TypeName {} ;
+  template <> struct TypeName<signed char> { static const char* typeName() { return "signed char" ; } } ;
+  template <> struct TypeName<unsigned char> { static const char* typeName() { return "unsigned char" ; } } ;
+  template <> struct TypeName<short> { static const char* typeName() { return "short" ; } } ;
+  template <> struct TypeName<unsigned short> { static const char* typeName() { return "unsigned short" ; } } ;
+  template <> struct TypeName<int> { static const char* typeName() { return "int" ; } } ;
+  template <> struct TypeName<unsigned int> { static const char* typeName() { return "unsigned int" ; } } ;
+  template <> struct TypeName<long> { static const char* typeName() { return "long" ; } } ;
+  template <> struct TypeName<unsigned long> { static const char* typeName() { return "unsigned long" ; } } ;
+  template <> struct TypeName<float> { static const char* typeName() { return "float" ; } } ;
+  template <> struct TypeName<double> { static const char* typeName() { return "double" ; } } ;
+  template <> struct TypeName<long double> { static const char* typeName() { return "long double" ; } } ;
+
+
+  template <typename T>
+  bool fromStr ( const char* valueStr )
+  {
+    cout << "Converting string \"" << valueStr << "\" to type " << TypeName<T>::typeName() << endl ;
+    T val ;
+    bool res = TypeTraits<T>::str2val ( valueStr, strlen(valueStr), val ) ;
+    if ( res ) {
+      cout << "Conversion successful, result = " << val << endl ;
+    } else {
+      cout << "Conversion failed" << endl ;
+    }
+    return res;
+  }
+
+  template <typename T>
+  bool fromStrFail ( const char* valueStr )
+  {
+    cout << "Converting string \"" << valueStr << "\" to type " << TypeName<T>::typeName() << " (and expect failure)" << endl ;
+    T val ;
+    bool res = TypeTraits<T>::str2val ( valueStr, strlen(valueStr), val ) ;
+    if ( res ) {
+      cout << "Conversion successful, result = " << val << endl ;
+    } else {
+      cout << "Conversion failed" << endl ;
+    }
+    return not res;
+  }
+
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_schar )
+{
+  BOOST_CHECK(fromStr<signed char>("64"));
+  BOOST_CHECK(fromStrFail<signed char> ( "128" ));
+  BOOST_CHECK(fromStrFail<signed char> ( "-129" ));
+  BOOST_CHECK(fromStrFail<signed char> ( "bad" ));
+  BOOST_CHECK(fromStrFail<signed char> ( "0xBAD" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_uchar )
+{
+  BOOST_CHECK(fromStr<unsigned char> ( "64" ));
+  BOOST_CHECK(fromStrFail<unsigned char> ( "256" ));
+  BOOST_CHECK(fromStrFail<unsigned char> ( "-1" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_short )
+{
+  BOOST_CHECK(fromStr<short> ( "32767" ));
+  BOOST_CHECK(fromStr<short> ( "-32768" ));
+  BOOST_CHECK(fromStrFail<short> ( "32768" ));
+  BOOST_CHECK(fromStrFail<short> ( "-32769" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_ushort )
+{
+  BOOST_CHECK(fromStr<unsigned short> ( "40000" ));
+  BOOST_CHECK(fromStr<unsigned short> ( "65535" ));
+  BOOST_CHECK(fromStrFail<unsigned short> ( "65536" ));
+  BOOST_CHECK(fromStrFail<unsigned short> ( "-1" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_int32 )
+{
+  BOOST_CHECK(fromStr<int> ( "2147483647" ));
+  BOOST_CHECK(fromStr<int> ( "-2147483648" ));
+  BOOST_CHECK(fromStrFail<int> ( "2147483648" ));
+  BOOST_CHECK(fromStrFail<int> ( "-2147483649" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_uint32 )
+{
+  BOOST_CHECK(fromStr<unsigned int> ( "2147483648" ));
+  BOOST_CHECK(fromStr<unsigned int> ( "4294967295" ));
+  BOOST_CHECK(fromStrFail<unsigned int> ( "4294967296" ));
+  BOOST_CHECK(fromStrFail<unsigned int> ( "-1" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_int64 )
+{
+  BOOST_CHECK(fromStr<long> ( "9223372036854775807" ));
+  BOOST_CHECK(fromStr<long> ( "-9223372036854775808" ));
+  BOOST_CHECK(fromStrFail<long> ( "9223372036854775808" ));
+  BOOST_CHECK(fromStrFail<long> ( "-9223372036854775809" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_uint64 )
+{
+  BOOST_CHECK(fromStr<unsigned long> ( "4294967296" ));
+  BOOST_CHECK(fromStr<unsigned long> ( "18446744073709551615" ));
+  BOOST_CHECK(fromStrFail<unsigned long> ( "18446744073709551616" ));
+  BOOST_CHECK(fromStrFail<unsigned long> ( "18446744073709551617" ));
+  BOOST_CHECK(fromStrFail<unsigned long> ( "-1" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_float )
+{
+  BOOST_CHECK(fromStr<float> ( "1.0" ));
+  BOOST_CHECK(fromStr<float> ( "1.0e10" ));
+  BOOST_CHECK(fromStr<float> ( "0.125" ));
+  BOOST_CHECK(fromStr<float> ( "3.4e+38" ));
+  BOOST_CHECK(fromStrFail<float> ( "1.0e-310" ));
+  BOOST_CHECK(fromStrFail<float> ( "1e+39" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_double )
+{
+  BOOST_CHECK(fromStr<double> ( "1.0" ));
+  BOOST_CHECK(fromStr<double> ( "1e+39" ));
+  BOOST_CHECK(fromStr<double> ( "1.0e-310" ));
+  BOOST_CHECK(fromStrFail<double> ( "1e+310" ));
+}
+
+// ==============================================================
+
+BOOST_AUTO_TEST_CASE( test_ldouble )
+{
+  BOOST_CHECK(fromStr<long double> ( "1.0" ));
+  BOOST_CHECK(fromStr<long double> ( "1.0e-310" ));
+  BOOST_CHECK(fromStr<long double> ( "1e+4932" ));
+  BOOST_CHECK(fromStrFail<long double> ( "1e+5000" ));
+}
+
+// ==============================================================
+
