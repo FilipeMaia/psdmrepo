8ef7a5af1d7a6ae4ce56fe7572f9a773a47ffafd
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-07-27 18:44:30 +0000

    Tagging XtcInput with V00-06-04
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4302 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ XtcInput/tags/V00-06-04/src/Exceptions.cpp
@@ -0,0 +1,44 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Exceptions...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "XtcInput/Exceptions.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace XtcInput {
+
+//----------------
+// Constructors --
+//----------------
+Exception::Exception (const ErrSvc::Context& ctx, const std::string& className, const std::string& what)
+  : ErrSvc::Issue(ctx, className+": "+what)
+{
+}
+
+} // namespace XtcInput
