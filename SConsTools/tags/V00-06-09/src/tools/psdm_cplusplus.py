d08cf7895ff646e5b7fdb6c6def418ed476829b7
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-07-27 18:43:32 +0000

    Tagging SConsTools with V00-06-09
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4300 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ SConsTools/tags/V00-06-09/src/tools/psdm_cplusplus.py
@@ -0,0 +1,60 @@
+"""
+Tool which selects correct C++ compiler version and options for PSDM releases.
+"""
+import os
+
+from SConsTools.trace import *
+from SConsTools.scons_functions import *
+
+_gcc_opt = { 'opt' : '-O3',
+            'dbg' : '-g',
+            'deb' : '-g',     # 'deb' was an unfortunate name for 'dbg'
+            'prof' : '-pg' }
+
+_ld_opt = { 'opt' : '',
+            'dbg' : '-g',
+            'deb' : '-g',     # 'deb' was an unfortunate name for 'dbg'
+            'prof' : '-pg -static' }
+
+def generate(env):
+    
+    os = env['SIT_ARCH_OS']
+    comp = env['SIT_ARCH_COMPILER']
+    opt = env['SIT_ARCH_OPT']
+    
+    if comp == 'gcc34' :
+        env['CC'] = 'gcc34'
+        env['CXX'] = 'g++34'
+        if os == 'slc4' :
+            env['CC'] = 'gcc-3.4'
+            env['CXX'] = 'g++-3.4'
+        env['CCFLAGS'] = _gcc_opt.get(opt,'') + ' -Wall'
+        env['CXXFLAGS'] = '-Wno-invalid-offsetof'
+        env['LDFLAGS'] = _ld_opt.get(opt,'')
+
+    elif comp == 'gcc41' :
+        env['CC'] = 'gcc'
+        env['CXX'] = 'g++'
+        env['CCFLAGS'] = _gcc_opt.get(opt,'') + ' -Wall -Wno-unknown-pragmas'
+        env['CXXFLAGS'] = '-Wno-invalid-offsetof'
+        env['LDFLAGS'] = _ld_opt.get(opt,'')
+
+    elif comp == 'gcc44' :
+        env['CC'] = 'gcc'
+        env['CXX'] = 'g++'
+        env['CCFLAGS'] = _gcc_opt.get(opt,'') + ' -Wall'
+        env['CXXFLAGS'] = '-Wno-invalid-offsetof'
+        env['LDFLAGS'] = _ld_opt.get(opt,'')
+
+    elif comp == 'gcc46' :
+        env['CC'] = 'gcc-4.6'
+        env['CXX'] = 'g++-4.6'
+        env['CCFLAGS'] = _gcc_opt.get(opt,'') + ' -Wall'
+        env['CXXFLAGS'] = '-Wno-invalid-offsetof'
+        env['LDFLAGS'] = _ld_opt.get(opt,'')
+
+    
+    trace ( "Initialized psdm_cplusplus tool", "psdm_python", 2 )
+
+def exists(env):
+    return _qtdir(env) is not None
---
+++ SConsTools/tags/V00-06-09/src/tools/psdm_python.py
@@ -0,0 +1,44 @@
+"""
+Tool which selects correct python version for PSDM releases.
+"""
+import os
+from os.path import join as pjoin
+
+from SConsTools.trace import *
+from SConsTools.scons_functions import *
+
+
+def generate(env):
+    
+    if env['SIT_ARCH_OS'] == 'rhel5':
+
+        prefix = "/usr"
+        version = "2.4"
+        libdir = env['LIB_ABI']
+    
+    elif env['SIT_ARCH_OS'] == 'rhel6':
+        
+        prefix = pjoin("/reg/g/psdm/sw/external/python/2.7.2", env['SIT_ARCH_BASE_OPT'])
+        version = "2.7"
+        libdir = "lib"
+        
+    elif env['SIT_ARCH_OS'] == 'ubu11':
+        
+        prefix = "/usr"
+        version = "2.7"
+        libdir = env['LIB_ABI']
+    
+    env['PYTHON_PREFIX'] = prefix
+    env['PYTHON_VERSION'] = version
+    env['PYTHON'] = "python"+version
+    env['PYTHON_INCDIR'] = pjoin(prefix, "include", env['PYTHON'])
+    env['PYTHON_LIBDIR'] = pjoin(prefix, libdir)
+    env['PYTHON_BINDIR'] = pjoin(prefix, "bin")
+    env['PYTHON_BIN'] = pjoin(env['PYTHON_BINDIR'], env['PYTHON'])
+    
+    env['SCRIPT_SUBS']['PYTHON'] = env['PYTHON_BIN']
+    
+    trace ( "Initialized psdm_python tool", "psdm_python", 2 )
+
+def exists(env):
+    return _qtdir(env) is not None
