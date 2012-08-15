68f1cc9033c6968020efaef626155515608d4cfd
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-08-15 23:00:48 +0000

    Tagging ImgAlgos with V00-00-12
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4414 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ ImgAlgos/tags/V00-00-12/include/GlobalMethods.h
@@ -0,0 +1,79 @@
+#ifndef IMGALGOS_GLOBALMETHODS_H
+#define IMGALGOS_GLOBALMETHODS_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class GlobalMethods.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+#include <string>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+#include "PSEvt/Event.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace ImgAlgos {
+
+/// @addtogroup ImgAlgos
+
+/**
+ *  @ingroup ImgAlgos
+ *
+ *  @brief Global methods for ImgAlgos package
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version $Id$
+ *
+ *  @author Mikhail S. Dubrovin
+ */
+
+class GlobalMethods  {
+public:
+  GlobalMethods () ;
+  virtual ~GlobalMethods () ;
+
+private:
+  // Copy constructor and assignment are disabled by default
+  GlobalMethods ( const GlobalMethods& ) ;
+  GlobalMethods& operator = ( const GlobalMethods& ) ;
+};
+
+//--------------------
+
+  std::string stringFromUint(unsigned number, unsigned width=6);
+  std::string stringTimeStamp(PSEvt::Event& evt, std::string fmt="%Y%m%d-%H%M%S%f"); //("%Y-%m-%d %H:%M:%S%f%z");
+  std::string stringRunNumber(PSEvt::Event& evt, unsigned width=4);
+
+//--------------------
+//--------------------
+//--------------------
+//--------------------
+//--------------------
+
+} // namespace ImgAlgos
+
+#endif // IMGALGOS_GLOBALMETHODS_H
