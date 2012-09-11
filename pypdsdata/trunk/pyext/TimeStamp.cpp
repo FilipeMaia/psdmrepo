3f756a8b6996cd1efc237f0945056488e8d6f322
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-09-11 23:49:19 +0000

    Tag: V00-00-73 2012-09-11 Andy Salnikov - fix for very old bug, TimeStamp.vector actually returned ticks() - pyxtcreader now prints vector() value
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4439 b967ad99-d558-0410-b138-e0f6c56caec7

--- pypdsdata/trunk/pyext/TimeStamp.cpp
+++ pypdsdata/trunk/pyext/TimeStamp.cpp
@@ -45,10 +45,11 @@ namespace {
   FUN0_WRAPPER_EMBEDDED(pypdsdata::TimeStamp, vector);
 
   PyMethodDef methods[] = {
-    { "ticks",    ticks,    METH_NOARGS, "self.ticks() -> int\n\nReturns the ticks value" },
-    { "fiducials", fiducials, METH_NOARGS, "self.fiducials() -> int\n\nReturns the fiducials value" },
-    { "control",  control,  METH_NOARGS, "self.control() -> int\n\nReturns the control value" },
-    { "vector",   ticks,    METH_NOARGS, "self.vector() -> int\n\nReturns the vector value" },
+    { "ticks",    ticks,    METH_NOARGS,
+        "self.ticks() -> int\n\n119MHz counter within the fiducial for eventcode which initiated the readout" },
+    { "fiducials", fiducials, METH_NOARGS, "self.fiducials() -> int\n\nReturns the fiducials value (360Hz pulse ID) module 17-bit" },
+    { "control",  control,  METH_NOARGS, "self.control() -> int\n\nInternal bits for alternate interpretation of XTC header fields" },
+    { "vector",   vector,   METH_NOARGS, "self.vector() -> int\n\n15-bit seed for event-level distribution (events since Configure)" },
     {0, 0, 0, 0}
    };
 
