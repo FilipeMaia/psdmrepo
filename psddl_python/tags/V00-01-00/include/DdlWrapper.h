2e2d8671041dc17e8821f9b2b3660e7d3191059c
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-09-12 00:20:45 +0000

    Tagging psddl_python with V00-01-00
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4454 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl_python/tags/V00-01-00/include/DdlWrapper.h
@@ -0,0 +1,35 @@
+#ifndef PSANA_DDLWRAPPER_H
+#define PSANA_DDLWRAPPER_H 1
+
+#include "EventGetter.h"
+#include "EnvObjectStoreGetter.h"
+
+#include <boost/python.hpp>
+#include <boost/python/class.hpp>
+#include <psddl_python/vector_indexing_suite_nocopy.hpp>
+#include <numpy/arrayobject.h>
+
+namespace psddl_python {
+  extern PyObject* ndConvert(const unsigned ndim, const unsigned* shape, int ptype, void* data);
+}
+
+#define std_vector_class_(T)\
+  boost::python::class_<vector<T> >("std::vector<" #T ">")\
+    .def(vector_indexing_suite_nocopy<std::vector<T> >())
+
+#define associate_PyArrayType(ctype, ptype) const int PyArray_ ## ctype = ptype
+associate_PyArrayType(int8_t, PyArray_BYTE);
+associate_PyArrayType(uint8_t, PyArray_UBYTE);
+associate_PyArrayType(int16_t, PyArray_SHORT);
+associate_PyArrayType(uint16_t, PyArray_USHORT);
+associate_PyArrayType(int32_t, PyArray_INT);
+associate_PyArrayType(uint32_t, PyArray_UINT);
+associate_PyArrayType(float, PyArray_FLOAT);
+associate_PyArrayType(double, PyArray_DOUBLE);
+
+#define ND_CONVERT(value, ctype, ndim) const ndarray<ctype, ndim>& a(value); return psddl_python::ndConvert(ndim, a.shape(), PyArray_ ## ctype, (void *) a.data())
+#define VEC_CONVERT(value, ctype) const ndarray<ctype, 1>& a(value); const std::vector<ctype> v(a.data(), a.data() + a.size()); return v
+#define ADD_ENV_OBJECT_STORE_GETTER(x) EnvObjectStoreGetter::addGetter(new x ## _Getter())
+#define ADD_EVENT_GETTER(x) EventGetter::addGetter(new x ## _Getter())
+
+#endif // PSANA_DDLWRAPPER_H
