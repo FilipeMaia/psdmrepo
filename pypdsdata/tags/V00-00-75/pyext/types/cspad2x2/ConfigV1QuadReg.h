ca969639d9a1da2c2d47f2e0e64e710ec15d9f2e
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-09-28 17:44:45 +0000

    Tagging pypdsdata with V00-00-75
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4512 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pypdsdata/tags/V00-00-75/pyext/types/cspad2x2/ConfigV1QuadReg.h
@@ -0,0 +1,62 @@
+#ifndef PYPDSDATA_CSPAD2X2_CONFIGV1QUADREG_H
+#define PYPDSDATA_CSPAD2X2_CONFIGV1QUADREG_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class ConfigV1QuadReg.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "../PdsDataType.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "pdsdata/cspad2x2/ConfigV1QuadReg.hh"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//              ---------------------
+//              -- Class Interface --
+//              ---------------------
+
+namespace pypdsdata {
+namespace CsPad2x2 {
+
+/**
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class ConfigV1QuadReg : public PdsDataType<ConfigV1QuadReg, Pds::CsPad2x2::ConfigV1QuadReg> {
+public:
+
+  typedef PdsDataType<ConfigV1QuadReg, Pds::CsPad2x2::ConfigV1QuadReg> BaseType;
+
+  /// Initialize Python type and register it in a module
+  static void initType( PyObject* module );
+
+};
+
+} // namespace CsPad2x2
+} // namespace pypdsdata
+
+#endif // PYPDSDATA_CSPAD2X2_CONFIGV1QUADREG_H
