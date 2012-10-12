34a53f4f714984bd6e2b9d2436441521b97f5468
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-10-12 02:27:08 +0000

    Tagging XtcInput with V00-06-06
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4597 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ XtcInput/tags/V00-06-06/include/Dgram.h
@@ -0,0 +1,101 @@
+#ifndef XTCINPUT_DGRAM_H
+#define XTCINPUT_DGRAM_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Dgram.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <boost/shared_ptr.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "pdsdata/xtc/Dgram.hh"
+#include "XtcInput/XtcFileName.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace XtcInput {
+
+/**
+ *  @brief Wrapper for Pds::Dgram class.
+ *  
+ *  This class wraps Pds::Datagram class and also adds some additional 
+ *  context information to it such as file name and position.
+ *  
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class Dgram {
+public:
+  
+  typedef boost::shared_ptr<Pds::Dgram> ptr;
+
+  /**
+   *  @brief Factory method which wraps existing object into a smart pointer.
+   */
+  static ptr make_ptr(Pds::Dgram* dg) ;
+
+  /**
+   *  Constructor takes a smart pointer to XTC datagram object and
+   *  the file name where datagram has originated.
+   */
+  Dgram(const ptr& dg, XtcFileName file) : m_dg(dg), m_file(file) {}
+
+  /**
+   *  Default ctor
+   */
+  Dgram() : m_dg(), m_file() {}
+
+  /// Return pointer to the datagream
+  ptr dg() const { return m_dg; }
+  
+  /// Return file name
+  const XtcFileName& file() const { return m_file; }
+
+  bool empty() const { return not m_dg.get(); }
+  
+private:
+  
+  /**
+   *  @brief This method will be used in place of regular delete.
+   */
+  static void destroy(const Pds::Dgram* dg) ;
+
+  /**
+   *  @brief Factory method which copies existing datagram and wraps new 
+   *  object into a smart pointer.
+   */
+  static ptr copy(Pds::Dgram* dg) ;
+
+  // Data members
+  ptr m_dg;
+  XtcFileName m_file;
+};
+
+} // namespace XtcInput
+
+#endif // XTCINPUT_DGRAM_H
---
+++ XtcInput/tags/V00-06-06/include/XtcStreamMerger.h
@@ -0,0 +1,104 @@
+#ifndef XTCINPUT_XTCSTREAMMERGER_H
+#define XTCINPUT_XTCSTREAMMERGER_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcStreamMerger.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <vector>
+#include <queue>
+#include <boost/utility.hpp>
+#include <boost/shared_ptr.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "XtcInput/Dgram.h"
+#include "XtcInput/StreamFileIterI.h"
+#include "XtcInput/XtcStreamDgIter.h"
+#include "XtcInput/XtcFileName.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace XtcInput {
+
+/**
+ *  @brief datagram iterator which merges data from several streams.
+ *
+ *  Class responsible for merging of the datagrams from several
+ *  XTC streams.
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class XtcStreamMerger : boost::noncopyable {
+public:
+
+  /**
+   *  @brief Make iterator instance
+   *
+   *  @param[in]  streamIter  Iterator for input files
+   *  @param[in]  maxDgSize Maximum allowed datagram size
+   *  @param[in]  l1OffsetSec Time offset to add to non-L1Accept transitions.
+   */
+  XtcStreamMerger(const boost::shared_ptr<StreamFileIterI>& streamIter,
+      size_t maxDgSize, double l1OffsetSec = 0 ) ;
+
+  // Destructor
+  ~XtcStreamMerger () ;
+
+  /**
+   *  @brief Return next datagram.
+   *
+   *  Read next datagram, return zero pointer after last file has been read,
+   *  throws exception for errors.
+   *
+   *  @return Shared pointer to datagram object
+   *
+   *  @throw FileOpenException Thrown in case chunk file cannot be open.
+   *  @throw XTCReadException Thrown for any read errors
+   *  @throw XTCLiveTimeout Thrown for timeout during live data reading
+   */
+  Dgram next() ;
+
+protected:
+
+  // update time in datagram
+  void updateDgramTime(Pds::Dgram& dgram) const ;
+
+private:
+
+  std::vector<boost::shared_ptr<XtcStreamDgIter> > m_streams ;   ///< Set of datagram iterators for individual streams
+  std::vector<Dgram> m_dgrams ;               ///< Current datagram for each of the streams
+  int32_t m_l1OffsetSec ;                     ///< Time offset to add to non-L1Accept transitions (seconds)
+  int32_t m_l1OffsetNsec ;                    ///< Time offset to add to non-L1Accept transitions (nanoseconds)
+  std::queue<Dgram> m_outputQueue;            ///< Output queue for datagrams
+
+};
+
+} // namespace XtcInput
+
+#endif // XTCINPUT_XTCSTREAMMERGER_H
