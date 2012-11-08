209f2e567ec169582ef90d3a9b5cd7bf570a7a8e
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-11-08 21:21:55 +0000

    Tagging psddl_python with V00-01-06
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4712 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl_python/tags/V00-01-06/include/GenericGetter.h
@@ -0,0 +1,55 @@
+// This is not a conventional include file, but rather a template
+// that is included by EventGetter.cpp and EnvObjectStore.cpp.
+
+#include <sstream>
+#include "PSEvt/EventKey.h" // operator<<(Pds::Src) defined here
+
+namespace psddl_python {
+//  static string toString(const Src& src) {
+//    std::ostringstream stream;
+//    stream << src;
+//    return stream.str();
+//  }
+//
+//  static void printGetResult(const char* typeName, const char* foundTypeName, const Src* foundSrc) {
+//    printf("get(%s) -> %s -> %s\n",
+//           typeName, foundTypeName, toString(*foundSrc).c_str());
+//  }
+
+  object CLASS::get(ARGS_DECL) {
+    Src src;
+    if (foundSrc == NULL) {
+      foundSrc = &src;
+    }
+    CLASS* getter = (CLASS*) MAP.getGetter(typeName);
+    if (getter) {
+      object result(getter->get(ARGS));
+      //printGetResult(typeName.c_str(), typeName.c_str(), foundSrc);
+      return result;
+    }
+    int versionMin, versionMax;
+    // A template is of the form PSana::Fli::FrameV1
+    const char* _template = MAP.getTemplate(typeName, &versionMin, &versionMax);
+    if (! _template) {
+      printf("%s: no getter exists for class '%s'\n", CLASS_NAME, typeName.c_str());
+      return object();
+    }
+    for (int version = versionMin; version <= versionMax; version++) {
+      char vTypeName[128];
+      sprintf(vTypeName, _template, version);
+      getter = (CLASS*) MAP.getGetter(vTypeName);
+      if (getter) {
+        object result(getter->get(ARGS));
+        if (result != object()) {
+          //printGetResult(typeName.c_str(), vTypeName, foundSrc);
+          return result;
+        }
+        //printf("%s: tried %s but it returned null\n", CLASS_NAME, vTypeName);
+      } else {
+        printf("%s: no getter exists for class '%s'\n", CLASS_NAME, typeName.c_str());
+      }
+    }
+    //printf("%s: no object found for %s\n", CLASS_NAME, _template);
+    return object();
+  }
+}
