38b11a31c63af734a1a87880f8a45192780dda1e
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2012-12-12 19:52:31 +0000

    Tagging psddl with V00-02-09
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@4951 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl/tags/V00-02-09/src/H5Attribute.py
@@ -0,0 +1,108 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id$
+#
+# Description:
+#  Module H5Attribute...
+#
+#------------------------------------------------------------------------
+
+"""Representation of the attribute in HDF5 schema
+
+This software was developed for the SIT project.  If you use all or 
+part of it, please give an appropriate acknowledgment.
+
+@version $Id$
+
+@author Andy Salnikov
+"""
+
+
+#------------------------------
+#  Module's version from CVS --
+#------------------------------
+__version__ = "$Revision$"
+# $Source$
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import sys
+
+#---------------------------------
+#  Imports of base class module --
+#---------------------------------
+
+#-----------------------------
+# Imports for other modules --
+#-----------------------------
+from psddl.Method import Method
+
+#----------------------------------
+# Local non-exported definitions --
+#----------------------------------
+
+#------------------------
+# Exported definitions --
+#------------------------
+
+#---------------------
+#  Class definition --
+#---------------------
+class H5Attribute ( object ) :
+
+    #----------------
+    #  Constructor --
+    #----------------
+    def __init__ ( self, **kw ) :
+        
+        self.name = kw.get('name')         # attribute name
+        self._type = kw.get('type', None)   # optional type
+        self.method = kw.get('method', self.name)   # corresponding method name in pstype, default is the same as name
+        self.rank = kw.get('rank', 0)      # attribute data type
+
+    #-------------------
+    #  Public methods --
+    #-------------------
+
+    def __str__(self):
+        
+        return "<H5Attribute(name=%s, type=%s, rank=%s, method=%s)>" % (self.name, self.type.name, self.rank, self.method)
+
+    def __repr__(self):
+        
+        return "<H5Attribute(name=%s, type=%s, rank=%s, method=%s)>" % (self.name, self.type.name, self.rank, self.method)
+
+    @property
+    def type(self):
+        """Get attribute type"""
+        
+        # if type is explicitly defined then return it
+        if self._type: return self._type
+        
+        # otherwise find corresponding method and use its type
+        methodname = self.method or self.name
+
+        # find pstype
+        pstype = self.parent.parent.pstype
+        if not pstype: raise ValueError('no corresponding pstype found')
+        
+        # find method
+        method = pstype.lookup(methodname, Method)
+        if not pstype: raise ValueError('no corresponding method is found in pstype')
+
+        type = method.type
+        
+        # it's not going to change so remember it
+        self._type = type
+        
+        return type
+
+#
+#  In case someone decides to run this module
+#
+if __name__ == "__main__" :
+
+    # In principle we can try to run test suite for this module,
+    # have to think about it later. Right now just abort.
+    sys.exit ( "Module is not supposed to be run as main module" )
