b74c3efd274c4306d932125a954d401df68e3918
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-01-15 00:42:11 +0000

    Tagging psana_python with V00-02-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5123 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psana_python/tags/V00-02-01/src/EnvWrapper.cpp
@@ -0,0 +1,32 @@
+
+#include <psana_python/EnvWrapper.h>
+
+#include <list>
+
+namespace psana_python {
+
+boost::python::list
+EnvWrapper::keys()
+{
+  boost::python::list l;
+  ConfigSvc::ConfigSvc cfg;
+  std::list<string> keys = cfg.getKeys(_name);
+  for (std::list<string>::iterator it = keys.begin(); it != keys.end(); ++it) {
+    string& key = *it;
+    l.append(key);
+  }
+  return l;
+}
+
+std::string
+EnvWrapper::configStr(const std::string& parameter, const std::string& _default)
+{
+  ConfigSvc::ConfigSvc cfg;
+  try {
+    return cfg.getStr(_name, parameter);
+  } catch (const ConfigSvc::ExceptionMissing& ex) {
+    return cfg.getStr(_className, parameter, _default);
+  }
+}
+
+}
