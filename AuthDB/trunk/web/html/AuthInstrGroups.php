f761003f71fa67f9a136fdf459d834ca0bf0e27b
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-01-28 20:16:50 +0000

    - added support for 'allied' instruments in the special group management script AuthInstrGroups.php
    M    doc/ChangeLog
    M    web/html/AuthInstrGroups.php
    
    
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5194 b967ad99-d558-0410-b138-e0f6c56caec7

--- AuthDB/trunk/web/html/AuthInstrGroups.php
+++ AuthDB/trunk/web/html/AuthInstrGroups.php
@@ -24,41 +24,41 @@ if( isset( $_GET['instr_name'] )) {
 $fix = isset( $_GET['fix'] );
 
 try {
-	AuthDB::instance()->begin();
+    AuthDB::instance()->begin();
 
-	if( $fix && !AuthDB::instance()->canEdit())
-            report_error( "<b>error:</b> your account doesn't posseses sufficient privileges to perform the operaton" );
+    if( $fix && !AuthDB::instance()->canEdit())
+        report_error( "<b>error:</b> your account doesn't posseses sufficient privileges to perform the operaton" );
 
-	LogBook::instance()->begin();
+    LogBook::instance()->begin();
 
-	$instrument_names = array();
-	if( isset( $instr_name )) {
-		if( is_null( LogBook::instance()->regdb()->find_instrument_by_name( $instr_name )))
-                    report_error( "<b>error:</b> the specified instrument isn't known" );
-		array_push( $instrument_names, $instr_name  );
-	} else {
+    $instrument_names = array();
+    if( isset( $instr_name )) {
+        if( is_null( LogBook::instance()->regdb()->find_instrument_by_name( $instr_name )))
+            report_error( "<b>error:</b> the specified instrument isn't known" );
+        array_push( $instrument_names, $instr_name  );
+    } else {
 
-		// Get all known instruments. Skip  pseudo-instruments.
-		//
-		foreach( LogBook::instance()->regdb()->instrument_names() as $name ) {
-			$instrument = LogBook::instance()->regdb()->find_instrument_by_name($name);
-			if( $instrument->is_location()) continue;
-			array_push( $instrument_names, $name  );
-		}
-	}
+        // Get all known instruments. Skip  pseudo-instruments.
+        //
+        foreach( LogBook::instance()->regdb()->instrument_names() as $name ) {
+            $instrument = LogBook::instance()->regdb()->find_instrument_by_name($name);
+            if( $instrument->is_location()) continue;
+            array_push( $instrument_names, $name  );
+        }
+    }
 
-	$extra_operations = <<<HERE
+    $extra_operations = <<<HERE
   <h2>Extra Operations</h2>
   <div style="padding-left:20px;">
-	<ul>
+    <ul>
       <li><a href="AuthInstrGroups.php">check</a> or <a href="AuthInstrGroups.php?fix">fix</a> authorizations status of all instruments</li>
 HERE;
-	foreach( $instrument_names as $name ) {
-		$extra_operations .= <<<HERE
+    foreach( $instrument_names as $name ) {
+        $extra_operations .= <<<HERE
       <li><a href="AuthInstrGroups.php?instr_name={$name}">check</a> or <a href="AuthInstrGroups.php?instr_name={$name}&fix">fix</a> authorizations status for instrument {$name}</li>
 HERE;
-	}
-	$extra_operations .= <<<HERE
+    }
+    $extra_operations .= <<<HERE
     </ul>
   </div>
 HERE;
@@ -118,7 +118,7 @@ td.table_cell_within_group {
        the role to the groups. Specifically, for each instrument 'XYZ', the tool will:</p>
     <ol>
       <li>check if there is such POSIX group as 'ps-xyz'</li>
-      <li>check if the group is registered as e-log 'Writer' for all experiments of instrument 'XYZ'</li>
+      <li>check if the group is registered as e-log 'Writer' ('Editor' for XPP and XCS) for all experiments of instrument 'XYZ'</li>
       <li>(optionally) if not then the group will be authorized for those experiments</li>
       <li>(optionally) report which changes which had to be made at step #3</li>
       <li>report the number of shifts for each experiment:
@@ -140,66 +140,160 @@ td.table_cell_within_group {
 
 <?php
 
-	$application = 'LogBook';
-	$role = 'Writer';
-	$header =
-		'<tr>'.
-		'<td class="table_hdr">Instrument</td>'.
-		'<td class="table_hdr">Id</td>'.
-		'<td class="table_hdr">Experiment</td>'.
-		'<td class="table_hdr">Group</td>'.
-		'<td class="table_hdr">Instrument Group</td>'.
-		'<td class="table_hdr">E-Log Role</td>'.
-		'<td class="table_hdr">Is Authorized</td>'.
-		'<td class="table_hdr"># shifts</td>'.
-	($fix ? '<td class="table_hdr">Action</td>' : '').
-	'</tr>';
-	print '<table><tbody>';
-	foreach( $instrument_names as $instr_name ) {
+    $application = 'LogBook';
 
-		print $header;
+    print '<table><tbody>';
+    foreach( $instrument_names as $instr_name ) {
 
-		$group = 'ps-'.strtolower( $instr_name );
-		$group4auth = 'gid:'.$group;
-		$experiments = LogBook::instance()->experiments_for_instrument( $instr_name );
-		foreach( $experiments as $experiment ) {
-			$has_role = AuthDB::instance()->hasRole( $group4auth, $experiment->id(), $application, $role );
-			$num_shifts = $experiment->num_shifts();
-			print
-				'<tr>'.
-				'<td class="table_cell table_cell_left">'.$instr_name.'</td>'.
-				'<td class="table_cell">'.$experiment->id().'</td>'.
-			    '<td class="table_cell"><a href="../portal/?exper_id='.$experiment->id().'">'.$experiment->name().'</a></td>'.
-				'<td class="table_cell"><a href="../authdb/?action=view_group&gid='.$experiment->POSIX_gid().'">'.$experiment->POSIX_gid().'</a></td>'.
-				'<td class="table_cell"><a href="../authdb/?action=view_group&gid='.$group.'">'.$group.'</a></td>'.
-				'<td class="table_cell">'.$role.'</td>';
-			if( $fix ) {
-				if( !$has_role ) {
-					AuthDB::instance()->createRolePlayer( $application, $role, $experiment->id(), $group4auth );
-					print
-						'<td class="table_cell"><span style="color:green;">Yes</span></td>'.
-						'<td class="table_cell"'.( $num_shifts == 0 ? ' style="font-color:red;"' : '' ).'>'.$num_shifts.'</td>'.
-						'<td class="table_cell table_cell_right"><span style="color:green; font-weight:bold;">just fixed</span></td>';
-				} else {
-					print
-						'<td class="table_cell">Yes</td>'.
-						'<td class="table_cell"'.( $num_shifts == 0 ? ' style="font-color:red;"' : '' ).'>'.$num_shifts.'</td>'.
-						'<td class="table_cell table_cell_right"></td>';
-				}
-			} else {
-				print
-					'<td class="table_cell">'.($has_role ? 'Yes' : '<span style="color:red;">No</span>').'</td>'.
-					'<td class="table_cell table_cell_right"'.( $num_shifts == 0 ? ' style="color:red;"' : '' ).'>'.$num_shifts.'</td>';
-			}
-			print
-				'</tr>';
-		}
-	}
-	print '</tbody><table>';
-	
-	LogBook::instance()->commit();
-	AuthDB::instance()->commit();
-	
+        $experiments = LogBook::instance()->experiments_for_instrument( $instr_name );
+
+        $instr_name_own    = $instr_name ;
+        $instr_name_allied = $instr_name ;
+        switch( $instr_name) {
+            case 'XPP':
+                $instr_name_allied = 'XCS';
+                break;
+            case 'XCS':
+                $instr_name_allied = 'XPP';
+                break;
+        }
+        if($instr_name_own != $instr_name_allied) {
+            // The special case for XPP and XCS to allow cros-instrument E-log access
+            // for members of each group to be e-log Editors of all experiments
+            // in boths instruments.
+            //
+            $role = 'Editor';
+            print
+                '<tr>'.
+                '<td class="table_hdr" rowspan="2">Instrument</td>'.
+                '<td class="table_hdr" rowspan="2">Id</td>'.
+                '<td class="table_hdr" rowspan="2">Experiment</td>'.
+                '<td class="table_hdr" rowspan="2">Group</td>'.
+                '<td class="table_hdr" rowspan="2"># shifts</td>'.
+                '<td class="table_hdr" colspan="4" align="center">OWN</td>'.
+                '<td class="table_hdr" colspan="4" align="center">ALLIED</td>'.
+                '</tr>'.
+                '<tr>'.
+                '<td class="table_hdr">Instrument Group</td>'.
+                '<td class="table_hdr">E-Log Role</td>'.
+                '<td class="table_hdr">Is Authorized</td>'.
+            ($fix ?
+                '<td class="table_hdr">Action</td>' : '').
+                '<td class="table_hdr">Instrument Group</td>'.
+                '<td class="table_hdr">E-Log Role</td>'.
+                '<td class="table_hdr">Is Authorized</td>'.
+            ($fix ?
+                '<td class="table_hdr">Action</td>' : '').
+                '</tr>';
+
+            $group_own      = 'ps-'.strtolower( $instr_name_own );
+            $group4auth_own = 'gid:'.$group_own;
+
+            $group_allied      = 'ps-'.strtolower( $instr_name_allied );
+            $group4auth_allied = 'gid:'.$group_allied;
+
+            foreach( $experiments as $experiment ) {
+                $num_shifts = $experiment->num_shifts();
+                print
+                    '<tr>'.
+                    '<td class="table_cell table_cell_left">'.$instr_name.'</td>'.
+                    '<td class="table_cell">'.$experiment->id().'</td>'.
+                    '<td class="table_cell"><a href="../portal/?exper_id='.$experiment->id().'">'.$experiment->name().'</a></td>'.
+                    '<td class="table_cell"><a href="../authdb/?action=view_group&gid='.$experiment->POSIX_gid().'">'.$experiment->POSIX_gid().'</a></td>'.
+                    '<td class="table_cell"'.( $num_shifts == 0 ? ' style="font-color:red;"' : '' ).'>'.$num_shifts.'</td>'.
+                    '<td class="table_cell"><a href="../authdb/?action=view_group&gid='.$group_own.'">'.$group_own.'</a></td>'.
+                    '<td class="table_cell">'.$role.'</td>';
+                $has_role_own = AuthDB::instance()->hasRole( $group4auth_own,    $experiment->id(), $application, $role );
+                if( $fix ) {
+                    if( !$has_role_own ) {
+                        AuthDB::instance()->createRolePlayer( $application, $role, $experiment->id(), $group4auth_own );
+                        print
+                            '<td class="table_cell"><span style="color:green;">Yes</span></td>'.
+                            '<td class="table_cell"><span style="color:green; font-weight:bold;">just fixed</span></td>';
+                    } else {
+                        print
+                            '<td class="table_cell">Yes</td>'.
+                            '<td class="table_cell"></td>';
+                    }
+                } else {
+                    print
+                        '<td class="table_cell">'.($has_role_own ? 'Yes' : '<span style="color:red;">No</span>').'</td>';
+                }
+                $has_role_allied = AuthDB::instance()->hasRole( $group4auth_allied, $experiment->id(), $application, $role );
+                print
+                    '<td class="table_cell"><a href="../authdb/?action=view_group&gid='.$group_allied.'">'.$group_allied.'</a></td>'.
+                    '<td class="table_cell">'.$role.'</td>';
+                if( $fix ) {
+                    if( !$has_role_allied ) {
+                        AuthDB::instance()->createRolePlayer( $application, $role, $experiment->id(), $group4auth_allied );
+                        print
+                            '<td class="table_cell"><span style="color:green;">Yes</span></td>'.
+                            '<td class="table_cell table_cell_right"><span style="color:green; font-weight:bold;">just fixed</span></td>';
+                    } else {
+                        print
+                            '<td class="table_cell">Yes</td>'.
+                            '<td class="table_cell table_cell_right"></td>';
+                    }
+                } else {
+                    print
+                        '<td class="table_cell table_cell_right">'.($has_role_allied ? 'Yes' : '<span style="color:red;">No</span>').'</td>';
+                }
+                print
+                    '</tr>';
+            }
+        } else {
+            $role = 'Writer';
+            print
+                '<tr>'.
+                '<td class="table_hdr">Instrument</td>'.
+                '<td class="table_hdr">Id</td>'.
+                '<td class="table_hdr">Experiment</td>'.
+                '<td class="table_hdr">Group</td>'.
+                '<td class="table_hdr"># shifts</td>'.
+                '<td class="table_hdr">Instrument Group</td>'.
+                '<td class="table_hdr">E-Log Role</td>'.
+                '<td class="table_hdr">Is Authorized</td>'.
+            ($fix ? '<td class="table_hdr">Action</td>' : '').
+            '</tr>';
+            $group = 'ps-'.strtolower( $instr_name );
+            $group4auth = 'gid:'.$group;
+            foreach( $experiments as $experiment ) {
+                $has_role = AuthDB::instance()->hasRole( $group4auth, $experiment->id(), $application, $role );
+                $num_shifts = $experiment->num_shifts();
+                print
+                    '<tr>'.
+                    '<td class="table_cell table_cell_left">'.$instr_name.'</td>'.
+                    '<td class="table_cell">'.$experiment->id().'</td>'.
+                    '<td class="table_cell"><a href="../portal/?exper_id='.$experiment->id().'">'.$experiment->name().'</a></td>'.
+                    '<td class="table_cell"><a href="../authdb/?action=view_group&gid='.$experiment->POSIX_gid().'">'.$experiment->POSIX_gid().'</a></td>'.
+                    '<td class="table_cell"'.( $num_shifts == 0 ? ' style="font-color:red;"' : '' ).'>'.$num_shifts.'</td>'.
+                    '<td class="table_cell"><a href="../authdb/?action=view_group&gid='.$group.'">'.$group.'</a></td>'.
+                    '<td class="table_cell">'.$role.'</td>';
+                if( $fix ) {
+                    if( !$has_role ) {
+                        AuthDB::instance()->createRolePlayer( $application, $role, $experiment->id(), $group4auth );
+                        print
+                            '<td class="table_cell"><span style="color:green;">Yes</span></td>'.
+                            '<td class="table_cell table_cell_right"><span style="color:green; font-weight:bold;">just fixed</span></td>';
+                    } else {
+                        print
+                            '<td class="table_cell">Yes</td>'.
+                            '<td class="table_cell table_cell_right"></td>';
+                    }
+                } else {
+                    print
+                        '<td class="table_cell table_cell_right">'.($has_role ? 'Yes' : '<span style="color:red;">No</span>').'</td>';
+                }
+                print
+                    '</tr>';
+            }
+        }
+    }
+    print '</tbody><table>';
+    
+    LogBook::instance()->commit();
+    AuthDB::instance()->commit();
+    
 } catch( Exception $e ) { report_error( $e.'<pre>'.print_r( $e->getTrace(), true ).'</pre>' ); }
   
 ?>
