61b4a6e3b58421599e4c41e31c1fdb9dea858bcf
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-02-01 01:02:45 +0000

    Tagging psana_python with V00-05-00
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5284 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psana_python/tags/V00-05-00/src/EventId.cpp
@@ -0,0 +1,113 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class EventId...
+//
+// Author List:
+//      Andy Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "psana_python/EventId.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "PSTime/Time.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  // type-specific methods
+  PyObject* EventId_time(PyObject* self, PyObject*);
+  PyObject* EventId_run(PyObject* self, PyObject*);
+  PyObject* EventId_fiducials(PyObject* self, PyObject*);
+  PyObject* EventId_vector(PyObject* self, PyObject*);
+
+  PyMethodDef methods[] = {
+    { "time",       EventId_time,     METH_NOARGS,
+        "self.time() -> tuple\n\nReturn the time for event, which is tuple containing seconds and nanoseconds." },
+    { "run",        EventId_run,      METH_NOARGS,
+        "self.run() -> int\n\nReturn the run number for event. If run number is not known -1 will be returned." },
+    { "fiducials",  EventId_fiducials,METH_NOARGS,
+        "self.fiducials() -> int\n\nReturns fiducials counter for the event.\n"
+        "Note that MCC sends fiducials as 17-bit number which overflows "
+        "frequently (fiducials clock runs at 360Hz) so this number is "
+        "not unique. In some cases (e.g. when reading from old HDF5 "
+        "files) fiducials is not know, 0 will be returned in this case." },
+    { "vector",     EventId_vector,   METH_NOARGS, "self.vector() -> int\n\nReturns event counter since Configure.\n"
+        "Note that counter is saved as 15-bits integer and will overflow "
+        "frequently. In some cases (e.g. when reading from old HDF5 "
+        "files) counter is not know, 0 will be returned  in this case." },
+    {0, 0, 0, 0}
+   };
+
+  char typedoc[] = "Event ID should include enough information to uniquely identify "
+      "an event (and possibly a location of the event in data file). "
+      "Currently we include event timestamp and run number into Event ID.";
+
+}
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+void
+psana_python::EventId::initType(PyObject* module)
+{
+  PyTypeObject* type = BaseType::typeObject() ;
+  type->tp_doc = ::typedoc;
+  type->tp_methods = ::methods;
+
+  BaseType::initType("EventId", module);
+}
+
+namespace {
+
+PyObject*
+EventId_time(PyObject* self, PyObject* )
+{
+  boost::shared_ptr<PSEvt::EventId> cself = psana_python::EventId::cppObject(self);
+  const PSTime::Time& time = cself->time();
+
+  PyObject* tuple = PyTuple_New(2);
+  PyTuple_SET_ITEM(tuple, 0, PyInt_FromLong(time.sec()));
+  PyTuple_SET_ITEM(tuple, 1, PyInt_FromLong(time.nsec()));
+
+  return tuple;
+}
+
+PyObject*
+EventId_run(PyObject* self, PyObject* )
+{
+  boost::shared_ptr<PSEvt::EventId> cself = psana_python::EventId::cppObject(self);
+  return PyInt_FromLong(cself->run());
+}
+
+PyObject*
+EventId_fiducials(PyObject* self, PyObject* )
+{
+  boost::shared_ptr<PSEvt::EventId> cself = psana_python::EventId::cppObject(self);
+  return PyInt_FromLong(cself->fiducials());
+}
+
+PyObject*
+EventId_vector(PyObject* self, PyObject* )
+{
+  boost::shared_ptr<PSEvt::EventId> cself = psana_python::EventId::cppObject(self);
+  return PyInt_FromLong(cself->vector());
+}
+
+}
