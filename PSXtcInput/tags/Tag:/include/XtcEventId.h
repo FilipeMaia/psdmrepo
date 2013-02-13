1ae6488f5b20649c3a140cdcb1499398ecea5541
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-02-13 01:04:38 +0000

    Tagging PSXtcInput with Tag:
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5438 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSXtcInput/tags/Tag:/include/XtcEventId.h
@@ -0,0 +1,112 @@
+#ifndef PSXTCINPUT_XTCEVENTID_H
+#define PSXTCINPUT_XTCEVENTID_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcEventId.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "PSEvt/EventId.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "PSTime/Time.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSXtcInput {
+
+/**
+ *  @brief Implementation of the EventId interface for XTC events.
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class XtcEventId : public PSEvt::EventId {
+public:
+
+  // Default constructor
+  XtcEventId (int run, const PSTime::Time& time, unsigned fiducials, unsigned vector) ;
+
+  // Destructor
+  ~XtcEventId () ;
+
+  /**
+   *  @brief Return the time for event.
+   */
+  virtual PSTime::Time time() const;
+
+  /**
+   *  @brief Return the run number for event.
+   *
+   *  If run number is not known -1 will be returned.
+   */
+  virtual int run() const;
+
+  /**
+   *  @brief Returns fiducials counter for the event.
+   *
+   *  Note that MCC sends fiducials as 17-bit number which overflows
+   *  frequently (fiducials clock runs at 360Hz) so this number is
+   *  not unique. In some cases (e.g. when reading from old HDF5
+   *  files) fiducials is not know, 0 will be returned in this case.
+   */
+  virtual unsigned fiducials() const;
+
+  /**
+   *  @brief Returns event counter since Configure.
+   *
+   *  Note that counter is saved as 15-bits integer and will overflow
+   *  frequently. In some cases (e.g. when reading from old HDF5
+   *  files) counter is not know, 0 will be returned  in this case.
+   */
+  virtual unsigned vector() const;
+
+  /// check if two event IDs refer to the same event
+  virtual bool operator==(const EventId& other) const;
+  
+  /// Compare two event IDs for ordering purpose
+  virtual bool operator<(const EventId& other) const;
+  
+  /// Dump object in human-readable format
+  virtual void print(std::ostream& os) const;
+  
+protected:
+
+private:
+
+  // Data members
+  int m_run;
+  PSTime::Time m_time;
+  unsigned m_fiducials;
+  unsigned m_vector;
+};
+
+} // namespace PSXtcInput
+
+#endif // PSXTCINPUT_XTCEVENTID_H
