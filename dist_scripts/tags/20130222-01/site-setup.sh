0bd684ec9c1b0e90a86be83fc27454abbf5da8bc
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-02-22 17:14:25 +0000

    Tagging dist_scripts with 20130222-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5485 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ dist_scripts/tags/20130222-01/site-setup.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+#
+# Script which prepares remote site/machine for installation of PSDM sw
+#
+
+sit_root=${1:?root directory is not specified}
+
+rpm_version=4.4.2.3
+rpm_release=6psdm
+apt_version=0.5.15lorg3.94a
+apt_release=8psdm
+
+# ====================================================================
+
+set -e
+
+# messaging
+err() { echo $* 1>&2 ; exit 1 ; }
+warn() { echo $* 1>&2 ; }
+
+# guess OS, copied from sit_setup.uss
+  os=`uname -s`
+  if [ "$os" = "Linux" -a -r /etc/redhat-release ] ; then
+    rh=`cat /etc/redhat-release | tr ' ' _`
+    case $rh in
+      Red_Hat_Enterprise_Linux_*_release_5.*)   os=rhel5 ;;
+      Red_Hat_Enterprise_Linux_*_release_6.*)   os=rhel6 ;;
+      CentOS_release_5.*)                       os=rhel5 ;;
+      CentOS_release_6.*)                       os=rhel6 ;;
+      Scientific_Linux_*_release_5.*)           os=rhel5 ;;
+      Scientific_Linux_*_release_6.*)           os=rhel6 ;;
+    esac
+  elif [ "$os" = "Linux" -a -r /etc/lsb-release ] ; then
+    . /etc/lsb-release
+    if [ "$DISTRIB_ID" = "Ubuntu" ] ; then
+      os=`echo ubu${DISTRIB_RELEASE} | cut -d. -f1`
+    fi
+  fi
+  test -n "$os" || err "Failed to determine OS type"
+
+# say we do 64-bit for now only
+proc="x86_64"
+
+# make dist directory
+echo "... Create $sit_root/sw/dist directory"
+mkdir -p "$sit_root/sw/dist"
+
+# copy rpm tar
+echo "... Download RPM"
+curl http://pswww.slac.stanford.edu/psdm-repo/rpm-$os-$proc-$rpm_version.tar.gz | \
+	tar -C "$sit_root/sw/dist/" -z -x -f -
+
+#  prepare relocation
+if [ "$sit_root" != "/reg/g/psdm" ] ; then
+  export SIT_ROOT=$sit_root
+  export RPMCONFIGDIR="$sit_root/sw/dist/apt-rpm/$os-$proc/lib/rpm"
+  rpmreloc="--relocate /reg/g/psdm=$sit_root"
+  sed -i "s%/reg/g/psdm%$sit_root%g" "$RPMCONFIGDIR/rpmrc"
+fi
+
+# make .rpmmacros
+echo "... Create \$HOME/.rpmmacros"
+if [ -f "$HOME/.rpmmacros" ] ; then
+	warn "\$HOME/.rpmmacros exists, renaming to \$HOME/.rpmmacros.dist"
+	mv "$HOME/.rpmmacros" "$HOME/.rpmmacros.dist"
+fi
+cat >"$HOME/.rpmmacros" << EOD
+%_topdir $sit_root/sw/dist/rpms
+%_dbpath $sit_root/sw/dist/rpmdb
+%_tmppath $sit_root/sw/dist/rpms/tmp
+%_dbapi                 4
+%_dbapi_rebuild         4
+%debug_package %{nil}
+EOD
+
+echo "... Creating RPM database directory"
+mkdir -p $sit_root/sw/dist/rpmdb
+mkdir -p $sit_root/sw/dist/rpms/tmp
+
+# install RPM and APT
+echo "... Installing RPM"
+$sit_root/sw/dist/apt-rpm/$os-$proc/bin/rpm -ivh $rpmreloc http://pswww.slac.stanford.edu/psdm-repo/$os/$proc/rpm-$os-$proc-$rpm_version-$rpm_release.$proc.rpm
+echo "... Installing APT"
+$sit_root/sw/dist/apt-rpm/$os-$proc/bin/rpm -ivh $rpmreloc http://pswww.slac.stanford.edu/psdm-repo/common/$proc/apt-rpm-$apt_version-$apt_release.$proc.rpm
+$sit_root/sw/dist/apt-rpm/$os-$proc/bin/rpm -ivh $rpmreloc http://pswww.slac.stanford.edu/psdm-repo/$os/$proc/apt-rpm-$os-$proc-$apt_version-$apt_release.$proc.rpm
+
+echo
+echo "Site setup finished. Please add these lines to .bashrc:"
+echo "============================================================================"
+echo "export SIT_ROOT=$sit_root"
+echo "export PATH=$sit_root/sw/dist/apt-rpm/$os-$proc/bin:\$PATH"
+echo "export APT_CONFIG=$sit_root/sw/dist/apt-rpm/$os-$proc/etc/apt/apt.conf"
+test -n "$RPMCONFIGDIR" && echo "export RPMCONFIGDIR=$RPMCONFIGDIR"
+echo "============================================================================"
+echo "(or equivalent to your .cshrc)"
