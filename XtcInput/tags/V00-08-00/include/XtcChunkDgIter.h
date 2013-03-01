0ca7dbc5982096c3e47d493df3c6055632237ae2
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-03-01 22:09:00 +0000

    Tagging XtcInput with V00-08-00
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5560 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ XtcInput/tags/V00-08-00/include/XtcChunkDgIter.h
@@ -0,0 +1,103 @@
+#ifndef XTCINPUT_XTCCHUNKDGITER_H
+#define XTCINPUT_XTCCHUNKDGITER_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcChunkDgIter.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+#include <boost/utility.hpp>
+#include <boost/shared_ptr.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+#include "XtcInput/DgHeader.h"
+#include "XtcInput/SharedFile.h"
+#include "XtcInput/XtcFileName.h"
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace XtcInput {
+
+/**
+ *  @brief Datagram iterator for datagrams in a single chunk file.
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class XtcChunkDgIter : boost::noncopyable {
+public:
+
+  /**
+   *  @brief Create iterator instance
+   *
+   *  Constructor for iterator which reads datagrams from single XTC file.
+   *  It tries to open specified path first, if it fails and path has
+   *  extension ".inprogress" then it removes extension and tries to
+   *  open file again. If that fails the exception will be thrown.
+   *  If file has an extension ".inprogress" and live timeout is non-zero
+   *  then it assumes that file can grow while we are reading it.
+   *
+   *  @param[in]  path         Path name for XTC file
+   *  @param[in]  liveTimeout  If non-zero then defines timeout in seconds for reading live
+   *                     data files, if zero assumes that files is closed already
+   *
+   *  @throw FileOpenException Thrown in case file cannot be open.
+   */
+  XtcChunkDgIter (const XtcFileName& path, unsigned liveTimeout = 0) ;
+
+  // Destructor
+  ~XtcChunkDgIter () ;
+
+  /**
+   *  @brief Returns next datagram header, zero on EOF, throws exceptions for errors
+   *
+   *  If reading ".inprogress" file stops at EOF and there is no
+   *  new data for "live timeout" seconds then XTCLiveTimeout exception
+   *  is generated.
+   *  File is assumed to be closed and does not grow any more when
+   *  it is renamed and ".inprogess" extension is dropped.
+   *
+   *  @return Shared pointer to datagram header object
+   *
+   *  @throw XTCReadException Thrown for any read errors
+   *  @throw XTCLiveTimeout Thrown for timeout during live data reading
+   */
+  boost::shared_ptr<DgHeader> next() ;
+
+protected:
+
+private:
+
+  SharedFile m_file;    ///< Single chunk file
+  off_t      m_off;     ///< offset in file of the next datagram to read
+
+};
+
+} // namespace XtcInput
+
+#endif // XTCINPUT_XTCCHUNKDGITER_H
