376bd623d61782b0b8d0f858cd4379a27689cdae
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-03-01 17:42:41 +0000

    Work in progress. Save all changes.
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5557 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pdscalibdata/trunk/src/CsPadBeamVectorV1.cpp
@@ -0,0 +1,80 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class CsPadBeamVectorV1...
+//
+// Author List:
+//      Mikhail S. Dubrovin
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "pdscalibdata/CsPadBeamVectorV1.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string.h>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+using namespace std;
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace pdscalibdata {
+
+//----------------
+// Constructors --
+//----------------
+
+CsPadBeamVectorV1::CsPadBeamVectorV1 ()
+{
+}
+
+//----------------
+
+CsPadBeamVectorV1::CsPadBeamVectorV1( const std::vector<double> v_parameters )
+{
+    if (v_parameters.size() != NUMBER_OF_PARAMETERS) {
+        WithMsgLog("CsPadBeamVectorV1", error, str) {
+        str << "Expected number of parameters is " << NUMBER_OF_PARAMETERS ;
+        str << ", read from file " << v_parameters.size() ;
+        str << ": check the file.\n" ;
+        }       
+        abort();
+    }
+    size_t arr_size = sizeof( double ) * v_parameters.size();
+    memcpy( &m_beam_vector, &v_parameters[0], arr_size );
+    //this->print();
+}
+
+void CsPadBeamVectorV1::print()
+{
+  cout << endl << "Beam Vector:" << endl;  
+  for( int i=0; i<NUMBER_OF_PARAMETERS; ++i ) { cout << "  " << m_beam_vector[i]; }
+  cout << endl;
+}
+
+//----------------
+//--------------
+// Destructor --
+//--------------
+CsPadBeamVectorV1::~CsPadBeamVectorV1 ()
+{
+}
+
+} // namespace pdscalibdata
