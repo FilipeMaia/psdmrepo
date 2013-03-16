18605d8abe86c9ca7c7d2cf4b84fab86b091b62e
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-03-16 00:01:00 +0000

    Tagging psddl_hdf2psana with V00-00-02
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5788 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl_hdf2psana/tags/V00-00-02/include/usdusb.ddlm.h
@@ -0,0 +1,56 @@
+#ifndef PSDDL_HDF2PSANA_USDUSB_DDLM_H
+#define PSDDL_HDF2PSANA_USDUSB_DDLM_H 1
+
+#include "psddl_psana/usdusb.ddl.h"
+#include "hdf5pp/Group.h"
+#include "hdf5pp/Type.h"
+
+namespace psddl_hdf2psana {
+namespace UsdUsb {
+
+
+namespace ns_DataV1_v0 {
+struct dataset_data {
+  static hdf5pp::Type native_type();
+  static hdf5pp::Type stored_type();
+
+  dataset_data();
+  ~dataset_data();
+
+  int32_t e_count[4];
+  uint16_t analog_in[4]; 
+  uint32_t timestamp; 
+  uint8_t digital_in; 
+
+};
+}
+
+
+class DataV1_v0 : public Psana::UsdUsb::DataV1 {
+public:
+  typedef Psana::UsdUsb::DataV1 PsanaType;
+  DataV1_v0() {}
+  DataV1_v0(hdf5pp::Group group, hsize_t idx)
+    : m_group(group), m_idx(idx) {}
+  DataV1_v0(const boost::shared_ptr<UsdUsb::ns_DataV1_v0::dataset_data>& ds) : m_ds_data(ds) {}
+  virtual ~DataV1_v0() {}
+  virtual uint8_t digital_in() const;
+  virtual uint32_t timestamp() const;
+  virtual ndarray<const uint8_t, 1> status() const;
+
+  virtual ndarray<const uint16_t, 1> analog_in() const;
+  /** Return lower 24 bits of _count array as signed integer values. */
+  ndarray<const int32_t, 1> encoder_count() const;
+
+private:
+  mutable hdf5pp::Group m_group;
+  hsize_t m_idx;
+  mutable boost::shared_ptr<UsdUsb::ns_DataV1_v0::dataset_data> m_ds_data;
+  void read_ds_data() const;
+};
+
+
+} // namespace UsdUsb
+} // namespace psddl_hdf2psana
+
+#endif // PSDDL_HDF2PSANA_USDUSB_DDLM_H
