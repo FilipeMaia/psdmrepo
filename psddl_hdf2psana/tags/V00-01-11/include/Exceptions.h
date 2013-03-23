a2b786b9d35c1110a8fbb1dc971d9c3e483cdcb4
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-03-23 06:01:10 +0000

    Tagging psddl_hdf2psana with V00-01-11
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5897 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl_hdf2psana/tags/V00-01-11/include/Exceptions.h
@@ -0,0 +1,86 @@
+#ifndef PSDDL_HDF2PSANA_EXCEPTIONS_H
+#define PSDDL_HDF2PSANA_EXCEPTIONS_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class Exceptions.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "ErrSvc/Issue.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace psddl_hdf2psana {
+
+/// @addtogroup psddl_hdf2psana
+
+/**
+ *  @ingroup psddl_hdf2psana
+ *
+ *  @brief Base class for exception classes for psddl_hdf2psana package.
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version $Id$
+ *
+ *  @author Andy Salnikov
+ */
+
+class Exception : public ErrSvc::Issue {
+public:
+
+  /// Constructor takes the reason for an exception
+  Exception ( const ErrSvc::Context& ctx, const std::string& what ) ;
+
+};
+
+/// Exception thrown when group name cannot be converted to source address
+class ExceptionGroupSourceName : public Exception {
+public:
+
+  ExceptionGroupSourceName ( const ErrSvc::Context& ctx, const std::string& group ) ;
+
+};
+
+/// Exception thrown when group name cannot be converted to TypeId
+class ExceptionGroupTypeIdName : public Exception {
+public:
+
+  ExceptionGroupTypeIdName ( const ErrSvc::Context& ctx, const std::string& group ) ;
+
+};
+
+/// Exception thrown when data rank is not as expected
+class ExceptionDataRank : public Exception {
+public:
+
+  ExceptionDataRank( const ErrSvc::Context& ctx, int rank, int expectedRank ) ;
+
+};
+
+
+} // namespace psddl_hdf2psana
+
+#endif // PSDDL_HDF2PSANA_EXCEPTIONS_H
