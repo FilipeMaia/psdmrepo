c405d10fd83714deb8dc99b7bfae7f4cd13ebbab
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-03-24 05:09:15 +0000

    Tagging psddl_hdf2psana with V00-01-13
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5907 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl_hdf2psana/tags/V00-01-13/include/fccd.ddlm.h
@@ -0,0 +1,63 @@
+#ifndef PSDDL_HDF2PSANA_FCCD_DDLM_H
+#define PSDDL_HDF2PSANA_FCCD_DDLM_H 1
+
+#include "psddl_psana/fccd.ddl.h"
+#include "hdf5pp/Group.h"
+#include "hdf5pp/Type.h"
+#include "PSEvt/Proxy.h"
+
+namespace psddl_hdf2psana {
+namespace FCCD {
+
+
+namespace ns_FccdConfigV2_v0 {
+struct dataset_config {
+  static hdf5pp::Type native_type();
+  static hdf5pp::Type stored_type();
+
+  dataset_config();
+  ~dataset_config();
+
+  uint16_t outputMode; 
+  uint8_t ccdEnable; 
+  uint8_t focusMode; 
+  uint32_t exposureTime; 
+  uint32_t width; 
+  uint32_t height; 
+  uint32_t trimmedWidth; 
+  uint32_t trimmedHeight; 
+  float dacVoltage[Psana::FCCD::FccdConfigV2::NVoltages];
+  uint16_t waveform[Psana::FCCD::FccdConfigV2::NWaveforms];
+};
+}
+
+
+class FccdConfigV2_v0 : public Psana::FCCD::FccdConfigV2 {
+public:
+  typedef Psana::FCCD::FccdConfigV2 PsanaType;
+  FccdConfigV2_v0() {}
+  FccdConfigV2_v0(hdf5pp::Group group, hsize_t idx)
+    : m_group(group), m_idx(idx) {}
+  virtual ~FccdConfigV2_v0() {}
+  virtual uint16_t outputMode() const;
+  virtual uint8_t ccdEnable() const;
+  virtual uint8_t focusMode() const;
+  virtual uint32_t exposureTime() const;
+  virtual ndarray<const float, 1> dacVoltages() const;
+  virtual ndarray<const uint16_t, 1> waveforms() const;
+  virtual uint32_t width() const;
+  virtual uint32_t height() const;
+  virtual uint32_t trimmedWidth() const;
+  virtual uint32_t trimmedHeight() const;
+private:
+  mutable hdf5pp::Group m_group;
+  hsize_t m_idx;
+  mutable boost::shared_ptr<FCCD::ns_FccdConfigV2_v0::dataset_config> m_ds_config;
+  void read_ds_config() const;
+};
+
+
+} // namespace FCCD
+} // namespace psddl_hdf2psana
+
+#endif // PSDDL_HDF2PSANA_FCCD_DDLM_H
