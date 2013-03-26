b9977b8ca8fb28dbe3598352c77e595bdfab0eff
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-03-26 16:42:49 +0000

    Tagging psddl_hdf2psana with V00-01-20
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@5965 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl_hdf2psana/tags/V00-01-20/src/evr.ddlm.cpp
@@ -0,0 +1,218 @@
+#include "psddl_hdf2psana/evr.ddlm.h"
+
+#include <boost/make_shared.hpp>
+
+#include "hdf5pp/CompoundType.h"
+#include "hdf5pp/EnumType.h"
+#include "hdf5pp/Utils.h"
+#include "hdf5pp/VlenType.h"
+
+namespace psddl_hdf2psana {
+namespace EvrData {
+
+
+hdf5pp::Type ns_DataV3_v0_dataset_data_stored_type()
+{
+  typedef ns_DataV3_v0::dataset_data DsType;
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<DsType>();
+  hdf5pp::VlenType _array_type_fifoEvents = hdf5pp::VlenType::vlenType(hdf5pp::TypeTraits<EvrData::ns_FIFOEvent_v0::dataset_data>::stored_type());
+  type.insert("fifoEvents", offsetof(DsType, vlen_fifoEvents), _array_type_fifoEvents);
+  return type;
+}
+
+hdf5pp::Type ns_DataV3_v0::dataset_data::stored_type()
+{
+  static hdf5pp::Type type = ns_DataV3_v0_dataset_data_stored_type();
+  return type;
+}
+
+hdf5pp::Type ns_DataV3_v0_dataset_data_native_type()
+{
+  typedef ns_DataV3_v0::dataset_data DsType;
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<DsType>();
+  hdf5pp::VlenType _array_type_fifoEvents = hdf5pp::VlenType::vlenType(hdf5pp::TypeTraits<EvrData::ns_FIFOEvent_v0::dataset_data>::native_type());
+  type.insert("fifoEvents", offsetof(DsType, vlen_fifoEvents), _array_type_fifoEvents);
+  return type;
+}
+
+hdf5pp::Type ns_DataV3_v0::dataset_data::native_type()
+{
+  static hdf5pp::Type type = ns_DataV3_v0_dataset_data_native_type();
+  return type;
+}
+ns_DataV3_v0::dataset_data::dataset_data()
+{
+  this->vlen_fifoEvents = 0;
+  this->fifoEvents = 0;
+}
+ns_DataV3_v0::dataset_data::~dataset_data()
+{
+  free(this->fifoEvents);
+}
+
+ndarray<const Psana::EvrData::FIFOEvent, 1> DataV3_v0::fifoEvents() const {
+  if (not m_ds_data) read_ds_data();
+  if (m_ds_storage_data_fifoEvents.empty()) {
+    unsigned shape[] = {m_ds_data->vlen_fifoEvents};
+    ndarray<Psana::EvrData::FIFOEvent, 1> tmparr(shape);
+    std::copy(m_ds_data->fifoEvents, m_ds_data->fifoEvents+m_ds_data->vlen_fifoEvents, tmparr.begin());
+    m_ds_storage_data_fifoEvents = tmparr;
+  }
+  return m_ds_storage_data_fifoEvents;
+}
+
+void DataV3_v0::read_ds_data() const {
+  // dataset name for EvrDataV3 has changed at some point from "evrData" to "data",
+  // we need to try both here
+  std::string dsname = "data";
+  if (not m_group.hasChild(dsname)) dsname = "evrData";
+  m_ds_data = hdf5pp::Utils::readGroup<EvrData::ns_DataV3_v0::dataset_data>(m_group, dsname, m_idx);
+}
+
+
+
+hdf5pp::Type ns_IOChannel_v0_dataset_data_stored_type()
+{
+  typedef ns_IOChannel_v0::dataset_data DsType;
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<DsType>();
+  type.insert("name", offsetof(DsType, name), hdf5pp::TypeTraits<const char*>::stored_type());
+
+  // variable-size type for info array
+  hdf5pp::Type infoType = hdf5pp::VlenType::vlenType ( hdf5pp::TypeTraits<Pds::ns_DetInfo_v0::dataset_data>::stored_type() );
+
+  type.insert("info", offsetof(DsType, ninfo), infoType);
+  return type;
+}
+
+hdf5pp::Type ns_IOChannel_v0::dataset_data::stored_type()
+{
+  static hdf5pp::Type type = ns_IOChannel_v0_dataset_data_stored_type();
+  return type;
+}
+
+hdf5pp::Type ns_IOChannel_v0_dataset_data_native_type()
+{
+  typedef ns_IOChannel_v0::dataset_data DsType;
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<DsType>();
+  type.insert("name", offsetof(DsType, name), hdf5pp::TypeTraits<const char*>::native_type());
+
+  // variable-size type for info array
+  hdf5pp::Type infoType = hdf5pp::VlenType::vlenType ( hdf5pp::TypeTraits<Pds::ns_DetInfo_v0::dataset_data>::native_type() );
+
+  type.insert("info", offsetof(DsType, ninfo), infoType);
+  return type;
+}
+
+hdf5pp::Type ns_IOChannel_v0::dataset_data::native_type()
+{
+  static hdf5pp::Type type = ns_IOChannel_v0_dataset_data_native_type();
+  return type;
+}
+ns_IOChannel_v0::dataset_data::dataset_data()
+{
+}
+ns_IOChannel_v0::dataset_data::~dataset_data()
+{
+}
+
+ns_IOChannel_v0::dataset_data::operator Psana::EvrData::IOChannel() const
+{
+  Pds::DetInfo dinfos[MaxInfos];
+  std::copy(infos, infos+ninfo, dinfos);
+  return Psana::EvrData::IOChannel(name, ninfo, dinfos);
+}
+
+boost::shared_ptr<Psana::EvrData::IOChannel>
+Proxy_IOChannel_v0::getTypedImpl(PSEvt::ProxyDictI* dict, const Pds::Src& source, const std::string& key)
+{
+  if (not m_data) {
+    boost::shared_ptr<EvrData::ns_IOChannel_v0::dataset_data> ds_data = hdf5pp::Utils::readGroup<EvrData::ns_IOChannel_v0::dataset_data>(m_group, "data", m_idx);
+    m_data = boost::make_shared<PsanaType>(*ds_data);
+  }
+
+  return m_data;
+}
+
+
+
+
+hdf5pp::Type ns_IOConfigV1_v0_dataset_config_stored_type()
+{
+  typedef ns_IOConfigV1_v0::dataset_config DsType;
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<DsType>();
+  hdf5pp::EnumType<int32_t> _enum_type_conn = hdf5pp::EnumType<int32_t>::enumType();
+  _enum_type_conn.insert("FrontPanel", Psana::EvrData::OutputMap::FrontPanel);
+  _enum_type_conn.insert("UnivIO", Psana::EvrData::OutputMap::UnivIO);
+  type.insert("conn", offsetof(DsType, conn), _enum_type_conn);
+  return type;
+}
+
+hdf5pp::Type ns_IOConfigV1_v0::dataset_config::stored_type()
+{
+  static hdf5pp::Type type = ns_IOConfigV1_v0_dataset_config_stored_type();
+  return type;
+}
+
+hdf5pp::Type ns_IOConfigV1_v0_dataset_config_native_type()
+{
+  typedef ns_IOConfigV1_v0::dataset_config DsType;
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<DsType>();
+  hdf5pp::EnumType<int32_t> _enum_type_conn = hdf5pp::EnumType<int32_t>::enumType();
+  _enum_type_conn.insert("FrontPanel", Psana::EvrData::OutputMap::FrontPanel);
+  _enum_type_conn.insert("UnivIO", Psana::EvrData::OutputMap::UnivIO);
+  type.insert("conn", offsetof(DsType, conn), _enum_type_conn);
+  return type;
+}
+
+hdf5pp::Type ns_IOConfigV1_v0::dataset_config::native_type()
+{
+  static hdf5pp::Type type = ns_IOConfigV1_v0_dataset_config_native_type();
+  return type;
+}
+
+ns_IOConfigV1_v0::dataset_config::dataset_config()
+{
+}
+
+ns_IOConfigV1_v0::dataset_config::~dataset_config()
+{
+}
+
+
+
+uint16_t IOConfigV1_v0::nchannels() const {
+  if (not m_ds_config.get()) read_ds_channels();
+  return m_ds_channels.shape()[0];
+}
+
+ndarray<const Psana::EvrData::IOChannel, 1> IOConfigV1_v0::channels() const {
+  if (m_ds_channels.empty()) read_ds_channels();
+  return m_ds_channels;
+}
+
+Psana::EvrData::OutputMap::Conn IOConfigV1_v0::conn() const {
+  if (not m_ds_config.get()) read_ds_config();
+  return Psana::EvrData::OutputMap::Conn(m_ds_config->conn);
+}
+
+void IOConfigV1_v0::read_ds_config() const {
+  m_ds_config = hdf5pp::Utils::readGroup<EvrData::ns_IOConfigV1_v0::dataset_config>(m_group, "config", m_idx);
+}
+
+void IOConfigV1_v0::read_ds_channels() const {
+  ndarray<EvrData::ns_IOChannel_v0::dataset_data, 1> arr = hdf5pp::Utils::readNdarray<EvrData::ns_IOChannel_v0::dataset_data, 1>(m_group, "channels", m_idx);
+  ndarray<Psana::EvrData::IOChannel, 1> tmp(arr.shape());
+  std::copy(arr.begin(), arr.end(), tmp.begin());
+  m_ds_channels = tmp;
+}
+
+
+uint32_t
+DataV3_v0::numFifoEvents() const
+{
+  if (not m_ds_data) read_ds_data();
+  return m_ds_data->vlen_fifoEvents;
+}
+
+} // namespace EvrData
+} // namespace psddl_hdf2psana
---
+++ psddl_hdf2psana/tags/V00-01-20/src/fccd.ddlm.cpp
@@ -0,0 +1,126 @@
+#include "psddl_hdf2psana/fccd.ddlm.h"
+
+#include <boost/lexical_cast.hpp>
+
+#include "hdf5pp/ArrayType.h"
+#include "hdf5pp/CompoundType.h"
+#include "hdf5pp/EnumType.h"
+#include "hdf5pp/VlenType.h"
+#include "hdf5pp/Utils.h"
+
+namespace psddl_hdf2psana {
+namespace FCCD {
+
+
+hdf5pp::Type ns_FccdConfigV2_v0_dataset_config_stored_type()
+{
+  typedef ns_FccdConfigV2_v0::dataset_config DsType;
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<DsType>();
+  type.insert("outputMode", offsetof(DsType, outputMode), hdf5pp::TypeTraits<uint16_t>::stored_type());
+  type.insert("ccdEnable", offsetof(DsType, ccdEnable), hdf5pp::TypeTraits<uint8_t>::stored_type());
+  type.insert("focusMode", offsetof(DsType, focusMode), hdf5pp::TypeTraits<uint8_t>::stored_type());
+  type.insert("exposureTime", offsetof(DsType, exposureTime), hdf5pp::TypeTraits<uint32_t>::stored_type());
+  type.insert("width", offsetof(DsType, width), hdf5pp::TypeTraits<uint32_t>::stored_type());
+  type.insert("height", offsetof(DsType, height), hdf5pp::TypeTraits<uint32_t>::stored_type());
+  type.insert("trimmedWidth", offsetof(DsType, trimmedWidth), hdf5pp::TypeTraits<uint32_t>::stored_type());
+  type.insert("trimmedHeight", offsetof(DsType, trimmedHeight), hdf5pp::TypeTraits<uint32_t>::stored_type());
+  for (int i = 0; i != Psana::FCCD::FccdConfigV2::NVoltages; ++ i) {
+    std::string name = "dacVoltage" + boost::lexical_cast<std::string>(i+1);
+    type.insert(name.c_str(), offsetof(DsType, dacVoltage)+i*sizeof(float), hdf5pp::TypeTraits<float>::stored_type());
+  }
+  for (int i = 0; i != Psana::FCCD::FccdConfigV2::NWaveforms; ++ i) {
+    std::string name = "waveform" + boost::lexical_cast<std::string>(i);
+    type.insert(name.c_str(), offsetof(DsType, waveform)+i*sizeof(uint16_t), hdf5pp::TypeTraits<uint16_t>::stored_type());
+  }
+  return type;
+}
+
+hdf5pp::Type ns_FccdConfigV2_v0::dataset_config::stored_type()
+{
+  static hdf5pp::Type type = ns_FccdConfigV2_v0_dataset_config_stored_type();
+  return type;
+}
+
+hdf5pp::Type ns_FccdConfigV2_v0_dataset_config_native_type()
+{
+  typedef ns_FccdConfigV2_v0::dataset_config DsType;
+  hdf5pp::CompoundType type = hdf5pp::CompoundType::compoundType<DsType>();
+  type.insert("outputMode", offsetof(DsType, outputMode), hdf5pp::TypeTraits<uint16_t>::native_type());
+  type.insert("ccdEnable", offsetof(DsType, ccdEnable), hdf5pp::TypeTraits<uint8_t>::native_type());
+  type.insert("focusMode", offsetof(DsType, focusMode), hdf5pp::TypeTraits<uint8_t>::native_type());
+  type.insert("exposureTime", offsetof(DsType, exposureTime), hdf5pp::TypeTraits<uint32_t>::native_type());
+  type.insert("width", offsetof(DsType, width), hdf5pp::TypeTraits<uint32_t>::native_type());
+  type.insert("height", offsetof(DsType, height), hdf5pp::TypeTraits<uint32_t>::native_type());
+  type.insert("trimmedWidth", offsetof(DsType, trimmedWidth), hdf5pp::TypeTraits<uint32_t>::native_type());
+  type.insert("trimmedHeight", offsetof(DsType, trimmedHeight), hdf5pp::TypeTraits<uint32_t>::native_type());
+  for (int i = 0; i != Psana::FCCD::FccdConfigV2::NVoltages; ++ i) {
+    std::string name = "dacVoltage" + boost::lexical_cast<std::string>(i+1);
+    type.insert(name.c_str(), offsetof(DsType, dacVoltage)+i*sizeof(float), hdf5pp::TypeTraits<float>::native_type());
+  }
+  for (int i = 0; i != Psana::FCCD::FccdConfigV2::NWaveforms; ++ i) {
+    std::string name = "waveform" + boost::lexical_cast<std::string>(i);
+    type.insert(name.c_str(), offsetof(DsType, waveform)+i*sizeof(uint16_t), hdf5pp::TypeTraits<uint16_t>::native_type());
+  }
+  return type;
+}
+
+hdf5pp::Type ns_FccdConfigV2_v0::dataset_config::native_type()
+{
+  static hdf5pp::Type type = ns_FccdConfigV2_v0_dataset_config_native_type();
+  return type;
+}
+ns_FccdConfigV2_v0::dataset_config::dataset_config()
+{
+}
+ns_FccdConfigV2_v0::dataset_config::~dataset_config()
+{
+}
+uint16_t FccdConfigV2_v0::outputMode() const {
+  if (not m_ds_config) read_ds_config();
+  return uint16_t(m_ds_config->outputMode);
+}
+uint8_t FccdConfigV2_v0::ccdEnable() const {
+  if (not m_ds_config) read_ds_config();
+  return uint8_t(m_ds_config->ccdEnable);
+}
+uint8_t FccdConfigV2_v0::focusMode() const {
+  if (not m_ds_config) read_ds_config();
+  return uint8_t(m_ds_config->focusMode);
+}
+uint32_t FccdConfigV2_v0::exposureTime() const {
+  if (not m_ds_config) read_ds_config();
+  return uint32_t(m_ds_config->exposureTime);
+}
+ndarray<const float, 1> FccdConfigV2_v0::dacVoltages() const {
+  if (not m_ds_config) read_ds_config();
+  boost::shared_ptr<float> ptr(m_ds_config, m_ds_config->dacVoltage);
+  return make_ndarray<float>(ptr, Psana::FCCD::FccdConfigV2::NVoltages);
+}
+ndarray<const uint16_t, 1> FccdConfigV2_v0::waveforms() const {
+  if (not m_ds_config) read_ds_config();
+  boost::shared_ptr<uint16_t> ptr(m_ds_config, m_ds_config->waveform);
+  return make_ndarray<uint16_t>(ptr, Psana::FCCD::FccdConfigV2::NWaveforms);
+}
+uint32_t FccdConfigV2_v0::width() const {
+  if (not m_ds_config) read_ds_config();
+  return uint32_t(m_ds_config->width);
+}
+uint32_t FccdConfigV2_v0::height() const {
+  if (not m_ds_config) read_ds_config();
+  return uint32_t(m_ds_config->height);
+}
+uint32_t FccdConfigV2_v0::trimmedWidth() const {
+  if (not m_ds_config) read_ds_config();
+  return uint32_t(m_ds_config->trimmedWidth);
+}
+uint32_t FccdConfigV2_v0::trimmedHeight() const {
+  if (not m_ds_config) read_ds_config();
+  return uint32_t(m_ds_config->trimmedHeight);
+}
+void FccdConfigV2_v0::read_ds_config() const {
+  m_ds_config = hdf5pp::Utils::readGroup<FCCD::ns_FccdConfigV2_v0::dataset_config>(m_group, "config", m_idx);
+}
+
+
+} // namespace FCCD
+} // namespace psddl_hdf2psana
