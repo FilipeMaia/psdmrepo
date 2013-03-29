c7ca2bdb769c3a6ce623c83200a705ae37286165
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-03-29 17:42:21 +0000

    Tagging hdf5pp with V00-05-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@6017 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ hdf5pp/tags/V00-05-01/include/GroupIter.h
@@ -0,0 +1,96 @@
+#ifndef HDF5PP_GROUPITER_H
+#define HDF5PP_GROUPITER_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class GroupIter.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "hdf5/hdf5.h"
+#include "hdf5pp/Group.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace hdf5pp {
+
+/// @addtogroup hdf5pp
+
+/**
+ *  @ingroup hdf5pp
+ *
+ *  @brief Class which implements iteration over groups in HDF5 group/file. 
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see Group
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class GroupIter  {
+public:
+
+  /// Enum specifying which links to include
+  enum LinkType {
+    HardLink = 0x1,
+    SoftLink = 0x2,
+    Any = 0x3
+  };
+
+  /**
+   *  @brief Constructor from existing group object.
+   *  
+   *  @param[in] group     Group object which is iterated
+   *  @param[in] skipSoft  if set to true then soft links are skipped
+   *  
+   *  Iterator will return all direct sub-groups of the specified group.
+   */
+  GroupIter (const Group& group, LinkType type = Any);
+
+  // Destructor
+  ~GroupIter () ;
+
+  /**
+   *  @brief Returns next group
+   *  
+   *  If there are no more groups then it returns invalid group object.
+   */
+  Group next();
+  
+protected:
+
+private:
+
+  Group m_group;        ///< Group object
+  LinkType m_type;      ///< type of links to include in iteration
+  hsize_t m_nlinks;     ///< Total number of links in a group
+  hsize_t m_idx;        ///< Current index
+};
+
+} // namespace hdf5pp
+
+#endif // HDF5PP_GROUPITER_H
