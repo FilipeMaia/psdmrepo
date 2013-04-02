dc52257cf14b4dcff47cad502b0a269215ed8c50
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-04-02 00:54:11 +0000

    Tagging CorAna with V00-00-12
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@6063 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ CorAna/tags/V00-00-12/src/ConfigFileGenerator.py
@@ -0,0 +1,280 @@
+#--------------------------------------------------------------------------
+# File and Version Information:
+#  $Id$
+#
+# Description:
+#  Module ConfigFileGenerator...
+#
+#------------------------------------------------------------------------
+
+"""Generates the configuration files for psana from current configuration parameters
+
+This software was developed for the LCLS project.  If you use all or 
+part of it, please give an appropriate acknowledgment.
+
+@version $Id: template!python!py 4 2008-10-08 19:27:36Z salnikov $
+
+@author Mikhail S. Dubrovin
+"""
+
+#------------------------------
+#  Module's version from CVS --
+#------------------------------
+__version__ = "$Revision: 4 $"
+# $Source$
+
+#--------------------------------
+#  Imports of standard modules --
+#--------------------------------
+import sys
+import os
+
+from ConfigParametersCorAna import confpars as cp
+from Logger                 import logger
+from FileNameManager        import fnm
+
+#import AppUtils.AppDataPath as apputils
+import           AppDataPath as apputils # My version, added in path the '../../data:'
+
+#-----------------------------
+
+class ConfigFileGenerator :
+    """Generates the configuration files for psana from current configuration parameters
+    """
+
+    def __init__ (self) :
+        """
+        @param path_in  path to the input psana configuration stub-file
+        @param path_out path to the output psana configuration file with performed substitutions
+        @param d_subs   dictionary of substitutions
+        @param keys     keys from the dictionary       
+        """
+        path_in  = None 
+        path_out = None 
+        d_subs   = None
+        keys     = None 
+
+#-----------------------------
+#-----------------------------
+#-----------------------------
+#-----------------------------
+
+    def make_psana_cfg_file_for_peds_scan (self) :
+        self.path_in  = apputils.AppDataPath('CorAna/scripts/psana-peds-scan.cfg').path()
+        self.path_out = fnm.path_peds_scan_psana_cfg()
+        self.d_subs   = {'FNAME_XTC'            : fnm.path_dark_xtc_cond(),
+                         'SKIP'                 : 'IS_NOT_USED',
+                         'EVENTS'               : 'FOR_ALL_EVENTS',
+                         'FNAME_TIMESTAMP_LIST' : fnm.path_peds_scan_tstamp_list()
+                         }
+
+        self.print_substitution_dict()
+        self.make_cfg_file()
+
+#-----------------------------
+
+    def make_psana_cfg_file_for_peds_aver (self) :
+        self.path_in  = apputils.AppDataPath('CorAna/scripts/psana-peds-aver.cfg').path()
+        self.path_out = fnm.path_peds_aver_psana_cfg()
+        self.d_subs   = {'FNAME_XTC'      : fnm.path_dark_xtc_cond(),
+                         'SKIP'           : str( cp.bat_dark_start.value() - 1 ),
+                         'EVENTS'         : str( cp.bat_dark_end.value() - cp.bat_dark_start.value() + 1 ),
+                         'IMG_REC_MODULE' : str( cp.bat_img_rec_mod.value() ),
+                         'DETINFO'        : str( cp.bat_det_info.value() ),
+                         'FNAME_PEDS_AVE' : fnm.path_pedestals_ave(),
+                         'FNAME_PEDS_RMS' : fnm.path_pedestals_rms()
+                         }
+
+        if cp.mask_hot_is_used.value() : 
+            self.d_subs['FNAME_HOTPIX_MASK'   ] = fnm.path_hotpix_mask()
+            self.d_subs['HOTPIX_THRESHOLD_ADU'] = str( cp.mask_hot_thr.value() )
+        else :
+            self.d_subs['FNAME_HOTPIX_MASK'   ] = ''
+            self.d_subs['HOTPIX_THRESHOLD_ADU'] = '10000'
+
+        self.print_substitution_dict()
+        self.make_cfg_file()
+
+#-----------------------------
+#-----------------------------
+#-----------------------------
+#-----------------------------
+
+    def make_psana_cfg_file_for_data_scan (self) :
+        self.path_in  = apputils.AppDataPath('CorAna/scripts/psana-data-scan.cfg').path()
+        self.path_out = fnm.path_data_scan_psana_cfg()
+        self.d_subs   = {'FNAME_XTC'                         : fnm.path_data_xtc_cond(),
+                         'SKIP'                              : 'IS_NOT_USED',
+                         'EVENTS'                            : 'FOR_ALL_EVENTS',
+                         'FNAME_TIMESTAMP_LIST'              : fnm.path_data_scan_tstamp_list(),
+                         'FNAME_INTENSITY_MONITORS_DATA'     : fnm.path_data_scan_monitors_data(),
+                         'FNAME_INTENSITY_MONITORS_COMMENTS' : fnm.path_data_scan_monitors_commments()
+                         }
+
+        self.print_substitution_dict()
+        self.make_cfg_file()
+
+#-----------------------------
+
+    def make_psana_cfg_file_for_data_aver (self) :
+        self.path_in  = apputils.AppDataPath('CorAna/scripts/psana-data-aver.cfg').path()
+        self.path_out = fnm.path_data_aver_psana_cfg()
+        self.d_subs   = {'FNAME_XTC'      : fnm.path_data_xtc_cond(),
+                         'SKIP'           : str( cp.bat_data_start.value() - 1 ),
+                         'EVENTS'         : str( cp.bat_data_end.value() - cp.bat_data_start.value() + 1 ),
+                         'IMG_REC_MODULE' : str( cp.bat_img_rec_mod.value() ),
+                         'DETINFO'        : str( cp.bat_det_info.value() ),
+                         'FNAME_DATA_AVE' : fnm.path_data_ave(),
+                         'FNAME_DATA_RMS' : fnm.path_data_rms(),
+                         'SAT_THR_ADU'    : str( cp.ccdset_adcsatu.value() ),
+                         'SATPIX_MASK'    : fnm.path_satpix_mask(),
+                         'SATPIX_FRAC'    : fnm.path_satpix_frac(),
+                         'HOTPIX_MASK'    : '', # fnm.path_hotpix_mask(),
+                         'HOTPIX_FRAC'    : '' # fnm.path_hotpix_frac()
+                         }
+        # cp.ccdset_ccdgain.value()
+        # cp.ccdset_ccdeff .value()
+
+        self.print_substitution_dict()
+        self.make_cfg_file()
+
+#-----------------------------
+#-----------------------------
+#-----------------------------
+#-----------------------------
+
+    def make_psana_cfg_file_for_cora_split (self) :
+        self.path_in  = apputils.AppDataPath('CorAna/scripts/psana-cora-split.cfg').path()
+        self.path_out = fnm.path_cora_split_psana_cfg()
+
+        self.d_subs   = {'FNAME_XTC'       : fnm.path_data_xtc_cond(),
+                         'SKIP'            : str( cp.bat_data_start.value() - 1 ),
+                         'EVENTS'          : str( cp.bat_data_end.value() - cp.bat_data_start.value() + 1 ),
+                         'IMG_REC_MODULE'  : str( cp.bat_img_rec_mod.value() ),
+                         'DETINFO'         : str( cp.bat_det_info.value() ),
+                         'PATH_PREFIX_CORA': str( fnm.path_prefix_cora() ),
+                         'IMG_NPARTS'      : str( cp.bat_img_nparts.value() ),
+                         'FNAME_PEDS_AVE'  : fnm.path_pedestals_ave(),
+                         }
+
+        fname_imon_cfg = fnm.path_cora_split_imon_cfg()
+        self.make_imon_cfg_file (fname_imon_cfg)
+        self.d_subs['FNAME_IMON_CFG' ] = str( fname_imon_cfg )
+
+
+        if cp.lld_type.value() == 'ADU' : #  ['NONE', 'ADU', 'RMS']
+            self.d_subs['THRESHOLD_ADU' ] = str( cp.lld_adu.value() )
+            self.d_subs['DO_CONST_THR'  ] = 'true'
+            self.d_subs['THRESHOLD_NRMS'] = '0'
+            self.d_subs['FNAME_PEDS_RMS'] = ''
+
+        elif cp.lld_type.value() == 'RMS' : 
+            self.d_subs['THRESHOLD_ADU' ] = '0'
+            self.d_subs['DO_CONST_THR'  ] = 'false'
+            self.d_subs['THRESHOLD_NRMS'] = str( cp.lld_rms.value() )
+            self.d_subs['FNAME_PEDS_RMS'] = fnm.path_pedestals_rms()
+
+        else : 
+            self.d_subs['THRESHOLD_ADU' ] = '0'
+            self.d_subs['DO_CONST_THR'  ] = 'false'
+            self.d_subs['THRESHOLD_NRMS'] = '0'
+            self.d_subs['FNAME_PEDS_RMS'] = ''
+
+        self.print_substitution_dict()
+        self.make_cfg_file()
+
+#-----------------------------
+#-----------------------------
+
+    def get_text_table_of_imon_pars (self) :
+        text = ''
+        for i, (name, ch1, ch2, ch3, ch4, norm, sele, sele_min, sele_max, norm_ave, short_name) in enumerate(cp.imon_pars_list) :
+            src_imon  = ' %s' % (name.value().ljust(32))
+            name_imon = ' %s' % (short_name.value().ljust(16))
+            bits      = ' %d %d %d %d   %d %d' % (ch1.value(), ch2.value(), ch3.value(), ch4.value(), norm.value(), sele.value())
+            vals      = '   %9.4f %9.4f %9.4f' % (sele_min.value(), sele_max.value(), norm_ave.value())
+            s         = src_imon + name_imon + bits + vals
+
+            #if norm.value() or sele.value() : text += s + '\n' # Short form of the imon_cfg file
+            text += s + '\n'
+        return text
+
+
+    def make_imon_cfg_file (self, fname='imon_cfg.txt') :
+        text_table = self.get_text_table_of_imon_pars()
+        logger.info('Make intensity monitors configuration file: ' + fname + '\n' + text_table, __name__)
+        fout = open(fname,'w')
+        fout.write(text_table)
+        fout.close() 
+
+#-----------------------------
+#-----------------------------
+#-----------------------------
+#-----------------------------
+#-----------------------------
+
+    def print_substitution_dict (self) :
+        logger.debug('Substitution dictionary:',__name__)
+        for k,v in self.d_subs.iteritems() :
+            msg = '%s : %s' % (k.ljust(16), v.ljust(32))
+            logger.debug(msg)
+
+
+#-----------------------------
+
+    def make_cfg_file (self) :
+
+        logger.info('Make configuration file: ' + self.path_out,__name__)
+        logger.debug('path_cfg_stub = ' + self.path_in)
+        logger.debug('path_cfg      = ' + self.path_out)
+        #print 'path_cfg      = ' + self.path_out
+
+        self.keys   = self.d_subs.keys()
+
+        fin  = open(self.path_in, 'r')
+        fout = open(self.path_out,'w')
+        for line in fin :
+            line_sub = self.line_with_substitution(line)
+            fout.write(line_sub)
+            #logger.info(line_sub)
+            #print line_sub,
+
+        fin .close() 
+        fout.close() 
+
+#-----------------------------
+
+    def line_with_substitution(self, line) :
+        fields = line.split()
+        line_sub = ''
+        for field in fields :
+
+            field_sub = self.field_substituted(field)
+            line_sub += field_sub + ' '
+
+        line_sub.rstrip(' ')
+        line_sub += '\n'
+        return line_sub
+
+#-----------------------------
+
+    def field_substituted(self, field) :
+        if field in self.keys : return self.d_subs[field]
+        else                  : return field
+
+#-----------------------------
+
+cfg = ConfigFileGenerator ()
+
+#-----------------------------
+#
+#  In case someone decides to run this module
+#
+if __name__ == "__main__" :
+
+    #cfg.make_psana_cfg_file_for_peds()
+    cfg.make_psana_cfg_file_for_peds_scan()
+
+    sys.exit ( 'End of test for ConfigFileGenerator' )
+
+#-----------------------------
