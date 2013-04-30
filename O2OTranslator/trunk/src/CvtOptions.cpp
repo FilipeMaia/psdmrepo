94016470ab482f85e9260dd15dad12d04c9fced8
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-04-30 20:23:58 +0000

    Tag: V00-28-03 2013-04-30 Andy Salnikov - fix for CvtOptions::chunkSize() for the case when initial guess for   chunk size from configuration data is way too high
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@6183 b967ad99-d558-0410-b138-e0f6c56caec7

--- O2OTranslator/trunk/src/CvtOptions.cpp
+++ O2OTranslator/trunk/src/CvtOptions.cpp
@@ -77,10 +77,10 @@ CvtOptions::chunkSize(hsize_t chunkSizeBytes, const hdf5pp::Type& type, hsize_t 
     chunk = ::max_objects_per_chunk;
   } else if (chunk < ::min_objects_per_chunk) {
     chunk = ::min_objects_per_chunk;
-    if (chunk*obj_size > ::abs_max_chunk_size) {
-      chunk = ::abs_max_chunk_size / obj_size;
-      if (chunk == 0) chunk = 1;
-    }
+  }
+  if (chunk*obj_size > ::abs_max_chunk_size) {
+    chunk = ::abs_max_chunk_size / obj_size;
+    if (chunk == 0) chunk = 1;
   }
   return chunk;
 }
