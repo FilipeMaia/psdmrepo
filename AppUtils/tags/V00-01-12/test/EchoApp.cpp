3e8f345ede73b70c0c2cffeff5640a0cf65e00cd
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-05-22 18:26:25 +0000

    Tagging AppUtils with V00-01-12
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@6357 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ AppUtils/tags/V00-01-12/test/EchoApp.cpp
@@ -0,0 +1,59 @@
+// ==================================================
+#include <string>
+#include <iostream>
+
+#include "AppUtils/AppBase.h"
+#include "AppUtils/AppCmdOpt.h"
+#include "AppUtils/AppCmdOptBool.h"
+#include "AppUtils/AppCmdArgList.h"
+#include "MsgLogger/MsgLogger.h"
+
+class EchoApp : public AppUtils::AppBase {
+public:
+  EchoApp( const std::string& appname ) ;
+  ~EchoApp() {}
+
+protected:
+
+  int runApp() ;
+
+private :
+  AppUtils::AppCmdOptBool m_newLine ;
+  AppUtils::AppCmdOpt<std::string> m_sep ;
+  AppUtils::AppCmdArgList<std::string> m_args ;
+};
+
+EchoApp::EchoApp( const std::string& appname )
+  : AppUtils::AppBase(appname)
+  , m_newLine( 'n', "disable output of new line", true )
+  , m_sep( 's', "separator", "string", "output word separator", " " )
+  , m_args ( "strings", "the list of strings to print", std::list<std::string>() )
+{
+  addOption( m_newLine ) ;
+  addOption( m_sep ) ;
+  addArgument( m_args ) ;
+}
+
+int EchoApp::runApp()
+{
+  MsgLogRoot( debug, "Starting with newLine=" << m_newLine.value() <<
+                     " and sep=\"" << m_sep.value() << "\"" );
+
+  bool first = true ;
+  typedef AppUtils::AppCmdArgList<std::string>::const_iterator Iter ;
+  for ( Iter i = m_args.begin() ; i != m_args.end() ; ++i ) {
+    if ( first ) {
+      first = false ;
+    } else {
+      std::cout << m_sep.value() ;
+    }
+    std::cout << *i ;
+  }
+  if ( m_newLine.value() ) std::cout << '\n' ;
+
+  return 0 ;
+}
+
+// this macro generates main() which runs above application
+APPUTILS_MAIN(EchoApp)
+// ==================================================
