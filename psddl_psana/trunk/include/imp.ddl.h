e8165ed9f815e35f3e23ddb674ea0bdd1ed37291
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-05-22 15:35:55 +0000

    Tag: V00-04-26 2013-05-22 Andy Salnikov - regenerate whole thing from psddldata V00-04-28
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@6334 b967ad99-d558-0410-b138-e0f6c56caec7

--- psddl_psana/trunk/include/imp.ddl.h
+++ psddl_psana/trunk/include/imp.ddl.h
@@ -59,14 +59,14 @@ public:
   Sample()
   {
   }
-  Sample(const uint32_t* arg__channels)
+  Sample(const uint16_t* arg__channels)
   {
     std::copy(arg__channels, arg__channels+(4), _channels);
   }
-  ndarray<const uint32_t, 1> channels() const { return make_ndarray(&_channels[0], 4); }
-  static uint32_t _sizeof()  { return ((((0+(4*(4)))+4)-1)/4)*4; }
+  ndarray<const uint16_t, 1> channels() const { return make_ndarray(&_channels[0], 4); }
+  static uint32_t _sizeof()  { return ((((0+(2*(4)))+2)-1)/2)*2; }
 private:
-  uint32_t	_channels[4];
+  uint16_t	_channels[4];
 };
 
 /** @class LaneStatus
@@ -112,8 +112,6 @@ public:
   virtual uint8_t vc() const = 0;
   virtual uint8_t lane() const = 0;
   virtual uint32_t frameNumber() const = 0;
-  virtual uint32_t ticks() const = 0;
-  virtual uint32_t fiducials() const = 0;
   virtual uint32_t range() const = 0;
   virtual const Imp::LaneStatus& laneStatus() const = 0;
   virtual ndarray<const Imp::Sample, 1> samples() const = 0;
