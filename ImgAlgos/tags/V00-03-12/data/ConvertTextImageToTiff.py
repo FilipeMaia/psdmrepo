bc0762b12b634b4d33b5b9a662208ee0fc554fe3
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-06-04 01:05:06 +0000

    Tagging ImgAlgos with V00-03-12
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@6384 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ ImgAlgos/tags/V00-03-12/data/ConvertTextImageToTiff.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#--------------------
+
+import os
+import sys
+import numpy as np
+import scipy.misc as scimisc
+from optparse import OptionParser
+
+#--------------------
+
+def get_input_parameters() :
+
+    def_fname = 'cspad2x2.1'
+    def_dname = '.'
+
+    parser = OptionParser(description='Process optional input parameters.', usage = "usage: %prog [options]")
+    parser.add_option('-f', '--fname', dest='fname', default=def_fname, action='store', type='string', help='input file name prefix, i.e. cspad2x2.1')
+    parser.add_option('-d', '--dname', dest='dname', default=def_dname, action='store', type='string', help='input/output directory name')
+    (opts, args) = parser.parse_args()
+    print 'opts:',opts
+    #print 'args:',args
+    return (opts, args)
+
+#--------------------
+
+def save_array_in_file(ofname, arr) :
+    scimisc.imsave(ofname, arr)
+
+#--------------------
+
+def get_array_from_file(fname) :
+    #print 'get_array_from_file:', fname
+    return np.loadtxt(fname, dtype=np.float32)
+
+#--------------------
+
+def get_list_of_files_to_convert(dirname, fname_prefix) :
+    list_of_files = []
+    for fname in os.listdir(dirname) :
+        if fname.split('-')[0] == fname_prefix and os.path.splitext(fname)[1] == '.txt' :
+            list_of_files.append(fname)
+    return  list_of_files
+
+#--------------------
+
+def do_main() :
+
+    opts, args = get_input_parameters()
+    fname = opts.fname
+    dname = opts.dname
+
+    for fname in get_list_of_files_to_convert(dname, fname) :
+        ifpath = dname + '/'+ fname        
+        ofpath = dname + '/'+ os.path.splitext(fname)[0] + '.tiff'
+        print 'Convert ' + ifpath + ' ===> ' + ofpath
+        arr = get_array_from_file(ifpath)
+        save_array_in_file(ofpath, arr)
+        #print 'arr=\n', arr
+  
+#--------------------
+
+if __name__ == '__main__' :
+    do_main()
+    sys.exit('The End')
+
+#--------------------
---
+++ ImgAlgos/tags/V00-03-12/data/PlotCameraImageFromFile.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+#--------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+import h5py
+
+#--------------------
+# Define graphical methods
+
+def plot_image (arr, img_range=None, zrange=None) :    # range = (left, right, low, high), zrange=(zmin,zmax)
+    fig = plt.figure(num=1, figsize=(12,12), dpi=80, facecolor='w', edgecolor='w', frameon=True)
+    fig.subplots_adjust(left=0.10, bottom=0.08, right=0.98, top=0.92, wspace=0.2, hspace=0.1)
+    imAxes = figAxes.imshow(arr, origin='upper', interpolation='nearest', aspect='auto', extent=img_range)
+    if zrange != None : imAxes.set_clim(zrange[0],zrange[1])
+    colbar = fig.colorbar(imAxes, pad=0.03, fraction=0.04, shrink=1.0, aspect=40, orientation=1)
+
+def plot_histogram(arr, amp_range=(0,500),figsize=(6,6), bins=40) :
+    fig = plt.figure(figsize=figsize, dpi=80, facecolor='w', edgecolor='w', frameon=True)
+    plt.hist(arr.flatten(), bins=bins, range=amp_range)
+    #fig.canvas.manager.window.move(500,10)
+    
+#--------------------
+
+def get_array_from_file(fname, dtype=np.float32) :
+    print 'get_array_from_text_file:', fname
+    return np.loadtxt(fname, dtype=dtype)
+
+def get_array_from_bin_file(fname, dtype=np.float32) :
+    print 'get_array_from_bin_file:', fname
+    return np.fromfile(fname, dtype)
+
+def get_numpy_array_from_file(fname) :
+    print 'get_numpy_array_from_file:', fname
+    return np.load(fname)
+
+#--------------------
+
+def get_input_parameters() :
+
+    fname_def = './image0_ev000115.txt'
+    Amin_def  =   0
+    Amax_def  = 100
+
+    nargs = len(sys.argv)
+    print 'sys.argv[0]: ', sys.argv[0]
+    print 'nargs: ', nargs
+
+    if nargs == 1 :
+        print 'Will use all default parameters\n',\
+              'Expected command: ' + sys.argv[0] + ' <infname> <Amin> <Amax>' 
+        sys.exit('CHECK INPUT PARAMETERS!')
+
+    if nargs  > 1 : fname = sys.argv[1]
+    else          : fname = fname_def
+
+    if nargs  > 2 : Amin = float(sys.argv[2])
+    else          : Amin = Amin_def
+
+    if nargs  > 3 : Amax = float(sys.argv[3])
+    else          : Amax = Amax_def
+
+    if nargs  > 4 :         
+        print 'WARNING: Too many input arguments! Exit program.\n'
+        sys.exit('CHECK INPUT PARAMETERS!')
+
+    ampRange = (Amin, Amax)
+    if ampRange[0]==None or ampRange[1]==None : ampRange = None
+
+    print 'Input file name  :', fname
+    print 'ampRange         :', ampRange
+ 
+    return fname,ampRange 
+
+#--------------------
+
+def get_array(fname) :
+
+    fname, ampRange = get_input_parameters()
+
+    if os.path.splitext(fname)[1] == '.txt' :
+        return get_array_from_file(fname) 
+
+    if os.path.splitext(fname)[1] == '.bin' :
+
+        arr = get_array_from_bin_file(fname, dtype=np.uint16)
+        if arr.shape[0] == 1300*1340 :
+            arr.shape=(1300,1340)
+            return arr
+
+        arr = get_array_from_bin_file(fname, dtype=np.float)
+        if arr.shape[0] == 400*400 :
+            arr.shape=(400,400)
+            return arr
+
+        else :
+            print "WARNING !!! Binary file array shape: %s, unknown case of reshaping to 2d..." % arr.shape
+
+    return np.zeros((100,100))
+
+#--------------------
+
+def do_main() :
+
+    fname, ampRange = get_input_parameters()
+
+    arr = get_array(fname)
+
+    #arr = get_array_from_file('/reg/neh/home1/sikorski/xcs_pyana_current/e167-r0020-s00-c00/2013-04-03-09-43-06-939033/e167-r0020-s00-c00_dark_img.txt'
+    #arr = get_array_from_file('z-comparison/e167-r0020-s00-c00-dark-img-from-marcin.txt') 
+    #arr -= get_array_from_file('z-comparison/t1-xcsi0112-r0020-peds-ave.txt') 
+
+    print 'arr:\n', arr
+    print 'arr.shape=', arr.shape
+
+    plot_image(arr, zrange=ampRange)
+    #plt.get_current_fig_manager().window.move(10,10)       # works for GTk
+    plt.get_current_fig_manager().window.geometry("+10+10") # works for Tk 
+
+    plot_histogram(arr, amp_range=ampRange)
+    plt.get_current_fig_manager().window.geometry("+950+10")
+
+    plt.show()
+
+#--------------------
+
+if __name__ == '__main__' :
+    do_main()
+    sys.exit('The End')
+
+#--------------------
