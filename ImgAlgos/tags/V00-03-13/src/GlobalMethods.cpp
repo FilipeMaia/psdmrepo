429de2c47f3242bf9d255c2667d6980dba4bd0d7
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-06-07 23:08:22 +0000

    Tagging ImgAlgos with V00-03-13
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@6393 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ ImgAlgos/tags/V00-03-13/src/GlobalMethods.cpp
@@ -0,0 +1,170 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class GlobalMethods...
+//
+// Author List:
+//      Mikhail S. Dubrovin
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "ImgAlgos/GlobalMethods.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <boost/lexical_cast.hpp>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include <boost/shared_ptr.hpp>
+#include "PSEvt/EventId.h"
+#include "PSTime/Time.h"
+//#include "psana/Module.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+using namespace std;
+using namespace ImgAlgos;
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace ImgAlgos {
+
+//----------------
+// Constructors --
+//----------------
+GlobalMethods::GlobalMethods ()
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+GlobalMethods::~GlobalMethods ()
+{
+}
+
+//--------------------
+
+std::string 
+stringFromUint(unsigned number, unsigned width, char fillchar)
+{
+  stringstream ssNum; ssNum << setw(width) << setfill(fillchar) << number;
+  return ssNum.str();
+}
+
+//--------------------
+
+std::string
+stringTimeStamp(PSEvt::Event& evt, std::string fmt) // fmt="%Y%m%dT%H:%M:%S%f"
+{
+  boost::shared_ptr<PSEvt::EventId> eventId = evt.get();
+  if (eventId.get()) return (eventId->time()).asStringFormat(fmt);
+  else               return std::string("time-stamp-is-unavailable");
+}
+
+//--------------------
+
+double
+doubleTime(PSEvt::Event& evt)
+{
+  boost::shared_ptr<PSEvt::EventId> eventId = evt.get();
+  if (eventId.get()) return double(eventId->time().sec()) + 1e-9*eventId->time().nsec();
+  else               return double(0);
+}
+
+//--------------------
+
+std::string  
+stringRunNumber(PSEvt::Event& evt, unsigned width)
+{
+  boost::shared_ptr<PSEvt::EventId> eventId = evt.get();
+  if (eventId.get()) return stringFromUint(eventId->run(), width);
+  else               return std::string("run-is-not-defined");
+}
+
+//--------------------
+
+/// Returns integer run number
+int 
+getRunNumber(PSEvt::Event& evt)
+{
+  boost::shared_ptr<PSEvt::EventId> eventId = evt.get();
+  if (eventId.get()) {
+    return eventId->run();
+  } else {
+    MsgLogRoot(warning, "Cannot determine run number, will use 0.");
+    return int(0);
+  }
+}
+
+//--------------------
+
+unsigned 
+fiducials(PSEvt::Event& evt)
+{
+  boost::shared_ptr<PSEvt::EventId> eventId = evt.get();
+  if (eventId.get()) return eventId->fiducials();
+  else               return 0;
+}
+
+//--------------------
+
+unsigned 
+eventCounterSinceConfigure(PSEvt::Event& evt)
+{
+  boost::shared_ptr<PSEvt::EventId> eventId = evt.get();
+  if (eventId.get()) return eventId->vector();
+  else               return 0;
+}
+
+//--------------------
+
+void 
+printSizeOfTypes()
+{
+  std::cout << "Size Of Types:" 
+            << "\nsizeof(bool    ) = " << sizeof(bool    ) << " with typeid(bool    ).name(): " << typeid(bool    ).name() 
+            << "\nsizeof(uint8_t ) = " << sizeof(uint8_t ) << " with typeid(uint8_t ).name(): " << typeid(uint8_t ).name()  
+            << "\nsizeof(uint16_t) = " << sizeof(uint16_t) << " with typeid(uint16_t).name(): " << typeid(uint16_t).name()  
+            << "\nsizeof(uint32_t) = " << sizeof(uint32_t) << " with typeid(uint32_t).name(): " << typeid(uint32_t).name()  
+            << "\nsizeof(int32_t ) = " << sizeof(int32_t ) << " with typeid(int32_t ).name(): " << typeid(int32_t ).name()  
+            << "\nsizeof(int     ) = " << sizeof(int     ) << " with typeid(int     ).name(): " << typeid(int     ).name()  
+            << "\nsizeof(float   ) = " << sizeof(float   ) << " with typeid(float   ).name(): " << typeid(float   ).name()  
+            << "\nsizeof(double  ) = " << sizeof(double  ) << " with typeid(double  ).name(): " << typeid(double  ).name()  
+            << "\n\n";
+}
+
+//--------------------
+// Define the shape or throw message that can not do that.
+void 
+defineImageShape(PSEvt::Event& evt, const PSEvt::Source& src, const std::string& key, unsigned* shape)
+{
+  if ( defineImageShapeForType<double>  (evt, src, key, shape) ) return;
+  if ( defineImageShapeForType<float>   (evt, src, key, shape) ) return;
+  if ( defineImageShapeForType<int>     (evt, src, key, shape) ) return;
+  if ( defineImageShapeForType<int32_t> (evt, src, key, shape) ) return;
+  if ( defineImageShapeForType<uint32_t>(evt, src, key, shape) ) return;
+  if ( defineImageShapeForType<uint16_t>(evt, src, key, shape) ) return;
+  if ( defineImageShapeForType<uint8_t> (evt, src, key, shape) ) return;
+
+  const std::string msg = "Image shape is tested for double, uint16_t, int, float, uint8_t and is not defined in the event(...)\nfor source:" 
+                        + boost::lexical_cast<std::string>(src) + " key:" + key + "\nEXIT psana...";
+  MsgLogRoot(error, msg);
+  throw std::runtime_error("EXIT psana...");
+}
+
+//--------------------
+//--------------------
+} // namespace ImgAlgos
