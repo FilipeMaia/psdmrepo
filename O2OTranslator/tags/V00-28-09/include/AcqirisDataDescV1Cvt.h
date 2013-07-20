415bda83fe7e83e10436d1d4c98a3a1119bda2f6
salnikov@SLAC.STANFORD.EDU <salnikov@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-07-20 12:47:15 +0000

    Tagging O2OTranslator with V00-28-09
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@6530 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ O2OTranslator/tags/V00-28-09/include/AcqirisDataDescV1Cvt.h
@@ -0,0 +1,107 @@
+#ifndef O2OTRANSLATOR_ACQIRISDATADESCV1CVT_H
+#define O2OTRANSLATOR_ACQIRISDATADESCV1CVT_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class AcqirisDataDescV1Cvt.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "O2OTranslator/EvtDataTypeCvt.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "H5DataTypes/AcqirisDataDescV1.h"
+#include "pdsdata/acqiris/DataDescV1.hh"
+#include "O2OTranslator/CvtOptions.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace O2OTranslator {
+
+class ConfigObjectStore;
+
+/// @addtogroup O2OTranslator
+
+/**
+ *  @ingroup O2OTranslator
+ *
+ *  @brief Special converter class for Pds::Acqiris::DataDescV1
+ *
+ *  This software was developed for the LUSI project.  If you use all or
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version $Id$
+ *
+ *  @author Andrei Salnikov
+ */
+
+class AcqirisDataDescV1Cvt : public EvtDataTypeCvt<Pds::Acqiris::DataDescV1> {
+public:
+
+  typedef Pds::Acqiris::DataDescV1 XtcType ;
+  typedef H5DataTypes::AcqirisDataDescV1 H5Type ;
+
+  // Default constructor
+  AcqirisDataDescV1Cvt ( const hdf5pp::Group& group,
+                         const std::string& typeGroupName,
+                         const Pds::Src& src,
+                         const ConfigObjectStore& configStore,
+                         const CvtOptions& cvtOptions,
+                         int schemaVersion ) ;
+
+  // Destructor
+  virtual ~AcqirisDataDescV1Cvt () ;
+
+protected:
+
+  /// method called to create all necessary data containers
+  virtual void makeContainers(hdf5pp::Group group, const Pds::TypeId& typeId, const O2OXtcSrc& src);
+
+  // typed conversion method
+  virtual void fillContainers(hdf5pp::Group group,
+                              const XtcType& data,
+                              size_t size,
+                              const Pds::TypeId& typeId,
+                              const O2OXtcSrc& src);
+
+  // fill containers for missing data
+  virtual void fillMissing(hdf5pp::Group group,
+                           const Pds::TypeId& typeId,
+                           const O2OXtcSrc& src);
+
+private:
+
+  typedef H5DataTypes::ObjectContainer<H5DataTypes::AcqirisDataDescV1> DataCont ;
+  typedef H5DataTypes::ObjectContainer<H5DataTypes::AcqirisTimestampV1> TimestampCont ;
+  typedef H5DataTypes::ObjectContainer<int16_t> WaveformCont ;
+
+  // Data members
+  const ConfigObjectStore& m_configStore;
+  boost::shared_ptr<DataCont> m_dataCont ;
+  boost::shared_ptr<TimestampCont> m_timestampCont ;
+  boost::shared_ptr<WaveformCont> m_waveformCont ;
+  size_t n_miss;
+
+};
+
+} // namespace O2OTranslator
+
+#endif // O2OTRANSLATOR_ACQIRISDATADESCV1CVT_H
---
+++ O2OTranslator/tags/V00-28-09/src/TimepixDataV1Cvt.cpp
@@ -0,0 +1,117 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class TimepixDataV1Cvt...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "O2OTranslator/TimepixDataV1Cvt.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include "MsgLogger/MsgLogger.h"
+#include "O2OTranslator/O2OExceptions.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+  const char logger[] = "TimepixDataV1Cvt" ;
+}
+
+//    ----------------------------------------
+//    -- Public Function Member Definitions --
+//    ----------------------------------------
+
+namespace O2OTranslator {
+
+//----------------
+// Constructors --
+//----------------
+TimepixDataV1Cvt::TimepixDataV1Cvt ( const hdf5pp::Group& group,
+    const std::string& typeGroupName,
+    const Pds::Src& src,
+    const CvtOptions& cvtOptions,
+    int schemaVersion )
+  : EvtDataTypeCvt<XtcType>(group, typeGroupName, src, cvtOptions, schemaVersion)
+  , m_dataCont()
+  , m_imageCont()
+  , n_miss(0)
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+TimepixDataV1Cvt::~TimepixDataV1Cvt ()
+{
+}
+
+// method called to create all necessary data containers
+void
+TimepixDataV1Cvt::makeContainers(hdf5pp::Group group, const Pds::TypeId& typeId, const O2OXtcSrc& src)
+{
+  // create container for frames
+  m_dataCont = makeCont<DataCont>("data", group, true);
+}
+
+// typed conversion method
+void
+TimepixDataV1Cvt::fillContainers(hdf5pp::Group group,
+    const XtcType& data,
+    size_t size,
+    const Pds::TypeId& typeId,
+    const O2OXtcSrc& src)
+{
+  // DataV1 images come with some strange shuffling. To un-shuffle it we transform it
+  // into DataV2 which knows how to do it.
+  unsigned objSize = sizeof(Pds::Timepix::DataV2) + data.data_size();
+  char* buf = new char[objSize];
+  Pds::Timepix::DataV2* data2 = new (buf) Pds::Timepix::DataV2(data);
+
+  uint32_t height = data2->height();
+  uint32_t width = data2->width();
+
+  // store the data
+  H5Type tpdata(*data2);
+  m_dataCont->append(tpdata);
+
+  hdf5pp::Type type = H5Type::stored_data_type(height, width) ;
+  if (not m_imageCont) {
+    m_imageCont = makeCont<ImageCont>("image", group, true, type);
+    if (n_miss) m_imageCont->resize(n_miss);
+  }
+  m_imageCont->append(*(uint16_t*)data2->data(), type);
+
+  delete [] buf;
+}
+
+// fill containers for missing data
+void
+TimepixDataV1Cvt::fillMissing(hdf5pp::Group group,
+                         const Pds::TypeId& typeId,
+                         const O2OXtcSrc& src)
+{
+  m_dataCont->resize(m_dataCont->size() + 1);
+  if (m_imageCont) {
+    m_imageCont->resize(m_imageCont->size() + 1);
+  } else {
+    ++ n_miss;
+  }
+}
+
+} // namespace O2OTranslator
