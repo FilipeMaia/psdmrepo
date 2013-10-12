dbd6dccc70e453ca2a4d261ef5e5d8912e5dfef8
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-10-12 01:19:28 +0000

    Tagging DataPortal with V00-05-00
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@6956 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ DataPortal/tags/V00-05-00/web/html/ws/runtable_calib_save.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Update calibration status of the specified run of the experiment.
+ * Return the updated record of that run.
+ */
+require_once 'dataportal/dataportal.inc.php' ;
+
+DataPortal\ServiceJSON::run_handler ('POST', function ($SVC) {
+    $exper_id = $SVC->required_int('exper_id') ;
+    $runnum   = $SVC->required_int('run') ;
+    $dark     = $SVC->required_int('dark') ;
+    $comment  = $SVC->optional_str('comment', '') ;
+
+    $experiment = $SVC->logbook()->find_experiment_by_id($exper_id) ;
+    if (!$experiment) $SVC->abort("no experiment found for id={$xper_id}") ;
+
+    $run = $experiment->find_run_by_num($runnum) ;
+    if (!$run) $SVC->abort("no run {$runnum} found within experiment with id={$xper_id}") ;
+
+    $run->set_attr_val_INT ('Calibrations', 'dark',    $dark) ;
+    $run->set_attr_val_TEXT('Calibrations', 'comment', $comment) ;
+
+    $runs = array() ;
+    $runs[$runnum] = array('dark' => $dark, 'comment' => $comment) ;
+
+    $SVC->finish (array('runs' => $runs)) ;
+}) ;
+
+?>
