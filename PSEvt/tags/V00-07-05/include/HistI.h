359218e6b166459b66e043d4d3ca2e8f72967322
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-11-08 21:46:31 +0000

    Tagging PSEvt V00-07-05
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7099 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSEvt/tags/V00-07-05/include/HistI.h
@@ -0,0 +1,82 @@
+#ifndef PSEVT_HISTI_H
+#define PSEVT_HISTI_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: 
+//
+// Description:
+//	Class HistI.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <boost/utility.hpp>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "PSEvt/EventKey.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSEvt {
+
+/**
+ *  @ingroup PSEvt
+ *  
+ *  @brief Class defining a history interface.
+ *  
+ *  This class supports a simple history interface, total number of
+ *  updates, and total number of updates for a specific EventKey.  
+ *
+ *  @see ProxyDictI
+ *
+ *  @version \$Id:
+ *
+ *  @author David Schneider
+ */
+
+class HistI : boost::noncopyable {
+public:
+
+  // Destructor
+  virtual ~HistI () {}
+
+  /**
+   *  @brief implement to provide number of updates
+   *
+   * Derived classes can provide a simple history mechanism by 
+   * overriding this class and returning the number of updates
+   *  
+   *  @param[out] total number of updates
+   */
+  virtual long totalUpdates() const = 0;
+
+  /**
+   *  @brief implement to provide total number of updates for a key.
+   *
+   *  Derived classes can implement this to provide a simple history on a key - 
+   *  the total number of updates
+   *  
+   *  @param[in]  key EventKey to get updates for
+   *  @param[out] number of updates
+   */
+  virtual long updates(const EventKey & key) const = 0;
+};
+
+} // namespace PSEvt
+
+#endif // PSEVT_HISTI_H
