434640439e2254104e4957710915e79175173adf
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-11-08 21:58:38 +0000

    tagging V00-07-06 of PSEvt
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7101 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSEvt/tags/V00-07-06/include/ProxyDictHist.h
@@ -0,0 +1,105 @@
+#ifndef PSEVT_PROXYDICTHIST_H
+#define PSEVT_PROXYDICTHIST_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: ProxyDict.h 2579 2011-10-28 22:11:09Z salnikov@SLAC.STANFORD.EDU $
+//
+// Description:
+//	Class ProxyDictHist.  keeps track of the number of updates done to each key.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "PSEvt/ProxyDict.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSEvt {
+
+/**
+ *  @ingroup PSEvt
+ *  
+ *  @brief A ProxyDict that implements the HistI interface.
+ *  
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version \$Id:
+ *
+ *  @author David Schneider
+ */
+
+class ProxyDictHist : public ProxyDict, private HistI {
+public:
+
+ ProxyDictHist() : ProxyDict(), m_totalUpdates(0) {}
+ 
+ /**
+   *  @brief returns total number of put and remove calls made with dictionary
+   */
+  virtual long totalUpdates() const { return m_totalUpdates; }
+
+  /**
+   *  @brief returns total number of put and remove calls made for given key
+   *  
+   *  @param[in] key     key to lookup update count for
+   */
+  virtual long updates(const EventKey &key) const { return m_updates[key]; }
+
+  virtual const HistI * hist() const { return this; }
+
+protected:
+
+  /**
+   *  @brief updates count after adding using base class to add to dict.
+   *  
+   *  @param[in] proxy   Proxy object for type T.
+   *  @param[in] key     Event key for the data object.
+   */
+  virtual void putImpl( const boost::shared_ptr<ProxyI>& proxy, const EventKey& key ) {
+    ProxyDict::putImpl(proxy, key);
+    ++m_updates[key];
+    ++m_totalUpdates;
+  }
+
+  /**
+   *  @brief updates count after adding using base class to remove from to dict.
+   *  
+   *  Does not update count if key was not present. 
+   *
+   *  @param[in] key     Event key for the data object.
+   *  @return false if object did not exist before this call
+   */
+  virtual bool removeImpl( const EventKey& key ) {
+    bool objectExistedBeforeCall = ProxyDict::removeImpl(key);
+    if (objectExistedBeforeCall) {
+      ++m_updates[key];
+      ++m_totalUpdates;
+    }
+    return objectExistedBeforeCall;
+  }
+private:
+  long m_totalUpdates;
+  mutable std::map<EventKey, long> m_updates;
+};
+
+} // namespace PSEvt
+
+#endif // PSEVT_PROXYDICTPUTHIST_H
---
+++ PSEvt/tags/V00-07-06/src/EventKey.cpp
@@ -0,0 +1,155 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class EventKey...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "PSEvt/EventKey.h"
+#include "PSEvt/TypeInfoUtils.h"
+ 
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <iostream>
+#include <cxxabi.h>
+#include <stdlib.h>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "pdsdata/xtc/BldInfo.hh"
+#include "pdsdata/xtc/DetInfo.hh"
+#include "pdsdata/xtc/ProcInfo.hh"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+
+  void printDetInfo(std::ostream& str, const Pds::DetInfo& src)
+  {
+    str << "DetInfo(";
+    if (src.detector() == 0xff) {
+      str << '*';
+    } else {
+      str << Pds::DetInfo::name(src.detector());
+    }
+    str << '.';
+    if (src.detId() == 0xff) {
+      str << '*';
+    } else {
+      str << src.detId();
+    }
+    str << ':';
+    if (src.device() == 0xff) {
+      str << '*';
+    } else {
+      str << Pds::DetInfo::name(src.device());
+    }
+    str << '.';
+    if (src.devId() == 0xff) {
+      str << '*';
+    } else {
+      str << src.devId();
+    }
+    str << ')';
+  }
+
+  void printBldInfo(std::ostream& str, const Pds::BldInfo& src)
+  {
+    str << "BldInfo(";
+    if (unsigned(src.type()) != 0xffffffff) str << Pds::BldInfo::name(src);
+    str << ')';
+  }
+
+  void printProcInfo(std::ostream& str, const Pds::ProcInfo& src)
+  {
+    uint32_t ip = src.ipAddr();
+    str << "ProcInfo(" 
+        << ((ip>>24)&0xff) 
+        << '.' << ((ip>>16)&0xff)
+        << '.' << ((ip>>8)&0xff) 
+        << '.' << (ip&0xff) 
+        << ", pid=" << src.processId() 
+        << ')';
+  }
+
+  void print(std::ostream& str, const Pds::Src& src)
+  {
+    if (src.level() == Pds::Level::Source) {
+      printDetInfo(str, static_cast<const Pds::DetInfo&>(src));
+    } else if (src.level() == Pds::Level::Reporter) {
+      printBldInfo(str, static_cast<const Pds::BldInfo&>(src));
+    } else if (src.level() == Pds::Level::NumberOfLevels) {
+      // special match-anything source, empty string
+    } else {
+      printProcInfo(str, static_cast<const Pds::ProcInfo&>(src));
+    }
+  }
+  
+  void print(std::ostream& str, const std::type_info* typeinfo)
+  {
+    str << PSEvt::TypeInfoUtils::typeInfoRealName(typeinfo);
+  }
+
+}
+
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace PSEvt {
+
+bool 
+EventKey::operator<(const EventKey& other) const 
+{
+  if (this->m_src < other.m_src) return true;
+  if (other.m_src < this->m_src) return false;
+  if( TypeInfoUtils::lessTypeInfoPtr()(m_typeinfo, other.m_typeinfo) ) return true;
+  if( TypeInfoUtils::lessTypeInfoPtr()(other.m_typeinfo, m_typeinfo) ) return false;
+  if (m_key < other.m_key) return true;
+  return false;
+}
+
+
+// format the key
+void 
+EventKey::print(std::ostream& str) const
+{
+  str << "EventKey(type=";
+  ::print(str, m_typeinfo);
+  if (m_src == anySource()) {
+    str << ", src=AnySource";
+  } else if (validSrc()) {
+    str << ", src=";
+    ::print(str, m_src);
+  }
+  if (not m_key.empty()) {
+    str << ", key=" << m_key; 
+  }
+  str << ')';
+}
+
+} // namespace PSEvt
+
+namespace Pds {
+/// Helper method to format Pds::Src to a standard stream
+std::ostream&
+operator<<(std::ostream& out, const Pds::Src& src)
+{
+  ::print(out, src);
+  return out;
+}
+}
