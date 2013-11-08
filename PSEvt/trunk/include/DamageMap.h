8362de3e4ae085aaad04d538d54cb1fe063d14c0
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-11-08 21:58:11 +0000

    Tag V00-07-06, PSEvt, add const correctness and documentation to DamageMap
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7100 b967ad99-d558-0410-b138-e0f6c56caec7

--- PSEvt/trunk/include/DamageMap.h
+++ PSEvt/trunk/include/DamageMap.h
@@ -42,26 +42,53 @@ class DamageMap {
 public:
   typedef std::map<PSEvt::EventKey, Pds::Damage> MapImpl;
   typedef MapImpl::iterator iterator;
+  typedef MapImpl::const_iterator const_iterator;
 
+  /**
+   * @brief add damage to DamageMap.  Works the same as std::map::operator[]
+   */
   Pds::Damage & operator[](const PSEvt::EventKey &eventKey) { return m_map[eventKey]; }
 
   iterator begin() { return m_map.begin(); }
   iterator end() {return m_map.end(); }
+  /**
+   * @brief finds damage in the DamageMap. Tries two keys if eventKey has key string.
+   *
+   * EventKey's have three componenents, type, Src and key, where key is a string that
+   * users may create, typically for data derived from original data in the Psana Event store.  
+   * If no damage has been recorded for the given key, and the given key includes a string 
+   * component, find() will also look for damage associated with the corresponding EventKey 
+   * where the string component is blank.
+   *
+   * @param[in] eventKey  EventKey to look up damage for
+   * @return iterator to eventKey position in map. 
+   */
   iterator find(const PSEvt::EventKey &eventKey);
 
+  const_iterator begin() const { return m_map.begin(); }
+  const_iterator end() const { return m_map.end(); }
+  /**
+   * @brief const version of find(), see documentation for find().
+   */
+  const_iterator find(const PSEvt::EventKey &eventKey) const;
   
   void addSrcDamage(Pds::Src src, Pds::Damage damage);
-  bool splitEvent() { return m_droppedContribs.size()>0; }
+  bool splitEvent() const { return m_droppedContribs.size()>0; }
 
-  /// returns a histogram of damage values in the DamageMap associated with EventKeys 
-  /// distinct Damage values are mapped to their frequency within the DamageMap.  No
-  /// reporting is provided for Src damage - use getSrcDroppedContributions or splitEvent
-  /// for this
-  std::map<uint32_t, int> damageCounts();
+  /**
+   * @brief returns a histogram of damage values in the DamageMap associated with EventKeys 
+   *
+   * distinct Damage values are mapped to their frequency within the DamageMap.  No reporting
+   * is provided for Src damage - use getSrcDroppedContributions or splitEvent for this
+   */
+  std::map<uint32_t, int> damageCounts() const;
 
-  /// returns the list of src only damage for the event.  All damage values will
-  /// contain the DroppedContribution bit.
-  std::vector<std::pair<Pds::Src,Pds::Damage> > getSrcDroppedContributions() { return m_droppedContribs; }
+  /**
+   * @brief returns the list of src only damage for the event.  
+   *
+   * All damage values will contain the DroppedContribution bit.
+   */
+  const std::vector<std::pair<Pds::Src,Pds::Damage> > & getSrcDroppedContributions() const { return m_droppedContribs; }
 
  private:
   MapImpl m_map;
@@ -69,7 +96,7 @@ public:
   
 };
    
-std::ostream & operator<<(std::ostream &, DamageMap &);
+std::ostream & operator<<(std::ostream &, const DamageMap &);
 
 }; // namespace
 
--- PSEvt/trunk/src/DamageMap.cpp
+++ PSEvt/trunk/src/DamageMap.cpp
@@ -17,6 +17,18 @@ PSEvt::DamageMap::iterator PSEvt::DamageMap::find(const PSEvt::EventKey &eventKe
   return pos;
 }
 
+PSEvt::DamageMap::const_iterator PSEvt::DamageMap::find(const PSEvt::EventKey &eventKey) const {
+  PSEvt::DamageMap::const_iterator pos = m_map.find(eventKey);
+  if (pos == m_map.end()) {
+    if (eventKey.key().size() > 0) {
+      PSEvt::EventKey eventKeyNoStr(eventKey.typeinfo(), eventKey.src(),"");
+      PSEvt::DamageMap::const_iterator posNoStr = m_map.find(eventKeyNoStr);
+      return posNoStr;
+    }
+  }
+  return pos;
+}
+
 void PSEvt::DamageMap::addSrcDamage(Pds::Src src, Pds::Damage damage) {
   if ( damage.bits() & (1<<Pds::Damage::DroppedContribution)) {
     m_droppedContribs.push_back(std::make_pair(src,damage));
@@ -26,9 +38,9 @@ void PSEvt::DamageMap::addSrcDamage(Pds::Src src, Pds::Damage damage) {
   }
 }
 
-std::map<uint32_t,int> PSEvt::DamageMap::damageCounts() {
+std::map<uint32_t,int> PSEvt::DamageMap::damageCounts() const {
   std::map<uint32_t,int> counts;
-  for (iterator pos = begin(); pos != end(); ++pos) {
+  for (const_iterator pos = begin(); pos != end(); ++pos) {
     uint32_t damageValue = pos->second.value();
     int &count = counts[damageValue];
     ++count;
@@ -38,24 +50,24 @@ std::map<uint32_t,int> PSEvt::DamageMap::damageCounts() {
 
 /// print everything in the DamageMap - EventKeys and damage as well as 
 /// DroppedContribution damage
-std::ostream & PSEvt::operator<<(std::ostream &o, PSEvt::DamageMap &damageMap) {
-  PSEvt::DamageMap::iterator pos;
+std::ostream & PSEvt::operator<<(std::ostream &o, const PSEvt::DamageMap &damageMap) {
+  PSEvt::DamageMap::const_iterator pos;
   bool first = true;
   o.setf(std::ios::hex,std::ios::basefield);
   for (pos = damageMap.begin(); pos != damageMap.end(); ++pos) {
     const PSEvt::EventKey &eventKey = pos->first;
-    Pds::Damage & damage = pos->second;
+    const Pds::Damage & damage = pos->second;
     if (not first) o << ", ";
     first = false;
     o << eventKey << " damage=0x" << damage.value();
   }
   if (damageMap.splitEvent()) {
-    std::vector<std::pair<Pds::Src,Pds::Damage> > droppedContribs = damageMap.getSrcDroppedContributions();
+    const std::vector<std::pair<Pds::Src,Pds::Damage> > &droppedContribs = damageMap.getSrcDroppedContributions();
     o << "DroppedContributions: ";
     for (size_t idx=0; idx < droppedContribs.size(); ++idx) {
-      std::pair<Pds::Src,Pds::Damage> & srcDamagePair = droppedContribs.at(idx);
-      Pds::Src & src = srcDamagePair.first;
-      Pds::Damage & damage = srcDamagePair.second;
+      const std::pair<Pds::Src,Pds::Damage> & srcDamagePair = droppedContribs.at(idx);
+      const Pds::Src & src = srcDamagePair.first;
+      const Pds::Damage & damage = srcDamagePair.second;
       o << "src="<< src << " dmg=0x" <<damage.value();
       if (idx < droppedContribs.size()-1) {
         o << ", ";
