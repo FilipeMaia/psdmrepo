7cce4aac1ec25999edde8d8306ef3d1e0179b366
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-11-16 01:41:55 +0000

    Tagging DataPortal with V00-05-03
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7188 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ DataPortal/tags/V00-05-03/web/html/js/Filemanager_Summary.js
@@ -0,0 +1,50 @@
+/**
+ * The application for displaying the integral info about the data files of the experiment
+ *
+ * @returns {Filemanager_Summary}
+ */
+function Filemanager_Summary (experiment, access_list) {
+
+    // -----------------------------------------
+    // Allways call the base class's constructor
+    // -----------------------------------------
+
+    FwkApplication.call(this) ;
+
+    // ------------------------------------------------
+    // Override event handler defined in the base class
+    // ------------------------------------------------
+
+    this.on_activate = function() {
+        this.on_update() ;
+    } ;
+
+    this.on_deactivate = function() {
+        this.init() ;
+    } ;
+
+    this.on_update = function (sec) {
+        if (this.active) {
+            this.init() ;
+        }
+    } ;
+
+    // -----------------------------
+    // Parameters of the application
+    // -----------------------------
+
+    this.experiment  = experiment ;
+    this.access_list = access_list ;
+
+    // --------------------
+    // Own data and methods
+    // --------------------
+
+    this.is_initialized = false ;
+
+    this.init = function () {
+        if (this.is_initialized) return ;
+        this.is_initialized = true ;
+    } ;
+}
+define_class (Filemanager_Summary, FwkApplication, {}, {});
