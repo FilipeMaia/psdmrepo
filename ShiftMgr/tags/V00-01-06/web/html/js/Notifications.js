c3efd453168182a6d4163b421e53a254e2f94b55
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2013-12-13 22:50:24 +0000

    Tagging ShiftMgr with V00-01-06
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7369 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ ShiftMgr/tags/V00-01-06/web/html/js/Notifications.js
@@ -0,0 +1,50 @@
+/**
+ * The application for managing e-mail notifications on changes in the database
+ *
+ * @returns {Notifications}
+ */
+function Notifications (instr_name) {
+
+    // -----------------------------------------
+    // Allways call the base class's constructor
+    // -----------------------------------------
+
+    FwkApplication.call(this) ;
+
+    // ------------------------
+    // Parameters of the object
+    // ------------------------
+
+    this.instr_name = instr_name || '' ;
+
+    // ------------------------------------------------
+    // Override event handler defined in the base class
+    // ------------------------------------------------
+    
+    this.on_activate = function() {
+        this.on_update() ;
+    } ;
+
+    this.on_deactivate = function() {
+        this.init() ;
+    } ;
+
+    this.on_update = function (sec) {
+        this.init() ;
+        if (this.active) {
+            ;
+        }
+    } ;
+
+    // --------------------
+    // Own data and methods
+    // --------------------
+
+    this.is_initialized = false ;
+
+    this.init = function () {
+        if (this.is_initialized) return ;
+        this.is_initialized = true ;
+    } ;
+}
+define_class (Notifications, FwkApplication, {}, {});
