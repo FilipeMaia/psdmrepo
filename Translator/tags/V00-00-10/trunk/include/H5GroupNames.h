711852092f2e26cefc21e4c812b7817e92dbe6be
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-01-13 22:23:23 +0000

    Tagging Translator with V00-00-10
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7546 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ Translator/tags/V00-00-10/trunk/include/H5GroupNames.h
@@ -0,0 +1,41 @@
+#ifndef TRANSLATOR_H5GROUPNAMES_H
+#define TRANSLATOR_H5GROUPNAMES_H
+
+#include <string>
+#include <typeinfo>
+#include "pdsdata/xtc/Src.hh"
+#include "Translator/TypeAliases.h"
+
+namespace Translator {
+
+/**
+ *  @ingroup Translator
+ *
+ *  @brief transforms C++ Psana types and src locations into hdf5 group names.
+ *
+ *  Also returns true if a C++ type is a NDArray, requires the set of 
+ *  NDArray types recognized in the system to be passed in for this.
+ *
+ *  @note This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @author David Schneider
+ */
+class H5GroupNames {
+ public:
+  H5GroupNames(bool short_bld_name, 
+               const TypeAliases::TypeInfoSet & ndarrays);
+  std::string nameForType(const std::type_info *typeInfoPtr);
+  std::string nameForSrc(const Pds::Src &src);
+  std::string nameForSrcKey(const Pds::Src &src, const std::string &key);
+  bool isNDArray(const std::type_info *typeInfoPtr) { 
+    return m_ndarrays.find(typeInfoPtr) != m_ndarrays.end(); 
+  }
+ private:
+  bool m_short_bld_name;
+  const TypeAliases::TypeInfoSet m_ndarrays;
+}; // class H5GroupNames
+
+} // namespace
+
+#endif
