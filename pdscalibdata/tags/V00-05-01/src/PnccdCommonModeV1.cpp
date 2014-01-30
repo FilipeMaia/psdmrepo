9e8383074448f74b063cc57e1a734c55fc18731c
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-01-30 06:54:32 +0000

    Tagging pdscalibdata with V00-05-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7593 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pdscalibdata/tags/V00-05-01/src/PnccdCommonModeV1.cpp
@@ -0,0 +1,63 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: PnccdCommonModeV1.cpp 2014-01-24 11:00:00Z dubrovin@SLAC.STANFORD.EDU $
+//
+// Description:
+//	Class PnccdCommonModeV1...
+//
+// Author List:
+//      Mikhail Dubrovin
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "pdscalibdata/PnccdCommonModeV1.h"
+#include "pdscalibdata/GlobalMethods.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <algorithm>
+#include <stdexcept>
+#include <fstream>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace pdscalibdata {
+
+//----------------
+// Constructors --
+//----------------
+PnccdCommonModeV1::PnccdCommonModeV1() 
+: PnccdBaseV1 ()
+{
+  std::fill_n(m_pars, int(CMSize), pars_t(0));
+}
+
+
+PnccdCommonModeV1::PnccdCommonModeV1 (const std::string& fname) 
+: PnccdBaseV1 ()
+{
+  load_pars_from_file <pars_t> (fname, "common_mode", CMSize, m_pars); 
+}
+
+
+void PnccdCommonModeV1::print()
+{
+  MsgLog("PnccdCommonModeV1", info, "common_mode:\n" << common_mode());
+}
+
+} // namespace pdscalibdata
