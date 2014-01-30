5432d5487d5a2a1433513c7e8bada2177f2d2b9c
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-01-30 06:54:02 +0000

    Work in progress. Save all changes.
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7592 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pdscalibdata/trunk/include/GlobalMethods.h
@@ -0,0 +1,147 @@
+#ifndef PDSCALIBDATA_GLOBALMETHODS_H
+#define PDSCALIBDATA_GLOBALMETHODS_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class GlobalMethods.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+#include <string>
+#include <fstream>   // ofstream
+#include <iomanip>   // for setw, setfill
+#include <sstream>   // for stringstream
+#include <iostream>
+#include <stdexcept>
+//#include <algorithm>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+#include "MsgLogger/MsgLogger.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace pdscalibdata {
+
+/// @addtogroup pdscalibdata
+
+/**
+ *  @ingroup pdscalibdata
+ *
+ *  @brief Global methods for pdscalibdata package
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version $Id$
+ *
+ *  @author Mikhail S. Dubrovin
+ */
+
+using namespace std;
+
+
+enum DATA_TYPE {FLOAT, DOUBLE, SHORT, UNSIGNED, INT, INT16, INT32, UINT, UINT8, UINT16, UINT32};
+
+
+class GlobalMethods  {
+public:
+  GlobalMethods () ;
+  virtual ~GlobalMethods () ;
+
+private:
+  // Copy constructor and assignment are disabled by default
+  GlobalMethods ( const GlobalMethods& ) ;
+  GlobalMethods& operator = ( const GlobalMethods& ) ;
+};
+
+//--------------------
+
+  std::string stringFromUint(unsigned number, unsigned width=6, char fillchar='0');
+
+  void printSizeOfTypes();
+
+//--------------------
+// For type=T returns the string with symbolic data type and its size, i.e. "d of size 8"
+  template <typename T>
+  std::string strOfDataTypeAndSize()
+  {
+    std::stringstream ss; ss << typeid(T).name() << " of size " << sizeof(T);
+    return ss.str();
+  }
+
+//--------------------
+  /**
+   * @brief Load parameters from file
+   * 
+   * @param[in]  fname - path to the file with parameters
+   * @param[in]  type  - type of parameters for messages
+   * @param[in]  size  - size of array with parameters; number of values to load from file
+   * @param[out] pars  - pointer to array with parameters
+   */
+
+  template <typename T>
+  void load_pars_from_file(const std::string& fname, const std::string& comment, const size_t size, T* pars)
+  { 
+    // open file
+    std::ifstream in(fname.c_str());
+    if (not in.good()) {
+      const std::string msg = "Failed to open pedestals file: "+fname;
+      MsgLogRoot(error, msg);
+      throw std::runtime_error(msg);
+    }
+  
+    // read all numbers
+    T* it = pars;
+    size_t count = 0;
+    while(in and count != size) {
+      in >> *it++;
+      ++ count;
+    }
+  
+    // check that we read whole array
+    if (count != size) {
+      const std::string msg = comment+" file does not have enough data: "+fname;
+      MsgLogRoot(error, msg);
+      throw std::runtime_error(msg);
+    }
+  
+    // and no data left after we finished reading
+    float tmp ;
+    if ( in >> tmp ) {
+      ++ count;
+      const std::string msg = comment+" file has extra data: "+fname;
+      MsgLogRoot(error, msg);
+      MsgLogRoot(error, "read " << count << " numbers, expecting " << size );
+      throw std::runtime_error(msg);
+    }
+  }
+
+//--------------------
+//--------------------
+//--------------------
+//--------------------
+//--------------------
+
+} // namespace pdscalibdata
+
+#endif // PDSCALIBDATA_GLOBALMETHODS_H
---
+++ pdscalibdata/trunk/include/PnccdBaseV1.h
@@ -0,0 +1,76 @@
+#ifndef PDSCALIBDATA_PNCCDBASEV1_H
+#define PDSCALIBDATA_PNCCDBASEV1_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: CsPadPedestalsV1.h 6832 2013-09-24 21:14:17Z dubrovin@SLAC.STANFORD.EDU $
+//
+// Description:
+//	Class CsPadPedestalsV1.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <string>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "ndarray/ndarray.h"
+#include "pdsdata/psddl/pnccd.ddl.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace pdscalibdata {
+
+/**
+ *  class PnccdBaseV1 contains common parameters and methods for pnCCD. 
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id: PnccdBaseV1.cpp 2014-01-24 11:00:00Z dubrovin@SLAC.STANFORD.EDU $
+ *
+ *  @author Mikhail Dubrovin
+ */
+
+class PnccdBaseV1 {
+public:
+
+  const static size_t   Segs = 4; 
+  const static size_t   Rows = 512; 
+  const static size_t   Cols = 512; 
+  const static size_t   Size = Segs*Rows*Cols; 
+  
+  // Default constructor
+  PnccdBaseV1 () {};
+  
+  // Destructor
+  ~PnccdBaseV1 () {};
+
+protected:
+
+private:
+
+  // Copy constructor and assignment are disabled by default
+  PnccdBaseV1 ( const PnccdBaseV1& ) ;
+  PnccdBaseV1& operator = ( const PnccdBaseV1& ) ;
+};
+
+} // namespace pdscalibdata
+
+#endif // PDSCALIBDATA_PNCCDBASEV1_H
