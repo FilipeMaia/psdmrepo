d1c6be58124cc8f21958fe5e3cedfa2da6ffd0c1
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-01-30 07:01:58 +0000

    Tagging PSCalib with V00-01-15
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7595 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSCalib/tags/V00-01-15/include/PnccdCalibPars.h
@@ -0,0 +1,262 @@
+#ifndef PSCALIB_PNCCDCALIBPARS_H
+#define PSCALIB_PNCCDCALIBPARS_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class PnccdCalibPars.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>  // open, close etc.
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "ndarray/ndarray.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "psddl_psana/pnccd.ddl.h"
+#include "pdsdata/xtc/Src.hh"
+
+#include "pdscalibdata/PnccdPedestalsV1.h"      
+#include "pdscalibdata/PnccdCommonModeV1.h"        
+#include "pdscalibdata/PnccdPixelStatusV1.h"        
+#include "pdscalibdata/PnccdPixelGainV1.h"        
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace PSCalib {
+
+/// @addtogroup PSCalib PSCalib
+
+/**
+ *  @ingroup PSCalib
+ *
+ *  @brief PnccdCalibPars class loads/holds/provides access to the CSPad2x2
+ *  geometry calibration parameters.
+ *
+ *  This software was developed for the LCLS project. If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see CalibFileFinder
+ *
+ *  @version $Id$
+ *
+ *  @author Mikhail S. Dubrovin
+ *
+ *
+ *
+ *
+ *  @anchor interface
+ *  @par<interface> Interface Description
+ * 
+ *  @li  Include and typedef
+ *  @code
+ *  #include "PSCalib/PnccdCalibPars.h"
+ *  typedef PSCalib::PnccdCalibPars CALIB;
+ *  @endcode
+ *
+ *  @li Instatiation
+ *  \n
+ *  For default constructor:
+ *  @code
+ *  CALIB *calibpars = new CALIB();  
+ *  @endcode
+ *  \n
+ *  For regular constructor:
+ *  @code
+ *  const std::string calibDir   = "/reg/d/psdm/AMO/amoa1214/calib";
+ *  const std::string groupName  = "PNCCD::CalibV1";
+ *  const std::string source     = "Camp.0:pnCCD.1";
+ *  unsigned long     runNumber  = 10;
+ *  Pds::Src src; env.get(...,&src);
+ *  CALIB *calibpars = new CALIB(calibDir, groupName, src, runNumber);  
+ *  @endcode
+ *  \n
+ *  For explicit constructor (depricated):
+ *  @code
+ *  const std::string calibDir   = "/reg/d/psdm/AMO/amoa1214/calib";
+ *  const std::string groupName  = "PNCCD::CalibV1";
+ *  const std::string source     = "Camp.0:pnCCD.1";
+ *  unsigned long     runNumber  = 10;
+ *  CALIB *calibpars = new CALIB(calibDir, groupName, source, runNumber);  
+ *  @endcode
+ *
+ *  @li Printing methods
+ *  @code
+ *  calibpars -> printInputPars();
+ *  calibpars -> printCalibPars();
+ *  calibpars -> printCalibParsStatus();
+ *  @endcode
+ *
+ *  @li Access methods
+ *  @code
+ *  size_t sect=1; // for example...
+ *  int status  = calibpars -> getCalibTypeStatus("pedestals") // Returns status: 0-default, 1-loaded from file
+ *  ndarray<pdscalibdata::PnccdPedestalsV1::pars_t, 3>   nda_peds = calibpars -> pedestals();
+ *  ndarray<pdscalibdata::PnccdPixelStatusV1::pars_t, 3> nda_stat = calibpars -> pixel_status();
+ *  ndarray<pdscalibdata::PnccdCommonModeV1::pars_t, 1>  nda_cmod = calibpars -> common_mode();
+ *  ndarray<pdscalibdata::PnccdPixelGainV1::pars_t, 3>   nda_gain = calibpars -> pixel_gain();
+ *  ... etc. for all other access methods
+ *  @endcode
+ *
+ *  @see CalibFileFinder
+ */
+
+//----------------
+
+class PnccdCalibPars  {
+public:
+
+  /// Default and test constructor
+  PnccdCalibPars ( bool isTestMode = false ) ;
+
+
+  /**
+   *  @brief DEPRICATED constructor, which use string& source
+   *  
+   *  @param[in] calibDir       Calibration directory for current experiment.
+   *  @param[in] typeGroupName  Data type and group names.
+   *  @param[in] source         The name of the data source.
+   *  @param[in] runNumber      Run number to search the valid file name.
+   *  @param[in] print_bits     =0-print no messages; +1-input parameters, +2-print msges from  PSCalib::CalibFileFinder, +4-use default, +8-missing type
+   */ 
+  PnccdCalibPars ( const std::string&   calibDir,           //  /reg/d/psdm/mec/mec73313/calib
+                   const std::string&   typeGroupName,      //  CsPad2x2::CalibV1
+                   const std::string&   source,             //  MecTargetChamber.0:Cspad2x2.1
+                   const unsigned long& runNumber,          //  10
+                   unsigned             print_bits=255 );
+
+  /**
+   *  @brief Regular constructor, which use Pds::Src& src
+   *  
+   *  @param[in] calibDir       Calibration directory for current experiment.
+   *  @param[in] typeGroupName  Data type and group names.
+   *  @param[in] src            The data source object, for example Pds::Src m_src; defined in the env.get(...,&m_src)
+   *  @param[in] runNumber      Run number to search the valid file name.
+   */ 
+  PnccdCalibPars ( const std::string&   calibDir,           //  /reg/d/psdm/mec/mec73313/calib
+                   const std::string&   typeGroupName,      //  CsPad2x2::CalibV1
+                   const Pds::Src&      src,                //  Pds::Src m_src; <- is defined in env.get(...,&m_src)
+                   const unsigned long& runNumber,          //  10
+                   unsigned             print_bits=255 ) ;  
+
+  /// Destructor
+  virtual ~PnccdCalibPars () ;
+
+  //size_t   getNRows             (){ return m_nrows;   };
+  //size_t   getNCols             (){ return m_ncols;   };
+
+  /// Makes member data vector with all supported calibration types such as center, tilt, ...
+  void fillCalibNameVector   ();
+
+  /// Define the path to the calibration file based on input parameters
+  void getCalibFileName      ();
+
+  /// Load all known calibration parameters
+  void loadCalibPars         ();
+
+  /// Fill calibration parameters from vector
+  void fillCalibParsV1       ();
+
+  /// Fill default calibration parameters
+  void fillDefaultCalibParsV1();
+
+  /// Generate error message in the log and abort
+  void fatalMissingFileName  ();
+
+  /// Generate warning message in the log
+  void msgUseDefault         ();
+
+  /// Prints calibration parameters
+  void printCalibPars        ();
+
+  /// Prints input parameters of the object
+  void printInputPars        ();
+
+  /// Prints calibration parameters status
+  void printCalibParsStatus  ();
+
+  /// Returns status of the calibration constants, 0-default, 1-loaded from file @param[in] type - calibration type string-name, for example "center" or "tilt"
+  int getCalibTypeStatus(const std::string&  type) { return m_calibtype_status[type]; };
+
+  /// Returns ndarray of pnCCD pedestals
+  ndarray<pdscalibdata::PnccdPedestalsV1::pars_t, 3> pedestals(){ return m_pedestals -> pedestals(); };
+
+  /// Returns ndarray of pnCCD pixel status
+  ndarray<pdscalibdata::PnccdPixelStatusV1::pars_t, 3> pixel_status(){ return m_pixel_status -> pixel_status(); };
+
+  /// Returns ndarray of pnCCD common mode
+  ndarray<pdscalibdata::PnccdCommonModeV1::pars_t, 1> common_mode(){ return m_common_mode -> common_mode(); };
+
+  /// Returns ndarray of pnCCD pixel gain
+  ndarray<pdscalibdata::PnccdPixelGainV1::pars_t, 3> pixel_gain(){ return m_pixel_gain -> pixel_gain(); };
+
+
+private:
+
+  /// Copy constructor is disabled by default
+  PnccdCalibPars ( const PnccdCalibPars& ) ;
+  /// Assignment is disabled by default
+  PnccdCalibPars operator = ( const PnccdCalibPars& ) ;
+
+//------------------
+// Static Members --
+//------------------
+
+  // Assuming path: /reg/d/psdm/AMO/amoa1214/calib/PNCCD::CalibV1/Camp.0:pnCCD.1/pedestals/1-end.data
+
+  std::string   m_calibdir;       // /reg/d/psdm/AMO/amoa1214/calib
+  std::string   m_calibfilename;  // 1-end.data
+
+  // Data members for regular constructor 
+  std::string   m_calibDir;
+  std::string   m_typeGroupName;
+  std::string   m_source;
+  Pds::Src      m_src;
+  std::string   m_dataType;
+  unsigned long m_runNumber;
+  unsigned      m_print_bits;
+
+  std::vector<std::string> v_calibname; // center, tilt, ...
+
+  std::map<std::string, int> m_calibtype_status; // =0-default, =1-from file
+
+  std::string m_cur_calibname;  
+  std::string m_fname;
+
+  bool m_isTestMode;
+
+  //size_t m_nrows; 
+  //size_t m_ncols; 
+
+  std::ifstream m_file;
+
+  pdscalibdata::PnccdPedestalsV1   *m_pedestals;
+  pdscalibdata::PnccdPixelGainV1   *m_pixel_gain;
+  pdscalibdata::PnccdCommonModeV1  *m_common_mode;
+  pdscalibdata::PnccdPixelStatusV1 *m_pixel_status; 
+
+};
+
+} // namespace PSCalib
+
+#endif // PSCALIB_PNCCDCALIBPARS_H
---
+++ PSCalib/tags/V00-01-15/src/PnccdCalibPars.cpp
@@ -0,0 +1,284 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class PnccdCalibPars...
+//
+// Author List:
+//      Mikhail S. Dubrovin
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "PSCalib/PnccdCalibPars.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <iostream>
+#include <iomanip>  // for std::setw
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+#include "PSCalib/CalibFileFinder.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+using namespace std;
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace PSCalib {
+
+const char logger[] = "PSCalib";
+
+//----------------
+// Constructors --
+//----------------
+
+PnccdCalibPars::PnccdCalibPars (bool isTestMode)
+  : m_calibDir     (std::string())
+  , m_typeGroupName(std::string())
+  , m_source       (std::string())
+  , m_runNumber    (0)
+  , m_print_bits   (255)
+{
+    // Test staff:
+    m_isTestMode = isTestMode;
+    if (m_isTestMode) {
+        m_calibdir      = "/reg/d/psdm/AMO/amotut13/calib/PNCCD::CalibV1/Camp.0:pnCCD.1";
+        m_calibfilename = "1-end.data";
+    }
+
+    fillCalibNameVector ();
+    loadCalibPars ();
+    //printCalibPars();
+}
+
+//----------------
+
+PnccdCalibPars::PnccdCalibPars ( const std::string&   calibDir,      //  "/reg/d/psdm/AMO/amoa1214/calib"
+                                 const std::string&   typeGroupName, //  "PNCCD::CalibV1"
+                                 const std::string&   source,        //  "Camp.0:pnCCD.0"
+                                 const unsigned long& runNumber,     //  10
+                                 unsigned             print_bits)
+  : m_calibDir(calibDir)
+  , m_typeGroupName(typeGroupName)
+  , m_source(source)
+  , m_runNumber(runNumber)
+  , m_print_bits(print_bits)
+{
+    m_isTestMode = false;
+
+    fillCalibNameVector ();
+    loadCalibPars ();
+
+    if( m_print_bits & 1 ) printInputPars ();
+    //printCalibPars();
+}
+
+//----------------
+
+PnccdCalibPars::PnccdCalibPars ( const std::string&   calibDir,      //  "/reg/d/psdm/AMO/amoa1214/calib"
+                                 const std::string&   typeGroupName, //  "PNCCD::CalibV1"
+                                 const Pds::Src&      src,           //  Pds::Src m_src; <- is defined in get(...,&m_src)
+                                 const unsigned long& runNumber,     //  10
+                                 unsigned             print_bits)
+  : m_calibDir(calibDir)
+  , m_typeGroupName(typeGroupName)
+  , m_source(std::string())
+  , m_src(src)
+  , m_runNumber(runNumber)
+  , m_print_bits(print_bits)
+{
+    m_isTestMode = false;
+
+    fillCalibNameVector ();
+    loadCalibPars ();
+
+    if( m_print_bits & 1 ) printInputPars ();
+    //printCalibPars();
+}
+
+//----------------
+
+void PnccdCalibPars::fillCalibNameVector ()
+{
+    v_calibname.clear();
+    v_calibname.push_back("pedestals");
+    v_calibname.push_back("pixel_status");
+    v_calibname.push_back("common_mode");
+    v_calibname.push_back("pixel_gain");
+}
+
+//----------------
+
+void PnccdCalibPars::loadCalibPars ()
+{
+    for( vector<std::string>::const_iterator iterCalibName  = v_calibname.begin();
+                                             iterCalibName != v_calibname.end(); iterCalibName++ )
+      {
+        m_cur_calibname = *iterCalibName;
+
+	getCalibFileName();
+
+        if (m_fname == std::string()) { 
+	  fillDefaultCalibParsV1 ();
+          if ( m_print_bits & 4 ) msgUseDefault ();
+          m_calibtype_status[m_cur_calibname] = 0; 
+        } 
+        else 
+        {
+	  fillCalibParsV1 ();
+          m_calibtype_status[m_cur_calibname] = 1; 
+	}
+      }
+}
+
+//----------------
+
+void PnccdCalibPars::getCalibFileName ()
+{
+  if ( m_isTestMode ) 
+    {
+      m_fname  = m_calibdir + "/"; 
+      m_fname += m_cur_calibname + "/"; 
+      m_fname += m_calibfilename; // "/0-end.data"; // !!! THIS IS A SIMPLIFIED CASE OF THE FILE NAME!!!
+    }
+  else if (m_calibDir == std::string())
+    {
+      m_fname = std::string();
+    }
+  else
+    {
+      unsigned print_bits_cff = ( m_print_bits & 2 ) ? 255 : 0;
+
+      PSCalib::CalibFileFinder *calibfinder = new PSCalib::CalibFileFinder(m_calibDir, m_typeGroupName, print_bits_cff);
+      //m_fname = calibfinder -> findCalibFile(m_src, m_cur_calibname, m_runNumber);
+
+      if (m_source == std::string())
+          m_fname = calibfinder -> findCalibFile(m_src, m_cur_calibname, m_runNumber);
+      else
+          m_fname = calibfinder -> findCalibFile(m_source, m_cur_calibname, m_runNumber);
+    }
+  MsgLog("PnccdCalibPars", debug, "getCalibFileName(): " << m_fname);
+}
+
+//----------------
+
+void PnccdCalibPars::fillCalibParsV1 ()
+{
+  if     ( m_cur_calibname == v_calibname[0] ) m_pedestals    = new pdscalibdata::PnccdPedestalsV1  (m_fname);
+  else if( m_cur_calibname == v_calibname[1] ) m_pixel_status = new pdscalibdata::PnccdPixelStatusV1(m_fname);
+  else if( m_cur_calibname == v_calibname[2] ) m_common_mode  = new pdscalibdata::PnccdCommonModeV1 (m_fname);
+  else if( m_cur_calibname == v_calibname[3] ) m_pixel_gain   = new pdscalibdata::PnccdPixelGainV1  (m_fname);
+}
+
+//----------------
+
+void PnccdCalibPars::fillDefaultCalibParsV1 ()
+{
+  // If default parameters are available - set them.
+  // For calib types where default parameters are not accaptable and the file is missing - error message and abort.
+  if     ( m_cur_calibname == v_calibname[0] ) m_pedestals    = new pdscalibdata::PnccdPedestalsV1  ();
+  else if( m_cur_calibname == v_calibname[1] ) m_pixel_status = new pdscalibdata::PnccdPixelStatusV1();
+  else if( m_cur_calibname == v_calibname[2] ) m_common_mode  = new pdscalibdata::PnccdCommonModeV1 ();
+  else if( m_cur_calibname == v_calibname[3] ) m_pixel_gain   = new pdscalibdata::PnccdPixelGainV1  ();
+
+  else if( m_print_bits & 8 ) fatalMissingFileName ();
+}
+
+//----------------
+
+void PnccdCalibPars::fatalMissingFileName ()
+{
+	MsgLog("PnccdCalibPars", warning, "In fillDefaultCalibParsV1(): the calibration file for the source=" << m_source 
+                  << ", type=" << m_cur_calibname 
+                  << ", run=" <<  m_runNumber
+                  << " is not found ..."
+	          << "\nWARNING: Default pnCCD intensity correction constants can not guarantee correct intensity transformation..."
+	          << "\nWARNING: Please provide all expected pnCCD intensity correction constants under the directory .../<experiment>/calib/...");
+	abort();
+}
+
+//----------------
+
+void PnccdCalibPars::msgUseDefault ()
+{
+	MsgLog("PnccdCalibPars", info, "In getCalibFileName(): the calibration file for the source=" << m_source 
+                  << ", type=" << m_cur_calibname 
+                  << ", run=" <<  m_runNumber
+                  << " is not found ..."
+	          << "\nWARNING: Default pnCCD alignment constants will be used.");
+}
+
+//----------------
+
+void PnccdCalibPars::printCalibPars()
+{
+    WithMsgLog("PnccdCalibPars", info, str) {
+      str << "printCalibPars()" ;
+      //str << "\n getColSize_um()    = " << getColSize_um() ;
+      //str << "\n getRowSize_um()    = " << getRowSize_um() ;
+    }        
+
+    m_pedestals    -> print();
+    m_pixel_status -> print();
+    m_common_mode  -> print();
+    m_pixel_gain   -> print();
+}
+
+//----------------
+
+void PnccdCalibPars::printInputPars()
+{
+    WithMsgLog("PnccdCalibPars", info, str) {
+      str << "printInputPars()" ;
+      str << "\n m_calibDir      = " << m_calibDir ;
+      str << "\n m_typeGroupName = " << m_typeGroupName ;
+      str << "\n m_source        = " << m_source ;
+      str << "\n m_runNumber     = " << m_runNumber ;
+      str << "\n m_print_bits    = " << m_print_bits ;
+    }        
+}
+
+//----------------
+
+void PnccdCalibPars::printCalibParsStatus ()
+{
+    WithMsgLog("PnccdCalibPars", info, str) {
+      str << "printCalibParsStatus()" ;
+
+      for( vector<std::string>::const_iterator iterCalibName  = v_calibname.begin();
+                                               iterCalibName != v_calibname.end(); iterCalibName++ )
+      {
+          m_cur_calibname = *iterCalibName;
+          str << "\n type: "  << std::left << std::setw(20) << m_cur_calibname
+              << " status = " << m_calibtype_status[m_cur_calibname]; 
+      }
+    }        
+}
+
+//--------------
+// Destructor --
+//--------------
+
+PnccdCalibPars::~PnccdCalibPars ()
+{
+  //delete [] m_data; 
+}
+
+//----------------
+//----------------
+//----------------
+//----------------
+
+} // namespace PSCalib
