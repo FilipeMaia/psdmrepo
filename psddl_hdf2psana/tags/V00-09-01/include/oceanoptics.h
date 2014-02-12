9f432740eafed0153444bd4628e115ef0f088627
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-02-12 23:31:10 +0000

    Tagging psddl_hdf2psana with V00-09-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7655 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ psddl_hdf2psana/tags/V00-09-01/include/oceanoptics.h
@@ -0,0 +1,80 @@
+#ifndef PSDDL_HDF2PSANA_OCEANOPTICS_H
+#define PSDDL_HDF2PSANA_OCEANOPTICS_H 1
+
+
+#include "psddl_psana/oceanoptics.ddl.h"
+#include "hdf5pp/Group.h"
+#include "hdf5pp/Type.h"
+#include "PSEvt/Proxy.h"
+#include "psddl_hdf2psana/ChunkPolicy.h"
+#include "psddl_hdf2psana/oceanoptics.ddl.h"
+
+namespace psddl_hdf2psana {
+namespace OceanOptics {
+
+namespace ns_DataV1_v0 {
+struct dataset_data {
+  static hdf5pp::Type native_type();
+  static hdf5pp::Type stored_type();
+
+  dataset_data();
+  dataset_data(const Psana::OceanOptics::DataV1& psanaobj);
+  ~dataset_data();
+
+  uint64_t frameCounter;
+  uint64_t numDelayedFrames;
+  uint64_t numDiscardFrames;
+  OceanOptics::ns_timespec64_v0::dataset_data timeFrameStart;
+  OceanOptics::ns_timespec64_v0::dataset_data timeFrameFirstData;
+  OceanOptics::ns_timespec64_v0::dataset_data timeFrameEnd;
+  int8_t numSpectraInData;
+  int8_t numSpectraInQueue;
+  int8_t numSpectraUnused;
+  double durationOfFrame;
+
+
+};
+}
+
+
+template <typename Config>
+class DataV1_v0 : public Psana::OceanOptics::DataV1 {
+public:
+  typedef Psana::OceanOptics::DataV1 PsanaType;
+  DataV1_v0() {}
+  DataV1_v0(hdf5pp::Group group, hsize_t idx, const boost::shared_ptr<Config>& cfg)
+    : m_group(group), m_idx(idx), m_cfg(cfg) {}
+  virtual ~DataV1_v0() {}
+  virtual ndarray<const uint16_t, 1> data() const;
+  virtual uint64_t frameCounter() const;
+  virtual uint64_t numDelayedFrames() const;
+  virtual uint64_t numDiscardFrames() const;
+  virtual const Psana::OceanOptics::timespec64& timeFrameStart() const;
+  virtual const Psana::OceanOptics::timespec64& timeFrameFirstData() const;
+  virtual const Psana::OceanOptics::timespec64& timeFrameEnd() const;
+  virtual int8_t numSpectraInData() const;
+  virtual int8_t numSpectraInQueue() const;
+  virtual int8_t numSpectraUnused() const;
+  virtual double durationOfFrame() const;
+    double nonlinerCorrected(uint32_t iPixel) const;
+
+private:
+  mutable hdf5pp::Group m_group;
+  hsize_t m_idx;
+  boost::shared_ptr<Config> m_cfg;
+  mutable ndarray<const uint16_t, 1> m_ds_spectra;
+  void read_ds_spectra() const;
+  mutable boost::shared_ptr<OceanOptics::ns_DataV1_v0::dataset_data> m_ds_data;
+  void read_ds_data() const;
+  mutable Psana::OceanOptics::timespec64 m_ds_storage_data_timeFrameStart;
+  mutable Psana::OceanOptics::timespec64 m_ds_storage_data_timeFrameFirstData;
+  mutable Psana::OceanOptics::timespec64 m_ds_storage_data_timeFrameEnd;
+};
+
+void make_datasets_DataV1_v0(const Psana::OceanOptics::DataV1& obj,
+      hdf5pp::Group group, const ChunkPolicy& chunkPolicy, int deflate, bool shuffle);
+void store_DataV1_v0(const Psana::OceanOptics::DataV1* obj, hdf5pp::Group group, long index, bool append);
+
+} // namespace OceanOptics
+} // namespace psddl_hdf2psana
+#endif // PSDDL_HDF2PSANA_OCEANOPTICS_DDL_H
