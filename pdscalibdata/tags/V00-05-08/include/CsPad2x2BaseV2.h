6cda1ea99e70b2b3f051348f2b574363ba8fe7da
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-03-06 23:57:20 +0000

    Tagging pdscalibdata with V00-05-08
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7794 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ pdscalibdata/tags/V00-05-08/include/CsPad2x2BaseV2.h
@@ -0,0 +1,77 @@
+#ifndef PDSCALIBDATA_CSPAD2X2BASEV2_H
+#define PDSCALIBDATA_CSPAD2X2BASEV2_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+//      $Id: CsPad2x2BaseV2.h 1 2014-03-01 18:00:00Z dubrovin@SLAC.STANFORD.EDU $
+//
+// Description:
+//	Class CsPad2x2BaseV2.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+// #include <string>
+
+//----------------------
+// Base Class Headers --
+//----------------------
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+// #include "ndarray/ndarray.h"
+// #include "pdsdata/psddl/cspad2x2.ddl.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace pdscalibdata {
+
+/**
+ *  class CsPad2x2BaseV2 contains common parameters and methods (if any) for CSPAD. 
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @see AdditionalClass
+ *
+ *  @version $Id: CsPad2x2BaseV2.cpp 2014-03-01 18:00:00Z dubrovin@SLAC.STANFORD.EDU $
+ *
+ *  @author Mikhail Dubrovin
+ */
+
+class CsPad2x2BaseV2 {
+public:
+
+  const static size_t   Ndim = 3; 
+  const static size_t   Segs = 2; 
+  const static size_t   Rows = 185; 
+  const static size_t   Cols = 388; 
+  const static size_t   Size = Rows*Cols*Segs; 
+  
+  // Default constructor
+  CsPad2x2BaseV2 () {};
+  
+  // Destructor
+  ~CsPad2x2BaseV2 () {};
+
+protected:
+
+private:
+
+  // Copy constructor and assignment are disabled by default
+  CsPad2x2BaseV2 ( const CsPad2x2BaseV2& ) ;
+  CsPad2x2BaseV2& operator = ( const CsPad2x2BaseV2& ) ;
+};
+
+} // namespace pdscalibdata
+
+#endif // PDSCALIBDATA_CSPAD2X2BASEV2_H
