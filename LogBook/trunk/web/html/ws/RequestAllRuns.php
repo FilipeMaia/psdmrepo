6b70df836b22095ee4bf6d137b45e44aa42ef122
gapon@SLAC.STANFORD.EDU <gapon@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-03-17 19:50:11 +0000

    - migrated to the new framework and extended the functionality of the Web service: RequestAllRuns.php
    M    doc/ChangeLog
    M    web/html/ws/RequestAllRuns.php
    M    web/nbproject/private/private.xml
    
    
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7884 b967ad99-d558-0410-b138-e0f6c56caec7

--- LogBook/trunk/web/html/ws/RequestAllRuns.php
+++ LogBook/trunk/web/html/ws/RequestAllRuns.php
@@ -1,164 +1,108 @@
 <?php
 
-require_once( 'logbook/logbook.inc.php' );
-require_once( 'lusitime/lusitime.inc.php' );
+/* This script will process a request for retreiving all runs of the experiments.
+ */
+require_once 'dataportal/dataportal.inc.php' ;
+require_once 'logbook/logbook.inc.php' ;
 
-use LogBook\LogBook;
-use LogBook\LogBookAuth;
 use LogBook\LogBookUtils;
-use LogBook\LogBookException;
 
-use LusiTime\LusiTime;
-use LusiTime\LusiTimeException;
 
-/* Package the error message into a JSON object and return the one
- * back to a caller. The script's execution will end at this point.
- */
-function report_error( $msg ) {
-    $status_encoded = json_encode( "error" );
-    $msg_encoded = json_encode( '<b><em style="color:red;" >Error:</em></b>&nbsp;'.$msg );
-    print <<< HERE
-{
-  "Status": {$status_encoded}, "status": {$status_encoded},
-  "Message": {$msg_encoded}, "message": {$msg_encoded}
-}
-HERE;
-    exit;
-}
+DataPortal\ServiceJSON::run_handler ('GET', function ($SVC) {
 
-/*
- * This script will process a request for retreiving all runs of the experiments.
- * Return a JSON object with the descriptions of runs.
- * Othersise return another JSON object with an explanation of a problem.
- */
-if( !isset( $_GET['exper_id'] )) report_error('no experiment identifier parameter found in the request');
-$exper_id = trim( $_GET['exper_id'] );
-if( $exper_id == '' ) report_error( 'experiment identifier found in the request is empty' );
+    $exper_id      = $SVC->required_int('exper_id') ;
+    $range_of_runs = $SVC->optional_str('range_of_runs', '') ;
 
-$range_of_runs = '';
-if( isset( $_GET['range_of_runs'] )) {
-    $range_of_runs = trim( $_GET['range_of_runs'] );
-}
+    $experiment = $SVC->logbook()->find_experiment_by_id($exper_id) ;
+    if (!$experiment) $SVC->abort("experiment for id={$exper_id}") ;
 
-try {
+    if (!$SVC->logbookauth()->canRead($experiment->id())) $SVC->abort('not authorized for the operation') ;
 
-    LogBook::instance()->begin();
+    $first_run_num = null ;
+    $last_run_num  = $first_run_num ;
 
-    $experiment = LogBook::instance()->find_experiment_by_id( $exper_id ) or report_error( 'no such experiment' );
+    if ('' != $range_of_runs) {
 
-    if( !LogBookAuth::instance()->canRead( $experiment->id())) report_error( 'You are not authorized to access any information about the experiment' );
-
-    $first_run_num = null;
-    $last_run_num  = $first_run_num;
-
-    if( '' != $range_of_runs ) {
-
-        /* Pasrse the run numbers first. If the parse succeeds and no last run
+        /* Parse the run numbers first. If the parse succeeds and no last run
          * is provided then assume the second run as the last one.
          */
-        if( strpos($range_of_runs, '-') === false ) {
-            $r1 = $range_of_runs;
-            $r2 = '';
+        if (strpos($range_of_runs, '-') === false) {
+            $r1 = $range_of_runs ;
+            $r2 = '' ;
         } else {
-            list($r1,$r2) = explode( '-', $range_of_runs, 2 );
+            list($r1,$r2) = explode('-', $range_of_runs, 2) ;
         }
-        $r1 = trim( $r1 );
-        $r2 = trim( $r2 );
-        if( '' == $r1 ) report_error( "syntax error in the range of runs" );
+        $r1 = trim($r1) ;
+        $r2 = trim($r2) ;
+        if ('' == $r1) $SVC->abort('syntax error in the range of runs') ;
 
-        $first_run_num = null;
-        if(( 1 != sscanf( $r1, "%d", $first_run_num )) or ( $first_run_num <= 0 ))
-            report_error( "syntax error in the first run number of the range" );
+        $first_run_num = null ;
+        if ((1 != sscanf($r1, "%d", $first_run_num)) or ($first_run_num <= 0))
+            $SVC->abort('syntax error in the first run number of the range') ;
 
-        $last_run_num = $first_run_num;
-        if( '' != $r2 )
-            if(( 1 != sscanf( $r2, "%d", $last_run_num )) or ( $last_run_num <= 0 ))
-                report_error( "syntax error in the last run number of the range" );
+        $last_run_num = $first_run_num ;
+        if ('' != $r2)
+            if ((1 != sscanf($r2, "%d", $last_run_num)) or ($last_run_num <= 0))
+                $SVC->abort('syntax error in the last run number of the range') ;
 
-        if( $last_run_num < $first_run_num ) report_error( "last run in the range can't be less than the first one" );
+        if ($last_run_num < $first_run_num)
+            $SVC->abort('last run in the range can\'t be less than the first one') ;
 
-        $first_run = $experiment->find_run_by_num( $first_run_num );
-        if( is_null( $first_run )) report_error( "run {$first_run_num} can't be found" );
-        $last_run = $experiment->find_run_by_num( $last_run_num );
-        if( is_null( $last_run )) report_error( "run {$last_run_num} can't be found" );
+        $first_run = $experiment->find_run_by_num($first_run_num) ;
+        if (is_null($first_run)) $SVC->abort("run {$first_run_num} can't be found") ;
+
+        $last_run = $experiment->find_run_by_num($last_run_num) ;
+        if (is_null($last_run)) $SVC->abort("run {$last_run_num} can't be found") ;
     }
 
-    $max_total_seconds = 1;
-    $runs = array();
-    foreach( $experiment->runs() as $r ) {
+    $max_total_seconds = 1 ;
+    $runs = array() ;
+    foreach ($experiment->runs() as $r) {
 
-        // Skip runs which are not allowed by the filter (if any provid3ed)
-        //
-        if( !is_null($first_run_num)) {
-            if(( $r->num() < $first_run_num ) || ( $r->num() > $last_run_num )) continue;
-        }
+        // -- skip runs which are not allowed by the filter (if any provided)
 
-        $total_seconds = is_null($r->end_time()) ? 0 : $r->end_time()->sec - $r->begin_time()->sec;
-        if( $total_seconds > $max_total_seconds ) $max_total_seconds = $total_seconds;
-        $durat = '';
-        if( $total_seconds ) {
-            $seconds_left = $total_seconds;
+        if (!is_null($first_run_num) &&
+            (($r->num() < $first_run_num) || ($r->num() > $last_run_num))) continue ;
 
-            $day          = floor( $seconds_left / ( 24 * 3600 ));
-            $seconds_left = $seconds_left % ( 24 * 3600 );
+        $total_seconds = is_null($r->end_time()) ? 0 : $r->end_time()->sec - $r->begin_time()->sec ;
+        if ($total_seconds > $max_total_seconds ) $max_total_seconds = $total_seconds ;
+        $durat  = '' ;
+        $durat1 = '' ;
+        if ($total_seconds) {
+            $seconds_left = $total_seconds ;
 
-            $hour         = floor( $seconds_left / 3600 );
-            $seconds_left = $seconds_left % 3600;
+            $day          = floor($seconds_left / (24 * 3600)) ;
+            $seconds_left = $seconds_left % (24 * 3600) ;
 
-            $min          = floor( $seconds_left / 60 );
-            $seconds_left = $seconds_left % 60;
+            $hour         = floor($seconds_left / 3600) ;
+            $seconds_left = $seconds_left % 3600 ;
+
+            $min          = floor($seconds_left / 60) ;
+            $seconds_left = $seconds_left % 60 ;
 
             $sec          = $seconds_left;
 
-            $durat = sprintf( "%02d:%02d.%02d", $hour, $min, $sec );
+            $durat = sprintf("%02d:%02d.%02d", $hour, $min, $sec) ;
+            $durat1 = LogBookUtils::format_seconds_1($total_seconds) ;
         }
-        array_push(
-            $runs,
-            array(
-                'id'     => $r->id(),
-                'num'    => $r->num(),
-                'day'    => $r->begin_time()->toStringDay(),
-                'ymd'    => $r->begin_time()->toStringDay(),
-                'hms'    => $r->begin_time()->toStringHMS(),
-                'ival'   => $r->begin_time()->toStringHMS().(is_null($r->end_time()) ? ' - <span style="color:red; font-weight:bold;">on-going</span>' : ' - '.$r->end_time()->toStringHMS()),
-                'durat'  => $durat,
-                'durat1' => LogBookUtils::format_seconds_1( $r->end_time()->sec - $r->begin_time()->sec ),
-                'sec'    => $total_seconds
-            )
-        );
+        array_push (
+            $runs ,
+            array (
+                'id'     => $r->id() ,
+                'num'    => $r->num() ,
+                'day'    => $r->begin_time()->toStringDay() ,
+                'ymd'    => $r->begin_time()->toStringDay() ,
+                'hms'    => $r->begin_time()->toStringHMS() ,
+                'ival'   => $r->begin_time()->toStringHMS().(is_null($r->end_time()) ? ' - <span style="color:red; font-weight:bold;">on-going</span>' : ' - '.$r->end_time()->toStringHMS()) ,
+                'durat'  => $durat ,
+                'durat1' => $durat1 ,
+                'sec'    => $total_seconds)) ;
     }
 
-    $status_encoded = json_encode( "success" );
-       $updated_encoded = json_encode( LusiTime::now()->toStringShort());
-    
-    header( 'Content-type: application/json' );
-    header( "Cache-Control: no-cache, must-revalidate" ); // HTTP/1.1
-    header( "Expires: Sat, 26 Jul 1997 05:00:00 GMT" );   // Date in the past
-
-       print <<< HERE
-{
-  "Status": {$status_encoded}, "status": {$status_encoded},
-  "Updated": {$updated_encoded}, "updated": {$updated_encoded},
-  "Runs": [
-
-HERE;
-
-
-    $first = true;
-    foreach( array_reverse($runs) as $r ) {
-        if( $first ) $first = false;
-        else echo ',';
-        echo json_encode( $r );
-    }
-    print <<< HERE
-  ],
-  "MaxSeconds" : {$max_total_seconds}
-}
-HERE;
-
-    LogBook::instance()->commit();
-
-} catch( LogBookException  $e ) { report_error( $e->toHtml()); }
-  catch( LusiTimeException $e ) { report_error( $e->toHtml()); }
+    $SVC->finish (array (
+        'Runs'       => array_reverse($runs) ,  // the newest run shoudl go first
+        'MaxSeconds' => $max_total_seconds
+    )) ;
+}) ;
 
 ?>
