7a45b979915dff814b8772f9d66fcaeecd13cb18
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-03-28 23:37:33 +0000

    Chenge default parameters for pnCCD
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@7945 b967ad99-d558-0410-b138-e0f6c56caec7

--- pdscalibdata/trunk/include/PnccdCommonModeV1.h
+++ pdscalibdata/trunk/include/PnccdCommonModeV1.h
@@ -62,11 +62,11 @@ public:
   {
     std::fill_n(m_pars, int(CMSize), pars_t(0));
     // default common_mode parameters
-    m_pars[0] = 1;   // Common mode algorithm number
+    m_pars[0] = 2;   // Common mode algorithm number
     m_pars[1] = 300; // Threshold; values below threshold are averaged
     m_pars[2] = 50;  // Maximal correction; correction is applied if it is less than this value
     m_pars[3] = 128; // Number of pixels for averaging
-    m_pars[4] = 0.2; // Fraction of pixels in cm peak ...
+    m_pars[4] = 0;   // Fraction of pixels in cm peak ...
   }
 
   /// Read parameters from file
