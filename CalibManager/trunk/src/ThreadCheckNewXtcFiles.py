9b9e13ceeae1d8811182d06e8b6e45255aec62b4
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-04-16 01:02:50 +0000

    Work in progress. Save all changes.
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8001 b967ad99-d558-0410-b138-e0f6c56caec7

--- CalibManager/trunk/src/ThreadCheckNewXtcFiles.py
+++ CalibManager/trunk/src/ThreadCheckNewXtcFiles.py
@@ -40,11 +40,12 @@ class ThreadCheckNewXtcFiles (QtCore.QThread) :
         self.print_bits = print_bits
         self.thread_id  = random.random()
 
+        self.exp_name   = cp.exp_name.value()
         self.counter = 0
         self.list_of_runs_old = []
 
         cp.thread_check_new_xtc_files = self
-        #self.connect( self, QtCore.SIGNAL('update(QString)'), self.testConnection )
+        self.connect( self, QtCore.SIGNAL('update(QString)'), self.testConnection )
 
 
     def testConnection(self, text) :
@@ -64,7 +65,13 @@ class ThreadCheckNewXtcFiles (QtCore.QThread) :
         list_of_runs = fnm.get_list_of_xtc_run_nums()
         if self.print_bits & 4 : print 'list_of_runs    : %s' % list_of_runs
         if self.print_bits & 8 : print 'list_of_runs_old: %s' % self.list_of_runs_old
-            
+
+        # If the experiment name has changed, it does not mean that new xtc is availble...
+        if cp.exp_name.value() != self.exp_name :
+            self.exp_name = cp.exp_name.value()
+            self.list_of_runs_old = list(list_of_runs)            
+            return False
+
         if list_of_runs != [] :
             if self.list_of_runs_old != []:
                 if list_of_runs[-1] != self.list_of_runs_old[-1] :
