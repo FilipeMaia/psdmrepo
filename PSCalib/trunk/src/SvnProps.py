0fb342da7e05894dc1fb32de9a9de10cf849e6b1
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-05-06 17:01:35 +0000

    SvnProps.py - get rid of nightly complains-2nd try
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8157 b967ad99-d558-0410-b138-e0f6c56caec7

--- PSCalib/trunk/src/SvnProps.py
+++ PSCalib/trunk/src/SvnProps.py
@@ -12,7 +12,7 @@ Also see: ~/.subversion/config
 This software was developed for the SIT project.
 If you use all or part of it, please give an appropriate acknowledgment.
 
-@version $Id$
+@version $Id: SvnProps.py 8156 2014-05-06 17:00:58Z dubrovin@SLAC.STANFORD.EDU $
 
 @author Mikhai S. Dubrovin
 """
@@ -20,8 +20,8 @@ If you use all or part of it, please give an appropriate acknowledgment.
 #------------------------------
 # Properties substituted by SVN 
 #------------------------------
-# __revision__ = "$Revision$"
-# __author__   = "$Author$"
+# __revision__ = "$Revision: 8156 $"
+# __author__   = "$Author: dubrovin@SLAC.STANFORD.EDU $"
 #------------------------------
 
 import sys
@@ -31,13 +31,13 @@ import sys
 class SvnProps :
     def __init__(self) : 
         self.updated  = "2014-05-05"
-        self.revision = "$Revision$"
-        self.author   = "$Author$"
-        self.id       = "$Id$"
-        self.headurl  = "$HeadURL:$"
+        self.revision = "$Revision: 8156 $"
+        self.author   = "$Author: dubrovin@SLAC.STANFORD.EDU $"
+        self.id       = "$Id: SvnProps.py 8156 2014-05-06 17:00:58Z dubrovin@SLAC.STANFORD.EDU $"
+        self.headurl  = "$HeadURL$"
         self.header   = "$Header:$"
-        self.datelc   = "$LastChangedDate$"
-        self.date     = "$Date$"
+        self.datelc   = "$LastChangedDate: 2014-05-06 10:00:58 -0700 (Tue, 06 May 2014) $"
+        self.date     = "$Date: 2014-05-06 10:00:58 -0700 (Tue, 06 May 2014) $"
 
 #------------------------------
 
