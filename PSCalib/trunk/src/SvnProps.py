540d3c60a49933c2e4c786ac6b48a75ac56602bf
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-05-06 17:00:58 +0000

    SvnProps.py - get rid of nightly complains
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8156 b967ad99-d558-0410-b138-e0f6c56caec7

--- PSCalib/trunk/src/SvnProps.py
+++ PSCalib/trunk/src/SvnProps.py
@@ -3,14 +3,16 @@
 """
 Class SvnProps stores updated by svn properties.
 
-NOTE: In order to always update revision number in this file when revision changes, use command:
-svn propset svn:keywords "Revision" SvnProps.py
+NOTE: To update revision number in this file when revision changes, use command:
+psvn mktxtprop src/SvnProps.py
+or
+svn propset svn:keywords "Revision" src/SvnProps.py
 Also see: ~/.subversion/config
 
 This software was developed for the SIT project.
 If you use all or part of it, please give an appropriate acknowledgment.
 
-@version $Id: SvnProps.py 8038 2014-04-19 00:58:50Z dubrovin@SLAC.STANFORD.EDU $
+@version $Id$
 
 @author Mikhai S. Dubrovin
 """
@@ -19,7 +21,7 @@ If you use all or part of it, please give an appropriate acknowledgment.
 # Properties substituted by SVN 
 #------------------------------
 # __revision__ = "$Revision$"
-# __author__   = "$Author: dubrovin@SLAC.STANFORD.EDU $"
+# __author__   = "$Author$"
 #------------------------------
 
 import sys
@@ -27,31 +29,32 @@ import sys
 #------------------------------
 
 class SvnProps :
-
-    updated  = "2014-04-29"
-    revision = "$Revision$"
-    author   = "$Author: dubrovin@SLAC.STANFORD.EDU $"
-    id       = "$Id: SvnProps.py 8038 2014-04-19 00:58:50Z dubrovin@SLAC.STANFORD.EDU $"
-    headurl  = "$HeadURL: https://pswww.slac.stanford.edu/svn/psdmrepo/PSCalib/trunk/src/SvnProps.py $"
-    header   = "$Header:$"
-    datelc   = "$LastChangedDate: 2014-04-18 17:58:50 -0700 (Fri, 18 Apr 2014) $"
-    date     = "$Date: 2014-04-18 17:58:50 -0700 (Fri, 18 Apr 2014) $"
+    def __init__(self) : 
+        self.updated  = "2014-05-05"
+        self.revision = "$Revision$"
+        self.author   = "$Author$"
+        self.id       = "$Id$"
+        self.headurl  = "$HeadURL:$"
+        self.header   = "$Header:$"
+        self.datelc   = "$LastChangedDate$"
+        self.date     = "$Date$"
 
 #------------------------------
 
-svnprops = SvnProps()  # use it as singleton
+svnprops = SvnProps()  # use it as a singleton
 
 #------------------------------
 
 if __name__ == "__main__" :
     
+    print 'svnprops.updated  : %s' % svnprops.updated
     print 'svnprops.revision : %s' % svnprops.revision
-    print 'svnprops.author   : %s' % svnprops.author  
-    print 'svnprops.id       : %s' % svnprops.id      
-    print 'svnprops.headurl  : %s' % svnprops.headurl 
-    print 'svnprops.header   : %s' % svnprops.header  
-    print 'svnprops.datelc   : %s' % svnprops.datelc  
-    print 'svnprops.date     : %s' % svnprops.date    
+    print 'svnprops.author   : %s' % svnprops.author
+    print 'svnprops.id       : %s' % svnprops.id
+    print 'svnprops.headurl  : %s' % svnprops.headurl
+    print 'svnprops.header   : %s' % svnprops.header
+    print 'svnprops.datelc   : %s' % svnprops.datelc
+    print 'svnprops.date     : %s' % svnprops.date
 
     sys.exit ( 'End of test' )
 
