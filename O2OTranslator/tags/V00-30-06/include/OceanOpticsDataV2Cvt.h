23b0ff2a6a50425a8539b8c9a65b1854e56b1e92
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-05-16 16:30:03 +0000

    Tagging O2OTranslator with V00-30-06
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8234 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ O2OTranslator/tags/V00-30-06/include/OceanOpticsDataV2Cvt.h
@@ -0,0 +1,106 @@
+#ifndef O2OTRANSLATOR_OCEANOPTICSDATAV2CVT_H
+#define O2OTRANSLATOR_OCEANOPTICSDATAV2CVT_H
+
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class OceanOpticsDataV2Cvt.
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+//----------------------
+// Base Class Headers --
+//----------------------
+#include "O2OTranslator/EvtDataTypeCvt.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "H5DataTypes/OceanOpticsDataV2.h"
+#include "O2OTranslator/CvtOptions.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+//		---------------------
+// 		-- Class Interface --
+//		---------------------
+
+namespace O2OTranslator {
+
+class ConfigObjectStore;
+
+/// @addtogroup O2OTranslator
+
+/**
+ *  @ingroup O2OTranslator
+ *
+ *  @brief Special converter class for Pds::OceanOptics::DataV2 XTC class
+ *
+ *  This software was developed for the LCLS project.  If you use all or 
+ *  part of it, please give an appropriate acknowledgment.
+ *
+ *  @version $Id$
+ *
+ *  @author Andy Salnikov
+ */
+
+class OceanOpticsDataV2Cvt : public EvtDataTypeCvt<Pds::OceanOptics::DataV2> {
+public:
+
+  typedef Pds::OceanOptics::DataV2 XtcType ;
+  typedef H5DataTypes::OceanOpticsDataV2 H5Type ;
+
+  // Default constructor
+  OceanOpticsDataV2Cvt(const hdf5pp::Group& group,
+                       const std::string& typeGroupName,
+                       const Pds::Src& src,
+                       const ConfigObjectStore& configStore,
+                       const CvtOptions& cvtOptions,
+                       int schemaVersion);
+
+  // Destructor
+  virtual ~OceanOpticsDataV2Cvt () ;
+
+protected:
+
+  /// method called to create all necessary data containers
+  virtual void makeContainers(hdf5pp::Group group, const Pds::TypeId& typeId, const O2OXtcSrc& src);
+
+  // typed conversion method
+  virtual void fillContainers(hdf5pp::Group group,
+                              const XtcType& data,
+                              size_t size,
+                              const Pds::TypeId& typeId,
+                              const O2OXtcSrc& src);
+
+  // fill containers for missing data
+  virtual void fillMissing(hdf5pp::Group group,
+                           const Pds::TypeId& typeId,
+                           const O2OXtcSrc& src);
+
+private:
+
+  typedef H5DataTypes::ObjectContainer<H5Type> ObjectCont ;
+  typedef H5DataTypes::ObjectContainer<uint16_t> DataCont ;
+  typedef H5DataTypes::ObjectContainer<float> CorrectedDataCont ;
+
+  // Data members
+  const ConfigObjectStore& m_configStore;
+  boost::shared_ptr<ObjectCont> m_objCont ;
+  boost::shared_ptr<DataCont> m_dataCont ;
+  boost::shared_ptr<CorrectedDataCont> m_corrDataCont ;
+  size_t n_miss;
+
+};
+
+} // namespace O2OTranslator
+
+#endif // O2OTRANSLATOR_OCEANOPTICSDATAV2CVT_H
---
+++ O2OTranslator/tags/V00-30-06/src/O2OXtcIterator.cpp
@@ -0,0 +1,162 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class O2OXtcIterator...
+//
+// Author List:
+//      Andrei Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "O2OTranslator/O2OXtcIterator.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <boost/shared_ptr.hpp>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+#include "O2OTranslator/O2OExceptions.h"
+#include "O2OTranslator/O2OXtcScannerI.h"
+#include "pdsdata/compress/CompressedXtc.hh"
+#include "pdsdata/xtc/DetInfo.hh"
+#include "pdsdata/xtc/Xtc.hh"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  const char logger[] = "O2OTranslator.O2OXtcIterator";
+
+}
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace O2OTranslator {
+
+//----------------
+// Constructors --
+//----------------
+O2OXtcIterator::O2OXtcIterator ( Xtc* xtc, O2OXtcScannerI* scanner, bool config )
+  : Pds::XtcIterator( xtc )
+  , m_scanner( scanner )
+  , m_src()
+  , m_config(config)
+  , m_typeIds()
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+O2OXtcIterator::~O2OXtcIterator ()
+{
+}
+
+// process one sub-XTC, returns >0 for success, 0 for error
+int
+O2OXtcIterator::process(Xtc* xtc)
+{
+  // DAQ sometimes send the same data in both compressed and uncompressed format,
+  // we need to record only one of those
+  if (xtc->contains.id() != Pds::TypeId::Id_Epics and xtc->contains.id() != Pds::TypeId::Id_Xtc and 
+      xtc->contains.id() != Pds::TypeId::Any) {
+    std::vector<Pds::TypeId>& typeIds = m_typeIds[xtc->src];
+    for (std::vector<Pds::TypeId>::const_iterator it = typeIds.begin(); it != typeIds.end(); ++it) {
+      const Pds::TypeId& stored = *it;
+      if (xtc->contains.id() == stored.id() and xtc->contains.compressed_version() == stored.compressed_version()) {
+        // was seen already
+        if (xtc->contains.compressed() != stored.compressed()) {
+          // one is compressed, another is uncompressed - OK, skip this one
+          return 1;
+        }
+      }
+    }
+    typeIds.push_back(xtc->contains);
+  }
+
+  return this->process_int(xtc);
+}
+
+// process one sub-XTC, returns >0 for success, 0 for error
+int
+O2OXtcIterator::process_int(Xtc* xtc)
+{
+  if (xtc->contains.compressed()) {
+    MsgLog(logger, debug, "Found compressed XTC, decompressing");
+    boost::shared_ptr<Pds::Xtc> uxtc = Pds::CompressedXtc::uncompress(*xtc);
+    return this->process_int(uxtc.get());
+  }
+
+  Pds::TypeId::Type type = xtc->contains.id() ;
+  uint32_t version = xtc->contains.version() ;
+  Pds::Level::Type level = xtc->src.level() ;
+
+  // sanity check
+  if ( xtc->sizeofPayload() < 0 ) {
+    MsgLog(logger, error, "Negative payload size in XTC: " << xtc->sizeofPayload()
+        << " level: " << int(level) << '#' << Pds::Level::name(level)
+        << " type: " << int(type) << '#' << Pds::TypeId::name(type) << "/V" << version) ;
+    throw O2OXTCGenException(ERR_LOC, "negative payload size") ;
+  }
+
+  m_src.push( xtc->src ) ;
+
+  // say we are at the new XTC level
+  if ( m_scanner ) m_scanner->levelStart ( xtc->src ) ;
+
+  MsgLog(logger, debug, "O2OXtcIterator::process -- new xtc: "
+              << int(type) << '#' << Pds::TypeId::name(type) << "/V" << version
+              << " payload = " << xtc->sizeofPayload()
+              << " damage: " << std::hex << std::showbase << xtc->damage.value()) ;
+
+  int result = 1 ;
+  if ( type == Pds::TypeId::Id_Xtc ) {
+
+    // scan all sub-xtcs, this takes care of the bad damage bit (IncompleteContribution)
+    this->iterate( xtc );
+
+  } else if ( type == Pds::TypeId::Any ) {
+
+    // NOTE: I do not know yet what this type is supposed to do, for now just ignore it
+
+  } else if ( xtc->src.level() == Pds::Level::Source
+      or xtc->src.level() == Pds::Level::Reporter
+      or xtc->src.level() == Pds::Level::Event
+      or xtc->src.level() == Pds::Level::Control ) {
+
+    if (m_config) {
+      m_scanner->configObject( xtc->payload(), xtc->sizeofPayload(), xtc->contains, m_src, xtc->damage ) ;
+    } else {
+      m_scanner->dataObject( xtc->payload(), xtc->sizeofPayload(), xtc->contains, m_src, xtc->damage ) ;
+    }
+
+  } else {
+
+    MsgLog(logger, warning, "O2OXtcIterator::process -- data object at level "
+                << int(level) << '#' << Pds::Level::name(level) << ": "
+                << int(type) << '#' << Pds::TypeId::name(type) << "/V" << version);
+
+  }
+
+  // say we are done with this XTC level
+  if ( m_scanner ) m_scanner->levelEnd ( xtc->src ) ;
+
+  m_src.pop() ;
+
+  return result ;
+}
+
+} // namespace O2OTranslator
