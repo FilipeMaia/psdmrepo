29cfdc5a02c829971e010887a6e0c66adbe0f43a
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-05-23 22:29:20 +0000

    Tagging PSCalib with V00-02-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8283 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSCalib/tags/V00-02-01/test/ex_princeton_calib_pars.cpp
@@ -0,0 +1,66 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Test class PrincetonCalibPars of the PSCalib packadge
+//
+// Author List:
+//      Mikhail Dubrovin
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+#include "PSCalib/CalibPars.h"
+#include "PSCalib/PrincetonCalibPars.h"
+#include "pdscalibdata/PrincetonBaseV1.h"
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  // Assuming: /reg/d/psdm/xcs/xcstut13/calib/Princeton::CalibV1/XcsBeamline.0:Princeton.0/pedestals/1-end.data
+
+  const std::string calibDir   = "/reg/d/psdm/xcs/xcstut13/calib";
+  const std::string groupName  = "Princeton::CalibV1";
+  const std::string source     = "XcsBeamline.0:Princeton.0";
+  unsigned long     runNumber  = 20;
+  unsigned          print_bits = 255; //0
+
+  cout << "Test of PSCalib::PnccdCalibPars\n";     
+
+  PSCalib::PrincetonCalibPars *calibpars = new PSCalib::PrincetonCalibPars(calibDir, groupName, source, runNumber, print_bits);  
+  //PSCalib::PrincetonCalibPars *calibpars = new PSCalib::PrincetonCalibPars(true);  // test mode
+
+  //calibpars->printCalibPars();
+  //calibpars->printCalibParsStatus();
+  calibpars->printInputPars();
+
+  const PSCalib::CalibPars::pedestals_t* peds = calibpars->pedestals();
+  for (unsigned i=0; i<10; i++) cout << peds[i] << "  "; cout << " ... - pedestals\n";
+
+  const PSCalib::CalibPars::pixel_status_t* status = calibpars->pixel_status();
+  for (unsigned i=0; i<10; i++) cout << status[i] << "  "; cout << " ... - pixel_status\n";
+
+  const PSCalib::CalibPars::pixel_gain_t* gain = calibpars->pixel_gain();
+  for (unsigned i=0; i<10; i++) cout << gain[i] << "  "; cout << " ... - pixel_gain\n";
+
+  const PSCalib::CalibPars::pixel_rms_t* rms = calibpars->pixel_rms();
+  for (unsigned i=0; i<10; i++) cout << rms[i] << "  "; cout << " ... - pixel_rms\n";
+
+  const PSCalib::CalibPars::common_mode_t* cmod = calibpars->common_mode();
+  for (unsigned i=0; i<pdscalibdata::PrincetonBaseV1::SizeCM; i++) cout << cmod[i] << "  "; cout << " - common_mode\n";
+
+  calibpars -> printCalibParsStatus();
+  calibpars -> printCalibPars();
+
+  return 0;
+}
+
+//-----------------
