57f0badab4ce17ac6a9beaae28db7cc19dd75bfc
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-06-17 17:03:09 +0000

    Translator: update documentation about storing epics
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8438 b967ad99-d558-0410-b138-e0f6c56caec7

--- Translator/trunk/doc/Translator-dox.h
+++ Translator/trunk/doc/Translator-dox.h
@@ -10,7 +10,7 @@
 
 The Translator Package implements the psana module H5Output. This translates Psana events into an hdf5 files. We shall call this system psana-translate.  psana-translate replaces o2o-translate.  o2o-translate is part of the package O2OTranslator which has been responsible for translating LCLS xtc files to hdf5 to date. The main reason for developing the new translator is to take advantage of DDL code generation for translating the many xtc data types into hdf5 datasets (implemented in the psddldata and psddl_hdf2psana packages). Developing the new translator as a psana module has also made it easy to support features such as event selection and translation of user data. In addition, as a psana module, this translator now shares all of psana's code for parsing xtc files.
 
-Documentation can be found in the default_psana.cfg file in the Translator/data directory as well as at 
+Documentation can be found in the default_psana.cfg file in the data sub-directory of this package, or the data/Translator subdirectory of working SConsTools based release directory, as well as at the confluence page:
 
 <a href="https://confluence.slac.stanford.edu/display/PSDM/The+XTC-to-HDF5+Translator">The XTC to HDF5 Translator</a>
 
