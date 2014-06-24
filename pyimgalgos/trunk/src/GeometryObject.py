bb2387df8c501313cba8e8d45afd77e5cafd79ae
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-06-24 21:34:43 +0000

    add GeometryAccess to work with list of geos
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8466 b967ad99-d558-0410-b138-e0f6c56caec7

--- pyimgalgos/trunk/src/GeometryObject.py
+++ pyimgalgos/trunk/src/GeometryObject.py
@@ -19,11 +19,17 @@ import os
 import sys
 import math
 import numpy as np
-#from time import time
-#import GlobalGraphics as gu # for test purpose
 
-#from PyCSPadImage.PixCoords2x1 import PixCoords2x1
 from PyCSPadImage.PixCoords2x1 import cspad2x1_one
+
+#------------------------------
+
+def rotation(X, Y, C, S) :
+    """For numpy arrays X and Y returns the numpy arrays of Xrot and Yrot
+    """
+    Xrot = X*C - Y*S 
+    Yrot = Y*C + X*S 
+    return Xrot, Yrot
 
 #------------------------------
 
@@ -71,6 +77,7 @@ class GeometryObject :
               '  x0:%8.0f  y0:%8.0f  z0:%8.0f' % (self.x0, self.y0, self.z0) + \
               '  rot_z:%6.1f  rot_y:%6.1f  rot_x:%6.1f' % (self.rot_z, self.rot_y, self.rot_x) + \
               '  tilt_z:%8.5f  tilt_y:%8.5f  tilt_x:%8.5f' % (self.tilt_z, self.tilt_y, self.tilt_x)
+
 #------------------------------
 
     def print_geo_children(self) :
@@ -102,9 +109,7 @@ class GeometryObject :
 
 #------------------------------
 
-    def transform_geo_coord_arrays(self, X, Y, Z) :
-
-        do_tilt = True
+    def transform_geo_coord_arrays(self, X, Y, Z, do_tilt = True) :
 
         angle_z = self.rot_z + self.tilt_z if do_tilt else self.rot_z
         angle_y = self.rot_y + self.tilt_y if do_tilt else self.rot_y
@@ -195,228 +200,13 @@ class GeometryObject :
 #------------------------------
 #------------------------------
 #------------------------------
-#------------------------------
-#------------------------------
-
-def rotation(X, Y, C, S) :
-    """For numpy arrays X and Y returns the numpy arrays of Xrot and Yrot
-    """
-    Xrot = X*C - Y*S 
-    Yrot = Y*C + X*S 
-    return Xrot, Yrot
-
-#------------------------------
-
-def load_pars_from_file(path) :
-
-    list_of_geos = []
-
-    f=open(path,'r')
-    for linef in f :
-        line = linef.strip('\n')
-        if not line :       continue # discard empty strings
-        if line[0] == '#' : continue # discard comments
-        #print line
-        #geo=parse_line(line)
-        list_of_geos.append(parse_line(line))
-
-    f.close()
-
-    set_relations(list_of_geos)
-
-    return list_of_geos
-
-#------------------------------
-
-def parse_line(line) :
-    keys = ['pname','pindex','oname','oindex','x0','y0','z0','rot_z','rot_y','rot_x','tilt_z','tilt_y','tilt_x']
-    f = line.split()
-    if len(f) != len(keys) :
-        print 'The list length for fields from file: %d is not equal to expected: %d' % (len(f), len(keys))
-        return
-
-    vals = [str  (f[0]),
-            int  (f[1]),
-            str  (f[2]),
-            int  (f[3]),
-            float(f[4]),
-            float(f[5]),
-            float(f[6]),
-            float(f[7]),
-            float(f[8]),
-            float(f[9]),
-            float(f[10]),
-            float(f[11]),
-            float(f[12])
-           ]
-
-    #print 'keys: ', keys
-    #print 'vals: ', vals
-
-    d = dict(zip(keys, vals))
-    #print 'd=', d
-    #return d
-    return GeometryObject(**d)
-
-#------------------------------
-
-def find_parent(geobj, list_of_geos) :
-
-    for geo in list_of_geos :
-        if geo == geobj : continue
-        if  geo.oname  == geobj.pname \
-        and geo.oindex == geobj.pindex :
-            return geo
-
-    # The name of parent object is not found among geo names in the list_of_geos
-    # add top parent object to the list
-    if geobj.pname is not None :
-        top_parent = GeometryObject(pname=None, pindex=-1, oname=geobj.pname, oindex=geobj.pindex)
-        list_of_geos.append(top_parent)
-        return top_parent
-               
-    return None # for top parent itself
-
-#------------------------------
-
-def get_geo(oname, oindex, list_of_geos) :
-    for geo in list_of_geos :
-        if  geo.oname  == oname \
-        and geo.oindex == oindex :
-            return geo
-    return None
-
-#------------------------------
-
-def get_top_geo(list_of_geos) :
-    return list_of_geos[-1]
-
-#------------------------------
-
-def set_relations(list_of_geos) :
-    for geo in list_of_geos :
-        #geo.print_geo()
-        parent = find_parent(geo, list_of_geos)        
-        if parent is not None :
-            geo.set_parent(parent)
-            parent.add_child(geo)
-            print 'geo:%s:%d has parent:%s:%d' % (geo.oname, geo.oindex, parent.oname, parent.oindex)
-
-
-
-
-#------------------------------
-#------------------------------
-#----------- TESTS ------------
-#------------------------------
-#------------------------------
-
-from time import time # for test purpose only
-import pyimgalgos.GlobalGraphics as gg # for test purpose
-import pyimgalgos.TestImageGenerator as tig # for test purpose only
-
-#------------------------------
-
-def test_load(fname) :
-    if not os.path.exists(fname) :
-        sys.exit ('file %s does not exist' % fname)
-    list_of_geos = load_pars_from_file(fname)
-    return list_of_geos
-
-#------------------------------
-
-def test_access(list_of_geos) :
-
-    for geo in list_of_geos : geo.print_geo()
-    for geo in list_of_geos : geo.print_geo_children()
-
-    print '\nTOP GEO:'
-    top_geo = get_top_geo(list_of_geos)
-    top_geo.print_geo_children()
-
-    print '\nINTERMEDIATE GEO (QUAD):'
-    geo = get_geo('QUAD:V1', 0, list_of_geos) 
-    geo.print_geo_children()
-
-    t0_sec = time()
-    X,Y,Z = geo.get_pixel_coords()
-    #X,Y = geo.get_2d_pixel_coords()
-    print 'X:\n', X
-    print 'Consumed time to get 3d pixel coordinates = %7.3f sec' % (time()-t0_sec)
-    print 'Geometry object: %s:%d X.shape:%s' % (geo.oname, geo.oindex, str(X.shape))
-
-#------------------------------
-
-def test_plot_quad(list_of_geos) :
-
-    # get pixel coordinate arrays:
-    geo = get_geo('QUAD:V1', 1, list_of_geos) 
-    X,Y,Z = geo.get_pixel_coords()
-    print 'X,Y,Z shape:', X.shape 
-
-    # get index arrays
-    pix_size = cspad2x1_one.get_pix_size_um()
-    xmin, ymin = X.min()-pix_size/2, Y.min()-pix_size/2 
-    iX, iY = np.array((X-xmin)/pix_size, dtype=np.uint), np.array((Y-ymin)/pix_size, dtype=np.uint)
-
-    # get intensity array
-    arr = tig.cspad_nparr(n2x1=X.shape[0])
-    arr.shape = (8,185,388)
-    amp_range = (0,185+388)
- 
-    print 'iX, iY, W shape:', iX.shape, iY.shape, arr.shape 
-    img = gg.getImageFromIndexArrays(iX,iY,W=arr)
-
-    gg.plotImageLarge(img,amp_range=amp_range)
-    gg.move(500,10)
-    gg.show()
-
-#------------------------------
-
-def test_plot_cspad(list_of_geos, fname_data) :
-
-    # get pixel coordinate arrays:
-    geo = get_top_geo(list_of_geos) 
-    X,Y,Z = geo.get_pixel_coords()
-    print 'X,Y,Z shape:', X.shape 
-
-    # get index arrays
-    pix_size = cspad2x1_one.get_pix_size_um()
-    #xmin, ymin = X.min()-pix_size/2, Y.min()-pix_size/2 
-    #iX, iY = np.array((X-xmin)/pix_size, dtype=np.uint), np.array((Y-ymin)/pix_size, dtype=np.uint)
-    offset_um = 850*pix_size
-    iX, iY = np.array((X+offset_um)/pix_size, dtype=np.uint), np.array((Y+offset_um)/pix_size, dtype=np.uint)
-
-    arr, amp_range = np.loadtxt(fname_data, dtype=np.float), (0,0.5)
-    arr.shape= (4,8,185,388)
-
-    print 'iX, iY, W shape:', iX.shape, iY.shape, arr.shape 
-    img = gg.getImageFromIndexArrays(iX,iY,W=arr)
-
-    gg.plotImageLarge(img,amp_range=amp_range)
-    gg.move(500,10)
-    gg.show()
-
-#------------------------------
 
 if __name__ == "__main__" :
-
-    #fname = '/reg/d/psdm/cxi/cxii0114/calib/CsPad::CalibV1/CxiDs1.0:Cspad.0/geometry/0-end.data'
-    basedir = '/reg/neh/home1/dubrovin/LCLS/CSPadAlignment-v01/calib-cxi-ds1-2013-12-20/'
-    fname_geometry = basedir + 'calib/CsPad::CalibV1/CxiDs1.0:Cspad.0/geometry/1-end.data'
-    fname_data     = basedir + 'cspad-ndarr-ave-cxi83714-r0136.dat'
-    list_of_geos = test_load(fname_geometry)
-
-    msg = 'Use command: sys.argv[0] <num>, wher num=[1,3]'
-
-    if len(sys.argv)==1   : print 'App needs in input parameter.' + msg
-    elif sys.argv[1]=='1' : test_access(list_of_geos)
-    elif sys.argv[1]=='2' : test_plot_quad(list_of_geos)
-    elif sys.argv[1]=='3' : test_plot_cspad(list_of_geos, fname_data)
-    else : print 'Wrong input parameter.' + msg
-
+    print 78*'='+'\n==  Tests for this module are available in pyimgalgos/src/GeometryAccess.py ==\n'+78*'='
     sys.exit ('End of %s' % sys.argv[0])
 
+#------------------------------
+#------------------------------
 #------------------------------
 
 
