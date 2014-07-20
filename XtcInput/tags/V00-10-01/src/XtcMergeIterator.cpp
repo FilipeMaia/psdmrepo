85c7d3218c10db9a2374397982dac75df86bbec3
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-07-20 19:20:30 +0000

    Tagging XtcInput with V00-10-01
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8698 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ XtcInput/tags/V00-10-01/src/XtcMergeIterator.cpp
@@ -0,0 +1,98 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Class XtcMergeIterator...
+//
+// Author List:
+//      Andy Salnikov
+//
+//------------------------------------------------------------------------
+
+//-----------------------
+// This Class's Header --
+//-----------------------
+#include "XtcInput/XtcMergeIterator.h"
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+#include <boost/make_shared.hpp>
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+#include "MsgLogger/MsgLogger.h"
+
+//-----------------------------------------------------------------------
+// Local Macros, Typedefs, Structures, Unions and Forward Declarations --
+//-----------------------------------------------------------------------
+
+namespace {
+
+  const char* logger = "XtcInput.XtcMergeIterator";
+
+}
+
+//		----------------------------------------
+// 		-- Public Function Member Definitions --
+//		----------------------------------------
+
+namespace XtcInput {
+
+//----------------
+// Constructors --
+//----------------
+XtcMergeIterator::XtcMergeIterator (const boost::shared_ptr<RunFileIterI>& runIter, 
+                                    double l1OffsetSec, int firstControlStream, 
+                                    unsigned maxStreamClockDiffSec)
+  : m_runIter(runIter)
+  , m_l1OffsetSec(l1OffsetSec)
+  , m_firstControlStream(firstControlStream)
+  , m_maxStreamClockDiffSec(maxStreamClockDiffSec)
+{
+}
+
+//--------------
+// Destructor --
+//--------------
+XtcMergeIterator::~XtcMergeIterator ()
+{
+}
+
+// Return next datagram.
+Dgram 
+XtcMergeIterator::next()
+{
+  Dgram dgram;
+  while (not dgram.dg()) {
+
+    if (not m_dgiter) {
+
+      // get next file name
+      boost::shared_ptr<StreamFileIterI> fileNameIter = m_runIter->next();
+
+      // if no more files then stop
+      if (not fileNameIter) break ;
+
+      // open next xtc file if there is none open
+      MsgLog(logger, trace, "processing run #" << m_runIter->run()) ;
+      m_dgiter = boost::make_shared<XtcStreamMerger>(fileNameIter, m_l1OffsetSec, 
+                                                     m_firstControlStream,
+                                                     m_maxStreamClockDiffSec);
+    }
+
+    // try to read next datagram from it
+    dgram = m_dgiter->next() ;
+
+    // if failed to read go to next file
+    if (not dgram.dg()) m_dgiter.reset();
+
+  }
+
+  return dgram ;
+
+}
+
+} // namespace XtcInput
