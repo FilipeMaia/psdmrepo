6f9a2e23721441d5a58fd9102161e279ccd27843
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-08-28 00:51:32 +0000

    Tagging PSCalib with V00-02-12
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8816 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ PSCalib/tags/V00-02-12/test/ex_seg_geometry_store.cpp
@@ -0,0 +1,68 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id$
+//
+// Description:
+//	Test class CSPadCalibPars of the PSCalib packadge
+//
+// Author List:
+//      Mikhail Dubrovin
+//
+//------------------------------------------------------------------------
+
+//-----------------
+// C/C++ Headers --
+//-----------------
+
+#include "PSCalib/SegGeometryStore.h"
+
+#include <string>
+#include <iostream>
+#include <stdlib.h> // for atoi
+
+//using std::cout;
+//using std::endl;
+//using std::atoi;
+using namespace std;
+
+typedef PSCalib::SegGeometry SG;
+
+//-----------------
+
+int test01 () // Test 01
+{
+  cout << "Test of PSCalib::SegGeometryStore::Create(...)\n";     
+
+  SG *seggeom = PSCalib::SegGeometryStore::Create("SENS2X1:V1", 0377);  
+  seggeom -> print_seg_info(0377);
+
+  return 0;
+}
+
+//-----------------
+
+int test02 () // Test for CXI
+{
+  cout << "Test of PSCalib::CSPadCalibPars\n";     
+  return 0;
+}
+
+//-----------------
+
+int main (int argc, char* argv[])
+{  
+  cout << "Number of input arguments = " << argc << endl; 
+
+  if      (argc == 1)                     {test01();}
+  else if (argc == 2 && atoi(argv[1])==1) {test01();}
+  else if (argc == 2 && atoi(argv[1])==2) {test02();}
+  else    {
+        cout << "WARNING!!! Unexpected input arguments, argc=" << argc << endl; 
+        for(int i = 0; i < argc; i++)
+            cout << "argv[" << i << "] = " << argv[i] << endl;
+        cout << "Use command: " << argv[0] << " N, where N stands for test number 1,2,3,...\n"; 
+  }
+  return 0;
+}
+
+//-----------------
