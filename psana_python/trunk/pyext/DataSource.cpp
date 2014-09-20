f3d395504052f26dd22b80b04fad62168adbd2de
cpo@SLAC.STANFORD.EDU <cpo@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-09-20 00:47:32 +0000

    Allow indexing users to specify end-of-job
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8943 b967ad99-d558-0410-b138-e0f6c56caec7

--- psana_python/trunk/pyext/DataSource.cpp
+++ psana_python/trunk/pyext/DataSource.cpp
@@ -39,6 +39,7 @@ namespace {
   PyObject* DataSource_steps(PyObject* self, PyObject*);
   PyObject* DataSource_events(PyObject* self, PyObject*);
   PyObject* DataSource_env(PyObject* self, PyObject*);
+  PyObject* DataSource_end(PyObject* self, PyObject*);
 
   PyMethodDef methods[] = {
     { "empty",   DataSource_empty,   METH_NOARGS, "self.empty() -> bool\n\nReturns true if data source has no associated data (\"null\" source)" },
@@ -46,6 +47,7 @@ namespace {
     { "steps",   DataSource_steps,   METH_NOARGS, "self.steps() -> iterator\n\nReturns iterator for contained steps (:py:class:`StepIter`)" },
     { "events",  DataSource_events,  METH_NOARGS, "self.events() -> iterator\n\nReturns iterator for contained events  (:py:class:`EventIter`)" },
     { "env",     DataSource_env,     METH_NOARGS, "self.env() -> object\n\nReturns environment object, cannot be called for \"null\" source" },
+    { "end",     DataSource_end,     METH_NOARGS, "self.end() -> for data sources using random access, allows user to specify end-of-job" },
     {0, 0, 0, 0}
    };
 
@@ -108,6 +110,21 @@ DataSource_env(PyObject* self, PyObject* )
   psana_python::pyext::DataSource* py_this = static_cast<psana_python::pyext::DataSource*>(self);
   PSEnv::Env& env = py_this->m_obj.env();
   return psana_python::Env::PyObject_FromCpp(env.shared_from_this());
+}
+
+PyObject*
+DataSource_end(PyObject* self, PyObject* )
+{
+  psana_python::pyext::DataSource* py_this = static_cast<psana_python::pyext::DataSource*>(self);
+  // sequential psana uses a "read-with-no-event" to learn that
+  // this is the end of the job.  that never happens naturally with
+  // random-access psana.  this method gives users the ability
+  // to indicate that they are done accessing the data, and causes
+  // module endJob methods to be called.  It is the analog of
+  // run.end().  It doesn't hurt to call this in sequential mode, but
+  // it isn't necessary.  - cpo
+  py_this->m_obj.events().next();
+  Py_RETURN_NONE;
 }
 
 }
