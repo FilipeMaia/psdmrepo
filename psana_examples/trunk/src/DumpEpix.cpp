cc406c9436c1479ae8c42a0a1550848f1e1db2d5
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-09-23 17:58:05 +0000

    update DumpEpix.cpp for Epix::Config10KV1 and Epix::Asic10kConfigV1
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@8950 b967ad99-d558-0410-b138-e0f6c56caec7

--- psana_examples/trunk/src/DumpEpix.cpp
+++ psana_examples/trunk/src/DumpEpix.cpp
@@ -163,6 +163,158 @@ DumpEpix::beginRun(Event& evt, Env& env)
       }
     }
   }
+
+
+  shared_ptr<Psana::Epix::Config10KV1> config10k1 = env.configStore().get(m_src);
+  if (config10k1) {
+    WithMsgLog(name(), info, str) {
+      str << "Psana::Epix::ConfigV1:";
+      str << "\n  version           = " << config10k1->version();
+      str << "\n  runTrigDelay      = " << config10k1->runTrigDelay();
+      str << "\n  daqTrigDelay      = " << config10k1->daqTrigDelay();
+      str << "\n  dacSetting        = " << config10k1->dacSetting();
+      str << "\n  asicGR            = " << int(config10k1->asicGR());
+      str << "\n  asicAcq           = " << int(config10k1->asicAcq());
+      str << "\n  asicR0            = " << int(config10k1->asicR0());
+      str << "\n  asicPpmat         = " << int(config10k1->asicPpmat());
+      str << "\n  asicPpbe          = " << int(config10k1->asicPpbe());
+      str << "\n  asicRoClk         = " << int(config10k1->asicRoClk());
+      str << "\n  asicGRControl     = " << int(config10k1->asicGRControl());
+      str << "\n  asicAcqControl    = " << int(config10k1->asicAcqControl());
+      str << "\n  asicR0Control     = " << int(config10k1->asicR0Control());
+      str << "\n  asicPpmatControl  = " << int(config10k1->asicPpmatControl());
+      str << "\n  asicPpbeControl   = " << int(config10k1->asicPpbeControl());
+      str << "\n  asicR0ClkControl  = " << int(config10k1->asicR0ClkControl());
+      str << "\n  prepulseR0En      = " << int(config10k1->prepulseR0En());
+      str << "\n  adcStreamMode     = " << config10k1->adcStreamMode();
+      str << "\n  testPatternEnable = " << int(config10k1->testPatternEnable());
+      /** new */
+      str << "\n  SyncMode          = " << int(config10k1->SyncMode());
+      /** new */
+      str << "\n  R0Mode            = " << int(config10k1->R0Mode());
+      /** new */
+      str << "\n  DoutPipelineDelay = " << int(config10k1->DoutPipelineDelay());
+      str << "\n  acqToAsicR0Delay  = " << int(config10k1->acqToAsicR0Delay());
+      str << "\n  asicR0ToAsicAcq   = " << int(config10k1->asicR0ToAsicAcq());
+      str << "\n  asicAcqWidth      = " << int(config10k1->asicAcqWidth());
+      str << "\n  asicAcqLToPPmatL  = " << int(config10k1->asicAcqLToPPmatL());
+      str << "\n  asicRoClkHalfT    = " << int(config10k1->asicRoClkHalfT());
+      str << "\n  adcReadsPerPixel  = " << int(config10k1->adcReadsPerPixel());
+      str << "\n  adcClkHalfT       = " << int(config10k1->adcClkHalfT());
+      str << "\n  asicR0Width       = " << int(config10k1->asicR0Width());
+      str << "\n  adcPipelineDelay  = " << int(config10k1->adcPipelineDelay());
+      /** new */
+      str << "\n  SyncWidth()       = " << int(config10k1->SyncWidth());
+      /** new */
+      str << "\n  SyncDelay()               = " << int(config10k1->SyncDelay());
+      str << "\n  prepulseR0Width           = " << config10k1->prepulseR0Width();
+      str << "\n  prepulseR0Delay           = " << config10k1->prepulseR0Delay();
+      str << "\n  digitalCardId0            = " << config10k1->digitalCardId0();
+      str << "\n  digitalCardId1            = " << config10k1->digitalCardId1();
+      str << "\n  analogCardId0             = " << config10k1->analogCardId0();
+      str << "\n  analogCardId1             = " << config10k1->analogCardId1();
+      str << "\n  lastRowExclusions         = " << config10k1->lastRowExclusions();
+      str << "\n  numberOfAsicsPerRow       = " << config10k1->numberOfAsicsPerRow();
+      str << "\n  numberOfAsicsPerColumn    = " << config10k1->numberOfAsicsPerColumn();
+      str << "\n  numberOfRowsPerAsic       = " << config10k1->numberOfRowsPerAsic();
+      /** for epix10k  176 */ 
+      str << "\n  numberOfPixelsPerAsicRow  = " << config10k1->numberOfPixelsPerAsicRow();
+      /** for epix10k 48*4 */
+      str << "\n  baseClockFrequency        = " << config10k1->baseClockFrequency();
+      str << "\n  asicMask                  = " << config10k1->asicMask();
+      str << "\n  scopeEnable               = " << int(config10k1->scopeEnable());
+      str << "\n  scopeTrigEdge             = " << int(config10k1->scopeTrigEdge());
+      str << "\n  scopeTrigChan             = " << int(config10k1->scopeTrigChan());
+      str << "\n  scopeArmMode              = " << int(config10k1->scopeArmMode());
+      str << "\n  scopeADCThreshold         = " << int(config10k1->scopeADCThreshold());
+      str << "\n  scopeTrigHoldoff          = " << int(config10k1->scopeTrigHoldoff());
+      str << "\n  scopeTrigOffset           = " << int(config10k1->scopeTrigOffset());
+      str << "\n  scopeTraceLength          = " << int(config10k1->scopeTraceLength());
+      str << "\n  scopeADCsameplesToSkip    = " << int(config10k1->scopeADCsameplesToSkip());
+      str << "\n  scopeChanAwaveformSelect  = " << int(config10k1->scopeChanAwaveformSelect());
+      str << "\n  scopeChanBwaveformSelect  = " << int(config10k1->scopeChanBwaveformSelect());
+
+      str << "\n  asicPixelConfigArray = " << config10k1->asicPixelConfigArray();
+
+      str << "\n  numberOfRows    = " << config10k1->numberOfRows();
+      str << "\n  numberOfColumns = " << config10k1->numberOfColumns();
+      str << "\n  numberOfAsics   = " << config10k1->numberOfAsics();
+
+      //virtual std::vector<int> asics_shape() const = 0;
+      str << "\n  asics_shape() =";
+      std::vector<int> v = config10k1->asics_shape();
+      for(std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) { str << " " << *it; }
+
+      const int nasics = config10k1->numberOfAsics();
+      for (int i = 0; i != nasics; ++ i) {
+        const Psana::Epix::Asic10kConfigV1& aconfig = config10k1->asics(i);
+        str << "\n    Psana::Epix::Asic10kConfigV1 #" << i << ":";
+
+        str << "\n      CompTH_DAC       = " << int(aconfig.CompTH_DAC());
+        str << "\n      CompEn_0         = " << int(aconfig.CompEn_0());
+        str << "\n      PulserSync       = " << int(aconfig.PulserSync());
+        str << "\n      dummyTest        = " << int(aconfig.dummyTest());
+        str << "\n      dummyMask        = " << int(aconfig.dummyMask());
+        str << "\n      dummyG           = " << int(aconfig.dummyG());
+        str << "\n      dummyGA          = " << int(aconfig.dummyGA());
+        str << "\n      dummyUpper12bits = " << int(aconfig.dummyUpper12bits());
+        str << "\n      pulser           = " << int(aconfig.pulser());
+        str << "\n      pbit             = " << int(aconfig.pbit());
+        str << "\n      atest            = " << int(aconfig.atest());
+        str << "\n      test             = " << int(aconfig.test());
+        str << "\n      sabTest          = " << int(aconfig.sabTest());
+        str << "\n      hrTest           = " << int(aconfig.hrTest());
+        str << "\n      pulserR          = " << int(aconfig.pulserR());
+        str << "\n      digMon1          = " << int(aconfig.digMon1());
+        str << "\n      digMon2          = " << int(aconfig.digMon2());
+        str << "\n      pulserDac        = " << int(aconfig.pulserDac());
+        str << "\n      monostPulser     = " << int(aconfig.monostPulser());
+        str << "\n      CompEn_1         = " << int(aconfig.CompEn_1());
+        str << "\n      CompEn_2         = " << int(aconfig.CompEn_2());
+        str << "\n      Dm1En            = " << int(aconfig.Dm1En());
+        str << "\n      Dm2En            = " << int(aconfig.Dm2En());
+        str << "\n      emph_bd          = " << int(aconfig.emph_bd());
+        str << "\n      emph_bc          = " << int(aconfig.emph_bc());
+        str << "\n      VRefDac          = " << int(aconfig.VRefDac());
+        str << "\n      vrefLow          = " << int(aconfig.vrefLow());
+        str << "\n      TpsTComp         = " << int(aconfig.TpsTComp());
+        str << "\n      TpsMux           = " << int(aconfig.TpsMux());
+        str << "\n      RoMonost         = " << int(aconfig.RoMonost());
+        str << "\n      TpsGr            = " << int(aconfig.TpsGr());
+        str << "\n      S2dGr            = " << int(aconfig.S2dGr());
+        str << "\n      PpOcbS2d         = " << int(aconfig.PpOcbS2d());
+        str << "\n      Ocb              = " << int(aconfig.Ocb());
+        str << "\n      Monost           = " << int(aconfig.Monost());
+        str << "\n      FastppEnable     = " << int(aconfig.FastppEnable());
+        str << "\n      Preamp           = " << int(aconfig.Preamp());
+        str << "\n      PixelCb          = " << int(aconfig.PixelCb());
+        str << "\n      Vld1_b           = " << int(aconfig.Vld1_b());
+        str << "\n      S2dTComp         = " << int(aconfig.S2dTComp());
+        str << "\n      FilterDac        = " << int(aconfig.FilterDac());
+        str << "\n      testVDTransmitter= " << int(aconfig.testVDTransmitter());
+        str << "\n      TC               = " << int(aconfig.TC());
+        str << "\n      S2d              = " << int(aconfig.S2d());
+        str << "\n      S2dDacBias       = " << int(aconfig.S2dDacBias());
+        str << "\n      TpsTcDac         = " << int(aconfig.TpsTcDac());
+        str << "\n      TpsDac           = " << int(aconfig.TpsDac());
+        str << "\n      S2dTcDac         = " << int(aconfig.S2dTcDac());
+        str << "\n      S2dDac           = " << int(aconfig.S2dDac());
+        str << "\n      TestBe           = " << int(aconfig.TestBe());
+        str << "\n      IsEn             = " << int(aconfig.IsEn());
+        str << "\n      DelExec          = " << int(aconfig.DelExec());
+        str << "\n      DelCckReg        = " << int(aconfig.DelCckReg());
+        str << "\n      RO_rst_en        = " << int(aconfig.RO_rst_en());
+        str << "\n      slvdSBit         = " << int(aconfig.slvdSBit());
+        str << "\n      FELmode          = " << int(aconfig.FELmode());
+        str << "\n      CompEnOn         = " << int(aconfig.CompEnOn());
+        str << "\n      RowStartAddr     = " << int(aconfig.RowStartAddr());
+        str << "\n      RowStopAddr      = " << int(aconfig.RowStopAddr());
+        str << "\n      ColStartAddr     = " << int(aconfig.ColStartAddr());
+        str << "\n      ColStopAddr      = " << int(aconfig.ColStopAddr());
+        str << "\n      chipID           = " << int(aconfig.chipID());
+      }
+    }
+  }
 }
 
 /// Method which is called with event data, this is the only required 
@@ -175,7 +327,7 @@ DumpEpix::event(Event& evt, Env& env)
   if (data1) {
 
     WithMsgLog(name(), info, str) {
-      str << "Epix::ElementV1 at " << actualSrc;
+      str << "Epix::ElementV1 at source " << actualSrc;
       str << "\n  vc = " << int(data1->vc());
       str << "\n  lane = " << int(data1->lane());
       str << "\n  acqCount = " << data1->acqCount();
