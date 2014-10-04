2995791c5f290dbf80465cd4e73d7813dbafdb1f
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-10-04 00:55:32 +0000

    Tagging Translator with V00-02-05
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@9028 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ Translator/tags/V00-02-05/include/H5MpiSplitScanDefaults.h
@@ -0,0 +1,13 @@
+#ifndef TRANSLATOR_H5MPISPLITSCANDEFAULT_H
+#define TRANSLATOR_H5MPISPLITSCANDEFAULT_H
+
+#include <stdint.h>
+
+namespace Translator {
+
+extern const int64_t MIN_EVENTS_PER_CALIB_FILE_DEFAULT;
+extern const int64_t NUM_EVENTS_CHECK_DONE_CALIB_FILE_DEFAULT;
+
+};
+
+#endif
