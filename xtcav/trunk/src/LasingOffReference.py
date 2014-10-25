55a8bff7d2ba708ccfdb88b0bdca21b2a60befd5
cpo@SLAC.STANFORD.EDU <cpo@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-10-25 00:03:56 +0000

    First commit of Alvaro's xtcav code
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@9148 b967ad99-d558-0410-b138-e0f6c56caec7

---
+++ xtcav/trunk/src/LasingOffReference.py
@@ -0,0 +1,21 @@
+
+import scipy.io
+import cPickle
+import numpy
+from CustomLoadmat import customloadmat
+from xtcav.Constants import Load as constLoad
+from xtcav.Constants import Save as constSave
+
+class LasingOffReference(object):
+    def __init__(self):
+        self.averagedProfiles=[]
+        self.runs=numpy.array([],dtype=int)
+        self.n=0
+        self.parameters=0
+        
+    def Save(self,path):        
+        constSave(self,path)
+
+    @staticmethod    
+    def Load(path):        
+        return constLoad(path)
