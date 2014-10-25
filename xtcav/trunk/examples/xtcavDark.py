8206db21e8a846da5877c586b9ea831a960faf4e
cpo@SLAC.STANFORD.EDU <cpo@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-10-25 05:58:23 +0000

    move Dark and LasingOff scripts from app to examples
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@9159 b967ad99-d558-0410-b138-e0f6c56caec7

--- xtcav/trunk/examples/xtcavDark.py
+++ xtcav/trunk/examples/xtcavDark.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from xtcav.GenerateDarkBackground import *
 
 GDB=GenerateDarkBackground();
--- xtcav/trunk/examples/xtcavLasingOff.py
+++ xtcav/trunk/examples/xtcavLasingOff.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from xtcav.GenerateLasingOffReference import *
 
 GLOC=GenerateLasingOffReference();
