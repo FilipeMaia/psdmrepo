0ee99b98d7660db609f276194c85c8e60064fefb
davidsch@SLAC.STANFORD.EDU <davidsch@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-11-25 02:25:32 +0000

    XtcInput: add check for corrupted xtc to address JIRA issue PSAS-71
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@9305 b967ad99-d558-0410-b138-e0f6c56caec7

--- XtcInput/trunk/src/XtcIterator.cpp
+++ XtcInput/trunk/src/XtcIterator.cpp
@@ -32,7 +32,7 @@
 namespace {
   
   const char* logger = "XtcInput.XtcIterator";
-  
+
 }
 
 //		----------------------------------------
@@ -98,12 +98,22 @@ XtcIterator::next()
   MsgLog(logger, debug, "XtcIterator: offset size = " << m_off.top());
 
   // at this point the XTC in stack can only be of Pds::TypeId::Id_Xtc type
-  Pds::Xtc* next = (Pds::Xtc*)(m_xtc.top()->payload() + m_off.top());
-  MsgLog(logger, debug, "XtcIterator: next payload = " << next->sizeofPayload());
-  MsgLog(logger, debug, "XtcIterator: next type = " << Pds::TypeId::name(next->contains.id()));
+  Pds::Xtc* nextXtc = (Pds::Xtc*)(m_xtc.top()->payload() + m_off.top());
+
+  // check for damaged xtc
+  if (nextXtc->sizeofPayload() < 0) {
+    MsgLog(logger, error, "XtcIterator: next payload is negative. Skipping.");
+    m_off.pop();
+    m_xtc.pop();
+    MsgLog(logger, debug, "negative next payload=" << nextXtc->sizeofPayload() << ", pop " << m_off.size());
+    return next();
+  }
+
+  MsgLog(logger, debug, "XtcIterator: next payload = " << nextXtc->sizeofPayload());
+  MsgLog(logger, debug, "XtcIterator: next type = " << Pds::TypeId::name(nextXtc->contains.id()));
 
   // adjust remaining size
-  int nextoff = m_off.top() + next->sizeofPayload() + sizeof(Pds::Xtc);
+  int nextoff = m_off.top() + nextXtc->sizeofPayload() + sizeof(Pds::Xtc);
   MsgLog(logger, debug, "XtcIterator: next offset = " << nextoff);
   if ( nextoff >  m_xtc.top()->sizeofPayload()) {
     // looks badly corrupted, print a warning
@@ -114,14 +124,14 @@ XtcIterator::next()
   MsgLog(logger, debug, "XtcIterator: set offset " << nextoff);
   m_off.top() = nextoff;
 
-  if ( next->contains.id() == Pds::TypeId::Id_Xtc ) {
+  if ( nextXtc->contains.id() == Pds::TypeId::Id_Xtc ) {
     // add it to stack too
     MsgLog(logger, debug, "XtcIterator: push " << m_off.size());
-    m_xtc.push(next);
+    m_xtc.push(nextXtc);
     m_off.push(0);
   }
   
-  return next;
+  return nextXtc;
 }
 
 } // namespace XtcInput
