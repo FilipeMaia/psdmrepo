c5d644496b8befb3ba32d7507f6d8e1e7e3cf90c
mitra@SLAC.STANFORD.EDU <mitra@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2014-11-25 20:36:40 +0000

    - python interface to C psalg functions and native python functions         => takes advantage of BOOST python converters defined in psana_python         => Uses BOOST to wrap native C/C++ functions for PYTHON - Removed  ScaleByN, chi2, and pypsalg_c native Python C API files - added example file
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@9315 b967ad99-d558-0410-b138-e0f6c56caec7

--- pypsalg/trunk/SConscript
+++ pypsalg/trunk/SConscript
@@ -29,4 +29,4 @@ Import('*')
 #    NEED_QT - set to True to enable Qt support
 #
 #
-standardSConscript(PYEXTMOD="pypsalg_c")
+standardSConscript(PYEXTMOD='pypsalg_cpp')
--- pypsalg/trunk/src/__init__.py
+++ pypsalg/trunk/src/__init__.py
@@ -3,7 +3,9 @@ from CircularBuffer import CircularBuffer
 from CommonModeRemoval import CommonModeRemoval
 from EventFilter import EventFilter
 from Histogram import hist1d
-from pypsalg_c import *
 from Mask import *
 from gsc import gsc
 from AngularIntegrationM import AngularIntegratorM
+
+# Load python interface to psalg
+from pypsalg_cpp import *
