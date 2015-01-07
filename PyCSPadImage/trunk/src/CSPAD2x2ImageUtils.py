8b3134eb1f45e41c702ed20fae76508938fed977
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2015-01-07 00:38:51 +0000

    CSPAD2x2ImageUtils.py - add support for non-standard (2,185,388) shape of cspad2x2 ndarray
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@9456 b967ad99-d558-0410-b138-e0f6c56caec7

--- PyCSPadImage/trunk/src/CSPAD2x2ImageUtils.py
+++ PyCSPadImage/trunk/src/CSPAD2x2ImageUtils.py
@@ -10,6 +10,58 @@ import pyimgalgos.GlobalGraphics as gg
 
 #------------------------------
 
+def getImageArrayForCSpad2x1Segment(arr2x1):
+    """Returns the image array for pair of ASICs"""
+
+    #arr2x1 = arr1ev[0:185,0:388,pair]
+    #arr2x1 = arr1ev[:,:,pair]
+    #print '2x1 array shape:', arr2x1.shape
+      
+    asics  = np.hsplit(arr2x1,2)
+    arrgap = np.zeros ((185,3), dtype=np.float32)
+    arr2d  = np.hstack((asics[0],arrgap,asics[1]))
+    return arr2d
+
+#---------------------
+
+def getImageArrayForCSpad2x2FromSegments(arrseg0, arrseg1):
+    """Returns the image array for the CSpad2x2Element or CSpad2x2"""       
+
+    arr2x1Pair0 = getImageArrayForCSpad2x1Segment(arrseg0) # arr1ev[:,:,0])
+    arr2x1Pair1 = getImageArrayForCSpad2x1Segment(arrseg1) # arr1ev[:,:,1])
+    wid2x1      = arr2x1Pair0.shape[0]
+    len2x1      = arr2x1Pair0.shape[1]
+
+    arrgapV = np.zeros( (20,len2x1), dtype=np.float ) # dtype=np.int16 
+    arr2d   = np.vstack((arr2x1Pair0, arrgapV, arr2x1Pair1))
+
+    #print 'arr2d.shape=', arr2d.shape
+    #print 'arr2d=',       arr2d
+    return arr2d
+
+#------------------------------
+
+def get_cspad2x2_non_corrected_image_for_raw_data_array(arr) :
+    if arr.shape[-1] == 388 :
+        arr.shape = (2,185,388)
+        #print 'Shaped in natural order:', arr.shape
+        return getImageArrayForCSpad2x2FromSegments(arr[0,:,:], arr[1,:,:])
+
+    if arr.shape[-1] == 2 or arr.size == 185*388*2 :
+        arr.shape = (185,388,2)
+        #print 'Shaped as data:', arr.shape
+        return getImageArrayForCSpad2x2FromSegments(arr[:,:,0], arr[:,:,1])
+
+        #coord = CSPAD2x2PixCoords()
+        #iX,iY = coord.get_cspad2x2_pix_coordinate_arrays_shapeed_as_data_pix ()    
+        #return gg.getImageFromIndexArrays(iX,iY,arr)
+
+    msg = 'Un-expected cspad2x2 array shape %s or size %s' % (str(arr.shape), arr.size)
+    print msg
+    return None
+    
+#------------------------------
+
 def test_plot_for_cspad2x2_coords(calib_path='.', run_num=0) :
     #calib_path  = '/reg/d/psdm/xpp/xpptut13/calib/CsPad2x2::CalibV1/XppGon.0:Cspad2x2.1/'
     #run_num   = 123
@@ -27,22 +79,6 @@ def test_plot_for_cspad2x2_coords(calib_path='.', run_num=0) :
 
     gg.plotImageLarge(img2d, amp_range=(-1, 185*388*2), figsize=(12,11))
     gg.show()
-
-#------------------------------
-
-def get_cspad2x2_non_corrected_image_for_raw_data_array(arr) :
-    if not (arr.shape == (185,388,2) or arr.shape == (185*388*2,)) :
-        msg = 'Non-expected array shape for cspad2x2: %s' % arr.shape
-        print msg
-        return None
-
-    arr.shape = (185,388,2)
-    
-    coord = CSPAD2x2PixCoords()
-    iX,iY = coord.get_cspad2x2_pix_coordinate_arrays_shapeed_as_data_pix ()
-    
-    img2d = gg.getImageFromIndexArrays(iX,iY,arr)
-    return img2d
 
 #------------------------------
  
