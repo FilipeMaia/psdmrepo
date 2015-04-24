1c285aad61a5945b0e7a3aad101ed999e9344980
dubrovin@SLAC.STANFORD.EDU <dubrovin@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2015-04-24 20:22:05 +0000

    GlobalGraphics.py - add methods move_fig, save_fig, hist1d (PSAS-34)
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@9959 b967ad99-d558-0410-b138-e0f6c56caec7

--- pyimgalgos/trunk/src/GlobalGraphics.py
+++ pyimgalgos/trunk/src/GlobalGraphics.py
@@ -27,6 +27,7 @@ import matplotlib.pyplot  as plt
 import matplotlib.lines   as lines
 import matplotlib.patches as patches
 
+from CalibManager.PlotImgSpeWidget import add_stat_text
 #---------------------
 #class CSPadImageProducer (object) :
 #    """This is an empty class"""
@@ -50,8 +51,8 @@ def fig_axes(figsize=(13,12), title='Image') :
     """ Creates and returns figure, and axes for image and color bar
     """
     fig  = plt.figure(figsize=figsize, dpi=80, facecolor='w', edgecolor='w', frameon=True)
-    axim = fig.add_axes([0.05,  0.03, 0.87, 0.93])
-    axcb = fig.add_axes([0.923, 0.03, 0.02, 0.93])
+    axim = fig.add_axes((0.05,  0.03, 0.87, 0.93))
+    axcb = fig.add_axes((0.923, 0.03, 0.02, 0.93))
     fig.canvas.set_window_title(title)
     store.fig, store.axim, store.axcb = fig, axim, axcb
     return fig, axim, axcb
@@ -84,16 +85,21 @@ def plot_img(img, mode=None, amin=None, amax=None) :
 
 #------------------------------
 
-def getArrangedImage() :
-    arr = np.arange(2400)
-    arr.shape = (40,60)
+def size_of_shape(shape=(2,3,8)) :
+    size=1
+    for d in shape : size *= d
+    return size
+
+#------------------------------
+
+def getArrangedImage(shape=(40,60)) :
+    arr = np.arange(size_of_shape(shape))
+    arr.shape = shape
     return arr
 
 #--------------------------------
-def getRandomImage() :
-    mu, sigma = 200, 25
-    arr = mu + sigma*np.random.standard_normal(size=2400)
-    arr.shape = (40,60)
+def getRandomImage(mu=200, sigma=25, shape=(40,60)) :
+    arr = mu + sigma*np.random.standard_normal(shape)
     return arr
 
 #------------------------------
@@ -105,7 +111,7 @@ def getImageAs2DHist(iX,iY,W=None) :
     ysize = np.ceil(iY).max()+1
     if W==None : weights = None
     else       : weights = W.flatten()
-    H,Xedges,Yedges = np.histogram2d(iX.flatten(), iY.flatten(), bins=[xsize,ysize], range=[[-0.5,xsize-0.5],[-0.5,ysize-0.5]], normed=False, weights=weights) 
+    H,Xedges,Yedges = np.histogram2d(iX.flatten(), iY.flatten(), bins=(xsize,ysize), range=((-0.5,xsize-0.5),(-0.5,ysize-0.5)), normed=False, weights=weights) 
     return H
 
 #------------------------------
@@ -123,9 +129,37 @@ def getImageFromIndexArrays(iX,iY,W=None) :
 
 #--------------------------------
 
-def plotHistogram(arr, amp_range=None, figsize=(6,6)) : # range=(0,500)
-    fig  = plt.figure(figsize=figsize, dpi=80, facecolor='w',edgecolor='w', frameon=True)
-    plt.hist(arr.flatten(), bins=100, range=amp_range)
+def plotHistogram(arr, amp_range=None, figsize=(6,6), bins=None, title='') :
+    """Makes historgam from input array of values (arr), which are sorted in number of bins (bins) in the range (amp_range=(amin,amax))
+    """
+    fig = plt.figure(figsize=figsize, dpi=80, facecolor='w',edgecolor='w', frameon=True)
+    axhi = fig.add_axes((0.15, 0.10, 0.78, 0.82))
+    hbins = bins if bins is not None else 100
+    hi = axhi.hist(arr.flatten(), bins=hbins, range=amp_range) #, log=logYIsOn)
+    #fig.canvas.set_window_title(title)
+    axhi.set_title(title, color='k', fontsize=20)
+    return fig, axhi, hi
+
+#--------------------------------
+
+def hist1d(arr, bins=None, amp_range=None, weights=None, color=None, show_stat=True, log=False, \
+           figsize=(6,5), axwin=(0.15, 0.10, 0.78, 0.82), \
+           title=None, xlabel=None, ylabel=None, titwin=None) :
+    """Makes historgam from input array of values (arr), which are sorted in number of bins (bins) in the range (amp_range=(amin,amax))
+    """
+    fig = plt.figure(figsize=figsize, dpi=80, facecolor='w', edgecolor='w', frameon=True)
+    axhi = fig.add_axes(axwin)
+    hbins = bins if bins is not None else 100
+    hi = axhi.hist(arr.flatten(), bins=hbins, range=amp_range, weights=weights, color=color, log=log) #, log=logYIsOn)
+    if amp_range is not None : axhi.set_xlim(amp_range) # axhi.set_autoscale_on(False) # suppress autoscailing
+    if titwin is not None : fig.canvas.set_window_title(titwin)
+    if title  is not None : axhi.set_title(title, color='k', fontsize=20)
+    if xlabel is not None : axhi.set_xlabel(xlabel, fontsize=14)
+    if ylabel is not None : axhi.set_ylabel(ylabel, fontsize=14)
+    if show_stat :
+        weights, bins, patches = hi
+        add_stat_text(axhi, weights, bins)
+    return fig, axhi, hi
 
 #--------------------------------
 
@@ -147,7 +181,7 @@ def plotImage(arr, img_range=None, amp_range=None, figsize=(12,5), title='Image'
 
 def plotImageLarge(arr, img_range=None, amp_range=None, figsize=(12,10), title='Image', origin='upper') : 
     fig  = plt.figure(figsize=figsize, dpi=80, facecolor='w', edgecolor='w', frameon=True)
-    axim = fig.add_axes([0.05,  0.03, 0.94, 0.94])
+    axim = fig.add_axes((0.05,  0.03, 0.94, 0.94))
     imsh = axim.imshow(arr, interpolation='nearest', aspect='auto', origin=origin, extent=img_range)
     colb = fig.colorbar(imsh, pad=0.005, fraction=0.09, shrink=1, aspect=40) # orientation=1
     if amp_range is not None : imsh.set_clim(amp_range[0],amp_range[1])
@@ -177,10 +211,9 @@ def plotImageAndSpectrum(arr, amp_range=None) : #range=(0,500)
 
 def plotGraph(x,y, figsize=(10,5)) : 
     fig = plt.figure(figsize=figsize, dpi=80, facecolor='w', edgecolor='w', frameon=True)
-    #fig.subplots_adjust(left=0.05, bottom=0.03, right=0.98, top=0.98, wspace=0.2, hspace=0.1)
-    #figAxes = fig.add_subplot(111)
     ax = fig.add_axes([0.15, 0.10, 0.78, 0.86])
     ax.plot(x,y,'b-')
+    return fig, ax
 
 #------------------------------
 
@@ -207,21 +240,34 @@ def drawRectangle(axes, xy, width, height, linewidth=1, color='w') :
     rect = patches.Rectangle(xy, width, height, linewidth=linewidth, color=color)
     axes.add_artist(rect)
 
-#--------------------------------
+#------------------------------
 
-def savefig(fname='img.png', do_print=True) :
-    if do_print :
-        print 'Save plot in file: %s' % fname 
+def save(fname='img.png', do_save=True, pbits=0377) :
+    if not do_save : return
+    if pbits & 1 : print 'Save plot in file: %s' % fname 
     plt.savefig(fname)
 
 #--------------------------------
 
+def savefig(fname='img.png', do_print=True) :
+    save(fname, do_save=True, pbits=0377 if do_print else 0)
+
+#------------------------------
+
+def save_fig(fig, fname='img.png', do_save=True, pbits=0377) :
+    if not do_save : return
+    if pbits & 1 : print 'Save plot in file: %s' % fname 
+    fig.savefig(fname)
+
+#--------------------------------
+
 def move(x0=200,y0=100) :
-    #plt.get_current_fig_manager().window.move(x0, y0)
-    move_str = '+' + str(x0) + '+' + str(y0)
-    plt.get_current_fig_manager().window.geometry(move_str)
-    #plt.get_current_fig_manager().window.geometry("+50+50")
-    pass
+    plt.get_current_fig_manager().window.geometry('+%d+%d' % (x0, y0))
+
+#--------------------------------
+
+def move_fig(fig, x0=200, y0=100) :
+    fig.canvas.manager.window.geometry('+%d+%d' % (x0, y0))
 
 #--------------------------------
 
@@ -243,6 +289,7 @@ def main() :
     elif sys.argv[1]=='3' : plotHistogram(arr, amp_range=(100,300), figsize=(10,5))
     elif sys.argv[1]=='4' : plotImage(arr, amp_range=(100,300), figsize=(10,10))
     elif sys.argv[1]=='5' : plotImageLarge(arr, amp_range=(100,300), figsize=(10,10))
+    elif sys.argv[1]=='6' : plotImageLarge(getArrangedImage(shape=(40,60)), figsize=(10,10))
     else :
         print 'Non-expected arguments: sys.argv=', sys.argv
         sys.exit ('Check input parameters')
