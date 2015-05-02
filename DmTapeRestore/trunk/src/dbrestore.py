5b9effc89a3d30084b85ff9c59ca9647e5fba5ad
wilko@SLAC.STANFORD.EDU <wilko@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2015-05-02 01:24:54 +0000

    Use DbConnection for db connection. Added function to find files by name
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@10019 b967ad99-d558-0410-b138-e0f6c56caec7

--- DmTapeRestore/trunk/src/dbrestore.py
+++ DmTapeRestore/trunk/src/dbrestore.py
@@ -2,22 +2,20 @@
 
 import MySQLdb as db
 
+from DbTools.DbConnection import DbConnection
 
-__host   = 'psdb.slac.stanford.edu'
-__user   = 'data_migration'
-__passwd = 'pcds'
-__db     = 'webportal'
-
+CONN_STR = "file:/reg/g/psdm/psdatmgr/etc/.taperestore-conn"
 
 def __escape_string(str):
 
-    conn = db.connect(host=__host, user=__user, passwd=__passwd, db=__db)
+    conn = DbConnection(conn_string=CONN_STR)
+    cursor = conn.cursor(dictcursor=True)
     return conn.escape_string(str)
 
 def __do_select(statement):
 
-    conn = db.connect(host=__host, user=__user, passwd=__passwd, db=__db)
-    cursor = conn.cursor(db.cursors.SSDictCursor)
+    conn = DbConnection(conn_string=CONN_STR)
+    cursor = conn.cursor(dictcursor=True)
     cursor.execute("SET SESSION SQL_MODE='ANSI'")
     cursor.execute(statement)
     rows = cursor.fetchall()
@@ -27,21 +25,20 @@ def __do_select(statement):
 
 def __do_select_many(statement):
 
-    conn = db.connect(host=__host, user=__user, passwd=__passwd, db=__db)
-    cursor = conn.cursor(db.cursors.SSDictCursor)
+    conn = DbConnection(conn_string=CONN_STR)
+    cursor = conn.cursor(dictcursor=True)
     cursor.execute("SET SESSION SQL_MODE='ANSI'")
     cursor.execute(statement)
     return cursor.fetchall()
 
 def __do_sql(statement):
 
-    conn = db.connect(host=__host, user=__user, passwd=__passwd, db=__db)
-    cursor = conn.cursor(db.cursors.SSDictCursor)
+    conn = DbConnection(conn_string=CONN_STR)
+    cursor = conn.cursor(dictcursor=True)
     cursor.execute("SET SESSION SQL_MODE='ANSI'")
     cursor.execute("BEGIN")
     cursor.execute(statement)
     cursor.execute("COMMIT")
-
 
 
 DONE = 'DONE'
@@ -50,7 +47,6 @@ FAILED = 'FAILED'
 RECEIVED = 'RECEIVED'
 
 def files_with_status(status=None):
-    
     sel = ""
     if status:
         sel = "WHERE status = '%s'" % status
@@ -63,26 +59,19 @@ def files_with_status(status=None):
 def set_status(flag, irods_fname):
     query = "UPDATE file_restore_requests SET status='%s' " \
             "WHERE irods_filepath = '%s'" % (flag, irods_fname)
-
-    #print query    
     __do_sql(query)
-    
-
 
 def set_submitted(irods_fname):
     query = "UPDATE file_restore_requests SET status='SUBMITTED' " \
         "WHERE irods_filepath = '%s'" % irods_fname
-
-    #print query    
     __do_sql(query)
     
 
 def file_by_name(name):
-
     sel = "WHERE irods_filepath like '%%%s%%'" % name
     query = "SELECT * FROM file_restore_requests %s " % sel
     print query
 
     files = __do_select_many("SELECT * FROM file_restore_requests %s " % sel)
-    print files
-    
+    for restore in files:
+        yield restore
