187a1e82042ac6e98db173693d64b6d8f7834ab5
yoon82@SLAC.STANFORD.EDU <yoon82@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2015-05-31 10:20:04 +0000

    change histogram to histogram4D
    git-svn-id: https://pswww.slac.stanford.edu/svn-readonly/psdmrepo@10216 b967ad99-d558-0410-b138-e0f6c56caec7

--- pypsalg/trunk/pyext/AreaDetHist.cpp
+++ pypsalg/trunk/pyext/AreaDetHist.cpp
@@ -11,31 +11,37 @@ AreaDetHist::AreaDetHist (ndarray<double,3> calib_data, int valid_min,
   _findIsolated(findIsolated),_minAduGap(minAduGap)
 {
   _histLength = (_valid_max-_valid_min+1)+2; // extra two pixels for underflow/overflow
-  _histogram = make_ndarray<uint32_t>(calib_data.size(),_histLength); // doesn't guarantee zeros
-  for (ndarray<uint32_t,2>::iterator p = _histogram.begin(); p != _histogram.end(); p++) {
+  const unsigned int *arrayShape = calib_data.shape();  
+  _segs = arrayShape[0];
+  _rows = arrayShape[1];
+  _cols = arrayShape[2];
+  _numPixPerSeg = _rows*_cols;
+  _histogram4D = make_ndarray<uint32_t>(_segs,_rows,_cols,_histLength);
+  for (ndarray<uint32_t,4>::iterator p = _histogram4D.begin(); p != _histogram4D.end(); p++) {
     *p = 0;
   }
 }
 
 AreaDetHist::~AreaDetHist () {}
 
-ndarray<uint32_t, 2> AreaDetHist::get() {return _histogram;}
+ndarray<uint32_t, 4> AreaDetHist::get() {return _histogram4D;}
 
 void AreaDetHist::_fillHistogram(ndarray<double,3> calib_data) {
-  int pixelInd = 0;
   int val;
-  // fill histogram
-  for (ndarray<double,3>::iterator p = calib_data.begin();
-       p != calib_data.end(); p++) {
-    val = (int) round(*p);   
+  // fill histograms given calibrated data
+  for (unsigned int i = 0; i < _segs; i++) {
+  for (unsigned int j = 0; j < _rows; j++) {
+  for (unsigned int k = 0; k < _cols; k++) {
+	val = (int) round(calib_data[i][j][k]);
     if ( val >= _valid_min && val <= _valid_max ) { // in range
-      _histogram[ pixelInd ][ val-_valid_min+1 ] += 1;
+      _histogram4D[ i ][ j ][ k ][ val-_valid_min+1 ] += 1;
     } else if ( val > _valid_max ) { // too large
-      _histogram[ pixelInd ][ _histLength-1 ] += 1;
+      _histogram4D[ i ][ j ][ k ][ _histLength-1 ] += 1;
     } else { // too small
-      _histogram[ pixelInd ][ 0 ] += 1;
+      _histogram4D[ i ][ j ][ k ][ 0 ] += 1;
     }
-    pixelInd++;
+  }
+  }
   }
 }
 
@@ -58,37 +64,29 @@ unsigned int getPixelIndex(const unsigned int numPixPerSeg, const unsigned int C
 }
 
 // Increment counter on histogram
-void AreaDetHist::_insertHistElement(double x, int pixelInd) {
+void AreaDetHist::_insertHistElement(double x, int seg, int row, int col) {
 	int val = (int) round(x);   
 	if ( val >= _valid_min && val <= _valid_max ) { // in range
-		_histogram[ pixelInd ][ val-_valid_min+1 ] += 1;
+		_histogram4D[ seg ][ row ][ col ][ val-_valid_min+1 ] += 1;
 	} else if ( val > _valid_max ) { // too large
-		_histogram[ pixelInd ][ _histLength-1 ] += 1;
+		_histogram4D[ seg ][ row ][ col ][ _histLength-1 ] += 1;
 	} else { // too small
-		_histogram[ pixelInd ][ 0 ] += 1;
+		_histogram4D[ seg ][ row ][ col ][ 0 ] += 1;
 	}
 }
 
 void AreaDetHist::update(ndarray<double,3> calib_data)
 {
   if (_findIsolated) {
-	const unsigned int *arrayShape = calib_data.shape();
-    
-	const unsigned int Segs = arrayShape[0];
-	const unsigned int Rows = arrayShape[1];
-	const unsigned int Cols = arrayShape[2];
-	const unsigned int numPixPerSeg = Rows*Cols;
-
 	double val = 0;
     int result = 0;
-	int pixelInd = 0;
 	unsigned int j = 0;
 	unsigned int k = 0;
 
 	// corner of detector (3 pixel neighborhood)
 	int numNeighbors = 3;
     std::vector<double> neighbors(numNeighbors);
-	for (unsigned int i = 0; i < Segs; i++) {
+	for (unsigned int i = 0; i < _segs; i++) {
 		// Upper left corner
 		j = 0;
 		k = 0;
@@ -98,24 +96,22 @@ void AreaDetHist::update(ndarray<double,3> calib_data)
 		neighbors[2] =	calib_data[i][j+1][k+1];
 		result = isIsolated(val, _minAduGap, &neighbors);
 		if (result) {
-			pixelInd = getPixelIndex(numPixPerSeg, Cols, i, j, k);
-			_insertHistElement(val, pixelInd);
+			_insertHistElement(val, i, j, k);
 		}
 
 		// Upper right corner
-		k = Cols-1;
+		k = _cols-1;
 		val = calib_data[i][j][k];
 		neighbors[0] = calib_data[i][j+1][k];
 		neighbors[1] = calib_data[i][j][k-1];
 		neighbors[2] = calib_data[i][j+1][k-1];
 		result = isIsolated(val, _minAduGap, &neighbors);
 		if (result) {
-			pixelInd = getPixelIndex(numPixPerSeg, Cols, i, j, k);
-			_insertHistElement(val, pixelInd);
+			_insertHistElement(val, i, j, k);
 		}
 
 		// Lower left corner
-		j = Rows-1;
+		j = _rows-1;
 		k = 0;
 		val = calib_data[i][j][k];
 		neighbors[0] = calib_data[i][j][k+1];
@@ -123,30 +119,28 @@ void AreaDetHist::update(ndarray<double,3> calib_data)
 		neighbors[2] = calib_data[i][j-1][k+1];
 		result = isIsolated(val, _minAduGap, &neighbors);
 		if (result) {
-			pixelInd = getPixelIndex(numPixPerSeg, Cols, i, j, k);
-			_insertHistElement(val, pixelInd);
+			_insertHistElement(val, i, j, k);
 		}
 
 		// Lower right corner
-		k = Cols-1;
+		k = _cols-1;
 		val = calib_data[i][j][k];
 		neighbors[0] = calib_data[i][j][k-1];
 		neighbors[1] = calib_data[i][j-1][k];
 		neighbors[2] = calib_data[i][j-1][k-1];
 		result = isIsolated(val, _minAduGap, &neighbors);
 		if (result) {
-			pixelInd = getPixelIndex(numPixPerSeg, Cols, i, j, k);
-			_insertHistElement(val, pixelInd);
+			_insertHistElement(val, i, j, k);
 		}
 	}
 
 	// side of detector (5 pixel neighborhood)
 	numNeighbors = 5;
     neighbors.resize(numNeighbors);
-	for (unsigned int i = 0; i < Segs; i++) {
+	for (unsigned int i = 0; i < _segs; i++) {
 		// Upper edge
 		j = 0;
-		for (unsigned int t = 1; t < Cols-1; t++) {
+		for (unsigned int t = 1; t < _cols-1; t++) {
 			val = calib_data[i][j][t];
 			neighbors[0] = calib_data[i][j][t-1];
 			neighbors[1] = calib_data[i][j][t+1];
@@ -155,14 +149,13 @@ void AreaDetHist::update(ndarray<double,3> calib_data)
 			neighbors[4] = calib_data[i][j+1][t+1];
 			result = isIsolated(val, _minAduGap, &neighbors);
 			if (result) {
-				pixelInd = getPixelIndex(numPixPerSeg, Cols, i, j, t);
-				_insertHistElement(val, pixelInd);
+				_insertHistElement(val, i, j, t);
 			}
 		}
 
 		// Lower edge
-		j = Rows-1;
-		for (unsigned int t = 1; t < Cols-1; t++) {
+		j = _rows-1;
+		for (unsigned int t = 1; t < _cols-1; t++) {
 			val = calib_data[i][j][t];
 			neighbors[0] = calib_data[i][j][t-1];
 			neighbors[1] = calib_data[i][j][t+1];
@@ -171,14 +164,13 @@ void AreaDetHist::update(ndarray<double,3> calib_data)
 			neighbors[4] = calib_data[i][j-1][t+1];
 			result = isIsolated(val, _minAduGap, &neighbors);
 			if (result) {
-				pixelInd = getPixelIndex(numPixPerSeg, Cols, i, j, t);
-				_insertHistElement(val, pixelInd);
+				_insertHistElement(val, i, j, t);
 			}
 		}
 
 		// Left edge
 		k = 0;
-		for (unsigned int t = 1; t < Rows-1; t++) {
+		for (unsigned int t = 1; t < _rows-1; t++) {
 			val = calib_data[i][t][k];
 			neighbors[0] = calib_data[i][t+1][k];
 			neighbors[1] = calib_data[i][t-1][k];
@@ -187,14 +179,13 @@ void AreaDetHist::update(ndarray<double,3> calib_data)
 			neighbors[4] = calib_data[i][t+1][k+1];
 			result = isIsolated(val, _minAduGap, &neighbors);
 			if (result) {
-				pixelInd = getPixelIndex(numPixPerSeg, Cols, i, t, k);
-				_insertHistElement(val, pixelInd);
+				_insertHistElement(val, i, t, k);
 			}
 		}
 
 		// Right edge
-		k = Cols-1;
-		for (unsigned int t = 1; t < Rows-1; t++) {
+		k = _cols-1;
+		for (unsigned int t = 1; t < _rows-1; t++) {
 			val = calib_data[i][t][k];
 			neighbors[0] = calib_data[i][t+1][k];
 			neighbors[1] = calib_data[i][t-1][k];
@@ -203,8 +194,7 @@ void AreaDetHist::update(ndarray<double,3> calib_data)
 			neighbors[4] = calib_data[i][t+1][k-1];
 			result = isIsolated(val, _minAduGap, &neighbors);
 			if (result) {
-				pixelInd = getPixelIndex(numPixPerSeg, Cols, i, t, k);
-				_insertHistElement(val, pixelInd);
+				_insertHistElement(val, i, t, k);
 			}
 		}
 	}
@@ -212,9 +202,9 @@ void AreaDetHist::update(ndarray<double,3> calib_data)
 	// non-edge of detector (8 pixel neighborhood)
 	numNeighbors = 8;
     neighbors.resize(numNeighbors);
-	for (unsigned int i = 0; i < Segs; i++) {
-	for (unsigned int j = 1; j < Rows-1; j++) {
-	for (unsigned int k = 1; k < Cols-1; k++) {
+	for (unsigned int i = 0; i < _segs; i++) {
+	for (unsigned int j = 1; j < _rows-1; j++) {
+	for (unsigned int k = 1; k < _cols-1; k++) {
 		val = calib_data[i][j][k];
 		neighbors[0] = calib_data[i][j-1][k-1];
 		neighbors[1] = calib_data[i][j-1][k];
@@ -226,8 +216,7 @@ void AreaDetHist::update(ndarray<double,3> calib_data)
 		neighbors[7] = calib_data[i][j+1][k+1];
 		result = isIsolated(val, _minAduGap, &neighbors);
 		if (result) {
-			pixelInd = getPixelIndex(numPixPerSeg, Cols, i, j, k);
-			_insertHistElement(val, pixelInd);
+			_insertHistElement(val, i, j, k);
 		}
 	}
 	}
--- pypsalg/trunk/pyext/AreaDetHist.h
+++ pypsalg/trunk/pyext/AreaDetHist.h
@@ -12,21 +12,22 @@ class AreaDetHist {
 
   AreaDetHist (ndarray<double,3> calib_data,int,int,
                bool findIsolated, double minAduGap);
-  virtual ~AreaDetHist () ;
+  virtual ~AreaDetHist ();
   
-  ndarray<uint32_t,2> get();
+  ndarray<uint32_t,4> get();
   void update(ndarray<double,3> calib_data);
 
  private:
   void _fillHistogram(ndarray<double,3> calib_data);
-  void _insertHistElement(double x, int pixelInd);
-  ndarray<uint32_t,2> _histogram; // per-pixel histogram (numPixels x histLength)
+  void _insertHistElement(double x, int seg, int row, int col);
+  ndarray<uint32_t,4> _histogram4D; // per-pixel histogram (Segs x Rows x Cols x histLength)
 
   int _valid_min;
   int _valid_max;
   unsigned _histLength;
   bool _findIsolated;
   double _minAduGap;
+  unsigned int _segs, _rows, _cols, _numPixPerSeg;
 
 };
 
